53d784dc866005682bae4353206e0de9b8136ac3
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2009-08-07 00:35:23 +0000

    Rating component - first commit
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@35448 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ src/javascript/rating.js
@@ -0,0 +1,328 @@
+Alloy.add('rating', function(AUI) {
+
+	var L = AUI.Lang;
+	var isBoolean = L.isBoolean;
+	var isNumber = L.isNumber;
+	var isString = L.isString;
+
+	var	BOUNDING_BOX = 'boundingBox';
+	var A = 'a';
+	var BLANK = '';
+	var CONTENT_BOX = 'contentBox';
+	var DEFAULT_SELECTED = 'defaultSelected';
+	var DESTROY = 'destroy';
+	var DISABLED = 'disabled';
+	var ELEMENT = 'element';
+	var HOVER = 'hover';
+	var INPUT = 'input';
+	var INPUT_NAME = 'inputName';
+	var OFF = 'off';
+	var ON = 'on';
+	var RATING = 'rating';
+	var SELECTED_INDEX = 'selectedIndex';
+	var SIZE = 'size';
+
+	var getCN = AUI.ClassNameManager.getClassName;
+
+	var C_CLEAR_FIX = 'aui-helper-clearfix';
+	var C_RATING_DISABLED = getCN(RATING, DISABLED);
+	var C_RATING_EL = getCN(RATING, ELEMENT);
+	var C_RATING_EL_HOVER  = getCN(RATING, ELEMENT, HOVER);
+	var C_RATING_EL_OFF = getCN(RATING, ELEMENT, OFF);
+	var C_RATING_EL_ON = getCN(RATING, ELEMENT, ON);
+
+	function Rating() {
+		Rating.superclass.constructor.apply(this, arguments);
+	}
+
+	AUI.mix(Rating, {
+		NAME: 'Rating',
+
+		ATTRS: {
+			defaultSelected: {
+				value: 0,
+				writeOnce: true,
+				validator: isNumber
+			},
+
+			disabled: {
+				value: false,
+				validator: isBoolean
+			},
+
+			elements: {
+				writeOnce: true,
+				readOnly: true,
+				validator: function(v) {
+					return (v instanceof AUI.NodeList);
+				}
+			},
+
+			hiddenInput: {
+				validator: function(v) {
+					return (v instanceof AUI.Node);
+				}
+			},
+
+			inputName: {
+				value: BLANK,
+				validator: isString
+			},
+
+			selectedIndex: {
+				value: -1,
+				validator: isNumber
+			},
+
+			size: {
+				value: 5,
+				validator: function(v) {
+					return isNumber(v) && (v > 0);
+				}
+			},
+
+			title: null,
+
+			value: null
+		}
+	});
+
+	AUI.extend(Rating, AUI.Widget, {
+		/*
+		* Lifecycle
+		*/
+		initializer: function(){
+			var instance = this;
+
+			instance.inputElementsData = {};
+
+			instance.after('disabledChange', this._afterSetDisabled);
+
+			instance.render();
+		},
+
+		renderUI: function () {
+			var instance = this;
+
+			instance._parseInputElements();
+			instance._initElements();
+		},
+
+		bindUI: function () {
+			var instance = this;
+
+			instance._delegateElements();
+		},
+
+		syncUI: function(){
+			var instance = this;
+
+			instance._syncElements();
+		},
+
+		unbindUI: function() {
+			var instance = this;
+			var boundingBox = instance.get(BOUNDING_BOX);
+
+			boundingBox.detachAll();
+		},
+
+		destructor: function(){
+			var instance = this;
+			var	boundingBox = instance.get(BOUNDING_BOX);
+
+			instance.unbindUI();
+
+			boundingBox.remove();
+		},
+
+		/*
+		* Methods
+		*/
+		clearSelection: function() {
+			var instance = this;
+
+			instance.get('elements').each(function(node) {
+				node.removeClass(C_RATING_EL_ON);
+				node.removeClass(C_RATING_EL_HOVER);
+			});
+		},
+
+		select: function(index) {
+			var instance = this;
+			var oldIndex = instance.get(SELECTED_INDEX);
+
+			// reset rating
+			if (oldIndex == index) {
+				index = -1;
+			}
+
+			instance.set(SELECTED_INDEX, index);
+
+			var selectedIndex = instance.get(SELECTED_INDEX);
+			var	data = instance._getInputData(selectedIndex);
+
+			var title = ('title' in data) ? data.title : BLANK;
+			var value = ('value' in data) ? data.value : selectedIndex;
+
+			instance.fillTo(selectedIndex);
+
+			instance.set('title', title);
+			instance.set('value', value);
+
+			instance.get('hiddenInput').setAttribute('title', title);
+			instance.get('hiddenInput').setAttribute('value', value);
+
+			instance.fire('select');
+		},
+
+		fillTo: function(index, className) {
+			var instance = this;
+
+			instance.clearSelection();
+
+			if (index >= 0) {
+				instance.get('elements').some(function(node, i) {
+					node.addClass(className || C_RATING_EL_ON);
+
+					return (index == i);
+				});
+			}
+		},
+
+		_parseInputElements: function() {
+			var instance = this;
+			var boundingBox = instance.get(BOUNDING_BOX);
+			var inputs = boundingBox.queryAll(INPUT);
+			var size = inputs.size();
+			var inputName = instance.get(INPUT_NAME);
+			var hiddenInput = AUI.Node.create('<input type="hidden" />');
+
+			if (size > 0) {
+
+				if (!inputName) {
+					inputName = inputs.item(0).getAttribute('name');
+				}
+
+				instance.set('size', size);
+
+				inputs.each(function(node, index) {
+					instance.inputElementsData[index] = {
+						value: node.getAttribute('value') || index,
+						title: node.getAttribute('title')
+					};
+				});
+
+				inputs.remove();
+			}
+
+			hiddenInput.setAttribute('name', inputName);
+
+			boundingBox.appendChild(hiddenInput);
+
+			instance.set('hiddenInput', hiddenInput);
+		},
+
+		_initElements: function() {
+			var instance = this;
+			var contentBox = instance.get(CONTENT_BOX);
+			var ratingElement = AUI.Node.create('<a href="javascript:void(0);"></a>');
+
+			contentBox.addClass(C_CLEAR_FIX);
+			ratingElement.addClass(C_RATING_EL);
+			ratingElement.addClass(C_RATING_EL_OFF);
+
+			for (var i = 0, size = this.get(SIZE); i < size; i++) {
+				var	data = instance._getInputData(i);
+				var element = ratingElement.cloneNode();
+
+				if (data && data.title) {
+					element.setAttribute('title', data.title);
+				}
+
+				contentBox.appendChild(element);
+			}
+
+			instance.set('elements', contentBox.queryAll(A));
+		},
+
+		_syncElements: function(){
+			var instance = this;
+			var selectedIndex = instance.get(DEFAULT_SELECTED) - 1;
+
+			instance.set(SELECTED_INDEX, selectedIndex);
+
+			instance.select();
+		},
+
+		_delegateElements: function() {
+			var instance = this;
+			var boundingBox = instance.get(BOUNDING_BOX);
+
+			boundingBox.delegate('click', AUI.bind(instance._onElementClick, this), A);
+			boundingBox.delegate('mouseover', AUI.bind(instance._onElementMouseOver, this), A);
+			boundingBox.delegate('mouseout', AUI.bind(instance._onElementMouseOut, this), A);
+		},
+
+		_getInputData: function(index) {
+			var instance = this;
+
+			return instance.inputElementsData[index] || {};
+		},
+
+		/*
+		* Delegated events
+		*/
+		_onElementClick: function(event) {
+			var instance = this;
+			var	elements = instance.get('elements');
+			var index = elements.indexOf(event.target);
+
+			instance.select(index);
+
+			instance.fire('click');
+		},
+
+		_onElementMouseOver: function(event) {
+			var instance = this;
+			var	elements = instance.get('elements');
+
+			var value = elements.indexOf(event.target);
+
+			instance.fillTo(value, C_RATING_EL_HOVER);
+
+			instance.fire('mouseover');
+		},
+
+		_onElementMouseOut: function(event) {
+			var instance = this;
+			var selectedIndex = instance.get(SELECTED_INDEX);
+
+			instance.fillTo(selectedIndex);
+
+			instance.fire('mouseout');
+		},
+
+		/*
+		* Attribute Listeners
+		*/
+		_afterSetDisabled: function(event) {
+			var instance = this;
+			var	contentBox = instance.get(CONTENT_BOX);
+
+			if (event.newVal == true) {
+				contentBox.addClass(C_RATING_DISABLED);
+
+				instance.unbindUI();
+			}
+			else {
+				contentBox.removeClass(C_RATING_DISABLED);
+
+				instance.bindUI();
+			}
+		}
+	});
+
+	AUI.Rating = Rating;
+
+}, '@VERSION' , { requires: [ 'widget' ] });
