fea453975a3eaab50cc1aeec369d18a559bd24fe
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2009-08-20 22:12:53 +0000

    Rollback rev. 36082
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@36084 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/javascript/defaults.js
+++ src/javascript/defaults.js
@@ -21,7 +21,7 @@
 
 			//base: '',
 
-			filter: 'raw',
+			//filter: 'raw',
 
 			modules: {
 				/*
@@ -67,21 +67,13 @@
 				*/
 				'dialog': {
 					fullpath: PATH_JAVASCRIPT + 'dialog.js',
-					requires: [ 'overlay', 'dialog-css', 'plugin' ],
-					use: [ 'dd-constrain', 'overlay-manager' ]
+					requires: [ 'overlay', 'dialog-css' ],
+					use: [ 'dd-constrain' ]
 				},
 				'dialog-css': {
 					ext: false,
 					fullpath: PATH_THEME_ROOT + 'dialog.css',
 					type: 'css'
-				},
-
-				/*
-				* OverlayManager
-				*/
-				'overlay-manager': {
-					fullpath: PATH_JAVASCRIPT + 'overlay-manager.js',
-					requires: [ 'overlay' ]
 				}
 			}
 		}
