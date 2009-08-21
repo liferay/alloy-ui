6db9eb2f852f17a57f0a8d2d513467bc605a064a
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2009-08-21 18:59:53 +0000

    OverlayMask skeleton (not implemented)
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@36178 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ src/javascript/overlay-mask.js
@@ -0,0 +1,28 @@
+AUI.add('overlay-mask', function(A) {
+
+var L = A.Lang,
+	isString = L.isString,
+
+	OVERLAY_MASK = 'OverlayMask';
+
+function OverlayMask(config) {
+ 	OverlayMask.superclass.constructor.apply(this, arguments);
+}
+
+A.mix(OverlayMask, {
+	NAME: OVERLAY_MASK,
+
+	NS: OVERLAY_MASK,
+
+	ATTRS: {
+
+	}
+});
+
+A.extend(OverlayMask, A.Plugin.Base, {
+
+});
+
+A.OverlayMask = OverlayMask;
+
+}, '@VERSION', { requires: [ 'overlay' ] });
