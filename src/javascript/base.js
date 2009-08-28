a54dd01575b101c01cb654a14b4398f19be5b8e2
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2009-08-28 03:42:41 +0000

    Added A.Node.globalEval, A.Node.evalScripts. A.Node.domManip support to set(INNER_HTML, v)
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@36444 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/javascript/base.js
+++ src/javascript/base.js
@@ -35,7 +35,7 @@ AUI.add('aui-node', function(A) {
 			var args = arguments, length = args.length;
 
 			if (length) {
-				A.Node.DEFAULT_SETTER.apply(this, [ INNER_HTML, args[0] ]);
+				this.set(INNER_HTML, args[0]);
 			}
 			else {
 				return this.get(INNER_HTML);
@@ -45,11 +45,80 @@ AUI.add('aui-node', function(A) {
 		}
 	}, true);
 
+
+	A.mix(A.Node, {
+		globalEval: function(data) {
+			if ( data && /\S/.test(data) ) {
+				var head = document.getElementsByTagName("head")[0] || document.documentElement;
+				var script = document.createElement("script");
+
+				script.type = "text/javascript";
+
+				if (AUI.support.scriptEval) {
+					script.appendChild(document.createTextNode(data));
+				}
+				else {
+					script.text = data;
+				}
+
+				head.insertBefore(script, head.firstChild);
+				head.removeChild(script);
+			}
+		},
+
+		evalScript: function(elem) {
+			try{
+			if (elem.src) {
+				A.io(elem.src, {
+					async: false,
+					on: {
+						complete: function(i, o) {
+							A.Node.globalEval(o.responseText);
+						}
+					}
+				});
+			}
+			else {
+				A.Node.globalEval(elem.text || elem.textContent || elem.innerHTML || "");
+			}
+		}catch(e){alert(e)}
+		},
+
+		domManip: function(html) {
+			var TPL_NODE = '<div></div>',
+				SCRIPT = 'script';
+
+			delete A.DOM._cloneCache[html];
+
+			// wrap in a nodeType == 1, nodeType 11 (document fragment) does not support getElementsByTagName
+			var temp = A.Node.create(TPL_NODE).append(
+				A.Node.create(html)
+			);
+
+			var scripts = temp.getElementsByTagName(SCRIPT);
+
+			scripts.each(function(elem) {
+				A.Node.evalScript( A.Node.getDOMNode(elem) );
+				elem.remove();
+			});
+
+			var html = temp.html();
+
+			// avoid IE6 memory leak
+			temp = null;
+
+			return html;
+		}
+	});
+
 	A.Node.ATTRS.innerHTML = {
 		setter: function(v) {
-			A.Node.DEFAULT_SETTER.apply(this, [ INNER_HTML, v ]);
+			if (isString(v)) {
+				// eval scripts
+				v = A.Node.domManip(v);
+			}
 
-			return v;
+			A.Node.DEFAULT_SETTER.apply(this, [ INNER_HTML, v ]);
 		}
 	};
 
