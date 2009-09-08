1fdbf066cc0b2bba4753e59d189aab996a6a15e9
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2009-09-08 17:49:38 +0000

    Moving Tool css into base required to guarantee the load order
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@37007 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/javascript/defaults.js
+++ src/javascript/defaults.js
@@ -79,12 +79,7 @@
 				*/
 				'tool-item': {
 					fullpath: PATH_JAVASCRIPT + 'tool-item.js',
-					requires: [ 'aui-base', 'state-interaction', 'tool-item-css' ]
-				},
-				'tool-item-css': {
-					ext: false,
-					fullpath: PATH_THEME_ROOT + 'tool-item.css',
-					type: 'css'
+					requires: [ 'aui-base', 'state-interaction' ]
 				},
 
 				/*
