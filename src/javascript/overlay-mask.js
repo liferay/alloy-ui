b657262928fafbd957aff1c3534a83f38d5a44de
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2009-09-14 15:23:22 +0000

    OverlayMask first commit
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@37411 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/javascript/overlay-mask.js
+++ src/javascript/overlay-mask.js
@@ -2,8 +2,19 @@ AUI.add('overlay-mask', function(A) {
 
 var L = A.Lang,
 	isString = L.isString,
+	isNumber = L.isNumber,
 
-	OVERLAY_MASK = 'OverlayMask';
+	ALIGN = 'align',
+	BACKGROUND = 'background',
+	BOUNDING_BOX = 'boundingBox'
+	HEIGHT = 'height',
+	OFFSET_HEIGHT = 'offsetHeight',
+	OFFSET_WIDTH = 'offsetWidth',
+	OPACITY = 'opacity',
+	OVERLAY_MASK = 'OverlayMask',
+	TARGET = 'target',
+	TL = 'tl',
+	WIDTH = 'width';
 
 function OverlayMask(config) {
  	OverlayMask.superclass.constructor.apply(this, arguments);
@@ -12,18 +23,128 @@ function OverlayMask(config) {
 A.mix(OverlayMask, {
 	NAME: OVERLAY_MASK,
 
-	NS: OVERLAY_MASK,
-
 	ATTRS: {
+		align: {
+            value: { node: null, points: [ TL, TL ] }
+        },
 
+		background: {
+			lazyAdd: false,
+			value: '#000',
+			validator: isString,
+			setter: function(v) {
+				this.get(BOUNDING_BOX).setStyle(BACKGROUND, v);
+
+				return v;
+			}
+		},
+
+		target: {
+			lazyAdd: false,
+			value: document,
+			setter: function(v) {
+				return A.get(v);
+			}
+		},
+
+		opacity: {
+			lazyAdd: false,
+			value: .5,
+			validator: isNumber,
+			setter: function(v) {
+				this.get(BOUNDING_BOX).setStyle(OPACITY, v);
+
+				return v;
+			}
+		},
+
+		shim: {
+			value: A.UA.ie
+		},
+
+		visible: {
+			value: false
+		},
+
+		zIndex: {
+			value: 1000
+		}
 	}
 });
 
-A.extend(OverlayMask, A.Plugin.Base, {
+A.extend(OverlayMask, A.Overlay, {
+	/*
+	* Lifecycle
+	*/
+	bindUI: function() {
+		var instance = this;
 
+		OverlayMask.superclass.bindUI.apply(this, arguments);
+
+		instance.after('targetChange', instance._afterTargetChange);
+
+		// window:resize YUI normalized event is not working, bug?
+		A.on('resize', A.bind(instance.refreshMask, instance));
+	},
+
+	syncUI: function() {
+		var instance = this;
+
+		instance.refreshMask();
+	},
+
+	/*
+	* Methods
+	*/
+	getTargetSize: function() {
+		var instance = this;
+		var target = instance.get(TARGET);
+		var height = 0, width = 0;
+
+		var isDoc = target.compareTo(document);
+		var isWin = target.compareTo(window);
+
+		if (isDoc) {
+		    width = A.DOM.docWidth();
+		    height = A.DOM.docHeight();
+		}
+		else if (isWin) {
+		    width = A.DOM.winWidth();
+		    height = A.DOM.winHeight();
+		}
+		else {
+			height = target.get(OFFSET_HEIGHT);
+			width = target.get(OFFSET_WIDTH);
+		}
+
+		return { height: height, width: width };
+	},
+
+	/*
+	* Methods
+	*/
+	refreshMask: function() {
+		var instance = this;
+		var align = instance.get(ALIGN);
+		var target = instance.get(TARGET);
+		var size = instance.getTargetSize();
+
+		instance.set(HEIGHT, size.height);
+		instance.set(WIDTH, size.width);
+
+		instance.align(target, align.points);
+	},
+
+	/*
+	* Listeners
+	*/
+	_afterTargetChange: function(event) {
+		var instance = this;
+
+		instance.refreshMask();
+	}
 });
 
-A.namespace('Plugin');
-A.Plugin.OverlayMask = OverlayMask;
+A.OverlayMask = OverlayMask;
 
-}, '@VERSION', { requires: [ 'overlay' ] });
+}, '@VERSION', { requires: [ 'overlay', 'event-resize' ] });
