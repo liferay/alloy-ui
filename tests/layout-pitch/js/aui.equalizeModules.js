e768edffc21f0027589925cf0ceb7f698d2fbe5e
Jonathan Neal <jonathan.neal@liferay.com>
2009-09-17 21:56:52 +0000

    no-js fallback support against subpixel rendering
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@37724 05bdf26c-840f-0410-9ced-eb539d925f36

--- tests/layout-pitch/js/aui.equalizeModules.js
+++ tests/layout-pitch/js/aui.equalizeModules.js
@@ -1,83 +1,72 @@
-/*
-	A function I wrote to keep columns the same height, fun but perhaps disregardable.
-	I think it's cool.
-	It doesn't affect the width or position of the columns, it's just an added bonus for height.
-*/
+(function () {
+	var win = window,
+	doc = document,
+	winxy = [0, 0],
+	$node;
 
-(function ($) {
-	var doc = document,
-	win = window;
-
-	function styleInt (elem, styleName) {
-		return parseInt((elem.currentStyle || doc.defaultView.getComputedStyle(elem, null))[styleName], 10) || 0;
+	function styleInt (node, styleName) {
+		return parseInt((node.currentStyle || doc.defaultView.getComputedStyle(node, null))[styleName], 10) || 0;
 	}
 
-	function equalizeModules(elem) {
-		var dim = arguments.callee.dim = arguments.callee.dim || [0, 0];
+	function equalizeColumns () {
+		var maxHeight = 0,
+		shave;
 
-		if (win.innerHeight != dim[0] || win.innerWidth != dim[1]) {
-			elem.each(
+		if (win.innerHeight != winxy[0] || win.innerWidth != winxy[1]) {
+			($node = $node || $('.aui-row')).each(
 				function () {
-					var maxHeight = 0;
+					maxHeight = 0;
 
-					$(this).children().children('.aui-mod').each(
+					$(this.childNodes).each(
+						function () {
+							if (!this.nextSibling) {
+								var index = -1,
+								childOffsetWidth = 0,
+								offset;
+				
+								while (++index < this.parentNode.childNodes.length) {
+									childOffsetWidth += (this.parentNode.childNodes[index].offsetWidth || 0);
+								}
+				
+								offset = this.parentNode.offsetWidth - childOffsetWidth;
+				
+								if (offset < 0) {
+									this.style.marginRight = offset + 'px';
+								}
+							}
+							else {
+								return 0;
+							}
+						}
+					).children().each(
 						function () {
 							this.parentNode.style.zoom = 0;
-
+			
 							this.style.height = '1%';
-
+			
 							maxHeight = Math.max(maxHeight, this.offsetHeight || 0);
 						}
 					).each(
 						function () {
-							var shave = styleInt(this, 'paddingTop') + styleInt(this, 'paddingBottom') + styleInt(this, 'borderTopWidth') + styleInt(this, 'borderBottomWidth');
-
+							shave = styleInt(this, 'paddingTop') + styleInt(this, 'paddingBottom') + styleInt(this, 'borderTopWidth') + styleInt(this, 'borderBottomWidth');
+			
 							this.style.height = (maxHeight - shave) + 'px';
-
+			
 							this.parentNode.style.zoom = 1;
 						}
 					);
 				}
 			);
-
-			dim = [win.innerHeight, win.innerWidth];
 		}
 	}
 
-	function unequalizeModules(elem) {
-		elem.each(
-			function () {
-				$(this).children().children('.aui-mod').css(
-					{
-						height: 'auto',
-						zoom: 1
-					}
-				);
-			}
-		);
-	}
+	$(doc).bind(
+		'ready',
+		equalizeColumns
+	);
 
-	$.fn.auiEqualizeModules = function () {
-		var elem = this,
-		fn = function() {
-			equalizeModules(elem);
-		};
-
-		equalizeModules(elem);
-
-		$(win).bind(
-			'resize',
-			fn
-		);
-	};
-
-	$.fn.auiUnequalizeModules = function () {
-		var elem = this;
-
-		unequalizeModules(elem);
-
-		$(win).unbind(
-			'resize'
-		);
-	};
-}(jQuery));
+	$(win).bind(
+		'resize',
+		equalizeColumns
+	);
+}());
