343bc5a7987cbd1361f8df242c818a892fc88d4c
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2009-09-24 00:39:19 +0000

    Calendar added firstDayOfWeek option
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@38171 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/javascript/i18n/calendar-pt-br.js
+++ src/javascript/i18n/calendar-pt-br.js
@@ -2,7 +2,7 @@ AUI().use('datatype-date', function(A) {
 
 	A.DataType.Date.Locale['pt-br'] = A.merge(
 		A.DataType.Date.Locale['en'], {
-			a: ['Seg', 'Ter', 'Qua', 'Qui', 'Thu', 'Fri', 'Sat'],
+			a: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Fri', 'Sat'],
 			A: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
 			b: ['Jan','Fev','Mar','Abr','Mai','Jun', 'Jul','Ago','Set','Out','Nov','Dez'],
 			B: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho', 'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
