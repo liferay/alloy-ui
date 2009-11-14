675e2a07564aed02ba614fa0806b1d877d49deb0
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2009-11-14 18:54:36 +0000

    Adding ComponentOverlay for our Overlay's based on the Component architecture
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@40821 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ src/javascript/component-overlay.js
@@ -0,0 +1,11 @@
+AUI().add(
+	'component-overlay',
+	function(A) {
+		A.ComponentOverlay = A.Base.build('overlay', A.Component, [A.WidgetPosition, A.WidgetStack, A.WidgetPositionExt, A.WidgetStdMod]);
+	},
+	'@VERSION',
+	{
+		requires: ['component', 'widget-position', 'widget-stack', 'widget-position-ext', 'widget-stdmod'],
+		use: []
+	}
+);
