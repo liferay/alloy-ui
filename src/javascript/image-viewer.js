da50fea6fb72c0b4019e84877aae4a1ef0fd91a9
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2009-12-07 20:54:32 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@42060 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/javascript/image-viewer.js
+++ src/javascript/image-viewer.js
@@ -669,7 +669,7 @@ A.extend(ImageViewer, A.ComponentOverlay, {
 		var instance = this;
 
 		// invoke A.Overlay _afterVisibleChange method
-		ImageViewer.superclass._afterVisibleChange.apply(this, arguments)
+		ImageViewer.superclass._afterVisibleChange.apply(this, arguments);
 
 		instance._syncControlsUI();
 	},
