fbb67c85c9c37f972fc7e4342721da9ef2af8b01
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2009-12-07 06:38:02 +0000

    Improving test to globaleval using parse-content
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@42040 05bdf26c-840f-0410-9ced-eb539d925f36

--- tests/assets/external.js
+++ tests/assets/external.js
@@ -1,3 +1,1 @@
-document.getElementById('log').innerHTML += 'external.js Declaring variable window.a<br/>';
-
-window.a = 'Value of window.a: Declared on external.js<br/>';
+AUI().getBody().append(index++ + 'external file<br/>');
