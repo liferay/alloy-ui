6e23ef6ee25e2aa4b8cd6dad1a3967f435480f15
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-01-28 01:36:17 +0000

    Renaming string-evaluation to Datatype namespace
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@44810 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/javascript/datatype.js
+++ src/javascript/datatype.js
@@ -1,32 +1,35 @@
-AUI.add('string-evaluation', function(A) {
+AUI.add('aui-datatype', function(A) {
 
 var L = A.Lang,
 	FALSE = 'false',
-	TRUE = 'true';
+	TRUE = 'true',
 
-L.parseBoolean = function(value) {
-	return (value == FALSE) ? false : !!value;
+	DB = A.namespace('DataType.Boolean'),
+	DS = A.namespace('DataType.String');
+
+DB.parse = function(data) {
+	return (data == FALSE) ? false : !!data;
 };
 
 // parseString inspired on Ariel Flesler jQuery Parse Plugin - http://plugins.jquery.com/project/Parse
-L.parseString = function(value) {
+DS.evaluate = function(data) {
 	// booleans
-	if (value == TRUE || value == FALSE) {
-		return L.parseBoolean(value);
+	if (data == TRUE || data == FALSE) {
+		return DB.parse(data);
 	}
 
 	// Handle positive & negative numbers (integer or float)
 	// Handle hexadecimal numbers: 0xFF -> 255
 	// Handle exponential notation: 1e5 -> 100000
-	if (value && L.isString(value)) {
-		var number = +value;
+	if (data && L.isString(data)) {
+		var number = +data;
 
 		if (!isNaN(number)) {
 			return number;
 		}
 	}
 
-	return value;
+	return data;
 };
 
 }, '@VERSION', { requires: [ 'aui-base' ] });
