cc366159779a67f6d61154592e5bdbfca1d1b705
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-02-17 07:35:53 +0000

    Removing build_tmp and build_rollup_tmp folders
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@46156 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-component-overlay/build_tmp/aui-component-overlay-min.js
+++
@@ -1,1 +0,0 @@
-AUI.add("aui-component-overlay",function(B){B.ComponentOverlay=B.Base.build("overlay",B.Component,[B.WidgetPosition,B.WidgetStack,B.WidgetPositionAlign,B.WidgetStdMod]);},"@VERSION@",{skinnable:false,requires:["aui-component","widget-position","widget-stack","widget-position-align","widget-stdmod"]});
--- src/aui-fieldset/build_tmp/aui-fieldset-debug.js
+++
@@ -1,4 +0,0 @@
-AUI.add('aui-fieldset', function(A) {
-A.Fieldset = A.Base.build('fieldset', A.Panel, []);
-
-}, '@VERSION@' ,{requires:['aui-panel'], skinnable:false});
