d56b9a5f36fea69ccfd705e3424d38c2e5efe9bd
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-03-04 02:46:26 +0000

    Documentation - LiveSearch
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@47405 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-live-search/js/aui-live-search.js
+++ src/aui-live-search/js/aui-live-search.js
@@ -1,3 +1,10 @@
+/**
+ * The LiveSearch Utility allow real-time filtering for DOM elements based on
+ * a input query.
+ *
+ * @module aui-live-search
+ */
+
 var L = A.Lang,
 	isString = L.isString,
 	isObject = L.isObject,
@@ -21,14 +28,70 @@ var L = A.Lang,
 		return (v instanceof A.NodeList);
 	};
 
+/**
+ * <p><img src="assets/images/aui-live-search/main.png"/></p>
+ *
+ * A base class for LiveSearch, providing:
+ * <ul>
+ *    <li>Real-time filtering for DOM elements based on a input query</li>
+ * </ul>
+ *
+ * Quick Example:<br/>
+ *
+ * <pre><code>var instance = new A.LiveSearch({
+ *	input: '#input',
+ *	nodes: '#search .entry'
+ * });
+ * </code></pre>
+ *
+ * Check the list of <a href="LiveSearch.html#configattributes">Configuration Attributes</a> available for
+ * LiveSearch.
+ *
+ * @param config {Object} Object literal specifying widget configuration properties.
+ *
+ * @class LiveSearch
+ * @constructor
+ * @extends Base
+ */
 function LiveSearch(config) {
 	LiveSearch.superclass.constructor.apply(this, arguments);
 }
 
 A.mix(LiveSearch, {
+	/**
+	 * Static property provides a string to identify the class.
+	 *
+	 * @property LiveSearch.NAME
+	 * @type String
+	 * @static
+	 */
 	NAME: LIVE_SEARCH,
 
+	/**
+	 * Static property used to define the default attribute
+	 * configuration for the LiveSearch.
+	 *
+	 * @property LiveSearch.ATTRS
+	 * @type Object
+	 * @static
+	 */
 	ATTRS: {
+		/**
+		 * <p>Function to extract the content from the node for the indexing. The
+         * default uses the <code>node.html()</code>. In case if you need to
+         * index the id of the nodes, here goes one example:</p>
+		 *
+		 * Example indexing the id of the node instead of the HTML:
+		 *
+		 * <pre><code>function(node) {
+		 *  return node.attr('id');
+		 * }
+		 * </code></pre>
+		 *
+		 * @attribute data
+		 * @default function(node) { return node.html(); }
+		 * @type function
+		 */
 		data: {
 			value: function(node) {
 				return node.html();
@@ -36,10 +99,26 @@ A.mix(LiveSearch, {
 			validator: isFunction
 		},
 
+		/**
+		 * Number of milliseconds the filter will be applied to the node list
+         * after the user stop typing.
+		 *
+		 * @attribute delay
+		 * @default 250
+		 * @type Number
+		 */
 		delay: {
 			value: 250
 		},
 
+		/**
+		 * Function to be executed to hide the node when the data of that node
+         * not matches with the filter.
+		 *
+		 * @attribute hide
+		 * @default function(node) { return node.hide(); }
+		 * @type function
+		 */
 		hide: {
 			value: function(node) {
 				return node.hide();
@@ -47,17 +126,40 @@ A.mix(LiveSearch, {
 			validator: isFunction
 		},
 
+		/**
+		 * Index for the nodes content.
+		 *
+		 * @attribute index
+		 * @default []
+		 * @type Array
+		 */
 		index: {
 			value: [],
 			validator: isObject
 		},
 
+		/**
+		 * The <code>value</code> of this input node is used to filter the
+         * results.
+		 *
+		 * @attribute input
+		 * @type Node | String
+		 */
 		input: {
 			setter: function(v) {
 				return A.get(v);
 			}
 		},
 
+		/**
+		 * The input <code>value</code> need to matches with this RegExp to be
+         * accept as a filter (i.e., in order to accept only digits you
+         * could use /\d+/g).
+		 *
+		 * @attribute matchRegex
+		 * @default /(.)*/g
+		 * @type RegExp
+		 */
 		matchRegex: {
 			validator: function(v) {
 				return (v instanceof RegExp);
@@ -65,12 +167,26 @@ A.mix(LiveSearch, {
 			value: /(.)*/g
 		},
 
+		/**
+		 * Nodes to be indexed for the filtering.
+		 *
+		 * @attribute nodes
+		 * @type Node | NodeList
+		 */
 		nodes: {
 			setter: function(v) {
 				return this._setNodes(v);
 			}
 		},
 
+		/**
+		 * Function to be executed to show the node when the data of that node
+         * matches with the filter.
+		 *
+		 * @attribute show
+		 * @default function(node) { return node.show(); }
+		 * @type function
+		 */
 		show: {
 			value: function(node) {
 				return node.show();
@@ -81,15 +197,40 @@ A.mix(LiveSearch, {
 });
 
 A.extend(LiveSearch, A.Base, {
+	/**
+	 * Stores the normalized query value given from
+     * <a href="LiveSearch.html#config__normalizeQuery">_normalizeQuery</a>.
+	 *
+	 * @property normalizedQuery
+	 * @type String
+	 * @protected
+	 */
 	normalizedQuery: BLANK,
 
+	/**
+	 * Stores the query value.
+	 *
+	 * @property query
+	 * @type String
+	 * @protected
+	 */
 	query: BLANK,
 
+	/**
+	 * Handles the <a href="YUI.html#method_later">later</a> Object.
+	 *
+	 * @property timer
+	 * @type Object
+	 * @protected
+	 */
 	timer: null,
 
-	/*
-	* Lifecycle
-	*/
+	/**
+	 * Construction logic executed during LiveSearch instantiation. Lifecycle.
+	 *
+	 * @method initializer
+	 * @protected
+	 */
 	initializer: function() {
 		var instance = this;
 
@@ -98,6 +239,12 @@ A.extend(LiveSearch, A.Base, {
 		instance.bindUI();
 	},
 
+	/**
+	 * Bind the events on the LiveSearch UI. Lifecycle.
+	 *
+	 * @method bindUI
+	 * @protected
+	 */
 	bindUI: function() {
 		var instance = this;
 		var input = instance.get(INPUT);
@@ -107,6 +254,13 @@ A.extend(LiveSearch, A.Base, {
 		instance.publish('search', { defaultFn: instance.search });
 	},
 
+	/**
+	 * Descructor lifecycle implementation for the LiveSearch class.
+	 * Purges events attached to the node (and all child nodes).
+	 *
+	 * @method destroy
+	 * @protected
+	 */
 	destroy: function() {
 		var instance = this;
 		var input = instance.get(INPUT);
@@ -114,9 +268,14 @@ A.extend(LiveSearch, A.Base, {
 		input.detach('keyup');
 	},
 
-	/*
-	* Methods
-	*/
+	/**
+	 * Filter the <a href="LiveSearch.html#config_nodes">nodes</a> based on
+     * the input value.
+	 *
+	 * @method filter
+	 * @param {String} query Query to filter results
+	 * @return {Array} Matched results.
+	 */
 	filter: function(query) {
 		var instance = this;
 		var results = [];
@@ -143,6 +302,11 @@ A.extend(LiveSearch, A.Base, {
 		return results;
 	},
 
+	/**
+	 * Refreshes the <a href="LiveSearch.html#config_index">index</a>.
+	 *
+	 * @method refreshIndex
+	 */
 	refreshIndex: function() {
 		var instance = this;
 		var index = [];
@@ -158,6 +322,13 @@ A.extend(LiveSearch, A.Base, {
 		instance.set(INDEX, index);
 	},
 
+	/**
+	 * Fires the search event.
+	 *
+	 * @method search
+	 * @param {EventFacade} event search event facade
+	 * @protected
+	 */
 	search: function(event) {
 		var instance = this;
 		var value = instance.get(INPUT).val();
@@ -178,6 +349,16 @@ A.extend(LiveSearch, A.Base, {
 		event.liveSearch.results = results;
 	},
 
+	/**
+	 * Normalize the input query. With <code>trim</code>,
+     * <code>matchRegex</code> and replace '*' to '' (on a regex empty match
+     * with everything like *).
+	 *
+	 * @method _normalizeQuery
+	 * @param {String} query Query to filter results
+	 * @protected
+	 * @return {String}
+	 */
 	_normalizeQuery: function(query) {
 		var instance = this;
 		var matchRegex = instance.get(MATCH_REGEX);
@@ -196,9 +377,14 @@ A.extend(LiveSearch, A.Base, {
 		return query;
 	},
 
-	/*
-	* Listeners
-	*/
+	/**
+	 * Fires the keyup event on
+     * <a href="LiveSearch.html#config_input">input</a>.
+	 *
+	 * @method _inputKeyUp
+	 * @param {EventFacade} event keyup event facade
+	 * @protected
+	 */
 	_inputKeyUp: function(event) {
 		var instance = this;
 		var delay = instance.get(DELAY);
@@ -221,9 +407,14 @@ A.extend(LiveSearch, A.Base, {
 		});
 	},
 
-	/*
-	* Setters
-	*/
+	/**
+	 * Setter for <a href="LiveSearch.html#config_nodes">nodes</a>.
+	 *
+	 * @method _setNodes
+	 * @param {Node | NodeList | String} v
+	 * @protected
+	 * @return {Node | NodeList | String}
+	 */
 	_setNodes: function(v) {
 		var instance = this;
 
