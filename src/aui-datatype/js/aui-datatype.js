0ef8827b70ab83f1bbd7a55ee3b20f2a6c6546fa
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-03-04 21:51:36 +0000

    Documentation - Datatype
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@47460 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-datatype/js/aui-datatype.js
+++ src/aui-datatype/js/aui-datatype.js
@@ -1,15 +1,56 @@
+/**
+ * The Datatype Utility
+ *
+ * @module aui-datatype
+ */
+
 var L = A.Lang,
 	FALSE = 'false',
 	TRUE = 'true',
 
+	/**
+	 * DataType.Boolean provides a set of utility to parse <code>falsey</code>
+     * value to <code>false</code> and <code>non-falsey</code> to
+     * <code>true</code>.
+	 *
+	 * @class DataType.Boolean
+	 * @static
+	 */
 	DB = A.namespace('DataType.Boolean'),
+
+	/**
+	 * DataType.String provides a set of utility to provides a simple function
+     * that evaluates a string to a primitive value (if possible). Supports
+     * <code>true</code> and <code>false</code> also.
+	 *
+	 * @class DataType.String
+	 * @static
+	 */
 	DS = A.namespace('DataType.String');
 
+/**
+ * Parses any <code>falsey</code> value to <code>false</code> and
+ * <code>non-falsey</code> to <code>true</code>.
+ * 
+ * @for DataType.Boolean
+ * @method parse
+ * @param {*} data falsey or non-falsey values (i.e., falsey values: null, false, undefined, NaN; non-falsey values: 1, true, 'abc').
+ * @return {boolean} Parsed value
+ */
 DB.parse = function(data) {
 	return (data == FALSE) ? false : !!data;
 };
 
-// parseString inspired on Ariel Flesler jQuery Parse Plugin - http://plugins.jquery.com/project/Parse
+/**
+ * Evaluates a string to a primitive value (if possible). Supports
+ * <code>true</code> and <code>false</code> also. Unrecognized strings are
+ * returned without any modification.
+ * 
+ * @for DataType.String
+ * @method evaluate
+ * @param {*} data Input data to be evaluated.
+ * @return {boolean | null | number | String | undefined} Parsed value
+ */
 DS.evaluate = function(data) {
 	// booleans
 	if (data == TRUE || data == FALSE) {
