347888b31eb324466f31cdf5efebb58eac063245
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-03-06 00:19:30 +0000

    Documentation - Editable
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@47603 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-editable/js/aui-editable.js
+++ src/aui-editable/js/aui-editable.js
@@ -1,3 +1,9 @@
+/**
+ * The Editable Utility
+ *
+ * @module aui-editable
+ */
+
 var Lang = A.Lang,
 	isFunction = Lang.isFunction,
 
@@ -11,13 +17,61 @@ var Lang = A.Lang,
 
 	CONTENT_BOX = 'contentBox';
 
+/**
+ * <p><img src="assets/images/aui-editable/main.png"/></p>
+ *
+ * A base class for Editable, providing:
+ * <ul>
+ *    <li>Widget Lifecycle (initializer, renderUI, bindUI, syncUI, destructor)</li>
+ *    <li>Edit in place elements</li>
+ * </ul>
+ *
+ * Quick Example:<br/>
+ *
+ * <pre><code>var instance = new A.Editable({
+ *   node: '#editor'
+ * }).render();
+ * </code></pre>
+ *
+ * Check the list of <a href="Editable.html#configattributes">Configuration Attributes</a> available for
+ * Editable.
+ *
+ * @param config {Object} Object literal specifying widget configuration properties.
+ *
+ * @class Editable
+ * @constructor
+ * @extends Component
+ */
 var Editable = function() {
 	Editable.superclass.constructor.apply(this, arguments);
 };
 
+/**
+ * Static property provides a string to identify the class.
+ *
+ * @property Editable.NAME
+ * @type String
+ * @static
+ */
 Editable.NAME = 'editable';
 
+/**
+ * Static property used to define the default attribute
+ * configuration for the Editable.
+ *
+ * @property Editable.ATTRS
+ * @type Object
+ * @static
+ */
 Editable.ATTRS = {
+	/**
+	 * <a href="ToolSet.html">ToolSet</a> constructor Object for the
+     * cancelButton.
+	 *
+	 * @attribute cancelButton
+	 * @default ToolSet constructor Object.
+	 * @type String
+	 */
 	cancelButton: {
 		valueFn: function() {
 			var instance = this;
@@ -32,6 +86,14 @@ Editable.ATTRS = {
 			};
 		}
 	},
+
+	/**
+	 * Content text.
+	 *
+	 * @attribute contentText
+	 * @default ''
+	 * @type String
+	 */
 	contentText: {
 		value: '',
 		setter: function(value) {
@@ -45,16 +107,36 @@ Editable.ATTRS = {
 		}
 	},
 
+	/**
+	 * Function to format the input text displayed on the input.
+	 *
+	 * @attribute formatInput
+	 * @default null
+	 * @type function
+	 */
 	formatInput: {
 		value: null,
 		validator: isFunction
 	},
 
+	/**
+	 * Function to format the output text displayed on the input.
+	 *
+	 * @attribute formatOutput
+	 * @default null
+	 * @type function
+	 */
 	formatOutput: {
 		value: null,
 		validator: isFunction
 	},
 
+	/**
+	 * Node to setup the editable.
+	 *
+	 * @attribute node
+	 * @type Node
+	 */
 	node: {
 		setter: function(value) {
 			var node = A.get(value);
@@ -70,10 +152,23 @@ Editable.ATTRS = {
 		}
 	},
 
+	/**
+	 * Event type to initialize the editable.
+	 *
+	 * @attribute eventType
+	 * @default 'click'
+	 * @type String
+	 */
 	eventType: {
 		value: 'click'
 	},
 
+	/**
+	 * Node to render the editable.
+	 *
+	 * @attribute renderTo
+	 * @type String | Node
+	 */
 	renderTo: {
 		value: document.body,
 		setter: function(value) {
@@ -99,6 +194,14 @@ Editable.ATTRS = {
 		}
 	},
 
+	/**
+	 * <a href="ToolSet.html">ToolSet</a> constructor Object for the
+     * saveButton.
+	 *
+	 * @attribute saveButton
+	 * @default ToolSet constructor Object.
+	 * @type String
+	 */
 	saveButton: {
 		valueFn: function() {
 			var instance = this;
@@ -114,10 +217,25 @@ Editable.ATTRS = {
 		}
 	},
 
+	/**
+	 * Array with tools for the <a href="ToolSet.html">ToolSet</a>.
+	 *
+	 * @attribute tools
+	 * @default []
+	 * @type Array
+	 */
 	tools: {
 		value: []
 	},
 
+	/**
+	 * Type of the input used to edit the <a
+     * href="Editable.html#config_node">node</a>.
+	 *
+	 * @attribute inputType
+	 * @default 'text'
+	 * @type String
+	 */
 	inputType: {
 		value: 'text',
 		setter: function(value) {
@@ -140,6 +258,12 @@ A.extend(
 	Editable,
 	A.Component,
 	{
+		/**
+		 * Construction logic executed during Editable instantiation. Lifecycle.
+		 *
+		 * @method initializer
+		 * @protected
+		 */
 		initializer: function() {
 			var instance = this;
 
@@ -156,6 +280,12 @@ A.extend(
 			instance._createEvents();
 		},
 
+		/**
+		 * Create the DOM structure for the Editable. Lifecycle.
+		 *
+		 * @method renderUI
+		 * @protected
+		 */
 		renderUI: function() {
 			var instance = this;
 
@@ -200,6 +330,12 @@ A.extend(
 			instance.inputNode = comboBox.get('node');
 		},
 
+		/**
+		 * Bind the events on the Editable UI. Lifecycle.
+		 *
+		 * @method bindUI
+		 * @protected
+		 */
 		bindUI: function() {
 			var instance = this;
 
@@ -217,6 +353,12 @@ A.extend(
 			A.getDoc().after('click', instance._afterFocusedChangeEditable, instance);
 		},
 
+		/**
+		 * Sync the Editable UI. Lifecycle.
+		 *
+		 * @method syncUI
+		 * @protected
+		 */
 		syncUI: function() {
 			var instance = this;
 
@@ -238,27 +380,61 @@ A.extend(
 			);
 		},
 
+		/**
+		 * Cancel the editable. Return to the original state.
+		 *
+		 * @method cancel
+		 */
 		cancel: function() {
 			var instance = this;
 
 			instance.fire('cancel');
 		},
 
+		/**
+		 * Save the editable. Fires the
+		 * <a href="Editable.html#event_save">save</a> event.
+		 *
+		 * @method save
+		 */
 		save: function(event) {
 			var instance = this;
 
 			instance.fire('save');
 		},
 
+		/**
+		 * Fires the <a href="Editable.html#event_stopEditing">stopEditing</a>
+         * event.
+		 *
+		 * @method _afterFocusedChangeEditable
+		 * @param {EventFacade} event
+		 * @protected
+		 */
 		_afterFocusedChangeEditable: function(event) {
 			var instance = this;
 
 			instance.fire('stopEditing', instance.get('visible'));
 		},
 
+		/**
+		 * Create the Events.
+		 *
+		 * @method _createEvents
+		 * @protected
+		 */
 		_createEvents: function() {
 			var instance = this;
 
+			/**
+			 * Handles the startEditing event.
+			 *
+			 * @event startEditing
+			 * @preventable _defStartEditingFn
+			 * @param {Event.Facade} event The startEditing event.
+			 * @bubbles
+			 * @type {Event.Custom}
+			 */
 			instance.publish(
 				'startEditing',
 				{
@@ -269,6 +445,15 @@ A.extend(
 				}
 			);
 
+			/**
+			 * Handles the stopEditing event.
+			 *
+			 * @event stopEditing
+			 * @preventable _defStopEditingFn
+			 * @param {Event.Facade} event The stopEditing event.
+			 * @bubbles
+			 * @type {Event.Custom}
+			 */
 			instance.publish(
 				'stopEditing',
 				{
@@ -279,6 +464,15 @@ A.extend(
 				}
 			);
 
+			/**
+			 * Handles the save event.
+			 *
+			 * @event save
+			 * @preventable _defSaveFn
+			 * @param {Event.Facade} event The save event.
+			 * @bubbles
+			 * @type {Event.Custom}
+			 */
 			instance.publish(
 				'save',
 				{
@@ -289,6 +483,15 @@ A.extend(
 				}
 			);
 
+			/**
+			 * Handles the cancel event.
+			 *
+			 * @event cancel
+			 * @preventable _defCancelFn
+			 * @param {Event.Facade} event The cancel event.
+			 * @bubbles
+			 * @type {Event.Custom}
+			 */
 			instance.publish(
 				'cancel',
 				{
@@ -300,12 +503,26 @@ A.extend(
 			);
 		},
 
+		/**
+		 * Fires the cancel event.
+		 *
+		 * @method _defCancelFn
+		 * @param {EventFacade} event cancel event facade
+		 * @protected
+		 */
 		_defCancelFn: function(event) {
 			var instance = this;
 
 			instance.fire('stopEditing', false);
 		},
 
+		/**
+		 * Fires the startEditing event.
+		 *
+		 * @method _defStartEditingFn
+		 * @param {EventFacade} event startEditing event facade
+		 * @protected
+		 */
 		_defStartEditingFn: function(event) {
 			var instance = this;
 
@@ -341,6 +558,13 @@ A.extend(
 			inputNode.select();
 		},
 
+		/**
+		 * Fires the stopEditing event.
+		 *
+		 * @method _defStopEditingFn
+		 * @param {EventFacade} event stopEditing event facade
+		 * @protected
+		 */
 		_defStopEditingFn: function(event, save) {
 			var instance = this;
 
@@ -356,12 +580,26 @@ A.extend(
 			}
 		},
 
+		/**
+		 * Fires the save event.
+		 *
+		 * @method _defSaveFn
+		 * @param {EventFacade} event save event facade
+		 * @protected
+		 */
 		_defSaveFn: function(event) {
 			var instance = this;
 
 			instance.fire('stopEditing', true);
 		},
 
+		/**
+		 * Fires <code>onkeypress</code> occurs on the editable element.
+		 *
+		 * @method _onKeypressEditable
+		 * @param {EventFacade} event
+		 * @protected
+		 */
 		_onKeypressEditable: function(event) {
 			var instance = this;
 
@@ -377,18 +615,40 @@ A.extend(
 			}
 		},
 
+		/**
+		 * Fires <code>onmouseenter</code> occurs on the editable element.
+		 *
+		 * @method _onMouseEnterEditable
+		 * @param {EventFacade} event
+		 * @protected
+		 */
 		_onMouseEnterEditable: function(event) {
 			var instance = this;
 
 			instance.get('node').addClass(CSS_HOVER);
 		},
 
+		/**
+		 * Fires <code>onmouseleave</code> occurs on the editable element.
+		 *
+		 * @method _onMouseLeaveEditable
+		 * @param {EventFacade} event
+		 * @protected
+		 */
 		_onMouseLeaveEditable: function(event) {
 			var instance = this;
 
 			instance.get('node').removeClass(CSS_HOVER);
 		},
 
+		/**
+		 * Set the value of the <a
+         * href="Editable.html#property_inputNode">inputNode</a>.
+		 *
+		 * @method _setInput
+		 * @param {String} value Value of the input.
+		 * @protected
+		 */
 		_setInput: function(value) {
 			var instance = this;
 
@@ -404,6 +664,14 @@ A.extend(
 			instance.inputNode.set('value', value);
 		},
 
+		/**
+		 * Set the <code>innerHTML</code> of the <a
+         * href="Editable.html#config_node">node</a>.
+		 *
+		 * @method _setOutput
+		 * @param {String} value
+		 * @protected
+		 */
 		_setOutput: function(value) {
 			var instance = this;
 
@@ -419,6 +687,13 @@ A.extend(
 			instance.get('node').set('innerHTML', value);
 		},
 
+		/**
+		 * Fires when start editing.
+		 *
+		 * @method _startEditing
+		 * @param {EventFacade} event
+		 * @protected
+		 */
 		_startEditing: function(event) {
 			var instance = this;
 
@@ -431,6 +706,13 @@ A.extend(
 			event.halt();
 		},
 
+		/**
+		 * Sync the content text.
+		 *
+		 * @method _syncContentText
+		 * @param {EventFacade} event
+		 * @protected
+		 */
 		_syncContentText: function(event) {
 			var instance = this;
 
@@ -442,12 +724,29 @@ A.extend(
 			}
 		},
 
+		/**
+		 * Converts the new lines <code>\n</code> to <code><br/></code> (i.e.,
+         * nl2br).
+		 *
+		 * @method _toHTML
+		 * @param {String} text Input text.
+		 * @protected
+		 * @return {String}
+		 */
 		_toHTML: function(text) {
 			var instance = this;
 
 			return String(text).replace(/\n/gim, '<br/>');
 		},
 
+		/**
+		 * Converts HTML to text.
+		 *
+		 * @method _toText
+		 * @param {String} text HTML input.
+		 * @protected
+		 * @return {String}
+		 */
 		_toText: function(text) {
 			var instance = this;
 
