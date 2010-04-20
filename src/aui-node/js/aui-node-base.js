a1047b26956b2df6bc5e0bbb29f4e6e6b32bb0c9
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2010-04-20 17:17:45 +0000

    AUI-31 - placeBefore and placeAfter should handle HTML strings
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@50600 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-node/js/aui-node-base.js
+++ src/aui-node/js/aui-node-base.js
@@ -393,13 +393,7 @@ A.mix(A.Node.prototype, {
 	placeAfter: function(newNode) {
 		var instance = this;
 
-		var parent = instance.get(PARENT_NODE);
-
-		if (parent) {
-			parent.insertBefore(newNode, instance.get(NEXT_SIBLING));
-		}
-
-		return instance;
+		return instance._place(newNode, instance.get(NEXT_SIBLING));
 	},
 
 	/**
@@ -421,13 +415,7 @@ A.mix(A.Node.prototype, {
 	placeBefore: function(newNode) {
 		var instance = this;
 
-		var parent = instance.get(PARENT_NODE);
-
-		if (parent) {
-			parent.insertBefore(newNode, instance);
-		}
-
-		return instance;
+		return instance._place(newNode, instance);
 	},
 
 	/**
@@ -719,6 +707,30 @@ A.mix(A.Node.prototype, {
 		}
 
 		return str.join('');
+	},
+
+	/**
+     * Place a node or html string at a specific location
+	 *
+     * @method _place
+     * @private
+     * @param {Node|String} newNode
+     * @param {Node} refNode
+     */
+	_place: function(newNode, refNode) {
+		var instance = this;
+
+		var parent = instance.get(PARENT_NODE);
+
+		if (parent) {
+			if (Lang.isString(newNode)) {
+				newNode = A.Node.create(newNode);
+			}
+
+			parent.insertBefore(newNode, refNode);
+		}
+
+		return instance;
 	}
 }, true);
 
