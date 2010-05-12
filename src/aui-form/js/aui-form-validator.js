d26b5bbfc23096a64e441ccdf33ce3f465dc8158
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-05-12 17:05:08 +0000

    AUI-51 FormValidator IE bugfixes
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@52448 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-form/js/aui-form-validator.js
+++ src/aui-form/js/aui-form-validator.js
@@ -2,11 +2,12 @@
 
 var L = A.Lang,
 	O = A.Object,
+	isArray = L.isArray,
 	isBoolean = L.isBoolean,
 	isDate = L.isDate,
 	isEmpty = O.isEmpty,
-	isNumber = L.isNumber,
 	isFunction = L.isFunction,
+	isNumber = L.isNumber,
 	isObject = L.isObject,
 	isString = L.isString,
 	trim = L.trim,
@@ -343,9 +344,10 @@ A.extend(FormValidator, A.Base, {
 		var substituteRulesMap = {};
 
 		if (rule in fieldRules) {
-			var ruleValue = fieldRules[rule];
+			var ruleValue = A.Array(fieldRules[rule]);
 
-			A.Array(ruleValue).map(
+			A.each(
+				ruleValue,
 				function(value, index) {
 					substituteRulesMap[index] = [value].join(EMPTY_STRING);
 				}
