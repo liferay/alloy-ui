960308b482465301a68e2a193a2d9947fac82033
Julio Camarero <julio.camarero@liferay.com>
2010-05-21 11:19:01 +0000

    AUI-73 Using thicker close icon in dialogs
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@53435 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-dialog/js/aui-dialog.js
+++ src/aui-dialog/js/aui-dialog.js
@@ -17,6 +17,7 @@ var L = A.Lang,
 	BUTTON = 'button',
 	BUTTONS = 'buttons',
 	CLOSE = 'close',
+	CLOSETHICK = 'closethick',
 	CONSTRAIN_TO_VIEWPORT = 'constrain2view',
 	CONTAINER = 'container',
 	DD = 'dd',
@@ -307,8 +308,8 @@ Dialog.prototype = {
 
 		if (close) {
 			var closeConfig = {
-				icon: CLOSE,
-				id: CLOSE,
+				icon: CLOSETHICK,
+				id: CLOSETHICK,
 				handler: {
 					fn: instance.close,
 					context: instance
