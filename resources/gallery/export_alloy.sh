ca71305e9c7add5274502f170321a0423fdb3fd4
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2010-05-23 22:09:47 +0000

    AUI-74 - Improve the gallery exporter
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@53572 05bdf26c-840f-0410-9ced-eb539d925f36

--- resources/gallery/export_alloy.sh
+++ resources/gallery/export_alloy.sh
@@ -15,16 +15,20 @@ if [[ ! $fromDir ]]; then
 	fromDir=$projectDir
 fi
 
-rm -rf $toDir
-cp -R $fromDir $toDir
-
-find $toDir -type d -name ".svn" | xargs rm -rf
+parentDir=`dirname $toDir`
 
 srcDir=${toDir}/src
 demosDir=${toDir}/demos
 buildDir=${toDir}/build
 
+rm -rf $toDir
+cp -R $fromDir $toDir
+
+find $toDir -type d -name ".svn" | xargs rm -rf
+
 find ${toDir} -type f -name "build.xml" | xargs perl -pi -e 's/<property name="build\.aui\.prefix" value=""\/>/<property name="build.aui.prefix" value="gallery-"\/>/g;'
+
+find $demosDir/* -type f -name "*.html" | xargs perl -pi -e 's/<script src="\.\.\/\.\.\/build\/aui\/aui\.js" type="text\/javascript"><\/script>/<script src="..\/..\/build\/yui\/yui.js" type="text\/javascript"><\/script><script src="..\/..\/build\/aui-base\/aui-base.js" type="text\/javascript"><\/script>/g;'
 
 find $srcDir/aui-base -type f -name "build.properties" | xargs perl -pi -e 's/(,widget-css)/$1,aui-skin-base/g;'
 
@@ -42,9 +46,71 @@ for f in `find $srcDir -name 'aui-*' -or -name 'build.aui-*'` ;
 do
 	cp -R $f ${f//aui-/gallery-aui-}
 done
+
 rm -rf `find $srcDir -name 'aui-*' -or -name 'build.aui-*'`
 rm -rf `find $buildDir -type d -name 'aui-*'`
 
-cd $toDir
-ant build-gallery
+#flatten rollups
+for f in `find $srcDir -name 'build.gallery-aui-*.properties'` ;
+do
+	newName=${f}
+	baseName=`basename $newName`
+	oldDirName=`dirname $newName`
+	baseModuleName=`basename $oldDirName`
 
+	moduleName=${baseName#build.}
+	moduleName=${moduleName%.properties}
+	
+	oldModuleDir=$oldDirName
+	newModuleDir=$srcDir/$moduleName
+	mkdir -p $newModuleDir/js
+
+	mv $oldModuleDir/build.$moduleName.properties $newModuleDir/build.properties
+	mv $oldModuleDir/build.$moduleName.xml $newModuleDir/build.xml
+	mv $oldModuleDir/js/$moduleName.js $newModuleDir/js/$moduleName.js
+
+	coreCSSFile=assets/${moduleName}-core.css
+	skinCSSFile=assets/skins/sam/${moduleName}-skin.css
+
+	if [[ -f $oldModuleDir/$coreCSSFile ]]; then
+		mkdir -p $newModuleDir/assets
+		mv $oldModuleDir/$coreCSSFile $newModuleDir/$coreCSSFile
+	fi
+	
+	if [[ -f $oldModuleDir/$skinCSSFile ]]; then
+		mkdir -p $newModuleDir/assets/skins/sam
+		mv $oldModuleDir/$skinCSSFile $newModuleDir/$skinCSSFile
+	fi
+
+	perl -pi -e 's/^(srcdir|global\.)/#$1/g;' $newModuleDir/build.properties
+	perl -pi -e "s/build\.${moduleName}\.properties/build.properties/g;" $newModuleDir/build.xml
+	
+	touch $oldModuleDir/js/${baseModuleName}.js
+	perl -pi -e 's/^(srcdir|global\.|component\.rollup)/#$1/g;' $oldModuleDir/build.properties
+	perl -pi -e "s/^(component=)/component.jsfiles=${baseModuleName}.js\n\$1/g;" $oldModuleDir/build.properties
+	perl -pi -e "s/^component.use=/component.requires=/g;" $oldModuleDir/build.properties
+done
+
+find ${toDir} -type f -name "build.xml" | xargs perl -pi -e 's/<import file="\.\.\/\.\.\/resources\/builder\/bootstrap\.xml" \/>/<import file="\${builddir}\/3.x\/bootstrap.xml" \/>/g;'
+
+find ${toDir} -type f -name "build.properties" | xargs perl -pi -e "s/^(component=)/lint.skip=true\n\$1/"
+find ${toDir} -type f -name "build.properties" | xargs perl -pi -e "s/^(component=)/builddir=..\/..\/..\/builder\/componentbuild\n\$1/"
+find ${toDir} -type f -name "build.properties" | xargs perl -pi -e "s/^(component=)/yui.variable=A\n\$1/"
+
+#Uncomment the following lines to move the builder to it's default location
+#above the project directory
+# perl -pi -e 's/<import file="resources\/builder\/bootstrap\.xml"\/>//' ${toDir}/build.xml
+# 
+# if [[ ! -d ${parentDir}/builder ]]; then
+# 	cp -R ${toDir}/lib/builder ${parentDir}/builder
+# fi
+
+cd $toDir
+
+ant build-gallery
+ant generate-skin-src
+
+if [[ $? = 0 ]]; then
+	find ${toDir}/src -type d -name "_diffs" | xargs rm -rf
+	find ${toDir}/src -type f -name "build.all-css.*" | xargs rm
+fi
