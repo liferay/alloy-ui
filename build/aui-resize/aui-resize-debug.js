fbb27a5483306974e246b842c418fdd5a35c6da1
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-07-03 01:26:30 +0000

    Source formatting - Removing old comment
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@57420 05bdf26c-840f-0410-9ced-eb539d925f36

--- build/aui-resize/aui-resize-debug.js
+++ build/aui-resize/aui-resize-debug.js
@@ -5,8 +5,6 @@ AUI.add('aui-resize', function(A) {
  * @module aui-resize
  */
 
-// TODO: Containment with proxy enabled and fix containment handles locking.
-// To fix the Containment #2528540 need to be fixed.
 var Lang = A.Lang,
 	isArray = Lang.isArray,
 	isBoolean = Lang.isBoolean,
