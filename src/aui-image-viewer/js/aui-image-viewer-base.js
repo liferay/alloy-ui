1a91d68368fbf962334efe4455a38e54d0631054
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-07-13 18:02:23 +0000

    AUI-125 ImageViewerBase added maxHeight/maxWidth options
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@58142 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-image-viewer/js/aui-image-viewer-base.js
+++ src/aui-image-viewer/js/aui-image-viewer-base.js
@@ -49,6 +49,8 @@ var L = A.Lang,
 	LOADING = 'loading',
 	LOADING_EL = 'loadingEl',
 	LOCK = 'lock',
+	MAX_HEIGHT = 'maxHeight',
+	MAX_WIDTH = 'maxWidth',
 	MODAL = 'modal',
 	OFFSET_HEIGHT = 'offsetHeight',
 	OFFSET_WIDTH = 'offsetWidth',
@@ -57,6 +59,7 @@ var L = A.Lang,
 	PRELOAD_ALL_IMAGES = 'preloadAllImages',
 	PRELOAD_NEIGHBOR_IMAGES = 'preloadNeighborImages',
 	PX = 'px',
+	REGION = 'region',
 	RIGHT = 'right',
 	SCROLL = 'scroll',
 	SHOW = 'show',
@@ -508,6 +511,30 @@ var ImageViewer = A.Component.create(
 				valueFn: function() {
 					return A.Node.create(TPL_LOADING);
 				}
+			},
+
+	        /**
+	         * The maximum height of the element
+	         *
+	         * @attribute maxHeight
+	         * @default Infinity
+	         * @type Number
+	         */
+			maxHeight: {
+				value: Infinity,
+				validator: isNumber
+			},
+
+	        /**
+	         * The maximum width of the element
+	         *
+	         * @attribute maxWidth
+	         * @default Infinity
+	         * @type Number
+	         */
+			maxWidth: {
+				value: Infinity,
+				validator: isNumber
 			}
 		},
 
@@ -1034,6 +1061,32 @@ var ImageViewer = A.Component.create(
 			},
 
 			/**
+			 * Calculate the resize ratio for the loaded image.
+			 *
+			 * @method _getRatio
+			 * @param {Number} width Image width
+			 * @param {Number} height Image height
+			 * @protected
+			 * @return {Number}
+			 */
+			_getRatio: function(width, height) {
+				var instance = this;
+
+				var ratio = 1;
+				var maxHeight = instance.get(MAX_HEIGHT);
+				var maxWidth = instance.get(MAX_WIDTH);
+
+				if ((height > maxHeight) || (width > maxWidth)) {
+					var hRatio = (height / maxHeight);
+					var wRatio = (width / maxWidth);
+
+					ratio = Math.max(hRatio, wRatio);
+				}
+
+				return ratio;
+			},
+
+			/**
 			 * Get the <a href="ImageViewer.html#config_info">info</a> template.
 			 *
 			 * @method _getInfoTemplate
@@ -1208,11 +1261,8 @@ var ImageViewer = A.Component.create(
 			 */
 			_onLoadImage: function(event) {
 				var instance = this;
-				var bodyNode = instance.bodyNode;
 				var image = event.currentTarget;
 
-				var offsetHeight = image.get(OFFSET_HEIGHT) + PX;
-				var offsetWidth = image.get(OFFSET_WIDTH) + PX;
 				var imageAnim = instance.get(IMAGE_ANIM);
 
 				if (instance.get(ANIM)) {
@@ -1231,10 +1281,7 @@ var ImageViewer = A.Component.create(
 
 				instance.setStdModContent(BODY, image);
 
-				bodyNode.setStyles({
-					width: offsetWidth,
-					height: offsetHeight
-				});
+				instance._uiSetImageSize(image);
 
 				instance._syncImageViewerUI();
 
@@ -1252,6 +1299,36 @@ var ImageViewer = A.Component.create(
 					instance.preloadImage(currentIndex + 1);
 					instance.preloadImage(currentIndex - 1);
 				}
+			},
+
+			/**
+			 * Set the size of the image and the overlay respecting the
+             * maxHeight/maxWidth ratio.
+			 *
+			 * @method _uiSetImageSize
+			 * @param {HTMLImage} image Image
+			 * @protected
+			 */
+			_uiSetImageSize: function(image) {
+				var instance = this;
+				var bodyNode = instance.bodyNode;
+				var imageRegion = image.get(REGION);
+
+				var ratio = instance._getRatio(
+					imageRegion.width,
+					imageRegion.height
+				);
+
+				var height = (imageRegion.height / ratio);
+				var width = (imageRegion.width / ratio);
+
+				image.set(OFFSET_HEIGHT, height);
+				image.set(OFFSET_WIDTH, width);
+
+				bodyNode.setStyles({
+					height: height + PX,
+					width: width + PX
+				});
 			}
 		}
 	}
