757fa0ca3c6fa6c477e02a542ba87a616af01f39
Bruno Basto <bruno.basto@liferay.com>
2010-09-16 21:14:25 +0000

    AUI-207 Alloy Taglibs - Make scriptPosition a reserved attribute
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@62522 05bdf26c-840f-0410-9ced-eb539d925f36

--- sandbox/taglibs/alloy-taglib/src/com/liferay/alloy/util/ReservedAttributeUtil.java
+++ sandbox/taglibs/alloy-taglib/src/com/liferay/alloy/util/ReservedAttributeUtil.java
@@ -35,7 +35,7 @@ public class ReservedAttributeUtil {
 			"servletContext", "scopedAttribute", "scopedAttributes",
 			"previousOut", "parent", "namespacedAttribute",
 			"attributeNamespace", "bodyContent", "class", "dynamicAttribute",
-			"dynamicAttributes", "id"
+			"dynamicAttributes", "id", "scriptPosition"
 		}
 	);
 
