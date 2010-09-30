49a23bb466a42e58ebcf7c7dcaa56aa80527d11d
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2010-09-30 16:09:06 +0000

    AUI-230 - Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@63381 05bdf26c-840f-0410-9ced-eb539d925f36

--- build/aui-tabs/aui-tabs-menu-plugin.js
+++ build/aui-tabs/aui-tabs-menu-plugin.js
@@ -13,8 +13,6 @@ var Lang = A.Lang,
 	LIST_NODE = 'listNode',
 	RENDERED = 'rendered',
 
-	CSS_HIDDEN = getClassName('helper-hidden'),
-
 	CSS_TAB = getClassName(TAB),
 	CSS_TABVIEW_LIST = getClassName(TABVIEW, 'list'),
 	CSS_TABVIEW_LIST_CONTENT = getClassName(TABVIEW, 'list', 'content'),
@@ -24,274 +22,331 @@ var Lang = A.Lang,
 	CSS_TABVIEW_MENU_TRIGGER = getClassName(TABVIEWMENU, 'trigger'),
 	CSS_TABVIEW_WRAPPER = getClassName(TABVIEW, 'wrapper'),
 
+	STR_FIRST = 'first',
+	STR_LAST = 'last',
+
 	TPL_CONTENT_MENU = '<ul></ul>',
-	TPL_CONTENT_MENU_ITEM = '<li class="' + CSS_TABVIEW_MENU_ITEM + '"><a href="javascript:;" class="' + CSS_TABVIEW_MENU_ITEM_LABEL + '">{0}</a></li>',
+	TPL_CONTENT_MENU_ITEM = '<li class="' + CSS_TABVIEW_MENU_ITEM + '" data-index="{0}"><a href="javascript:;" class="' + CSS_TABVIEW_MENU_ITEM_LABEL + '">{1}</a></li>',
 	TPL_CONTENT_MENU_TRIGGER = '<div></div>';
 
 var TabViewMenu = A.Component.create(
-{
-	NAME: TABVIEWMENU_PLUGIN,
+	{
+		NAME: TABVIEWMENU_PLUGIN,
 
-	NS: TABVIEWMENU,
+		NS: TABVIEWMENU,
 
-	EXTENDS: A.Plugin.Base,
+		EXTENDS: A.Plugin.Base,
 
-	prototype: {
-		initializer: function() {
-			var instance = this;
+		prototype: {
+			initializer: function() {
+				var instance = this;
 
-			instance.afterHostMethod('bindUI', instance.bindUI);
-			instance.afterHostMethod('renderUI', instance.renderUI);
-			instance.afterHostMethod('addTab', instance.addTab);
-			instance.afterHostMethod('removeTab', instance.removeTab);
-			instance.afterHostMethod('selectTab', instance.selectTab);
-			instance.afterHostMethod('_onActiveTabChange', instance._onActiveTabChange);
-			instance.afterHostMethod('_renderTabs', instance._renderTabs);
-		},
+				instance.afterHostMethod('renderUI', instance.renderUI);
+				instance.afterHostMethod('bindUI', instance.bindUI);
 
-		bindUI: function () {
-			var instance = this;
-			var host = instance.get(HOST);
+				instance.afterHostMethod('addTab', instance.addTab);
+				instance.afterHostMethod('removeTab', instance.removeTab);
+				instance.afterHostMethod('selectTab', instance.selectTab);
 
-			A.on('windowresize',
-				function (event) {
-					if (instance._menuNode != null) {
-						var contentNode = host.get(CONTENT_NODE);
+				instance.afterHostMethod('_onActiveTabChange', instance._onActiveTabChange);
+				instance.afterHostMethod('_renderTabs', instance._renderTabs);
 
-						instance._contentWidth = contentNode.get('offsetWidth') - instance._listNodePadding;
+				instance._updateMenuTask = new A.DelayedTask(instance._updateMenu, instance);
+				instance._updateUITask = new A.DelayedTask(instance._updateUI, instance);
+			},
 
-						instance._updateMenu();
-					}
-					else {
-						instance._updateUI();
-					}
+			bindUI: function() {
+				var instance = this;
+
+				var host = instance.get(HOST);
+
+				A.on('windowresize', instance._onWindowResize, instance);
+			},
+
+			renderUI: function() {
+				var instance = this;
+
+				var host = instance.get(HOST);
+				var listNode = host.get(LIST_NODE);
+
+				var wrapper = instance._wrapper;
+
+				instance._listNodeOuterWidth = (
+					parseFloat(listNode.getComputedStyle('marginLeft')) +
+					parseFloat(wrapper.getComputedStyle('borderLeftWidth')) +
+					parseFloat(listNode.getComputedStyle('paddingLeft')) +
+					parseFloat(listNode.getComputedStyle('paddingRight')) +
+					parseFloat(wrapper.getComputedStyle('borderRightWidth')) +
+					parseFloat(listNode.getComputedStyle('marginRight'))
+				);
+
+				instance._updateUITask.delay(1);
+			},
+
+			addTab: function(tab, index) {
+				var instance = this;
+
+				var host = instance.get(HOST);
+
+				if (host.get(RENDERED)) {
+					instance._updateUITask.delay(1);
 				}
-			);
-		},
+			},
 
-		renderUI: function () {
-			var instance = this;
+			removeTab: function(index) {
+				var instance = this;
 
-			var host = instance.get(HOST);
-			var listNode = host.get(LIST_NODE);
+				var host = instance.get(HOST);
 
-			instance._listNodePadding = parseFloat(listNode.getComputedStyle('marginLeft')) +
-										parseFloat(instance._wrapper.getComputedStyle('borderLeftWidth')) +
-										parseFloat(listNode.getComputedStyle('paddingLeft')) +
-										parseFloat(listNode.getComputedStyle('paddingRight')) +
-										parseFloat(instance._wrapper.getComputedStyle('borderRightWidth')) +
-										parseFloat(listNode.getComputedStyle('marginRight'));
-
-			instance._updateUI();
-		},
-
-		addTab: function (tab, index) {
-			var instance = this;
-
-			var host = instance.get(HOST);
-
-			if (host.get(RENDERED)) {
-				instance._updateUI();
-			}
-		},
-
-		removeTab: function (index) {
-			var instance = this;
-
-			var host = instance.get(HOST);
-
-			if (host.get(RENDERED)) {
-				instance._updateUI();
-			}
-		},
-
-		selectTab: function (index) {
-			var instance = this;
-
-			instance._updateMenu();
-		},
-
-		_onActiveTabChange: function (event) {
-			var instance = this;
-
-			instance._updateMenu();
-		},
-
-		_removeMenu: function () {
-			var instance = this;
-			var host = instance.get(HOST);
-
-			var listNode = host.get(LIST_NODE);
-
-			listNode.all('.' + CSS_TAB).removeClass(CSS_HIDDEN);
-
-			if (instance._triggerNode != null) {
-				instance._triggerNode.remove();
-
-				instance._triggerNode = null;
-			}
-
-			if (instance._menuNode != null) {
-				instance._menuNode.remove();
-
-				instance._menuNode = null;
-			}
-
-			if (instance._menuOverlay != null) {
-				instance._menuOverlay.destroy();
-
-				instance._menuOverlay = null;
-			}
-		},
-
-		_renderTabs: function () {
-			var instance = this;
-			var host = instance.get(HOST);
-
-			var contentNode = host.get(CONTENT_NODE);
-			var listNode = host.get(LIST_NODE);
-
-			listNode.removeClass(CSS_TABVIEW_LIST);
-			listNode.addClass(CSS_TABVIEW_LIST_CONTENT);
-
-			var contentEl = host._createDefaultContentContainer();
-
-			contentEl.addClass(CSS_TABVIEW_LIST);
-
-			var wrapperEl = host._createDefaultContentContainer();
-
-			wrapperEl.addClass(CSS_TABVIEW_WRAPPER);
-			wrapperEl.append(contentEl);
-
-			contentNode.insert(wrapperEl, listNode);
-			contentEl.append(listNode);
-
-			instance._wrapper = wrapperEl;
-			instance._content = contentEl;
-		},
-
-		_updateMenu: function () {
-			var instance = this;
-			var host = instance.get(HOST);
-
-			if (instance._menuNode != null) {
-				var removeMenu = true;
-				var wrapperWidth = instance._wrapper.get('offsetWidth');
-
-				if (instance._itemsWidth[instance._itemsWidth.length - 1] > instance._contentWidth) {
-					var listNode = host.get(LIST_NODE);
-					var tabs = listNode.all('.' + CSS_TAB);
-
-					var selectedIndex = host.getTabIndex(host.get('activeTab'));
-					var selectedOffset = (selectedIndex != 0 ? instance._itemsWidth[selectedIndex] - instance._itemsWidth[selectedIndex - 1] : 0);
-
-					instance._menuNode.empty();
-
-					tabs.each(
-						function (node, i, nodeList) {
-							var offset = (i < selectedIndex ? selectedOffset : 0);
-
-							if (i != selectedIndex && instance._itemsWidth[i] + offset > instance._contentWidth) {
-								node.addClass(CSS_HIDDEN);
-
-								var tplObj = Lang.sub(TPL_CONTENT_MENU_ITEM, [node.get('text')]);
-
-								var menuItem = A.Node.create(tplObj);
-
-								instance._menuNode.insert(menuItem);
-
-								instance._menuNode.all('li a').last().on('click',
-									function (index) {
-										return function () {
-											host.selectTab(index);
-										}
-									} (i)
-								);
-
-								removeMenu = false;
-							}
-							else {
-								node.removeClass(CSS_HIDDEN);
-							}
-						}
-					);
+				if (host.get(RENDERED)) {
+					instance._updateUITask.delay(1);
 				}
+			},
 
-				if (removeMenu) {
-					instance._removeMenu();
+			selectTab: function(index) {
+				var instance = this;
+
+				instance._updateMenuTask.delay(1);
+
+				instance.fire(
+					'selectTab',
+					{
+						index: index
+					}
+				);
+			},
+
+			_hideMenu: function() {
+				var instance = this;
+
+				var host = instance.get(HOST);
+
+				var listNode = host.get(LIST_NODE);
+
+				listNode.all('.' + CSS_TAB).show();
+
+				if (instance._menuOverlay) {
+					instance._menuOverlay.hide();
+
+					instance._triggerNode.hide();
+				}
+			},
+
+			_onActiveTabChange: function(event) {
+				var instance = this;
+
+				instance._updateMenuTask.delay(1);
+			},
+
+			_onWindowResize: function(event) {
+				var instance = this;
+
+				if (instance._menuNode) {
+					var contentNode = instance.get(HOST).get(CONTENT_NODE);
+
+					instance._contentWidth = contentNode.get('offsetWidth') - instance._listNodeOuterWidth;
+
+					instance._updateMenuTask.delay(1);
 				}
 				else {
-					var borderWidth = parseFloat(instance._wrapper.getComputedStyle('borderLeftWidth')) + parseFloat(instance._wrapper.getComputedStyle('borderRightWidth'))
-
-					instance._triggerNode.setStyle('left', (wrapperWidth - instance._triggerNode.get('offsetWidth') - borderWidth) + 'px');
+					instance._updateUITask.delay(1);
 				}
-			}
-		},
+			},
 
-		_updateUI: function () {
-			var instance = this;
-			var host = instance.get(HOST);
+			_renderMenu: function() {
+				var instance = this;
 
-			instance._removeMenu();
-
-			var contentNode = host.get(CONTENT_NODE);
-			var listNode = host.get(LIST_NODE);
-			var tabs = listNode.all('.' + CSS_TAB);
-
-			instance._contentWidth = contentNode.get('offsetWidth') - instance._listNodePadding;
-			instance._itemsWidth = [];
-
-			var listNodePadding = parseFloat(listNode.getComputedStyle('paddingLeft')) + parseFloat(listNode.getComputedStyle('paddingRight'));
-
-			tabs.each(
-				function (node, i, nodeList) {
-					var listItemMargin = parseFloat(node.getComputedStyle('marginRight')) + parseFloat(node.getComputedStyle('marginLeft'));
-
-					if (i > 0) {
-						instance._itemsWidth[i - 1] = listNodePadding + listItemMargin + node.get('offsetLeft');
-					}
-
-					if (i == nodeList.size() - 1) {
-						instance._itemsWidth[i] = instance._itemsWidth[i - 1] + node.get('offsetWidth');
-					}
-				}
-			);
-
-			if (instance._itemsWidth[instance._itemsWidth.length - 1] > instance._contentWidth) {
 				var trigger = A.Node.create(TPL_CONTENT_MENU_TRIGGER);
-				trigger.addClass(CSS_TABVIEW_MENU_TRIGGER);
-
-				instance._content.append(trigger);
-
-				instance._triggerNode = trigger;
-
 				var menuNode = A.Node.create(TPL_CONTENT_MENU);
 
-				menuNode.addClass(CSS_TABVIEW_MENU_LIST);
-				menuNode.appendTo(A.one('body'));
+				trigger.addClass(CSS_TABVIEW_MENU_TRIGGER);
+
+				instance._wrapper.append(trigger);
+
+				var menuOverlay = new A.OverlayContext(
+					{
+						align: {
+							points: ['tr', 'br']
+						},
+						contentBox: menuNode,
+						cancellableHide: true,
+						cssClass: CSS_TABVIEW_MENU_LIST,
+						hideDelay: 1000,
+						hideOn: 'mouseout',
+						showDelay: 0,
+						showOn: 'click',
+						trigger: trigger
+					}
+				).render();
+
+				menuOverlay.refreshAlign();
 
 				instance._menuNode = menuNode;
+				instance._triggerNode = trigger;
+				instance._menuOverlay = menuOverlay;
 
-				instance._updateMenu();
+				instance.after('selectTab', menuOverlay.hide, menuOverlay);
 
-				instance._menuOverlay = new A.OverlayContext(
-					{
-						trigger: instance._triggerNode,
-						boundingBox: instance._menuNode,
-						showOn: 'click',
-						cancellableHide: false,
-						hideOn: 'mouseout',
-						hideDelay: 1000,
-						showDelay: 0,
-						align: { node: instance._triggerNode, points: ['tr', 'br'] }
+				var host = instance.get(HOST);
+
+				menuNode.delegate(
+					'click',
+					function(event) {
+						var index = event.currentTarget.get('parentNode').attr('data-index');
+
+						host.selectTab(index);
+					},
+					'li a'
+				);
+			},
+
+			_renderTabs: function() {
+				var instance = this;
+
+				var host = instance.get(HOST);
+
+				var contentNode = host.get(CONTENT_NODE);
+				var listNode = host.get(LIST_NODE);
+
+				listNode.removeClass(CSS_TABVIEW_LIST);
+				listNode.addClass(CSS_TABVIEW_LIST_CONTENT);
+
+				var contentEl = host._createDefaultContentContainer();
+
+				contentEl.addClass(CSS_TABVIEW_LIST);
+
+				var wrapperEl = host._createDefaultContentContainer();
+
+				wrapperEl.addClass(CSS_TABVIEW_WRAPPER);
+				wrapperEl.append(contentEl);
+
+				contentNode.insert(wrapperEl, listNode);
+				contentEl.append(listNode);
+
+				instance._wrapper = wrapperEl;
+				instance._content = contentEl;
+			},
+
+			_updateMenu: function() {
+				var instance = this;
+
+				var host = instance.get(HOST);
+
+				var menuNode = instance._menuNode;
+
+				var wrapper = instance._wrapper;
+
+				if (menuNode) {
+					var hideMenu = true;
+					var wrapperWidth = wrapper.get('offsetWidth');
+
+					var itemsWidth = instance._itemsWidth;
+
+					if (itemsWidth[itemsWidth.length - 1] > instance._contentWidth) {
+						var listNode = host.get(LIST_NODE);
+						var tabs = listNode.all('.' + CSS_TAB);
+
+						var selectedIndex = host.getTabIndex(host.get('activeTab'));
+						var selectedOffset = (selectedIndex != 0 ? itemsWidth[selectedIndex] - itemsWidth[selectedIndex - 1] : 0);
+
+						var contentWidth = instance._contentWidth;
+
+						var selectTab = host.selectTab;
+						var textBuffer = [];
+						var tplBuffer = [];
+
+						tabs.each(
+							function(item, index, collection) {
+								var offset = (index < selectedIndex ? selectedOffset : 0);
+
+								if (index != selectedIndex && itemsWidth[index] + offset > contentWidth) {
+									item.hide();
+
+									textBuffer[0] = index;
+									textBuffer[1] = item.get('text');
+
+									var tplObj = Lang.sub(TPL_CONTENT_MENU_ITEM, textBuffer);
+
+									tplBuffer.push(tplObj);
+
+									hideMenu = false;
+								}
+								else {
+									item.show();
+								}
+							}
+						);
+
+						menuNode.setContent(tplBuffer.join(''));
+
+						var menuItems = menuNode.all('li');
+
+						menuItems.first().addClass(STR_FIRST);
+						menuItems.last().addClass(STR_LAST);
+					}
+
+					if (hideMenu) {
+						instance._hideMenu();
+					}
+					else {
+						instance._triggerNode.show();
+					}
+				}
+			},
+
+			_updateUI: function() {
+				var instance = this;
+
+				var host = instance.get(HOST);
+
+				instance._hideMenu();
+
+				var contentNode = host.get(CONTENT_NODE);
+				var listNode = host.get(LIST_NODE);
+				var tabs = listNode.all('.' + CSS_TAB);
+
+				instance._contentWidth = contentNode.get('offsetWidth') - instance._listNodeOuterWidth;
+				instance._itemsWidth = [];
+
+				var itemsWidth = instance._itemsWidth;
+
+				var listNodePadding = (
+					parseFloat(listNode.getComputedStyle('paddingLeft')) +
+					parseFloat(listNode.getComputedStyle('paddingRight'))
+				);
+
+				var lastIndex = tabs.size() - 1;
+
+				tabs.each(
+					function(item, index, collection) {
+						var listItemMargin = (
+							parseFloat(item.getComputedStyle('marginRight')) +
+							parseFloat(item.getComputedStyle('marginLeft'))
+						);
+
+						var previousIndex = index - 1;
+
+						if (index > 0) {
+							itemsWidth[previousIndex] = listNodePadding + listItemMargin + item.get('offsetLeft');
+						}
+
+						if (index == lastIndex) {
+							itemsWidth[index] = itemsWidth[previousIndex] + item.get('offsetWidth');
+						}
 					}
 				);
 
-				instance._menuOverlay.render();
+				if (itemsWidth[itemsWidth.length - 1] > instance._contentWidth) {
+					if (!instance._menuOverlay) {
+						instance._renderMenu();
+					}
+
+					instance._updateMenuTask.delay(1);
+				}
 			}
 		}
 	}
-});
+);
 
 A.namespace('Plugin').TabViewMenu = TabViewMenu;
 
