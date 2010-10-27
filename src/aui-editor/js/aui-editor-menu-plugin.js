949e83d243207d4a1bdd3ec70c7b35d8923fe9c5
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2010-10-27 03:42:33 +0000

    AUI-254 - Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@64901 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-editor/js/aui-editor-menu-plugin.js
+++ src/aui-editor/js/aui-editor-menu-plugin.js
@@ -5,7 +5,8 @@ var Lang = A.Lang,
 
 	NAME = 'editormenu',
 	NAME_PLUGIN = 'editormenuplugin',
-	EDITORMENU_PLUGIN = 'menu',
+
+	NS_EDITORMENU = 'menu',
 
 	CONTENT = 'content',
 
@@ -14,199 +15,198 @@ var Lang = A.Lang,
 	CSS_MENU_CONTENT_ITEM = getClassName(NAME, CONTENT, 'item'),
 
 	TPL_TAG_EMPTY = '<{1}{2}>{0}</{1}>',
+
+	TPL_MENU_CONTENT_ITEM = '<li class="' + CSS_MENU_CONTENT_ITEM + '">' + TPL_TAG_EMPTY + '</li>',
 	TPL_MENU_CONTENT_LIST = '<ul class="' + CSS_MENU_CONTENT_LIST + '"></ul>',
-	TPL_MENU_CONTENT_TEXT = '<li class="' + CSS_MENU_CONTENT_TEXT + '"><span>{0}</span></li>',
-	TPL_MENU_CONTENT_ITEM = '<li class="' + CSS_MENU_CONTENT_ITEM + '">' + TPL_TAG_EMPTY + '</li>';
-
-	function generateTagAttr(config) {
-		var output = '';
-
-		if (config.attributes) {
-			for (var i in config.attributes) {
-				output += ' ' + i + '="' + config.attributes[i] + '"';
-			}
-		}
-
-		if (config.styles) {
-			output = ' style="';
-
-			for (var i in config.styles) {
-				output += i + ': ' + config.styles[i] + ';';
-			}
-
-			output += '"';
-		}
-
-		return output;
-	}
+	TPL_MENU_CONTENT_TEXT = '<li class="' + CSS_MENU_CONTENT_TEXT + '"><span>{0}</span></li>';
 
 var EditorMenu = A.Component.create(
-		{
-			NAME: NAME,
+	{
+		NAME: NAME,
 
-			EXTENDS: A.OverlayContext,
+		EXTENDS: A.OverlayContext,
 
-			ATTRS: {
-				headerContent: {
-					value: '',
-					setter: function(value) {
-						var instance = this;
+		ATTRS: {
+			headerContent: {
+				value: '',
+				setter: function(value) {
+					var instance = this;
 
-						instance._headerContent = value;
+					instance._headerContent = value;
 
-						return '';
-					}
-				},
-				host: {
-					value: false
-				},
-				items: {
-					value: null
+					return '';
 				}
 			},
 
-			prototype: {
-				renderUI: function() {
-					var instance = this;
+			host: {
+				value: false
+			},
 
-					EditorMenu.superclass.renderUI.apply(instance, arguments);
+			items: {
+				value: null
+			}
+		},
 
-					var host = instance.get('host');
+		prototype: {
+			renderUI: function() {
+				var instance = this;
 
-					var	contentBox = instance.get('contentBox');
-					var headerContent = instance._headerContent;
-					var items = instance.get('items');
+				EditorMenu.superclass.renderUI.apply(instance, arguments);
 
-					var menuList = A.Node.create(TPL_MENU_CONTENT_LIST);
+				var host = instance.get('host');
 
-					A.each(
-						items,
-						function(config) {
-							var output = '';
+				var	contentBox = instance.get('contentBox');
+				var headerContent = instance._headerContent;
+				var items = instance.get('items');
 
-							if (isString(config)) {
-								output += Lang.sub(TPL_MENU_CONTENT_TEXT, [config]);
-							}
-							else {
-								var attr = generateTagAttr(config);
+				var menuList = A.Node.create(TPL_MENU_CONTENT_LIST);
 
-								output += Lang.sub(TPL_MENU_CONTENT_ITEM, [config.label, config.tag, attr]);
-							}
+				A.each(
+					items,
+					function(item, index, collection) {
+						var output = '';
 
-							var item = A.Node.create(output);
-							item.setData(NAME, config);
-
-							menuList.append(item);
+						if (isString(item)) {
+							output += Lang.sub(TPL_MENU_CONTENT_TEXT, [item]);
 						}
-					);
+						else {
+							var attr = instance._generateTagAttr(item);
 
-					var panel = new A.Panel(
-						{
-							collapsible: false,
-							headerContent: headerContent,
-							icons: [
-								{
-									icon: 'close',
-									handler: {
-										fn: instance.hide,
-										context: instance
-									}
+							output += Lang.sub(TPL_MENU_CONTENT_ITEM, [item.label, item.tag, attr]);
+						}
+
+						var node = A.Node.create(output);
+
+						node.setData(NAME, item);
+
+						menuList.append(node);
+					}
+				);
+
+				var panel = new A.Panel(
+					{
+						collapsible: false,
+						headerContent: headerContent,
+						icons: [
+							{
+								icon: 'close',
+								handler: {
+									fn: instance.hide,
+									context: instance
 								}
-							]
-						}
-					).render(contentBox);
+							}
+						]
+					}
+				).render(contentBox);
 
-					panel.bodyNode.append(menuList);
+				panel.bodyNode.append(menuList);
 
-					instance._menuList = menuList;
-				},
+				instance._menuList = menuList;
+			},
 
-				bindUI: function() {
-					var instance = this;
+			bindUI: function() {
+				var instance = this;
 
-					EditorMenu.superclass.bindUI.apply(instance, arguments);
+				EditorMenu.superclass.bindUI.apply(instance, arguments);
 
-					var host = instance.get('host');
+				instance._menuList.delegate('click', instance._onMenuListItemClick, '.' + CSS_MENU_CONTENT_ITEM, instance);
+			},
 
-					var menuList = instance._menuList;
+			_generateTagAttr: function(obj) {
+				var instance = this;
 
-					menuList.delegate(
-						'click',
-						function(event) {
-							var instance = this;
+				var buffer = [];
 
-							var item = event.currentTarget,
-								config = item.getData(NAME);
+				var output = '';
+				var attributes = obj.attributes;
+				var styles = obj.styles;
 
-							var attr = generateTagAttr(config),
-								output = Lang.sub(TPL_TAG_EMPTY, ['{0}', config.tag, attr]);
-
-							host.execCommand('wraphtml', output);
-							host.focus();
-
-							instance.hide();
-						},
-						'.' + CSS_MENU_CONTENT_ITEM,
-						instance
-					);
-				},
-
-				_uiSetHeight: function(val) {
-					var instance = this;
-
-					var	boundingBox = instance.get('boundingBox');
-					var menuList = instance._menuList;
-
-					boundingBox.setStyle('height', 'auto');
-
-					menuList.setStyle('height', val);
-				},
-
-				_uiSetWidth: function(val) {
-					var instance = this;
-
-					var	boundingBox = instance.get('boundingBox');
-					var menuList = instance._menuList;
-
-					boundingBox.setStyle('width', 'auto');
-
-					menuList.setStyle('width', val);
+				if (attributes) {
+					for (var i in attributes) {
+						buffer.push(' ' + i + '="' + attributes[i] + '"');
+					}
 				}
+
+				if (styles) {
+					buffer = [' style="'];
+
+					for (var i in styles) {
+						buffer.push(i + ': ' + styles[i] + ';');
+					}
+
+					buffer.push('"');
+				}
+
+				return buffer.join('');
+			},
+
+			_onMenuListItemClick: function(event) {
+				var instance = this;
+
+				var listItem = event.currentTarget;
+				var	config = listItem.getData(NAME);
+
+				var attr = instance._generateTagAttr(config);
+				var output = Lang.sub(TPL_TAG_EMPTY, ['{0}', config.tag, attr]);
+
+				var host = instance.get('host');
+
+				host.execCommand('wraphtml', output);
+				host.focus();
+
+				instance.hide();
+			},
+
+			_uiSetHeight: function(val) {
+				var instance = this;
+
+				var	boundingBox = instance.get('boundingBox');
+				var menuList = instance._menuList;
+
+				boundingBox.setStyle('height', 'auto');
+
+				menuList.setStyle('height', val);
+			},
+
+			_uiSetWidth: function(val) {
+				var instance = this;
+
+				var	boundingBox = instance.get('boundingBox');
+				var menuList = instance._menuList;
+
+				boundingBox.setStyle('width', 'auto');
+
+				menuList.setStyle('width', val);
 			}
 		}
-	);
+	}
+);
 
 var EditorMenuPlugin = A.Component.create(
-		{
-			NAME: NAME_PLUGIN,
+	{
+		EXTENDS: A.Plugin.Base,
 
-			NS: EDITORMENU_PLUGIN,
+		NAME: NAME_PLUGIN,
 
-			EXTENDS: A.Plugin.Base,
+		NS: NS_EDITORMENU,
 
-			ATTRS: {
-				host: {
-					value: false
-				}
-			},
+		prototype: {
+			add: function(config) {
+				var instance = this;
 
-			prototype: {
-				add: function(config) {
-					var instance = this;
+				var host = instance.get('host');
 
-					var host = instance.get('host');
-
-					return new EditorMenu(
-						A.mix(
-							{
-								host: host
-							},
-							config
-						)
-					).render();
-				}
+				return new EditorMenu(
+					A.mix(
+						{
+							host: host
+						},
+						config
+					)
+				).render();
 			}
 		}
-	);
+	}
+);
 
-A.namespace('Plugin').EditorMenuPlugin = EditorMenuPlugin;
+A.namespace('Plugin').EditorMenu = EditorMenuPlugin;
