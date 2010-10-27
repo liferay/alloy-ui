f4be07b6463804812d44762c055cf8c2de866cd4
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2010-10-27 03:32:11 +0000

    AUI-262 - Adding Select form field type
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@64900 05bdf26c-840f-0410-9ced-eb539d925f36

--- build/aui-form/aui-form-debug.js
+++ build/aui-form/aui-form-debug.js
@@ -613,7 +613,7 @@ var Combobox = A.Component.create(
 
 A.Combobox = Combobox;
 
-}, '@VERSION@' ,{skinnable:true, requires:['aui-form-textarea','aui-toolbar']});
+}, '@VERSION@' ,{requires:['aui-form-textarea','aui-toolbar'], skinnable:true});
 AUI.add('aui-form-field', function(A) {
 var Lang = A.Lang,
 
@@ -1138,6 +1138,111 @@ var Field = A.Component.create(
 A.Field = Field;
 
 }, '@VERSION@' ,{requires:['aui-base','aui-component','substitute']});
+AUI.add('aui-form-select', function(A) {
+var Lang = A.Lang,
+	isArray = Lang.isArray,
+	isObject = Lang.isObject,
+
+	getClassName = A.ClassNameManager.getClassName,
+
+	NAME = 'select',
+
+	CSS_SELECT = getClassName(NAME),
+	TPL_SELECT = '<select {multiple} class="{cssClass}" id="{id}" name="{name}"></select>';
+
+var Select = A.Component.create(
+	{
+		NAME: NAME,
+
+		ATTRS: {
+
+			multiple: {
+				value: false
+			},
+
+			options: {
+				value: [],
+				setter: '_setOptions'
+			},
+
+			selectedIndex: {
+				value: -1
+			}
+		},
+
+		UI_ATTRS: ['multiple', 'options', 'selectedIndex'],
+
+		EXTENDS: A.Field,
+
+		HTML_PARSER: {
+			node: 'select'
+		},
+
+		prototype: {
+			FIELD_TEMPLATE: TPL_SELECT,
+			FIELD_TYPE: NAME,
+
+			_setOptions: function(value) {
+				var instance = this;
+
+				if (!isArray(value)) {
+					value = [value];
+				}
+
+				return value;
+			},
+
+			_uiSetMultiple: function(value) {
+				var instance = this;
+
+				instance.get('node').attr('multiple', value);
+			},
+
+			_uiSetOptions: function(value) {
+				var instance = this;
+
+				var buffer = [];
+
+				var option;
+				var optionLabel;
+				var optionValue;
+				var selectedIndex = 0;
+
+				for (var i = 0; i < value.length; i++) {
+					option = value[i];
+					optionLabel = option.labelText || option;
+					optionValue = option.value || option;
+
+					if (option.selected) {
+						selectedIndex = i;
+					}
+
+					buffer.push('<option value="' + optionValue + '">' + optionLabel + '</option>');
+				}
+
+				var node = instance.get('node');
+
+				node.all('option').remove(true);
+
+				node.append(buffer.join(''));
+
+				instance.set('selectedIndex', selectedIndex);
+			},
+
+			_uiSetSelectedIndex: function(value) {
+				var instance = this;
+
+				if (value > -1) {
+					instance.get('node').attr('selectedIndex', value);
+				}
+			}
+		}
+	}
+);
+
+A.Select = Select;
+
+}, '@VERSION@' ,{requires:['aui-form-field']});
 AUI.add('aui-form-textarea', function(A) {
 var Lang = A.Lang,
 
@@ -1366,7 +1471,7 @@ var Textarea = A.Component.create(
 
 A.Textarea = Textarea;
 
-}, '@VERSION@' ,{skinnable:true, requires:['aui-form-textfield']});
+}, '@VERSION@' ,{requires:['aui-form-textfield'], skinnable:true});
 AUI.add('aui-form-textfield', function(A) {
 var Lang = A.Lang,
 
@@ -2298,5 +2403,5 @@ A.FormValidator = FormValidator;
 }, '@VERSION@' ,{requires:['aui-base','aui-event-input','selector-css3','substitute']});
 
 
-AUI.add('aui-form', function(A){}, '@VERSION@' ,{use:['aui-form-base','aui-form-combobox','aui-form-field','aui-form-textarea','aui-form-textfield','aui-form-validator'], skinnable:false});
+AUI.add('aui-form', function(A){}, '@VERSION@' ,{skinnable:false, use:['aui-form-base','aui-form-combobox','aui-form-field','aui-form-select','aui-form-textarea','aui-form-textfield','aui-form-validator']});
 
