a1e8f00251062b48b64bcb3f0a36a457a82ec614
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-12-28 20:23:38 +0000

    AUI-301 Add hability to show/hide events on SchedulerEvent and SchedulerCalendar
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@69106 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-scheduler/js/aui-scheduler-event.js
+++ src/aui-scheduler/js/aui-scheduler-event.js
@@ -22,16 +22,18 @@ var Lang = A.Lang,
 	_PROPAGATE_SET = '_propagateSet',
 
 	ACTIVE_VIEW = 'activeView',
+	BORDER_STYLE = 'borderStyle',
 	BORDER_WIDTH = 'borderWidth',
-	COLOR_BRIGHTNESS_FACTOR = 'colorBrightnessFactor',
-	COLOR_SATURATION_FACTOR = 'colorSaturationFactor',
 	CHANGE = 'Change',
 	COLOR = 'color',
+	COLOR_BRIGHTNESS_FACTOR = 'colorBrightnessFactor',
+	COLOR_SATURATION_FACTOR = 'colorSaturationFactor',
 	CONTENT = 'content',
 	CONTENT_NODE = 'contentNode',
 	DURATION = 'duration',
 	END_DATE = 'endDate',
 	EVENTS = 'events',
+	HIDDEN = 'hidden',
 	ID = 'id',
 	ISO_TIME = 'isoTime',
 	LOCALE = 'locale',
@@ -50,7 +52,6 @@ var Lang = A.Lang,
 	TITLE = 'title',
 	TITLE_DATE_FORMAT = 'titleDateFormat',
 	TITLE_NODE = 'titleNode',
-	BORDER_STYLE = 'borderStyle',
 
 	TITLE_DT_FORMAT_ISO = '%H:%M',
 	TITLE_DT_FORMAT_US = '%I:%M',
@@ -58,8 +59,9 @@ var Lang = A.Lang,
 	getCN = A.ClassNameManager.getClassName,
 
 	CSS_SCHEDULER_EVENT = getCN(SCHEDULER_EVENT),
-	CSS_SCHEDULER_EVENT_RECORDER = getCN(SCHEDULER_EVENT, RECORDER),
 	CSS_SCHEDULER_EVENT_CONTENT = getCN(SCHEDULER_EVENT, CONTENT),
+	CSS_SCHEDULER_EVENT_HIDDEN = getCN(SCHEDULER_EVENT, HIDDEN),
+	CSS_SCHEDULER_EVENT_RECORDER = getCN(SCHEDULER_EVENT, RECORDER),
 	CSS_SCHEDULER_EVENT_REPEATED = getCN(SCHEDULER_EVENT, REPEATED),
 	CSS_SCHEDULER_EVENT_TITLE = getCN(SCHEDULER_EVENT, TITLE),
 
@@ -241,6 +243,12 @@ var SchedulerEvent = A.Component.create({
 			return DateMath.getSecondsOffset(instance.get(END_DATE), instance.get(START_DATE));
 		},
 
+		hide: function() {
+			var instance = this;
+
+			instance.get(NODE).addClass(CSS_SCHEDULER_EVENT_HIDDEN);
+		},
+
 		sameEndDate: function(evt) {
 			var instance = this;
 
@@ -251,6 +259,12 @@ var SchedulerEvent = A.Component.create({
 			var instance = this;
 
 			return DateMath.compare(instance.get(START_DATE), evt.get(START_DATE));
+		},
+
+		show: function() {
+			var instance = this;
+
+			instance.get(NODE).removeClass(CSS_SCHEDULER_EVENT_HIDDEN);
 		},
 
 		isAfter: function(evt) {
