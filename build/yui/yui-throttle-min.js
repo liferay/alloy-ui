21b9fc1efff066f0e603960b13a8089fbb9e22e5
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-01-11 22:43:31 +0000

    Built files after run init-yui task
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@70125 05bdf26c-840f-0410-9ced-eb539d925f36

--- build/yui/yui-throttle-min.js
+++ build/yui/yui-throttle-min.js
@@ -5,6 +5,6 @@ http://developer.yahoo.com/yui/license.html
 version: 3.2.0
 build: nightly
 */
-YUI.add("yui-throttle",function(Y){
-/* Based on work by Simon Willison: http://gist.github.com/292562 */
-Y.throttle=function(fn,ms){ms=(ms)?ms:(Y.config.throttleTime||150);if(ms===-1){return(function(){fn.apply(null,arguments);});}var last=Y.Lang.now();return(function(){var now=Y.Lang.now();if(now-last>ms){last=now;fn.apply(null,arguments);}});};},"3.2.0",{requires:["yui-base"]});
+YUI.add("yui-throttle",function(a){
+/*! Based on work by Simon Willison: http://gist.github.com/292562 */
+a.throttle=function(c,b){b=(b)?b:(a.config.throttleTime||150);if(b===-1){return(function(){c.apply(null,arguments);});}var d=a.Lang.now();return(function(){var e=a.Lang.now();if(e-d>b){d=e;c.apply(null,arguments);}});};},"3.2.0",{requires:["yui-base"]});
