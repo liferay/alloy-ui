410c61fe3e914d4139d53008ed7eb2aff6d5182e
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2011-03-07 01:04:13 +0000

    AUI-347 Rollback
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@74641 05bdf26c-840f-0410-9ced-eb539d925f36

--- build/aui-dialog-iframe/aui-dialog-iframe.js
+++ build/aui-dialog-iframe/aui-dialog-iframe.js
@@ -34,10 +34,6 @@ var DialogIframePlugin = A.Component.create(
 				}
 			},
 
-			originalParent: {
-				setter: '_setOriginalParent'
-			},
-
 			uri: {
 			}
 		},
@@ -119,10 +115,6 @@ var DialogIframePlugin = A.Component.create(
 				var instance = this;
 
 				instance._bodyNode.plug(A.LoadingMask).loadingmask.show();
-
-				instance._setOriginalParent(
-					instance.get('originalParent')
-				);
 			},
 
 			_afterUriChange: function(event) {
@@ -170,17 +162,6 @@ var DialogIframePlugin = A.Component.create(
 				BUFFER_CSS_CLASS[1] = value;
 
 				return BUFFER_CSS_CLASS.join(' ');
-			},
-
-			_setOriginalParent: function(val) {
-				var instance = this;
-				var node = instance.node;
-
-				if (node) {
-					node.setData('originalParent', val);
-				}
-
-				return val;
 			},
 
 			_uiSetUri: function(value) {
