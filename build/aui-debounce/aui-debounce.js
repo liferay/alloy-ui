46388d0a3101ba764a5e2ba4113dc8a8a30bf7a1
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2011-03-15 15:43:43 +0000

    AUI-362 - Adding modules so that it's available, but not changing dependencies
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/ee/1.0.x@75214 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ build/aui-debounce/aui-debounce.js
@@ -0,0 +1,76 @@
+AUI.add('aui-debounce', function(A) {
+var Lang = A.Lang;
+
+var AArray = A.Array;
+var isArray = Lang.isArray;
+var isUndefined = Lang.isUndefined;
+
+var DEFAULT_ARGS = [];
+
+var toArray = function(arr, fallback, index, arrayLike) {
+	return !isUndefined(arr) ? AArray(arr, index || 0, (arrayLike !== false)) : fallback;
+};
+
+A.debounce = function(fn, delay, context, args) {
+	var id;
+	var tempArgs;
+
+	delay = delay || 0;
+
+	args = toArray(arguments, DEFAULT_ARGS, 3);
+
+	var clearFn = function() {
+		clearInterval(id);
+
+		id = null;
+	};
+
+	var base = function() {
+		clearFn();
+
+		var result = fn.apply(context, tempArgs || args || DEFAULT_ARGS);
+
+		tempArgs = null;
+
+		return result;
+	};
+
+	var delayFn = function(delayTime, newArgs, newContext, newFn) {
+		wrapped.cancel();
+
+		delayTime = !isUndefined(delayTime) ? delayTime : delay;
+
+		fn = newFn || fn;
+		context = newContext || context;
+
+		if (newArgs != args) {
+			tempArgs = toArray(newArgs, DEFAULT_ARGS, 0, false).concat(args);
+		}
+
+		if (delayTime > 0) {
+			id = setInterval(base, delayTime);
+		}
+		else {
+			return base();
+		}
+	};
+
+	var cancelFn = function() {
+		if (id) {
+			clearFn();
+		}
+	};
+
+	var wrapped = function() {
+		var currentArgs = arguments.length ? arguments : args;
+
+		return wrapped.delay(delay, currentArgs, context || this);
+	};
+
+	wrapped.cancel = cancelFn;
+	wrapped.delay = delayFn;
+
+	return wrapped;
+};
+
+}, '@VERSION@' ,{skinnable:false});
