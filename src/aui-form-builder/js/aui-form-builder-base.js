1be1f674daea68b01893ee08f4a0e47d76ef9aab
Bruno Basto <bruno.basto@liferay.com>
2011-03-21 18:13:36 +0000

    AUI-369 FormBuilder - Dropping a field outside the Drop Container breaks the DD
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@75751 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-form-builder/js/aui-form-builder-base.js
+++ src/aui-form-builder/js/aui-form-builder-base.js
@@ -232,6 +232,13 @@ A.mix(FormBuilderFieldSupport.prototype, {
 		return (instance.indexOf(field) > -1);
 	},
 
+	getField: function(index) {
+		var instance = this;
+		var fields = instance.get(FIELDS);
+
+		return fields[index];
+	},
+
 	indexOf: function(field) {
 		var instance = this;
 		var fields = instance.get(FIELDS);
@@ -757,19 +764,15 @@ var FormBuilder = A.Component.create({
 			}
 
 			if (!field) {
-				var config = instance._getFieldDefaultConfig(type)
-
-				field = instance._renderField(config);
+				field = instance._getFieldDefaultConfig(type);
 			}
 
 			var parent = instance._getFieldParentByNode(node);
+			var index = nodes.indexOf(node);
 
-			parent.insertField(
-				nodes.indexOf(node),
-				field
-			);
+			parent.insertField(index, field);
 
-			return field;
+			return parent.getField(index);
 		},
 
 		/**
@@ -863,9 +866,9 @@ var FormBuilder = A.Component.create({
 					instance._tabs.selectTab(TAB_INDEX_DRAG);
 				}
 
-				field.get(PARENT).removeField(field);
-
 				field.set(SELECTED, false);
+
+				field.get(PARENT).removeField(field);
 			}
 		},
 
@@ -968,9 +971,7 @@ var FormBuilder = A.Component.create({
 				instance.selectField(field);
 			}
 			else {
-				if (dragNode) {
-					dragNode.remove();
-				}
+				drag.set(NODE, instance.originalNode);
 			}
 
 			if (dragContainerNode.contains(dragNode) &&
