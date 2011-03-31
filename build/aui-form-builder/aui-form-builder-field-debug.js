0c9a9ac7d17a13928665397b216aadc092125b15
Bruno Basto <bruno.basto@liferay.com>
2011-03-31 21:31:45 +0000

    AUI-381 Build files
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@76823 05bdf26c-840f-0410-9ced-eb539d925f36

--- build/aui-form-builder/aui-form-builder-field-debug.js
+++ build/aui-form-builder/aui-form-builder-field-debug.js
@@ -636,7 +636,6 @@ var L = A.Lang,
 	CSS_FIELD_INPUT_TEXT = getCN(FIELD, INPUT, TEXT),
 	CSS_FORM_BUILDER_FIELD = getCN(FORM_BUILDER_FIELD),
 	CSS_FORM_BUILDER_FIELD_NODE = getCN(FORM_BUILDER_FIELD, NODE),
-	CSS_FORM_BUILDER_INPUT_FIELD_NODE = getCN(FORM_BUILDER_INPUT_FIELD, NODE),
 	CSS_STATE_DEFAULT = getCN(STATE, DEFAULT),
 
 	TPL_INPUT = '<input id="{id}" class="' + [CSS_FORM_BUILDER_FIELD_NODE, CSS_FIELD_INPUT].join(SPACE) + '" name="{name}" type="{type}" value="{value}" />',
@@ -1543,207 +1542,6 @@ var L = A.Lang,
 		return (v instanceof A.NodeList);
 	},
 
-	BOUNDING_BOX = 'boundingBox',
-	CONTENT_BOX = 'contentBox',
-	CONTAINER = 'container',
-	DOT = '.',
-	DRAG = 'drag',
-	DRAG_CONTAINER = 'dragContainer',
-	DRAG_CONTAINER_NODE = 'dragContainerNode',
-	DRAG_NODES_LIST = 'dragNodesList',
-	DROP = 'drop',
-	DROP_CONTAINER = 'dropContainer',
-	DROP_CONTAINER_NODE = 'dropContainerNode',
-	EMPTY_STR = '',
-	FIELD = 'field',
-	FIELDS = 'fields',
-	FORM_BUILDER_FIELD = 'form-builder-field',
-	FORM_BUILDER_INPUT_FIELD = 'form-builder-input-field',
-	ID = 'id',
-	ICON = 'icon',
-	INPUT = 'input',
-	LABEL = 'label',
-	NAME = 'name',
-	NODE = 'node',
-	PORTAL_LAYOUT = 'portalLayout',
-	PREDEFINED_VALUE = 'predefinedValue',
-	PROXY = 'proxy',
-	SPACE = ' ',
-	TEMPLATE = 'template',
-	TEMPLATE_NODE = 'templateNode',
-	TEXT = 'text',
-	VALUE = 'value',
-
-	getCN = A.ClassNameManager.getClassName,
-
-	CSS_FIELD_INPUT = getCN(FIELD, INPUT),
-	CSS_FIELD_INPUT_TEXT = getCN(FIELD, INPUT, TEXT),
-	CSS_FORM_BUILDER_FIELD = getCN(FORM_BUILDER_FIELD),
-	CSS_FORM_BUILDER_FIELD_NODE = getCN(FORM_BUILDER_FIELD, NODE),
-	CSS_FORM_BUILDER_INPUT_FIELD_NODE = getCN(FORM_BUILDER_INPUT_FIELD, NODE),
-	CSS_STATE_DEFAULT = getCN(STATE, DEFAULT),
-
-	TPL_INPUT = '<input id="{id}" class="' + [CSS_FORM_BUILDER_FIELD_NODE, CSS_FIELD_INPUT, CSS_FIELD_INPUT_TEXT].join(SPACE) + '" name="{name}" type="text" value="{value}" />'
-
-var FormBuilderInputField = A.Component.create({
-
-	NAME: FORM_BUILDER_INPUT_FIELD,
-
-	ATTRS: {
-
-		/**
-		 * The HTML template of the field
-		 *
-		 * @attribute template
-		 */
-		template: {
-			valueFn: function() {
-				return TPL_INPUT;
-			}
-		},
-
-		/*
-		* HTML_PARSER attributes
-		*/
-		templateNode: {
-			valueFn: 'getNode'
-		}
-
-	},
-
-	UI_ATTRS: [ACCEPT_CHILDREN, PREDEFINED_VALUE, LABEL, NAME, SHOW_LABEL],
-
-	CSS_PREFIX: CSS_FORM_BUILDER_FIELD,
-
-	HTML_PARSER: {
-		templateNode: DOT + CSS_FORM_BUILDER_FIELD_NODE
-	},
-
-	EXTENDS: A.FormBuilderField,
-
-	prototype: {
-
-		/**
-		 * Bind phase
-		 *
-		 * @method bindUI
-		 */
-		bindUI: function() {
-			var instance = this;
-
-			A.FormBuilderInputField.superclass.bindUI.apply(instance, arguments);
-
-			var templateNode = instance.get(TEMPLATE_NODE);
-
-			templateNode.on(
-				{
-					'keyup': A.bind(instance._onValueKeyUp, instance)
-				}
-			);
-		},
-
-		/**
-		 * Returns the HTML content of the field
-		 *
-		 * @method getHTML
-		 */
-		getHTML: function() {
-			var instance = this;
-			var template = instance.get(TEMPLATE);
-			var id = instance.get(ID);
-			var label = instance.get(LABEL);
-			var name = instance.get(NAME);
-			var size = instance.get(SIZE);
-			var value = instance.get(PREDEFINED_VALUE);
-
-			return A.substitute(
-				template,
-				{
-					id: id,
-					label: label,
-					name: name,
-					value: value
-				}
-			)
-		},
-
-		/**
-		 * Returns the A.Node of the field's HTML content
-		 *
-		 * @method getFieldNode
-		 */
-		getNode: function() {
-			var instance = this;
-
-			return A.Node.create(instance.getHTML());
-		},
-
-		renderSettings: function() {
-			var instance = this;
-			var formBuilder = instance.get(FORM_BUILDER);
-			var formNode = formBuilder.get(SETTINGS_FORM_NODE);
-
-			A.FormBuilderCheckBoxField.superclass.renderSettings.apply(instance, arguments);
-
-			if (!instance._renderedInputSettings) {
-				instance._renderedInputSettings = true;
-
-				var predefinedValueNode = formNode.one('input[name=predefinedValue]');
-
-				predefinedValueNode.on(
-					{
-						'keyup': A.bind(instance._onValueKeyUp, instance)
-					}
-				);
-			}
-		},
-
-		/**
-		 * Handles the onKeyUp event for the value nodes
-		 *
-		 * @method _onValueKeyUp
-		 */
-		_onValueKeyUp: function(event) {
-			var instance = this;
-			var target = event.target;
-
-			instance.set(PREDEFINED_VALUE, target.val());
-		},
-
-		_uiSetPredefinedValue: function(val) {
-			var instance = this;
-			var formBuilder = instance.get(FORM_BUILDER);
-			var formNode = formBuilder.get(SETTINGS_FORM_NODE);
-			var predefinedValueNode = formNode.one('input[name=predefinedValue]');
-			var templateNode = instance.get(TEMPLATE_NODE);
-
-			templateNode.val(val);
-
-			if (predefinedValueNode && instance.get(SELECTED)) {
-				predefinedValueNode.val(val);
-			}
-		}
-
-	}
-
-});
-
-A.FormBuilderInputField = FormBuilderInputField;
-
-A.FormBuilder.types['text'] = A.FormBuilderInputField;
-var L = A.Lang,
-	isArray = L.isArray,
-	isNumber = L.isNumber,
-	isString = L.isString,
-
-	isNode = function(v) {
-		return (v instanceof A.Node);
-	},
-
-	isNodeList = function(v) {
-		return (v instanceof A.NodeList);
-	},
-
 	toInitialCap = A.cached(
 		function(str) {
 			return str.substring(0, 1).toUpperCase() + str.substring(1);
@@ -1810,7 +1608,6 @@ var L = A.Lang,
 
 	CSS_FORM_BUILDER_FIELD = getCN(FORM_BUILDER_FIELD),
 	CSS_FORM_BUILDER_FIELD_NODE = getCN(FORM_BUILDER_FIELD, NODE),
-	CSS_FORM_BUILDER_INPUT_FIELD_NODE = getCN(FORM_BUILDER_INPUT_FIELD, NODE),
 
 	CSS_STATE_DEFAULT = getCN(STATE, DEFAULT),
 
@@ -2484,7 +2281,6 @@ var L = A.Lang,
 	CSS_FIELD_INPUT_TEXT = getCN(FIELD, INPUT, TEXT),
 	CSS_FORM_BUILDER_FIELD = getCN(FORM_BUILDER_FIELD),
 	CSS_FORM_BUILDER_FIELD_NODE = getCN(FORM_BUILDER_FIELD, NODE),
-	CSS_FORM_BUILDER_INPUT_FIELD_NODE = getCN(FORM_BUILDER_INPUT_FIELD, NODE),
 	CSS_STATE_DEFAULT = getCN(STATE, DEFAULT),
 
 	TPL_SELECT = '<select id="{id}" class="' + [CSS_FORM_BUILDER_FIELD_NODE].join(SPACE) + '" name="{name}" value="{value}"></select>'
@@ -2629,6 +2425,14 @@ var L = A.Lang,
 	isNumber = L.isNumber,
 	isString = L.isString,
 
+	isNode = function(v) {
+		return (v instanceof A.Node);
+	},
+
+	isNodeList = function(v) {
+		return (v instanceof A.NodeList);
+	},
+
 	BOUNDING_BOX = 'boundingBox',
 	CONTENT_BOX = 'contentBox',
 	CONTAINER = 'container',
@@ -2644,38 +2448,35 @@ var L = A.Lang,
 	FIELD = 'field',
 	FIELDS = 'fields',
 	FORM_BUILDER_FIELD = 'form-builder-field',
-	FORM_BUILDER_TEXTAREA_FIELD = 'form-builder-textarea-field',
+	FORM_BUILDER_TEXT_FIELD = 'form-builder-input-field',
 	ID = 'id',
 	ICON = 'icon',
+	INPUT = 'input',
 	LABEL = 'label',
 	NAME = 'name',
 	NODE = 'node',
 	PORTAL_LAYOUT = 'portalLayout',
 	PREDEFINED_VALUE = 'predefinedValue',
 	PROXY = 'proxy',
-	SIZE = 'size',
 	SPACE = ' ',
 	TEMPLATE = 'template',
 	TEMPLATE_NODE = 'templateNode',
 	TEXT = 'text',
-	TEXTAREA = 'textarea',
 	VALUE = 'value',
 
 	getCN = A.ClassNameManager.getClassName,
 
-	CSS_FIELD = getCN(FIELD),
-	CSS_FIELD_TEXT = getCN(FIELD, TEXT),
-	CSS_FIELD_TEXTAREA = getCN(FIELD, TEXTAREA),
+	CSS_FIELD_INPUT = getCN(FIELD, INPUT),
+	CSS_FIELD_INPUT_TEXT = getCN(FIELD, INPUT, TEXT),
 	CSS_FORM_BUILDER_FIELD = getCN(FORM_BUILDER_FIELD),
 	CSS_FORM_BUILDER_FIELD_NODE = getCN(FORM_BUILDER_FIELD, NODE),
-	CSS_FORM_BUILDER_INPUT_FIELD_NODE = getCN(FORM_BUILDER_INPUT_FIELD, NODE),
 	CSS_STATE_DEFAULT = getCN(STATE, DEFAULT),
 
-	TPL_TEXTAREA = '<textarea id="{id}" class="' + [CSS_FORM_BUILDER_FIELD_NODE, CSS_FIELD, CSS_FIELD_TEXT, CSS_FIELD_TEXTAREA].join(SPACE) + '" name="{name}">{value}</textarea>'
+	TPL_INPUT = '<input id="{id}" class="' + [CSS_FORM_BUILDER_FIELD_NODE, CSS_FIELD_INPUT, CSS_FIELD_INPUT_TEXT].join(SPACE) + '" name="{name}" type="text" value="{value}" />'
 
-var FormBuilderTextAreaField = A.Component.create({
+var FormBuilderTextField = A.Component.create({
 
-	NAME: FORM_BUILDER_TEXTAREA_FIELD,
+	NAME: FORM_BUILDER_TEXT_FIELD,
 
 	ATTRS: {
 
@@ -2686,7 +2487,7 @@ var FormBuilderTextAreaField = A.Component.create({
 		 */
 		template: {
 			valueFn: function() {
-				return TPL_TEXTAREA;
+				return TPL_INPUT;
 			}
 		},
 
@@ -2719,7 +2520,7 @@ var FormBuilderTextAreaField = A.Component.create({
 		bindUI: function() {
 			var instance = this;
 
-			A.FormBuilderInputField.superclass.bindUI.apply(instance, arguments);
+			A.FormBuilderTextField.superclass.bindUI.apply(instance, arguments);
 
 			var templateNode = instance.get(TEMPLATE_NODE);
 
@@ -2771,7 +2572,201 @@ var FormBuilderTextAreaField = A.Component.create({
 			var formBuilder = instance.get(FORM_BUILDER);
 			var formNode = formBuilder.get(SETTINGS_FORM_NODE);
 
-			A.FormBuilderCheckBoxField.superclass.renderSettings.apply(instance, arguments);
+			A.FormBuilderTextField.superclass.renderSettings.apply(instance, arguments);
+
+			if (!instance._renderedInputSettings) {
+				instance._renderedInputSettings = true;
+
+				var predefinedValueNode = formNode.one('input[name=predefinedValue]');
+
+				predefinedValueNode.on(
+					{
+						'keyup': A.bind(instance._onValueKeyUp, instance)
+					}
+				);
+			}
+		},
+
+		/**
+		 * Handles the onKeyUp event for the value nodes
+		 *
+		 * @method _onValueKeyUp
+		 */
+		_onValueKeyUp: function(event) {
+			var instance = this;
+			var target = event.target;
+
+			instance.set(PREDEFINED_VALUE, target.val());
+		},
+
+		_uiSetPredefinedValue: function(val) {
+			var instance = this;
+			var formBuilder = instance.get(FORM_BUILDER);
+			var formNode = formBuilder.get(SETTINGS_FORM_NODE);
+			var predefinedValueNode = formNode.one('input[name=predefinedValue]');
+			var templateNode = instance.get(TEMPLATE_NODE);
+
+			templateNode.val(val);
+
+			if (predefinedValueNode && instance.get(SELECTED)) {
+				predefinedValueNode.val(val);
+			}
+		}
+
+	}
+
+});
+
+A.FormBuilderTextField = FormBuilderTextField;
+
+A.FormBuilder.types['text'] = A.FormBuilderTextField;
+var L = A.Lang,
+	isArray = L.isArray,
+	isNumber = L.isNumber,
+	isString = L.isString,
+
+	BOUNDING_BOX = 'boundingBox',
+	CONTENT_BOX = 'contentBox',
+	CONTAINER = 'container',
+	DOT = '.',
+	DRAG = 'drag',
+	DRAG_CONTAINER = 'dragContainer',
+	DRAG_CONTAINER_NODE = 'dragContainerNode',
+	DRAG_NODES_LIST = 'dragNodesList',
+	DROP = 'drop',
+	DROP_CONTAINER = 'dropContainer',
+	DROP_CONTAINER_NODE = 'dropContainerNode',
+	EMPTY_STR = '',
+	FIELD = 'field',
+	FIELDS = 'fields',
+	FORM_BUILDER_FIELD = 'form-builder-field',
+	FORM_BUILDER_TEXTAREA_FIELD = 'form-builder-textarea-field',
+	ID = 'id',
+	ICON = 'icon',
+	LABEL = 'label',
+	NAME = 'name',
+	NODE = 'node',
+	PORTAL_LAYOUT = 'portalLayout',
+	PREDEFINED_VALUE = 'predefinedValue',
+	PROXY = 'proxy',
+	SIZE = 'size',
+	SPACE = ' ',
+	TEMPLATE = 'template',
+	TEMPLATE_NODE = 'templateNode',
+	TEXT = 'text',
+	TEXTAREA = 'textarea',
+	VALUE = 'value',
+
+	getCN = A.ClassNameManager.getClassName,
+
+	CSS_FIELD = getCN(FIELD),
+	CSS_FIELD_TEXT = getCN(FIELD, TEXT),
+	CSS_FIELD_TEXTAREA = getCN(FIELD, TEXTAREA),
+	CSS_FORM_BUILDER_FIELD = getCN(FORM_BUILDER_FIELD),
+	CSS_FORM_BUILDER_FIELD_NODE = getCN(FORM_BUILDER_FIELD, NODE),
+	CSS_STATE_DEFAULT = getCN(STATE, DEFAULT),
+
+	TPL_TEXTAREA = '<textarea id="{id}" class="' + [CSS_FORM_BUILDER_FIELD_NODE, CSS_FIELD, CSS_FIELD_TEXT, CSS_FIELD_TEXTAREA].join(SPACE) + '" name="{name}">{value}</textarea>'
+
+var FormBuilderTextAreaField = A.Component.create({
+
+	NAME: FORM_BUILDER_TEXTAREA_FIELD,
+
+	ATTRS: {
+
+		/**
+		 * The HTML template of the field
+		 *
+		 * @attribute template
+		 */
+		template: {
+			valueFn: function() {
+				return TPL_TEXTAREA;
+			}
+		},
+
+		/*
+		* HTML_PARSER attributes
+		*/
+		templateNode: {
+			valueFn: 'getNode'
+		}
+
+	},
+
+	UI_ATTRS: [ACCEPT_CHILDREN, PREDEFINED_VALUE, LABEL, NAME, SHOW_LABEL],
+
+	CSS_PREFIX: CSS_FORM_BUILDER_FIELD,
+
+	HTML_PARSER: {
+		templateNode: DOT + CSS_FORM_BUILDER_FIELD_NODE
+	},
+
+	EXTENDS: A.FormBuilderField,
+
+	prototype: {
+
+		/**
+		 * Bind phase
+		 *
+		 * @method bindUI
+		 */
+		bindUI: function() {
+			var instance = this;
+
+			A.FormBuilderTextAreaField.superclass.bindUI.apply(instance, arguments);
+
+			var templateNode = instance.get(TEMPLATE_NODE);
+
+			templateNode.on(
+				{
+					'keyup': A.bind(instance._onValueKeyUp, instance)
+				}
+			);
+		},
+
+		/**
+		 * Returns the HTML content of the field
+		 *
+		 * @method getHTML
+		 */
+		getHTML: function() {
+			var instance = this;
+			var template = instance.get(TEMPLATE);
+			var id = instance.get(ID);
+			var label = instance.get(LABEL);
+			var name = instance.get(NAME);
+			var size = instance.get(SIZE);
+			var value = instance.get(PREDEFINED_VALUE);
+
+			return A.substitute(
+				template,
+				{
+					id: id,
+					label: label,
+					name: name,
+					value: value
+				}
+			)
+		},
+
+		/**
+		 * Returns the A.Node of the field's HTML content
+		 *
+		 * @method getFieldNode
+		 */
+		getNode: function() {
+			var instance = this;
+
+			return A.Node.create(instance.getHTML());
+		},
+
+		renderSettings: function() {
+			var instance = this;
+			var formBuilder = instance.get(FORM_BUILDER);
+			var formNode = formBuilder.get(SETTINGS_FORM_NODE);
+
+			A.FormBuilderTextAreaField.superclass.renderSettings.apply(instance, arguments);
 
 			if (!instance._renderedTextareaSettings) {
 				instance._renderedTextareaSettings = true;
