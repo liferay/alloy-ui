8067be2c22c3c4a05dfbd811582fbbbb884b760e
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2011-04-17 22:13:03 +0000

    AUI-402 - Changing component to autogenerate the CSS_PREFIX
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@78320 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-component/js/aui-component.js
+++ src/aui-component/js/aui-component.js
@@ -333,6 +333,14 @@ var COMP_PROTO = Component.prototype;
 
 var DEFAULT_UI_ATTRS = A.Widget.prototype._UI_ATTRS;
 
+Component._applyCssPrefix = function(component) {
+	if (component && !('CSS_PREFIX' in component)) {
+		component.CSS_PREFIX = A.getClassName(component.NAME);
+	}
+
+	return component;
+};
+
 Component.create = function(config) {
 	config = config || {};
 
@@ -391,13 +399,21 @@ Component.create = function(config) {
 		component = A.Base.build(config.NAME, component, augmentsClasses, { dynamic: false });
 	}
 
-	if (!('CSS_PREFIX' in component)) {
-		component.CSS_PREFIX = A.getClassName(component.NAME);
-	}
+	Component._applyCssPrefix(component);
 
 	return component;
 };
 
 Component.CSS_PREFIX = getClassName('component');
+
+var Base = A.Base;
+
+Component.build = function() {
+	var component = Base.build.apply(Base, arguments);
+
+	Component._applyCssPrefix(component);
+
+	return component;
+};
 
 A.Component = Component;
