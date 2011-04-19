662661e340f54efa174c08b95f85b17129e89654
Bruno Basto <bruno.basto@liferay.com>
2011-04-19 00:23:09 +0000

    AUI-402 yui3 prefix to taglibs css classes
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@78469 05bdf26c-840f-0410-9ced-eb539d925f36

--- sandbox/taglibs/alloy-taglib/src/com/liferay/alloy/util/Constants.java
+++ sandbox/taglibs/alloy-taglib/src/com/liferay/alloy/util/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
 	public static final String CSS_CLASS_DELIMITER = "-";
 
-	public static final String CSS_CLASS_PREFIX = "aui";
+	public static final String CSS_CLASS_PREFIX = "yui3-aui";
 
 }
