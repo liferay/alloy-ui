9252995c1a0b95e0ccca227b9428d0af52c3c7e1
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-04-22 20:20:02 +0000

    Build files
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@78898 05bdf26c-840f-0410-9ced-eb539d925f36

--- build/aui-button-item/aui-button-item.js
+++ build/aui-button-item/aui-button-item.js
@@ -6,11 +6,14 @@ AUI.add('aui-button-item', function(A) {
  */
 
 var Lang = A.Lang,
+	isString = Lang.isString,
 
 	getClassName = A.getClassName,
 
 	NAME = 'buttonitem',
 
+	BOUNDING_BOX = 'boundingBox',
+	BUTTON = 'button',
 	CONTENT_BOX = 'contentBox',
 	DOT = '.',
 	ICON = 'icon',
@@ -18,8 +21,11 @@ var Lang = A.Lang,
 	LABEL = 'label',
 	LABEL_NODE = 'labelNode',
 	ONLY = 'only',
+	RESET = 'reset',
 	STATE = 'state',
+	SUBMIT = 'submit',
 	TITLE = 'title',
+	TYPE = 'type',
 
 	CSS_BUTTON = getClassName(NAME),
 	CSS_BUTTON_ICON = getClassName(NAME, ICON),
@@ -245,6 +251,20 @@ var ButtonItem = A.Component.create(
 			title: {
 				setter: '_setTitle',
 				value: false
+			},
+
+			/**
+			 * Button type.
+			 *
+			 * @attribute type
+			 * @default button
+			 * @type String
+			 */
+			type: {
+				validator: function(val) {
+					return (val === BUTTON || val === SUBMIT || val === RESET);
+				},
+				value: BUTTON
 			}
 		},
 
@@ -287,7 +307,7 @@ var ButtonItem = A.Component.create(
 		},
 
 		constructor: function(config) {
-			if (Lang.isString(config)) {
+			if (isString(config)) {
 				config = {
 					icon: config
 				};
@@ -296,7 +316,7 @@ var ButtonItem = A.Component.create(
 			ButtonItem.superclass.constructor.call(this, config);
 		},
 
-		UI_ATTRS: [ICON, LABEL, TITLE],
+		UI_ATTRS: [ICON, LABEL, TITLE, TYPE],
 
 		prototype: {
 			BOUNDING_TEMPLATE: TPL_BUTTON,
@@ -463,7 +483,7 @@ var ButtonItem = A.Component.create(
 				var hasLabelOnly = (!icon && label);
 				var hasIconOnly = (icon && !label);
 
-				var boundingBox = instance.get('boundingBox');
+				var boundingBox = instance.get(BOUNDING_BOX);
 
 				boundingBox.toggleClass(CSS_BUTTON_ICON_LABEL, hasIconAndLabel);
 				boundingBox.toggleClass(CSS_BUTTON_ICON_ONLY, hasIconOnly);
@@ -534,9 +554,24 @@ var ButtonItem = A.Component.create(
 			_uiSetTitle: function(val) {
 				var instance = this;
 
-				var boundingBox = instance.get('boundingBox');
+				var boundingBox = instance.get(BOUNDING_BOX);
 
-				boundingBox.setAttribute('title', val);
+				boundingBox.setAttribute(TITLE, val);
+			},
+
+			/**
+			 * Updates the UI for the type in response to the <a href="ButtonItem.html#event_typeChange">typeChange</a> event.
+			 *
+			 * @method _uiSetType
+			 * @param {String} val The new value
+			 * @protected
+			 */
+			_uiSetType: function(val) {
+				var instance = this;
+
+				var boundingBox = instance.get(BOUNDING_BOX);
+
+				boundingBox.setAttribute(TYPE, val);
 			}
 		}
 	}
