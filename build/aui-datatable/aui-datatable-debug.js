9d8d15bbde61eb6e3146828e2bb8f5c6c91f8b59
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-04-27 20:36:01 +0000

    Build files
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@79311 05bdf26c-840f-0410-9ced-eb539d925f36

--- build/aui-datatable/aui-datatable-debug.js
+++ build/aui-datatable/aui-datatable-debug.js
@@ -923,7 +923,7 @@ var BaseCellEditor = A.Component.create({
 					instance.formatValue(instance.get(OUTPUT_FORMATTER), val)
 				);
 
-				A.later(0, elements, elements.selectText);
+				A.later(30, elements, elements.selectText);
 			}
 		}
 
--- build/aui-datatable/aui-datatable-edit-debug.js
+++ build/aui-datatable/aui-datatable-edit-debug.js
@@ -625,7 +625,7 @@ var BaseCellEditor = A.Component.create({
 					instance.formatValue(instance.get(OUTPUT_FORMATTER), val)
 				);
 
-				A.later(0, elements, elements.selectText);
+				A.later(30, elements, elements.selectText);
 			}
 		}
 
