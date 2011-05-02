84090fc91c3d11b142189a1437fe7d9b890092dd
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2011-05-02 22:48:25 +0000

    AUI-422 - Build
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@79715 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ build/aui-simple-anim/aui-simple-anim-debug.js
@@ -0,0 +1,137 @@
+AUI.add('aui-simple-anim', function(A) {
+var Lang = A.Lang,
+	now = Lang.now;
+
+var SimpleAnim = A.Component.create(
+	{
+		EXTENDS: Object,
+
+		constructor: function(config) {
+			var instance = this;
+
+			instance.active = false;
+
+			instance.duration = config.duration || 200;
+			instance.easing = config.easing || instance._easeOutQuad;
+			instance.from = config.from;
+			instance.to = config.to;
+
+			instance._ontween = config.onTween;
+			instance._oncomplete = config.onComplete;
+		},
+
+		prototype: {
+			animate: function() {
+				var instance = this;
+
+				var duration = instance.duration;
+
+				var continueAnimation = false;
+
+				if (instance.active) {
+					var time = now() - instance._startTime;
+
+					if (instance._ontween) {
+						var pos = instance.easing(time, instance.from, instance.to - instance.from, duration);
+
+						if (pos) {
+							instance._ontween(pos);
+						}
+					}
+
+					if (time >= duration) {
+						instance.active = false;
+
+						if (instance._oncomplete) {
+							instance._oncomplete();
+						}
+					}
+					else {
+						continueAnimation = true;
+					}
+				}
+
+				return continueAnimation;
+			},
+
+			start: function() {
+				var instance = this;
+
+				instance._startTime = now();
+
+				SimpleAnim.queue(instance);
+			},
+
+			stop: function() {
+				var instance = this;
+
+				instance.active = false;
+			},
+
+			_easeOutQuad: function(t, b, c, d) {
+				return -c * (t /= d) * (t - 2) + b;
+			}
+		},
+
+		// Static methods/properties
+		active: false,
+
+		queue: function(animObj) {
+			var instance = this;
+
+			instance._queue.push(animObj);
+			animObj.active = true;
+
+			if (!instance.active) {
+				instance.start();
+			}
+		},
+
+		animate: function() {
+			var instance = this;
+
+			var active = 0;
+
+			for (var i = 0, length = instance._queue.length; i < length; i++) {
+				var animObj = instance._queue[i];
+
+				if (animObj.active) {
+					animObj.animate();
+					active++;
+				}
+			}
+
+			if (active == 0 && instance._timer) {
+				instance.stop();
+			}
+		},
+
+		start: function() {
+			var instance = this;
+
+			if (!instance._timer && !instance.active) {
+				instance.active = true;
+
+				instance._timer = setInterval(A.bind(instance.animate, instance), instance._intervalRate);
+			}
+		},
+
+		stop: function() {
+			var instance = this;
+
+			clearInterval(instance._timer);
+
+			instance._timer = null;
+			instance.active = false;
+			instance._queue = [];
+		},
+
+		_intervalRate: 20,
+		_queue: [],
+		_timer: null
+	}
+);
+
+A.SimpleAnim = SimpleAnim;
+
+}, '@VERSION@' ,{skinnable:false, requires:['aui-base']});
