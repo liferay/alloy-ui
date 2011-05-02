38e2f8c998bf1895a09293e4ee6ef2b408e138c3
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2011-05-02 22:48:27 +0000

    AUI-423 - Convert Scroller to use SimpleAnim
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@79717 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-scroller/js/aui-scroller.js
+++ src/aui-scroller/js/aui-scroller.js
@@ -6,15 +6,26 @@ var L = A.Lang,
 	CLIENT_WIDTH = 'clientWidth',
 	CONTENT_BOX = 'contentBox',
 	DURATION = 'duration',
+	EDGE_PROXIMITY = 'edgeProximity',
+
+	HEIGHT = 'Height',
+	LEFT = 'Left',
+	OFFSET = 'offset',
+	SCROLL = 'scroll',
+	TOP = 'Top',
+	WIDTH = 'Width',
+
 	HORIZONTAL = 'horizontal',
-	OFFSET_HEIGHT = 'offsetHeight',
-	OFFSET_LEFT = 'offsetLeft',
-	OFFSET_TOP = 'offsetTop',
-	OFFSET_WIDTH = 'offsetWidth',
+	OFFSET_HEIGHT = OFFSET + HEIGHT,
+	OFFSET_LEFT = OFFSET + LEFT,
+	OFFSET_TOP = OFFSET + TOP,
+	OFFSET_WIDTH = OFFSET + WIDTH,
 	ORIENTATION = 'orientation',
 	PX = 'px',
-	SCROLL_HEIGHT = 'scrollHeight',
+	SCROLL_HEIGHT = SCROLL + HEIGHT,
 	SCROLLER = 'scroller',
+	SCROLLCONTENT = 'scrollcontent',
+	UNDERLINE = '_',
 	VERTICAL = 'vertical',
 
 	getCN = A.getClassName,
@@ -29,10 +40,15 @@ var Scroller = A.Component.create (
 
 		ATTRS: {
 			duration: {
-				validator: function (val) {
-					return isNumber(val);
+				setter: function(value) {
+					return value * 1000;
 				},
-				value: 0.5
+				validator: isNumber,
+				value: 0.1
+			},
+
+			edgeProximity: {
+				value: 0.1
 			},
 
 			itemSelector: {
@@ -40,7 +56,7 @@ var Scroller = A.Component.create (
 			},
 
 			orientation: {
-				validator: function (val) {
+				validator: function(val) {
 					return isString(val) && (val === HORIZONTAL || val === VERTICAL);
 				},
 				value: HORIZONTAL
@@ -55,124 +71,235 @@ var Scroller = A.Component.create (
 			initializer: function() {
 				var instance = this;
 
-				instance._boundingBox = instance.get(BOUNDING_BOX);
-				instance._contentBox = instance.get(CONTENT_BOX);
+				var boundingBox = instance.get(BOUNDING_BOX);
+				var contentBox = instance.get(CONTENT_BOX);
+
+				instance._boundingBox = boundingBox;
+				instance._contentBox = contentBox;
+
 				instance._duration = instance.get(DURATION);
+				instance._edgeProximity = instance.get(EDGE_PROXIMITY);
 				instance._orientation = instance.get(ORIENTATION);
 
-				instance._boundingBoxXY = instance._boundingBox.getXY();
+				instance.boundingBoxXY = instance._boundingBox.getXY();
+
+				instance._boundingBoxEl = boundingBox.getDOM();
+				instance._contentBoxEl = contentBox.getDOM();
+
+				instance._setCoords(instance._orientation == HORIZONTAL);
 
 				instance._updateNodeSelection();
-
-				instance._contentBox.plug(
-					A.Plugin.NodeFX, 
-					{
-						duration: instance._duration,
-						easing: A.Easing.easeOutStrong,
-						to: {}
-					}
-				);
 			},
 
-			bindUI: function () {
+			bindUI: function() {
 				var instance = this;
 
 				instance.publish(
-					'scroll',
+					SCROLLCONTENT,
 					{
 						defaultFn: instance._defaultScrollFn
 					}
 				);
 
-				instance.after(
+				var contentBox = instance._contentBox;
+
+				instance.after(['durationChange', 'edgeProximityChange'], instance._setPrivateAttr);
+
+				contentBox.on('mouseenter', A.rbind(instance._updateDimensions, instance));
+
+				contentBox.on('mousemove', A.rbind(instance._onMouseMove, instance, instance._boundingBox, instance._contentBox, instance._orientation));
+
+				instance._boundingBox.on('focus', instance._onItemFocus, instance);
+
+				instance._createAnimation();
+			},
+
+			_adjustToEdge: function(value) {
+				var instance = this;
+
+				var edgeProximity = instance._edgeProximity;
+
+				var adjusted = value;
+
+				if (edgeProximity) {
+					value = Math.max(value, edgeProximity);
+					value = Math.min(value, 1 - edgeProximity);
+
+					value = value - edgeProximity;
+
+					adjusted = value / (1 - (edgeProximity * 2));
+				}
+
+				return adjusted;
+			},
+
+			_animate: function(event) {
+				var instance = this;
+
+				var fx = instance._fx;
+
+				var boundingBoxEl = instance._boundingBoxEl;
+				var orientation = instance._orientation;
+
+				var scroll = SCROLL + instance._coordTL;
+				var coordXY = instance._coordXY.toLowerCase();
+
+				var from = boundingBoxEl[scroll] || 0;
+				var to = event[coordXY];
+
+				fx.from = from;
+				fx.to = to;
+
+				fx.start();
+			},
+
+			_constrain: function(value, min, max) {
+				var instance = this;
+
+				return Math.max(Math.min(value, max), min);
+			},
+
+			_createAnimation: function() {
+				var instance = this;
+
+				var boundingBoxEl = instance._boundingBoxEl;
+
+				var from = boundingBoxEl[SCROLL + instance._coordTL] || 0;
+				var to = from;
+
+				instance._fx = new A.SimpleAnim(
 					{
-						durationChange: instance._afterDurationChange,
-						orientationChange: instance._afterOrientationChange
+						duration: instance._duration,
+						from: from,
+						to: to,
+						onTween: function(value) {
+							boundingBoxEl[SCROLL + instance._coordTL] = value;
+						}
 					}
 				);
 
-				instance._contentBox.on('mouseenter', A.rbind(instance._updateXY, instance, instance._boundingBox));
-
-				instance._contentBox.on('mousemove', A.rbind(instance._onMouseMove, instance, instance._boundingBox, instance._contentBox, instance._orientation));
+				instance._throttleAnimate = A.throttle(A.bind(instance._animate, instance), 50);
 			},
 
-			_afterDurationChange: function (event) {
+			_defaultScrollFn: function(event) {
 				var instance = this;
 
-				instance._duration = event.newVal;
+				instance._throttleAnimate(event);
 			},
 
-			_afterOrientationChange: function (event) {
+			_getPositionData: function(absoluteX, absoluteY) {
 				var instance = this;
 
-				instance._orientation = event.newVal;
+				var boundingBoxXY = instance.boundingBoxXY;
+
+				var relativeMouseX = absoluteX - boundingBoxXY[0];
+				var relativeMouseY = absoluteY - boundingBoxXY[1];
+
+				var scaledX = relativeMouseX * instance.ratioX;
+				var scaledY = relativeMouseY * instance.ratioY;
+
+				var relativeX = instance._constrain(relativeMouseX / instance.boundingBoxWidth, 0, 1);
+				var relativeY = instance._constrain(relativeMouseY / instance.boundingBoxHeight, 0, 1);
+
+				relativeX = instance._adjustToEdge(relativeX);
+				relativeY = instance._adjustToEdge(relativeY);
+
+				var adjustedX = (instance.deltaX * relativeX);
+				var adjustedY = (instance.deltaY * relativeY);
+
+				return {
+					x: adjustedX,
+					y: adjustedY,
+					scaledX: scaledX,
+					scaledY: scaledY,
+					relativeX: relativeMouseX,
+					relativeY: relativeMouseY
+				};
 			},
 
-			_defaultScrollFn: function (event) {
+			_onItemFocus: function(event) {
 				var instance = this;
 
-				var contentBox = instance._contentBox;
-				var orientation = instance._orientation;
+				instance._updateDimensions();
 
-				var fx = contentBox.fx;
+				var xy = event.target.getXY();
 
-				var key;
-				var value;
+				var data = instance._getPositionData.apply(instance, xy);
 
-				if(orientation == HORIZONTAL) {
-					key= 'to.left';
-					value = -event.offsetX + PX;
-				}
-				else {
-					key = 'to.top';
-					value = -event.offsetY + PX;
-				}
+				instance.fire(SCROLLCONTENT, data);
 
-				fx.stop();
-
-				fx.set(key, value);
-
-				fx.run();
+				instance._updatePrevXY(data.relativeX, data.relativeY);
 			},
 
 			_onMouseMove: function(event, boundingBox, contentBox, orientation) {
 				var instance = this;
 
+				var data = instance._getPositionData(event.pageX, event.pageY);
+
+				instance.fire(SCROLLCONTENT, data);
+
+				instance._updatePrevXY(data.relativeX, data.relativeY);
+			},
+
+			_setCoords: function(horizontal) {
+				var instance = this;
+
+				var coordTL = TOP;
+				var coordXY = 'Y';
+
+				if (horizontal) {
+					coordTL = LEFT;
+					coordXY = 'X';
+				}
+
+				instance._coordTL = coordTL;
+				instance._coordXY = coordXY;
+			},
+
+			_setPrivateAttr: function(event) {
+				var instance = this;
+
+				instance[UNDERLINE + event.attrName] = event.newVal;
+			},
+
+			_updateDimensions: function(event) {
+				var instance = this;
+
+				var boundingBox = instance._boundingBox;
+				var contentBox = instance._contentBox;
+
 				var boundingBoxOffsetHeight = boundingBox.get(OFFSET_HEIGHT);
 				var boundingBoxOffsetWidth = boundingBox.get(OFFSET_WIDTH);
 
-				var absMouseX = event.pageX - instance._boundingBoxXY[0];
-				var absMouseY = event.pageY - instance._boundingBoxXY[1];
+				var contentBoxClientWidth = contentBox.get(CLIENT_WIDTH);
+				var contentBoxScrollHeight = contentBox.get(SCROLL_HEIGHT);
 
-				var diffX = contentBox.get(CLIENT_WIDTH) - boundingBoxOffsetWidth;
-				var diffY = contentBox.get(SCROLL_HEIGHT) - boundingBoxOffsetHeight;
+				var deltaX = contentBoxClientWidth - boundingBoxOffsetWidth;
+				var deltaY = contentBoxScrollHeight - boundingBoxOffsetHeight;
 
-				var factorX = diffX / boundingBoxOffsetWidth;
-				var factorY = diffY / boundingBoxOffsetHeight;
+				instance.boundingBoxXY = boundingBox.getXY();
 
-				var offsetX = absMouseX * factorX;
-				var offsetY = absMouseY * factorY;
+				instance.ratioX = deltaX / boundingBoxOffsetWidth;
+				instance.ratioY = deltaY / boundingBoxOffsetHeight;
 
-				instance.fire(
-					'scroll',
-					{
-						factorX: factorX,
-						factorY: factorY,
-						offsetX: offsetX,
-						offsetY: offsetY,
-						x: absMouseX,
-						y: absMouseY
-					}
-				);
+				instance.deltaX = deltaX;
+				instance.deltaY = deltaY;
+
+				instance.boundingBoxHeight = boundingBoxOffsetHeight;
+				instance.boundingBoxWidth = boundingBoxOffsetWidth;
+
+				instance.contentBoxHeight = contentBoxScrollHeight;
+				instance.contentBoxWidth = contentBoxClientWidth;
 			},
 
-			_updateXY: function (event, boundingBox) {
+			_updatePrevXY: function(x, y) {
 				var instance = this;
 
-				instance._boundingBoxXY = boundingBox.getXY();
+				instance._prevX = x;
+				instance._prevY = y;
 			},
 
-			_uiSetOrientation: function (val) {
+			_uiSetOrientation: function(val) {
 				var instance = this;
 
 				var boundingBox = instance._boundingBox;
@@ -181,15 +308,22 @@ var Scroller = A.Component.create (
 
 				boundingBox.toggleClass(CSS_HORIZONTAL, horizontal);
 				boundingBox.toggleClass(CSS_VERTICAL, !horizontal);
+
+				instance._setCoords(horizontal);
+
+				instance._orientation = val;
 			},
 
-			_updateNodeSelection: function () {
+			_updateNodeSelection: function() {
 				var instance = this;
 
 				var itemSelector = instance.get('itemSelector');
 
 				instance.nodeSelection = instance._contentBox.all(itemSelector).addClass(CSS_ITEM);
-			}
+			},
+
+			_prevX: 0,
+			_prevY: 0
 		}
 	}
 );
