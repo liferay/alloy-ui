40202d009a57420fa6228afcb972e75cce591e91
Jonathan Mak <jonathan.mak@liferay.com>
2011-05-04 16:32:29 +0000

    AUI-427 - Update AUI Tree IO requests to use uriFormatter
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/ee/1.0.x@79899 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-tree/js/aui-tree-node.js
+++ src/aui-tree/js/aui-tree-node.js
@@ -1097,7 +1097,7 @@ var TreeNodeIO = A.Component.create(
 					loader(io.url, io.cfg, instance);
 				}
 				else {
-					A.io(io.url, io.cfg);
+					A.io.request(io.url, io.cfg);
 				}
 			},
 
@@ -1142,11 +1142,11 @@ var TreeNodeIO = A.Component.create(
 				var length = args.length;
 
 				// if using the first argument as the JSON object
-				var nodes = args[0];
+				var nodes = args[1];
 
-				// if using (id, o) yui callback syntax
-				if (length >= 2) {
-					var o = args[1];
+				// if using (event, id, o) yui callback syntax
+				if (length >= 3) {
+					var o = args[2];
 					// try to convert responseText to JSON
 					try {
 						nodes = A.JSON.parse(o.responseText);
