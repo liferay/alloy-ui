db56c5d49637b04f4d2496a1fd87bd837c781fe0
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-06-01 14:25:27 +0000

    AUI-443 TreeView is not propagating the inherited attributes for nested children
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@82303 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-tree/js/aui-tree-node.js
+++ src/aui-tree/js/aui-tree-node.js
@@ -942,20 +942,6 @@ var TreeNodeIO = A.Component.create(
 
 		prototype: {
 			/**
-			 * Create the DOM structure for the TreeNodeIO. Lifecycle.
-			 *
-			 * @method renderUI
-			 * @protected
-			 */
-			renderUI: function() {
-				var instance = this;
-
-				instance._inheritOwnerTreeAttrs();
-
-				A.TreeNodeIO.superclass.renderUI.apply(this, arguments);
-			},
-
-			/**
 			 * Bind the events on the TreeNodeIO UI. Lifecycle.
 			 *
 			 * @method bindUI
@@ -1015,18 +1001,16 @@ var TreeNodeIO = A.Component.create(
 					instance.set(LOADED, false);
 				}
 
-				if (!io || loaded) {
-					A.TreeNodeIO.superclass.expand.apply(this, arguments);
+				if (io && !loaded && !loading && !this.hasChildNodes()) {
+					if (!cache) {
+						// remove all children to reload
+						instance.empty();
+					}
+
+					instance.initIO();
 				}
 				else {
-					if (!loading) {
-						if (!cache) {
-							// remove all children to reload
-							instance.empty();
-						}
-
-						instance.initIO();
-					}
+					A.TreeNodeIO.superclass.expand.apply(this, arguments);
 				}
 			},
 
