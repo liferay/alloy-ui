257ad6d627eb4e66891049c69fcf76868fce0605
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2011-06-03 19:15:22 +0000

    AUI-446 - Build
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/ee/1.0.x@82599 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ build/aui-messaging/aui-messaging.js
@@ -0,0 +1,184 @@
+AUI.add('aui-messaging', function(A) {
+var Lang = A.Lang,
+	isString = Lang.isString,
+
+	lastHash,
+
+	ENV = A.Env,
+	G_ENV = AUI.Env,
+
+	WIN = A.config.win,
+	LOCATION = WIN.location,
+
+	NATIVE_MSG = !!(WIN.postMessage),
+
+	TOKEN_CLOSE = '_A=',
+	TOKEN_OPEN = '=A_',
+
+	STR_REGEX_CONTENT = '.*?',
+	STR_REGEX_TOKEN_OPEN = TOKEN_OPEN + '\\d+&',
+
+	REGEX_TOKEN_EXTRACT_HASH = new RegExp(STR_REGEX_TOKEN_OPEN + '(' + STR_REGEX_CONTENT + ')' + TOKEN_CLOSE),
+	REGEX_TOKEN_REPLACE_HASH = new RegExp(STR_REGEX_TOKEN_OPEN + STR_REGEX_CONTENT + TOKEN_CLOSE);
+
+fnPostMessage = function(message, targetURI, targetFrame) {
+	if (targetURI) {
+		if (Lang.isObject(message)) {
+			message = A.QueryString.stringify(message);
+		}
+
+		try {
+			targetFrame = A.one(targetFrame);
+
+			targetFrame = targetFrame && targetFrame.getDOM().contentWindow;
+		}
+		catch (e) {
+		}
+
+		targetFrame = targetFrame || WIN.parent;
+
+		if (NATIVE_MSG) {
+			targetURI = targetURI.replace(/([^:]+:\/\/[^\/?]+).*/, '$1');
+
+			targetFrame.postMessage(message, targetURI);
+		}
+		else {
+			var uriPieces = targetURI.split('#');
+
+			var uri = uriPieces[1] || '';
+
+			// If we reuse a uri that has the token in it,
+			// let's replace it.
+			uri = uri.replace(REGEX_TOKEN_REPLACE_HASH, '');
+
+			var cacheBreaker = Lang.now() + (++ENV._uidx);
+
+			uri = (TOKEN_OPEN + cacheBreaker + '&' + message + TOKEN_CLOSE) + uri;
+
+			uriPieces[1] = uri;
+
+			targetFrame.location = uriPieces.join('#');
+		}
+	}
+};
+
+fnReceiveMessage = function(callback, sourceURI, interval) {
+	if (NATIVE_MSG) {
+		var previousCallbackWrapper = Messaging._callbackFn;
+
+		if (previousCallbackWrapper) {
+			G_ENV.remove(WIN, 'message', previousCallbackWrapper);
+
+			Messaging._callbackFn = null;
+		}
+
+		if (callback) {
+			var callbackFn = A.rbind(Messaging._validateCallback, Messaging, sourceURI, callback);
+
+			Messaging._callbackFn = callbackFn;
+
+			G_ENV.add(WIN, 'message', callbackFn);
+		}
+	}
+	else {
+		var intervalId = Messaging._INTERVAL_ID;
+
+		if (intervalId) {
+			A.clearInterval(intervalId);
+
+			Messaging._INTERVAL_ID = null;
+		}
+
+		if (callback) {
+			if (Lang.isUndefined(interval)) {
+				interval = 100;
+
+				if (Lang.isNumber(sourceURI)) {
+					interval = sourceURI;
+				}
+			}
+
+			Messaging._INTERVAL_ID = A.setInterval(Messaging._pollHash, interval, Messaging, callback);
+		}
+	}
+};
+
+Messaging = {
+	post: fnPostMessage,
+	receive: fnReceiveMessage,
+
+	_formatEventObject: function(obj) {
+		var instance = this;
+
+		var data = obj.data;
+
+		var responseData = instance._getResponseData(data);
+
+		obj.data = data;
+		obj.responseData = responseData;
+
+		return obj;
+	},
+
+	_getResponseData: function(data) {
+		var instance = this;
+
+		var responseData = data;
+
+		if (responseData && /\w+=\w+/.test(responseData)) {
+			responseData = A.QueryString.parse(responseData);
+		}
+
+		return responseData;
+	},
+
+	_pollHash: function(callback) {
+		var instance = Messaging;
+
+		var hash = LOCATION.hash;
+
+		if (hash != lastHash && REGEX_TOKEN_REPLACE_HASH.test(hash)) {
+			lastHash = hash;
+
+			var data = hash.match(REGEX_TOKEN_EXTRACT_HASH);
+
+			data = (data && data[1]) || '';
+
+			var obj = {
+				data: data
+			};
+
+			instance._formatEventObject(obj);
+
+			callback.call(Messaging, obj);
+		}
+	},
+
+	_validateCallback: function(event, sourceURI, callback) {
+		var instance = Messaging;
+
+		var origin = event.origin;
+
+		var valid = false;
+
+		if (Lang.isFunction(sourceURI)) {
+			valid = sourceURI(origin);
+		}
+		else if (isString(sourceURI)) {
+			valid = (sourceURI == origin);
+		}
+
+		if (valid) {
+			instance._formatEventObject(event);
+
+			callback.call(Messaging, event);
+		}
+	}
+};
+
+A.postMessage = fnPostMessage;
+A.receiveMessage = fnReceiveMessage;
+
+A.Messaging = Messaging;
+
+}, '@VERSION@' ,{skinnable:false, requires:['aui-base','aui-task-manager','querystring']});
