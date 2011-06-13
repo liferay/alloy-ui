b2a2ebd1c0c8cd7c2dd7d3d388a7d34184196153
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-06-13 17:06:49 +0000

    AUI-413 DataTable focus bugfixes
    git-svn-id: svn://svn.liferay.com/repos/public/alloy/trunk@83373 05bdf26c-840f-0410-9ced-eb539d925f36

--- src/aui-datatable/js/aui-datatable-edit.js
+++ src/aui-datatable/js/aui-datatable-edit.js
@@ -266,7 +266,9 @@ A.mix(CellEditorSupport.prototype, {
 			var cell = instance.getCellNode(activeRecord, activeColumn);
 			var row = instance.getRowNode(activeRecord);
 
-			selection.select(cell, row);
+			if (!event.newVal) {
+				selection.select(cell, row);
+			}
 		}
 	},
 
@@ -718,6 +720,12 @@ var BaseCellEditor = A.Component.create({
 		_defInitEditFn: function(event) {
 		},
 
+		_syncElementsFocus: function() {
+			var instance = this;
+
+			instance.elements.selectText();
+		},
+
 		_syncElementsName: function() {
 			var instance = this;
 
@@ -725,6 +733,12 @@ var BaseCellEditor = A.Component.create({
 				NAME,
 				instance.get(ELEMENT_NAME)
 			);
+		},
+
+		_syncFocus: function() {
+			var instance = this;
+
+			A.later(10, instance, instance._syncElementsFocus);
 		},
 
 		_uiSetEditable: function(val) {
@@ -756,9 +770,9 @@ var BaseCellEditor = A.Component.create({
 				elements.val(
 					instance.formatValue(instance.get(OUTPUT_FORMATTER), val)
 				);
-
-				A.later(30, elements, elements.selectText);
 			}
+
+			instance._syncFocus();
 		}
 
 		/*
@@ -1129,7 +1143,10 @@ var BaseOptionsCellEditor = A.Component.create({
 				AArray.each(AArray(val), function(value) {
 					options.filter('[value="' + value + '"]').set(instance.get(SELECTED_ATTR_NAME), true);
 				});
+
 			}
+
+			instance._syncFocus();
 
 			return val;
 		}
@@ -1226,6 +1243,12 @@ var DropDownCellEditor = A.Component.create({
 			return instance.elements.get(VALUE);
 		},
 
+		_syncElementsFocus: function() {
+			var instance = this;
+
+			instance.elements.focus();
+		},
+
 		_uiSetMultiple: function(val) {
 			var instance = this;
 			var elements = instance.elements;
@@ -1274,6 +1297,15 @@ var CheckboxCellEditor = A.Component.create({
 			var instance = this;
 
 			return instance._getSelectedOptions().get(VALUE);
+		},
+
+		_syncElementsFocus: function() {
+			var instance = this;
+			var options = instance.options;
+
+			if (options && options.size()) {
+				options.item(0).focus();
+			}
 		}
 	}
 });
