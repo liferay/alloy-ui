4ab48a771beb3007ef23f674f1eaa129f7bcc33a
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-08-04 17:13:18 -0300

    Build files

--- build/aui-property-list/aui-property-list-debug.js
+++ build/aui-property-list/aui-property-list-debug.js
@@ -87,7 +87,6 @@ var PropertyList = A.Component.create({
 		initializer: function() {
 			var instance = this;
 
-			instance.after(RECORDSET_CHANGE, instance._plugDependencies);
 			instance.after(instance._syncScrollWidth, instance, '_uiSetWidth');
 			instance.after(instance._syncScrollHeight, instance, '_uiSetHeight');
 
