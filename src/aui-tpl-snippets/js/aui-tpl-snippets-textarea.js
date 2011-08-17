e910e434b079e821db9a11c0a3388fd07e536310
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-08-17 14:49:01 -0300

    AUI-485 Adding aui-tpl-snippets input and textarea snippets

---
+++ src/aui-tpl-snippets/js/aui-tpl-snippets-textarea.js
@@ -0,0 +1,9 @@
+A.Template.register(
+	'textarea',
+	[
+		'<tpl if="values.label !== undefined">',
+			'<label class="{[A.TplSnippets.getClassName(values.auiLabelCssClass, values.labelCssClass)]}" for="{id}" id="{labelId}" name="{labelName}">{label}</label>',
+		'</tpl>',
+		'<textarea class="{[A.TplSnippets.getClassName(values.auiCssClass, values.cssClass)]}" id="{id}" name="{name}" placeholder="{placeholder}" value="{valueAttr}">{value}</textarea>'
+	]
+);
