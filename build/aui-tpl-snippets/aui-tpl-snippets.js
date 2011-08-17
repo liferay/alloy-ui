9136784e11cefadfed2eefad8cb2884fd43614f2
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-08-17 14:53:18 -0300

    Build files

--- build/aui-tpl-snippets/aui-tpl-snippets.js
+++ build/aui-tpl-snippets/aui-tpl-snippets.js
@@ -28,7 +28,31 @@ A.Template.register(
 );
 
 }, '@VERSION@' ,{skinnable:false, requires:['aui-tpl-snippets-base']});
+AUI.add('aui-tpl-snippets-input', function(A) {
+A.Template.register(
+	'input',
+	[
+		'<tpl if="values.label !== undefined">',
+			'<label class="{[A.TplSnippets.getClassName(values.auiLabelCssClass, values.labelCssClass)]}" for="{id}" id="{labelId}" name="{labelName}">{label}</label>',
+		'</tpl>',
+		'<input autocomplete="{autocomplete}" class="{[A.TplSnippets.getClassName(values.auiCssClass, values.cssClass)]}" id="{id}" name="{name}" placeholder="{placeholder}" type="{type}" value="{value}" />'
+	]
+);
+
+}, '@VERSION@' ,{skinnable:false, requires:['aui-tpl-snippets-base']});
+AUI.add('aui-tpl-snippets-textarea', function(A) {
+A.Template.register(
+	'textarea',
+	[
+		'<tpl if="values.label !== undefined">',
+			'<label class="{[A.TplSnippets.getClassName(values.auiLabelCssClass, values.labelCssClass)]}" for="{id}" id="{labelId}" name="{labelName}">{label}</label>',
+		'</tpl>',
+		'<textarea class="{[A.TplSnippets.getClassName(values.auiCssClass, values.cssClass)]}" id="{id}" name="{name}" placeholder="{placeholder}" value="{valueAttr}">{value}</textarea>'
+	]
+);
+
+}, '@VERSION@' ,{skinnable:false, requires:['aui-tpl-snippets-base']});
 
 
-AUI.add('aui-tpl-snippets', function(A){}, '@VERSION@' ,{use:['aui-tpl-snippets-base','aui-tpl-snippets-select'], skinnable:false});
+AUI.add('aui-tpl-snippets', function(A){}, '@VERSION@' ,{use:['aui-tpl-snippets-base','aui-tpl-snippets-select','aui-tpl-snippets-input','aui-tpl-snippets-textarea'], skinnable:false});
 
