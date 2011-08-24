de4813b33de721b33fdcb5ba2cddd55d18ef9691
Nate Cavanaugh <nate@shift22.com>
2011-08-24 07:21:30 -0700

    AUI-484 - Source formatting

--- build/aui-template/aui-template.js
+++ build/aui-template/aui-template.js
@@ -523,11 +523,11 @@ var Lang = A.Lang,
 
 	var NODE_PROTO = A.Node.prototype;
 
-	NODE_PROTO.toTemplate = function() {
+	NODE_PROTO.toTpl = function() {
 		return Template.from(this);
 	};
 
-	NODE_PROTO.renderTemplate = function(tpl, data) {
+	NODE_PROTO.renderTpl = function(tpl, data) {
 		var instance = this;
 
 		if (isString(tpl) || isArray(tpl)) {
@@ -544,7 +544,7 @@ var Lang = A.Lang,
 	A.NodeList.importMethod(
 		NODE_PROTO,
 		[
-			'renderTemplate'
+			'renderTpl'
 		]
 	);
 
--- src/aui-template/js/aui-template.js
+++ src/aui-template/js/aui-template.js
@@ -522,11 +522,11 @@ var Lang = A.Lang,
 
 	var NODE_PROTO = A.Node.prototype;
 
-	NODE_PROTO.toTemplate = function() {
+	NODE_PROTO.toTpl = function() {
 		return Template.from(this);
 	};
 
-	NODE_PROTO.renderTemplate = function(tpl, data) {
+	NODE_PROTO.renderTpl = function(tpl, data) {
 		var instance = this;
 
 		if (isString(tpl) || isArray(tpl)) {
@@ -543,7 +543,7 @@ var Lang = A.Lang,
 	A.NodeList.importMethod(
 		NODE_PROTO,
 		[
-			'renderTemplate'
+			'renderTpl'
 		]
 	);
 
