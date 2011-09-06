eae02c0eca4cc467cb8a7bcd0ef0a6b33e4e87dc
Bruno Basto <bruno.basto@liferay.com>
2011-09-06 14:37:00 -0300

    AUI-501 Diagram Builder - Automatically save recordset

--- src/aui-diagram-builder/js/aui-diagram-builder-impl.js
+++ src/aui-diagram-builder/js/aui-diagram-builder-impl.js
@@ -644,8 +644,6 @@ var DiagramBuilder = A.Component.create({
 					editingConnector.set(data.attributeName, data.value);
 				});
 			}
-
-			instance.closeEditProperties();
 		},
 
 		_renderGraphic: function() {
