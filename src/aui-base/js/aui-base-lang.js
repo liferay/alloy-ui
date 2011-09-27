ea4166a142dd853136b909680cc85641f8ca8dab
Nate Cavanaugh <nate@shift22.com>
2011-09-26 18:01:12 -0700

    AUI-514 - Split AUI Lang extensions into separate module

---
+++ src/aui-base/js/aui-base-lang.js
@@ -0,0 +1,345 @@
+var Lang = A.Lang,
+	isNumber = Lang.isNumber,
+	isUndefined = Lang.isUndefined,
+
+	LString = A.namespace('Lang.String'),
+
+	STR_BLANK = '',
+
+	DOC = A.config.doc,
+	INNER_HTML = 'innerHTML',
+	REGEX_DASH = /-([a-z])/gi,
+	REGEX_ESCAPE_REGEX = /([.*+?^$(){}|[\]\/\\])/g,
+	REGEX_NL2BR = /\r?\n/g,
+	REGEX_STRIP_SCRIPTS = /(?:<script.*?>)((\n|\r|.)*?)(?:<\/script>)/gi,
+	REGEX_STRIP_TAGS = /<\/?[^>]+>/gi,
+	REGEX_UNCAMELIZE = /([a-zA-Z][a-zA-Z])([A-Z])([a-z])/g,
+	REGEX_UNCAMELIZE_REPLACE_SEPARATOR = /([a-zA-Z][a-zA-Z])([A-Z])([a-z])/g,
+
+	STR_AMP = '&',
+	STR_CHEVRON_LEFT = '<',
+	STR_ELLIPSIS = '...',
+	STR_END = 'end',
+	STR_HASH = '#',
+	STR_MIDDLE = 'middle',
+	STR_START = 'start',
+	STR_ZERO = '0',
+
+	STR_G = 'g',
+	STR_S = 's',
+
+	mathBuffer = ['return value ', null, ';'],
+	htmlUnescapedValues = [],
+
+	cachedMathFn = A.cached(
+		function(mathArgs) {
+			mathBuffer[1] = mathArgs;
+
+			return new Function('value', mathBuffer.join(STR_BLANK));
+		}
+	),
+
+	MAP_HTML_CHARS_ESCAPED = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&#034;',
+		'\'': '&#039;',
+		'/': '&#047;',
+		'`': '&#096;'
+	},
+	MAP_HTML_CHARS_UNESCAPED = {};
+
+	for (var i in MAP_HTML_CHARS_ESCAPED) {
+		if (MAP_HTML_CHARS_ESCAPED.hasOwnProperty(i)) {
+			var escapedValue = MAP_HTML_CHARS_ESCAPED[i];
+
+			MAP_HTML_CHARS_UNESCAPED[escapedValue] = i;
+
+			htmlUnescapedValues.push(i);
+		}
+	}
+
+	var REGEX_HTML_ESCAPE = new RegExp('[' + htmlUnescapedValues.join(STR_BLANK) + ']', 'g'),
+		REGEX_HTML_UNESCAPE = /&([^;]+);/g;
+
+A.mix(
+	LString,
+	{
+		camelize: A.cached(
+			function(str, separator) {
+				var regex = REGEX_DASH;
+
+				str = String(str);
+
+				if (separator) {
+					regex = new RegExp(separator + '([a-z])', 'gi');
+				}
+
+				return str.replace(regex, LString._camelize);
+			}
+		),
+
+		capitalize: A.cached(
+			function(str) {
+				if (str) {
+					str = String(str);
+
+					str = str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+				}
+
+				return str;
+			}
+		),
+
+		contains: function(s, ss) {
+		  return s.indexOf(ss) != -1;
+		},
+
+		defaultValue: function(str, defaultValue) {
+			if (isUndefined(str) || str == STR_BLANK) {
+				if (isUndefined(defaultValue)) {
+					defaultValue = STR_BLANK;
+				}
+
+				str = defaultValue;
+			}
+
+			return str;
+		},
+
+		endsWith: function(str, suffix) {
+			var length = (str.length - suffix.length);
+
+			return ((length >= 0) && (str.indexOf(suffix, length) == length));
+		},
+
+		escapeHTML: function(str) {
+			return str.replace(REGEX_HTML_ESCAPE, LString._escapeHTML);
+		},
+
+		// Courtesy of: http://simonwillison.net/2006/Jan/20/escape/
+		escapeRegEx: function(str) {
+			return str.replace(REGEX_ESCAPE_REGEX, '\\$1');
+		},
+
+		math: function(value, mathArgs) {
+			return cachedMathFn(mathArgs)(value);
+		},
+
+		nl2br: function(str) {
+			var instance = this;
+
+			return String(str).replace(REGEX_NL2BR, '<br />');
+		},
+
+		padNumber: function(num, length, precision) {
+			var str = precision ? Number(num).toFixed(precision) : String(num);
+			var index = str.indexOf('.');
+
+			if (index == -1) {
+				index = str.length;
+			}
+
+			return LString.repeat(STR_ZERO, Math.max(0, length - index)) + str;
+		},
+
+		pluralize: function(count, singularVersion, pluralVersion) {
+			var suffix;
+
+			if (count == 1) {
+				suffix = singularVersion;
+			}
+			else {
+				suffix = pluralVersion || singularVersion + STR_S;
+			}
+
+			return count + ' ' + suffix;
+		},
+
+		remove: function(s, substitute, all) {
+			var re = new RegExp(LString.escapeRegEx(substitute), all ? STR_G : STR_BLANK);
+
+			return s.replace(re, STR_BLANK);
+		},
+
+		removeAll: function(s, substitute) {
+			return LString.remove(s, substitute, true);
+		},
+
+		repeat: function(string, length) {
+			return new Array(length + 1).join(string);
+		},
+
+		round: function(value, precision) {
+			value = Number(value);
+
+			if (isNumber(precision)) {
+				precision = Math.pow(10, precision);
+				value = Math.round(value * precision) / precision;
+			}
+
+			return value;
+		},
+
+		startsWith: function(str, prefix) {
+			return (str.lastIndexOf(prefix, 0) == 0);
+		},
+
+		stripScripts: function(str) {
+			if (str) {
+				str = String(str).replace(REGEX_STRIP_SCRIPTS, STR_BLANK);
+			}
+
+			return str;
+		},
+
+		stripTags: function(str) {
+			var instance = this;
+
+			if (str) {
+				str = String(str).replace(REGEX_STRIP_TAGS, STR_BLANK);
+			}
+
+			return str;
+		},
+
+		substr: function(str, start, length) {
+			return String(str).substr(start, length);
+		},
+
+		uncamelize: A.cached(
+			function(str, separator) {
+				separator = separator || ' ';
+
+				str = String(str);
+
+				str = str.replace(REGEX_UNCAMELIZE, '$1' + separator + '$2$3');
+				str = str.replace(REGEX_UNCAMELIZE_REPLACE_SEPARATOR, '$1' + separator + '$2');
+
+				return str;
+			}
+		),
+
+		toLowerCase: function(str) {
+			return String(str).toLowerCase();
+		},
+
+		toUpperCase: function(str) {
+			return String(str).toUpperCase();
+		},
+
+		trim: Lang.trim,
+
+		truncate: function(str, length, where) {
+			str = String(str);
+
+			var strLength = str.length;
+
+			if (str && strLength > length) {
+				where = where || STR_END;
+
+				if (where == STR_END) {
+					str = str.substr(0, length - STR_ELLIPSIS.length) + STR_ELLIPSIS;
+				}
+				else if (where == STR_MIDDLE) {
+					var middlePoint = Math.floor(length / 2);
+
+					str = str.substr(0, middlePoint) + STR_ELLIPSIS + str.substr(strLength - middlePoint);
+				}
+				else if (where == STR_START) {
+					str = STR_ELLIPSIS + str.substr(strLength - length);
+				}
+			}
+
+			return str;
+		},
+
+		undef: function(str) {
+			if (isUndefined(str)) {
+				str = STR_BLANK;
+			}
+
+			return str;
+		},
+
+		// inspired from Google unescape entities
+		unescapeEntities: function(str) {
+			if (LString.contains(str, STR_AMP)) {
+				if (DOC && !LString.contains(str, STR_CHEVRON_LEFT)) {
+					str = LString._unescapeEntitiesUsingDom(str);
+				}
+				else {
+					str = LString.unescapeHTML(str);
+				}
+			}
+
+			return str;
+		},
+
+		unescapeHTML: function(str) {
+			return str.replace(REGEX_HTML_UNESCAPE, LString._unescapeHTML);
+		},
+
+		_camelize: function(match0, match1) {
+			return match1.toUpperCase();
+		},
+
+		_escapeHTML: function(match) {
+			return MAP_HTML_CHARS_ESCAPED[match];
+		},
+
+		_unescapeHTML: function(match) {
+			var value = MAP_HTML_CHARS_UNESCAPED[match];
+
+			if (!value && entity.charAt(0) == STR_HASH) {
+				var charCode = Number(STR_ZERO + entity.substr(1));
+
+				if (!isNaN(charCode)) {
+					value = String.fromCharCode(charCode);
+				}
+			}
+
+			return value;
+		},
+
+		_unescapeEntitiesUsingDom: function(str) {
+			var el = LString._unescapeNode;
+
+			el[INNER_HTML] = str;
+
+			if (el[NORMALIZE]) {
+				el[NORMALIZE]();
+			}
+
+			str = el.firstChild.nodeValue;
+
+			el[INNER_HTML] = STR_BLANK;
+
+			return str;
+		},
+
+		_unescapeNode: DOC.createElement('a')
+	}
+);
+
+A.mix(
+	Lang,
+	{
+		emptyFn: function() {},
+
+		emptyFnFalse: function() {
+			return false;
+		},
+
+		emptyFnTrue: function() {
+			return true;
+		},
+
+		isGuid: function(id) {
+			var instance = this;
+
+			return String(id).indexOf(A.Env._guidp) === 0;
+		}
+	}
+);
