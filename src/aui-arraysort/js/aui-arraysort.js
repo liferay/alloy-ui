ce40f38f1fceab8c297cfb4d4071bcb8e9b05f3c
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-10-20 14:57:13 -0300

    AUI-536 Adding aui-arraysort module

---
+++ src/aui-arraysort/js/aui-arraysort.js
@@ -0,0 +1,25 @@
+ASort = A.ArraySort;
+
+A.mix(
+	ASort,
+	{
+		compareIgnoreWhiteSpace: function(a, b, desc) {
+			var sort;
+
+			if ((a === '') && (b === '')) {
+				sort = 0;
+			}
+			else if (a === '') {
+				sort = 1;
+			}
+			else if (b === '') {
+				sort = -1;
+			}
+			else {
+				sort = ASort.compare(a, b, desc);
+			}
+
+			return sort;
+		}
+	}
+);
