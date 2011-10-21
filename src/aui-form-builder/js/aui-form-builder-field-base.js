8d578bd75ded0bb70925ba84bd23f800d2d65be5
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-10-20 21:28:21 -0300

    AUI-537 Rollback; AUI-538 FormBuilder Add allowRemoveRequiredFields attribute

--- src/aui-form-builder/js/aui-form-builder-field-base.js
+++ src/aui-form-builder/js/aui-form-builder-field-base.js
@@ -6,6 +6,7 @@ var L = A.Lang,
 	AArray = A.Array,
 
 	ACCEPT_CHILDREN = 'acceptChildren',
+	ALLOW_REMOVE_REQUIRED_FIELDS = 'allowRemoveRequiredFields',
 	AVAILABLE_FIELD_ID = 'availableFieldId',
 	BODY_CONTENT = 'bodyContent',
 	BOOLEAN = 'boolean',
@@ -75,7 +76,6 @@ var L = A.Lang,
 	PORTAL_LAYOUT = 'portalLayout',
 	PREDEFINED_VALUE = 'predefinedValue',
 	PROXY = 'proxy',
-	READ_ONLY = 'readOnly',
 	READ_ONLY_ATTRIBUTES = 'readOnlyAttributes',
 	RENDERED = 'rendered',
 	REQUIRED = 'required',
@@ -200,11 +200,6 @@ var FormBuilderField = A.Component.create({
 			value: EMPTY_STR
 		},
 
-		readOnly: {
-			validator: isBoolean,
-			value: false
-		},
-
 		readOnlyAttributes: {
 			validator: isArray,
 			value: []
@@ -240,7 +235,6 @@ var FormBuilderField = A.Component.create({
 				no: 'No',
 				options: 'Options',
 				predefinedValue: 'Predefined Value',
-				readOnly: 'Read Only',
 				required: 'Required',
 				reset: 'Reset',
 				showLabel: 'Show Label',
@@ -327,7 +321,7 @@ var FormBuilderField = A.Component.create({
 	},
 
 	buildFieldName: function(type) {
-		return type + (++A.Env._uidx);	
+		return type + (++A.Env._uidx);
 	},
 
 	HTML_PARSER: {
@@ -475,17 +469,6 @@ var FormBuilderField = A.Component.create({
 					name: strings[SHOW_LABEL]
 				},
 				{
-					attributeName: READ_ONLY,
-					editor: new A.RadioCellEditor({
-						options: {
-							'true': strings[YES],
-							'false': strings[NO]
-						}
-					}),
-					formatter: A.bind(instance._booleanFormatter, instance),
-					name: strings[READ_ONLY]
-				},
-				{
 					attributeName: REQUIRED,
 					editor: new A.RadioCellEditor({
 						options: {
@@ -604,12 +587,10 @@ var FormBuilderField = A.Component.create({
 		_handleDeleteEvent: function(event) {
 			var instance = this;
 
-			if (!instance.get(REQUIRED)) {
-				var strings = instance.getStrings();
+			var strings = instance.getStrings();
 
-				if (confirm(strings[DELETE_FIELDS_MESSAGE])) {
-					instance.destroy();
-				}
+			if (confirm(strings[DELETE_FIELDS_MESSAGE])) {
+				instance.destroy();
 			}
 		},
 
@@ -643,12 +624,12 @@ var FormBuilderField = A.Component.create({
 
 		_uiSetRequired: function(val) {
 			var instance = this;
-			var requiredFlagNode = instance.get(REQUIRED_FLAG_NODE);
+			var builder = instance.get(BUILDER);
 			var controlsToolbar = instance.controlsToolbar;
 			var strings = instance.getStrings();
 
 			if (controlsToolbar) {
-				if (val) {
+				if (val && !builder.get(ALLOW_REMOVE_REQUIRED_FIELDS)) {
 					controlsToolbar.remove(DELETE_EVENT);
 				}
 				else {
@@ -661,7 +642,7 @@ var FormBuilderField = A.Component.create({
 				}
 			}
 
-			requiredFlagNode.toggleClass(CSS_HELPER_HIDDEN, !val);
+			instance.get(REQUIRED_FLAG_NODE).toggleClass(CSS_HELPER_HIDDEN, !val);
 		},
 
 		_uiSetSelected: function(val) {
