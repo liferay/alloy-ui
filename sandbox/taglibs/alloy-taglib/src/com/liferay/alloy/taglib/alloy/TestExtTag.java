12feb57d74bcd4dc70598fbfe6bb34bfc700e8d0
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-11-28 16:11:14 -0800

    AUI-565 Missing file

---
+++ sandbox/taglibs/alloy-taglib/src/com/liferay/alloy/taglib/alloy/TestExtTag.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.alloy.taglib.alloy;
+
+import com.liferay.alloy.taglib.alloy.base.BaseTestExtTag;
+
+/**
+ * @author Eduardo Lundgren
+ * @author Bruno Basto
+ * @author Nathan Cavanaugh
+ */
+public class TestExtTag extends BaseTestExtTag {
+}
