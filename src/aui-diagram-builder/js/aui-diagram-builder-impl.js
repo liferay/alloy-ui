790076a80917cba345c69ad416b135e179292453
bruno.basto <bruno.basto@liferay.com>
2011-12-27 21:20:27 -0300

    AUI-588 Diagram Builder - User Interface allows user to drag connections out of visible area

--- src/aui-diagram-builder/js/aui-diagram-builder-impl.js
+++ src/aui-diagram-builder/js/aui-diagram-builder-impl.js
@@ -69,6 +69,7 @@ var Lang = A.Lang,
 	RADIUS = 'radius',
 	RECORDS = 'records',
 	RECORDSET = 'recordset',
+	REGION = 'region',
 	RENDERED = 'rendered',
 	REQUIRED = 'required',
 	SELECTED = 'selected',
@@ -1181,6 +1182,8 @@ var DiagramNode = A.Component.create({
 			var builder = instance.get(BUILDER);
 			var mouseXY = event.mouseXY;
 
+			instance._constrainMouseXY(mouseXY, instance._canvasRegion);
+
 			builder.connector.set(P2, mouseXY);
 
 			if (builder.publishedTarget) {
@@ -1228,6 +1231,9 @@ var DiagramNode = A.Component.create({
 		connectStart: function(event) {
 			var instance = this;
 			var builder = instance.get(BUILDER);
+			var canvas = builder.get(CANVAS);
+
+			instance._canvasRegion = canvas.get(REGION);
 
 			builder.connector.show().set(P1, event.startXY);
 
@@ -1428,6 +1434,26 @@ var DiagramNode = A.Component.create({
 				mouseenter: A.bind(instance._onBoundaryMouseEnter, instance),
 				mouseleave: A.bind(instance._onBoundaryMouseLeave, instance)
 			});
+		},
+
+		_constrainMouseXY: function(mouseXY, region) {
+			var instance = this;
+			
+			if (mouseXY[0] <= region.left) {
+				mouseXY[0] = region.left;
+			}
+
+			if (mouseXY[0] >= region.right) {
+				mouseXY[0] = region.right;
+			}
+
+			if (mouseXY[1] >= region.bottom) {
+				mouseXY[1] = region.bottom;
+			}
+
+			if (mouseXY[1] <= region.top) {
+				mouseXY[1] = region.top;
+			}
 		},
 
 		_createDataSet: function() {
