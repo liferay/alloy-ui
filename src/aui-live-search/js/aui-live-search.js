76871bed7b807c89c334dfbf37522aab7ed1dd03
Iliyan Peychev <iliyan.peychev@liferay.com>
2012-01-17 12:02:28 +0100

    AUI-598 - LiveSearch fires search event twice

--- src/aui-live-search/js/aui-live-search.js
+++ src/aui-live-search/js/aui-live-search.js
@@ -381,8 +381,6 @@ var LiveSearch = A.Component.create(
 			_afterSearchValueChange: function(event) {
 				var instance = this;
 
-				instance.fire('search');
-
 				if (event.SRC == UI_SRC) {
 					instance.get(INPUT).val(event.newVal);
 				}
