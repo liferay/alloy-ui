a6baaf81f139ba948c9faab3d86640e70434e21f
Jonathan Mak <jonathan.mak@liferay.com>
2012-02-02 12:43:38 -0800

    AUI-610 - Check if options are passed into "_updateIndexNext" and "_updateIndexPrev" method in Carousel

--- src/aui-carousel/js/aui-carousel.js
+++ src/aui-carousel/js/aui-carousel.js
@@ -141,6 +141,8 @@ var Carousel = A.Component.create(
 			},
 
 			next: function() {
+				var instance = this;
+
 				instance._updateIndexNext();
 			},
 
@@ -452,7 +454,9 @@ var Carousel = A.Component.create(
 					newIndex = 0;
 				}
 
-				options.src = UI_SRC;
+				if (options) {
+					options.src = UI_SRC;
+				}
 
 				instance.set('activeIndex', newIndex, options);
 			},
@@ -468,7 +472,9 @@ var Carousel = A.Component.create(
 					newIndex = instance.nodeSelection.size() - 1;
 				}
 
-				options.src = UI_SRC;
+				if (options) {
+					options.src = UI_SRC;
+				}
 
 				instance.set('activeIndex', newIndex, options);
 			},
