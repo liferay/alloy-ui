c7d5ba732f5afbe74c0390f2eab703ac0a314aa1
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2012-03-13 15:58:59 -0700

    AUI-626 - Build files

--- build/aui-node/aui-node-base-debug.js
+++ build/aui-node/aui-node-base-debug.js
@@ -1623,4 +1623,4 @@ AArray.each(
 	}
 );
 
-}, '@VERSION@' ,{requires:['aui-base-lang','aui-classnamemanager','node']});
+}, '@VERSION@' ,{requires:['array-extras','aui-base-lang','aui-classnamemanager','node']});
