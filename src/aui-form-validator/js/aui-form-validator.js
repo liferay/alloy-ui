b8afb6ed0801d2e58d89d4cdfc48ce90703dc2e2
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2012-06-07 16:46:47 -0700

    AUI-669 - Reimplementing fix (other regex isn't working)

--- src/aui-form-validator/js/aui-form-validator.js
+++ src/aui-form-validator/js/aui-form-validator.js
@@ -796,26 +796,38 @@ var FormValidator = A.Component.create({
 				var formEl = form.getDOM();
 				var inputs = formEl.elements;
 
+				var ruleNameMatch = [];
+
+				var ruleMatcher = function(m1, m2) {
+					ruleNameMatch.push(m2);
+				};
+
 				for (var i = 0, length = inputs.length; i < length; i++) {
 					var el = inputs[i];
 
 					var className = el.className;
 					var fieldName = el.name;
 
-					var ruleNameMatch = className.match(regex);
+					className.replace(regex, ruleMatcher);
 
 					if (ruleNameMatch) {
-						if (!rules[fieldName]) {
-							rules[fieldName] = {};
+						var fieldRules = rules[fieldName];
+
+						if (!fieldRules) {
+							fieldRules = {};
+
+							rules[fieldName] = fieldRules;
 						}
 
 						for (var j = 0, ruleNameLength = ruleNameMatch.length; j < ruleNameLength; j ++) {
 							var rule = ruleNameMatch[j];
 
-							if (!(rules[fieldName][rule] in ruleNameMatch)) {
-								rules[fieldName][rule] = true;
+							if (!(rule in fieldRules)) {
+								fieldRules[rule] = true;
 							}
 						}
+
+						ruleNameMatch.length = 0;
 					}
 				}
 			}
