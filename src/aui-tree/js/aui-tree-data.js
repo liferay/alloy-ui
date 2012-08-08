c9f227367c44d85601280ee580f3567c8722012c
Bruno Basto <bruno.basto@liferay.com>
2012-08-08 19:27:12 -0300

    AUI-683 Leaf attribute was being ignored

--- src/aui-tree/js/aui-tree-data.js
+++ src/aui-tree/js/aui-tree-data.js
@@ -790,6 +790,10 @@ A.mix(TreeData.prototype, {
 
 		instance.updateIndex({});
 
+		if (v.length > 0) {
+			instance.set(LEAF, false);
+		}
+
 		A.Array.each(v, function(node, index) {
 			if (node) {
 				if (!isTreeNode(node) && isObject(node)) {
@@ -798,7 +802,6 @@ A.mix(TreeData.prototype, {
 					var children = node[CHILDREN];
 					var hasChildren = children && children.length;
 
-					node[LEAF] = !hasChildren;
 					node[OWNER_TREE] = ownerTree;
 					node[PARENT_NODE] = instance;
 
