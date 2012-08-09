11b69d728791a2567a162a7551f23276a1ef9536
Djalma Araujo <djalma.araujo@gmail.com>
2012-08-09 18:40:49 -0300

    AUI-693 - Regenerate

---
+++ build/aui-drawing-safari/aui-drawing-safari-debug.js
@@ -0,0 +1,19 @@
+AUI.add('aui-drawing-safari', function(A) {
+A.Drawing.prototype.safari = function() {
+	var instance = this;
+
+	var rect = instance.rect(-99, -99, instance.get('width') + 99, instance.get('height') + 99).attr(
+		{
+			stroke: 'none'
+		}
+	);
+
+	setTimeout(
+		function() {
+			rect.remove();
+		},
+		0
+	);
+};
+
+}, '@VERSION@' ,{condition: {name: 'aui-drawing-safari', trigger: 'aui-drawing-base',test: function(A){var UA = A.UA; return UA.safari && (UA.version.major < 4 || (UA.iphone || UA.ipad));}}, requires:['aui-drawing-base']});
