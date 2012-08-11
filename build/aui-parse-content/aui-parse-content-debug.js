226e569e17460b7a2582d7012d610057eb71f618
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2012-08-11 14:38:32 -0300

    AUI-675 - Build files

--- build/aui-parse-content/aui-parse-content-debug.js
+++ build/aui-parse-content/aui-parse-content-debug.js
@@ -1,9 +1,9 @@
 AUI.add('aui-parse-content', function(A) {
 /**
- * The ParseContent Utility - Parse the content of a Node so that all of the 
+ * The ParseContent Utility - Parse the content of a Node so that all of the
  * javascript contained in that Node will be executed according to the order
  * that it appears.
- * 
+ *
  * @module aui-parse-content
  */
 
@@ -24,7 +24,12 @@ var L = A.Lang,
 	PARSE_CONTENT = 'ParseContent',
 	QUEUE = 'queue',
 	SCRIPT = 'script',
-	SRC = 'src';
+	SRC = 'src',
+
+	SCRIPT_TYPES = {
+		'': 1,
+		'text/javascript': 1
+	};
 
 /**
  * A base class for ParseContent, providing:
@@ -209,7 +214,11 @@ var ParseContent = A.Component.create(
 					fragment.append(content);
 				}
 
-				output.js = fragment.all(SCRIPT).each(
+				output.js = fragment.all(SCRIPT).filter(function(script) {
+					return SCRIPT_TYPES[script.getAttribute('type').toLowerCase()];
+				});
+
+				output.js.each(
 					function(node, i) {
 						node.remove();
 					}
