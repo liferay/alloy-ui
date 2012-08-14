8db76d4ec57433c81558466986c555901caa371c
Djalma Araujo <djalma.araujo@gmail.com>
2012-08-14 17:44:03 -0300

    AUI-694 - Regenerate

--- build/aui-datatable/aui-datatable.js
+++ build/aui-datatable/aui-datatable.js
@@ -301,6 +301,7 @@ var BaseCellEditor = A.Component.create({
 
 		inputFormatter: {
 			value: function(val) {
+
 				if (isString(val)) {
 					val = val.replace(REGEX_NL, TPL_BR);
 				}
@@ -1329,6 +1330,37 @@ var DateCellEditor = A.Component.create({
 			setter: '_setCalendar',
 			validator: isObject,
 			value: null
+		},
+
+		dateFormat: {
+			value: '%D',
+			validator: isString
+		},
+
+		inputFormatter: {
+			value: function(val) {
+				var instance = this,
+					values = [];
+
+				AArray.each(val, function (date, index) {
+					values.push(instance.formatDate(date).toString());
+				});
+
+				return values;
+			}
+		},
+
+		outputFormatter: {
+			value: function(val) {
+				var instance = this,
+					values = [];
+
+				AArray.each(val, function (date, index) {
+					values.push(DataType.Date.parse(date));
+				});
+
+				return values;
+			}
 		}
 	},
 
@@ -1338,13 +1370,28 @@ var DateCellEditor = A.Component.create({
 		initializer: function() {
 			var instance = this;
 
-			instance.on('calendar:dateClick', A.bind(instance._onDateSelect, instance));
+			instance.after('calendar:dateClick', A.bind(instance._afterDateSelect, instance));
 		},
 
 		getElementsValue: function() {
 			var instance = this;
-			var elements = instance.calendar.get('selectedDates').join(_COMMA);
-			return elements;
+
+			return instance.calendar.get('selectedDates');
+		},
+
+		formatDate: function (date) {
+			var instance = this,
+				mask = instance.get('dateFormat'),
+				locale = instance.get('locale');
+
+			return DataType.Date.format(date, { format: mask });
+		},
+
+		_afterDateSelect: function(event) {
+			var instance = this,
+				selectedDates = instance.calendar.get('selectedDates');
+
+			instance.elements.val(AArray.invoke(selectedDates, 'getTime').join(_COMMA));
 		},
 
 		_afterRender: function() {
@@ -1356,12 +1403,6 @@ var DateCellEditor = A.Component.create({
 				instance.get(CALENDAR)
 			)
 			.render(instance.bodyNode);
-		},
-
-		_onDateSelect: function(event) {
-			var instance = this;
-
-			instance.elements.val(event.date);
 		},
 
 		_setCalendar: function(val) {
@@ -1377,20 +1418,20 @@ var DateCellEditor = A.Component.create({
 
 		_uiSetValue: function(val) {
 			var instance = this,
-				calendar = instance.calendar;
+				calendar = instance.calendar,
+				formatedValue;
 
 			if (calendar) {
-				if (!isArray(val) && isString(val)) {
+
+				if (!isArray(val)) {
 					val = [val];
 				}
 
-				AArray.each(val, function (date, index) {
-					val[index] = new Date(date);
-				});
+				formatedValue = instance.formatValue(instance.get(OUTPUT_FORMATTER), val)
 
-				instance.calendar._clearSelection(); // Forced to call because it's private method
-				instance.calendar.selectDates(val);
-				instance.calendar.set('date', val[0]);
+				calendar._clearSelection(); // Should be a public method
+				calendar.set('date', formatedValue[0]);
+				calendar.selectDates(formatedValue);
 			}
 		}
 	}
@@ -1398,7 +1439,7 @@ var DateCellEditor = A.Component.create({
 
 A.DateCellEditor = DateCellEditor;
 
-}, '@VERSION@' ,{skinnable:true, requires:['datatable-base','calendar','datatype-date','aui-toolbar','aui-form-validator','overlay','sortable']});
+}, '@VERSION@' ,{skinnable:true, requires:['datatable-base','calendar','aui-datatype','aui-toolbar','aui-form-validator','overlay','sortable']});
 AUI.add('aui-datatable-selection', function(A) {
 var Lang = A.Lang,
 	isArray = Lang.isArray,
