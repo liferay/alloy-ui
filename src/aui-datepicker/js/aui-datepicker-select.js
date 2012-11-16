b5d2c37a4061fa89e848dc474d53c3d52c6ca9f9
Shinn Lok <shinn.lok@liferay.com>
2012-11-16 12:10:04 -0800

    AUI-771 - Current dates aren't updated on select, use selectedDates

--- src/aui-datepicker/js/aui-datepicker-select.js
+++ src/aui-datepicker/js/aui-datepicker-select.js
@@ -38,9 +38,6 @@ var Lang = A.Lang,
 	CLEARFIX = 'clearfix',
 	CONTENT_BOX = 'contentBox',
 	CONTENT = 'content',
-	CURRENT_DAY = 'currentDay',
-	CURRENT_MONTH = 'currentMonth',
-	CURRENT_YEAR = 'currentYear',
 	DATA_COMPONENT_ID = 'data-auiComponentID',
 	DATEPICKER = 'datepicker',
 	DAY = 'day',
@@ -556,18 +553,20 @@ var DatePickerSelect = A.Component.create(
 			 * @return {Object}
 			 */
 			_normalizeYearMonth: function(year, month, day) {
-				var instance = this;
+				var instance = this,
+					selectedDates = instance.calendar.get('selectedDates'),
+					date = selectedDates.length ? selectedDates[0] : new Date();
 
 				if (!isValue(day)) {
-					day = instance.get(CURRENT_DAY);
+					day = date.getDate();
 				}
 
 				if (!isValue(month)) {
-					month = instance.get(CURRENT_MONTH);
+					month = date.getMonth();
 				}
 
 				if (!isValue(year)) {
-					year = instance.get(CURRENT_YEAR);
+					year = date.getFullYear();
 				}
 
 				return { year: year, month: month, day: day };
