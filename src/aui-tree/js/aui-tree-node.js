438ffdc61967cf87c0827d66ac10a65d20c32472
Jonathan Mak <jonathan.mak@liferay.com>
2012-11-27 16:31:38 -0800

    AUI-757 - Source Formatting

--- src/aui-tree/js/aui-tree-node.js
+++ src/aui-tree/js/aui-tree-node.js
@@ -1166,6 +1166,8 @@ var TreeNodeIO = A.Component.create(
 				var instance = this;
 
 				var io = instance.get(IO);
+				var ownerTree = instance.get(OWNER_TREE);
+
 				var args = Array.prototype.slice.call(arguments);
 				var length = args.length;
 
@@ -1191,8 +1193,6 @@ var TreeNodeIO = A.Component.create(
 				instance.createNodes(nodes);
 
 				instance.expand();
-
-				var ownerTree = instance.get(OWNER_TREE);
 
 				if (ownerTree && ownerTree.ddDelegate) {
 					ownerTree.ddDelegate.syncTargets();
