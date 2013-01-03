9aa3c5703448d9d84df0eb8d1feac35961160ba4
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-01-03 18:05:44 +0100

    AUI-812 Tree paginator separated
    Conflicts:
    
    	demos/ace-editor/index.html
    	src/aui-tree/assets/aui-tree-view-core.css
    	src/aui-tree/js/aui-tree-node.js

---
+++ src/aui-tree/js/aui-tree-paginator.js
@@ -0,0 +1,217 @@
+var Lang = A.Lang,
+	isObject = Lang.isObject,
+	isValue = Lang.isValue,
+
+	getCN = A.ClassNameManager.getClassName,
+
+	CHILDREN = 'children',
+	CONTAINER = 'container',
+	END = 'end',
+	IO = 'io',
+	LIMIT = 'limit',
+	MORE_RESULTS_LABEL = 'Load more results',
+	NODE = 'node',
+	OWNER_TREE = 'ownerTree',
+	PAGINATOR = 'paginator',
+	START = 'start',
+	TREE = 'tree',
+	TREE_NODE_IO = 'tree-node-io',
+
+	EV_TREE_NODE_PAGINATOR_CLICK = 'paginatorClick',
+
+	CSS_TREE_NODE_PAGINATOR = getCN(TREE, NODE, PAGINATOR),
+
+	TPL_PAGINATOR = '<a class="' + CSS_TREE_NODE_PAGINATOR + '" href="javascript:void(0);">{moreResultsLabel}</a>';
+
+function TreeViewPaginator(config) {
+	var instance = this;
+
+	A.after(instance._bindPaginatorUI, this, 'bindUI');
+
+	A.after(instance._syncPaginatorUI, this, 'syncUI');
+}
+
+TreeViewPaginator.ATTRS = {
+	paginator: {
+		setter: function(value) {
+			var instance = this;
+
+			var paginatorNode = A.Node.create(
+				Lang.sub(
+					TPL_PAGINATOR,
+					{
+						moreResultsLabel: (value && value.moreResultsLabel) || MORE_RESULTS_LABEL
+					}
+				)
+			);
+
+			return A.merge(
+				{
+					alwaysVisible: false,
+					autoFocus: true,
+					element: paginatorNode,
+					endParam: END,
+					limitParam: LIMIT,
+					start: 0,
+					startParam: START
+				},
+				value
+			);
+		},
+		validator: isObject
+	}
+};
+
+
+TreeViewPaginator.prototype = {
+	/**
+	 * Bind events to the paginator "show more" link.
+	 *
+	 * @method _bindPaginatorUI
+	 * @protected
+	 */
+	_bindPaginatorUI: function() {
+		var instance = this;
+
+		var paginator = instance.get(PAGINATOR);
+
+		if (paginator) {
+			var handlePaginator = A.bind(instance._handlePaginatorClickEvent, instance);
+
+			paginator.element.on('click', handlePaginator);
+		}
+
+		instance._createEvents();
+	},
+
+	/**
+	 * Create custom events.
+	 *
+	 * @method _createEvents
+	 * @private
+	 */
+	_createEvents: function() {
+		var instance = this;
+
+		instance.publish(
+			EV_TREE_NODE_PAGINATOR_CLICK,
+			{
+				defaultFn: instance._defPaginatorClickFn,
+				prefix: TREE_NODE_IO
+			}
+		);
+	},
+	
+	/**
+	 * Default paginatorClick event handler. Increment the
+	 * <code>paginator.start</code> to the next <code>paginator.limit</code>.
+	 *
+	 * @method _defPaginatorClickFn
+	 * @param {EventFacade} event The Event object
+	 * @protected
+	 */
+	_defPaginatorClickFn: function(event) {
+		var instance = this;
+
+		var paginator = instance.get(PAGINATOR);
+
+		if (isValue(paginator.limit)) {
+			paginator.start += paginator.limit;
+		}
+
+		if (instance.get(IO)) {
+			instance.initIO();
+		}
+	},
+
+	/**
+	 * Fires the paginatorClick event.
+	 *
+	 * @method _handlePaginatorClickEvent
+	 * @param {EventFacade} event paginatorClick event facade
+	 * @protected
+	 */
+	_handlePaginatorClickEvent: function(event) {
+		var instance = this;
+
+		var ownerTree = instance.get(OWNER_TREE);
+		var output = instance.getEventOutputMap(instance);
+
+		instance.fire(EV_TREE_NODE_PAGINATOR_CLICK, output);
+
+		if (ownerTree) {
+			ownerTree.fire(EV_TREE_NODE_PAGINATOR_CLICK, output);
+		}
+
+		event.halt();
+	},
+
+	/**
+	 * Adds two extra IO data parameter to the request to handle the
+	 * paginator. By default these parameters are <code>limit</code> and
+	 * <code>start</code>.
+	 *
+	 * @method _syncPaginatorIOData
+	 * @protected
+	 */
+	_syncPaginatorIOData: function(io) {
+		var instance = this;
+
+		var paginator = instance.get(PAGINATOR);
+
+		if (paginator && isValue(paginator.limit)) {
+			var data = io.cfg.data || {};
+
+			data[ paginator.limitParam ] = paginator.limit;
+			data[ paginator.startParam ] = paginator.start;
+			data[ paginator.endParam ] = (paginator.start + paginator.limit);
+
+			io.cfg.data = data;
+		}
+	},
+
+	/**
+	 * Sync the paginator link UI.
+	 *
+	 * @method _syncPaginatorUI
+	 * @protected
+	 */
+	_syncPaginatorUI: function(newNodes) {
+		var instance = this;
+
+		var children = instance.get(CHILDREN);
+		var paginator = instance.get(PAGINATOR);
+
+		if (paginator) {
+			var hasMoreData = true;
+
+			if (newNodes) {
+				hasMoreData = (newNodes.length > 0);
+			}
+
+			var childrenLength = instance.getChildrenLength();
+			var start = paginator.start;
+			var total = paginator.total || childrenLength;
+
+			var showPaginator = hasMoreData && (total > childrenLength);
+
+			if (paginator.alwaysVisible || showPaginator) {
+				instance.get(CONTAINER).append(
+					paginator.element.show()
+				);
+
+				if (paginator.autoFocus) {
+					try {
+						paginator.element.focus();
+					}
+					catch(e) {}
+				}
+			}
+			else {
+				paginator.element.hide();
+			}
+		}
+	}
+};
+
+A.TreeViewPaginator = TreeViewPaginator;
--- src/aui-tree/js/aui-tree-view.js
+++ src/aui-tree/js/aui-tree-view.js
@@ -154,7 +154,7 @@ var TreeView = A.Component.create(
 			}
 		},
 
-		AUGMENTS: [A.TreeData],
+		AUGMENTS: [A.TreeData, A.TreeViewPaginator, A.TreeViewIO],
 
 		prototype: {
 			CONTENT_TEMPLATE: '<ul></ul>',
@@ -178,6 +178,18 @@ var TreeView = A.Component.create(
 				var instance = this;
 
 				instance._delegateDOM();
+			},
+
+			createNodes: function(nodes) {
+				var instance = this;
+
+				A.Array.each(A.Array(nodes), function(node) {
+					var newNode = instance.createNode(node);
+
+					instance.appendChild(newNode);
+				});
+
+				instance._syncPaginatorUI(nodes);
 			},
 
 			/**
@@ -282,6 +294,7 @@ var TreeView = A.Component.create(
 			 */
 			_delegateDOM: function() {
 				var instance = this;
+
 				var boundingBox = instance.get(BOUNDING_BOX);
 
 				// expand/collapse delegations
@@ -302,6 +315,7 @@ var TreeView = A.Component.create(
 			 */
 			_onClickNodeEl: function(event) {
 				var instance = this;
+
 				var treeNode = instance.getNodeByChild( event.currentTarget );
 
 				if (treeNode) {
