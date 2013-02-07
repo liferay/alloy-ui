91b59ea648fc13b6497106b1faecfd0e1886f151
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2013-02-07 20:50:18 -0300

    Adding aui-scheduler module

---
+++ src/aui-scheduler/js/aui-scheduler-base.js
@@ -0,0 +1,622 @@
+A.SchedulerEvents = A.Base.create('scheduler-events', A.ModelList, [], {
+	comparator: function(model) {
+		var startDateTime = model.get(START_DATE),
+			endDateTime = model.get(END_DATE);
+
+		return startDateTime + 1/(endDateTime - startDateTime);
+	},
+	model: A.SchedulerEvent
+}, {
+	ATTRS: {
+		scheduler: {}
+	}
+});
+
+var SchedulerEventSupport = function() {};
+
+SchedulerEventSupport.ATTRS = {};
+
+A.mix(SchedulerEventSupport.prototype, {
+	calendarModel: A.SchedulerCalendar,
+
+	eventModel: A.SchedulerEvent,
+
+	eventsModel: A.SchedulerEvents,
+
+	initializer: function(config) {
+		var instance = this;
+
+		instance._events = new instance.eventsModel({
+			after: {
+				add: A.bind(instance._afterAddEvent, instance)
+			},
+			bubbleTargets: instance,
+			scheduler: instance
+		});
+
+		instance.addEvents(config.items || config.events);
+	},
+
+	addEvents: function(models) {
+		var instance = this,
+			events = instance._toSchedulerEvents(models);
+
+		return instance._events.add(events);
+	},
+
+	eachEvent: function(fn) {
+		var instance = this;
+
+		return instance._events.each(fn);
+	},
+
+	flushEvents: function() {
+		var instance = this;
+
+		instance._events.each(function(evt) {
+			delete evt._filtered;
+		});
+	},
+
+	getEventByClientId: function(clientId) {
+		var instance = this;
+
+		return instance._events.getByClientId(clientId);
+	},
+
+	getEvents: function(filterFn) {
+		var instance = this,
+			events = instance._events;
+
+		// TODO: Check why the items are not being sorted on add
+		events.sort({ silent: true });
+
+		if (filterFn) {
+			events = events.filter(filterFn);
+		}
+		else {
+			events = events.toArray();
+		}
+
+		return events;
+	},
+
+	getEventsByDay: function(date, includeOverlap) {
+		var instance = this;
+
+		date = DateMath.safeClearTime(date);
+
+		return instance.getEvents(function(evt) {
+			return DateMath.compare(evt.getClearStartDate(), date) ||
+					(includeOverlap && DateMath.compare(evt.getClearEndDate(), date));
+		});
+	},
+
+	getIntersectEvents: function(date) {
+		var instance = this;
+
+		date = DateMath.safeClearTime(date);
+
+		return instance.getEvents(function(evt) {
+			var startDate = evt.getClearStartDate();
+			var endDate = evt.getClearEndDate();
+
+			return (evt.get(VISIBLE) &&
+					(DateMath.compare(date, startDate) ||
+					DateMath.compare(date, endDate) ||
+					DateMath.between(date, startDate, endDate)));
+		});
+	},
+
+	removeEvents: function(models) {
+		var instance = this,
+			events = instance._toSchedulerEvents(models);
+
+		return instance._events.remove(events);
+	},
+
+	resetEvents: function(models) {
+		var instance = this,
+			events = instance._toSchedulerEvents(models);
+
+		return instance._events.reset(events);
+	},
+
+	_afterAddEvent: function(event) {
+		var instance = this;
+
+		event.model.set(SCHEDULER, instance);
+	},
+
+	_toSchedulerEvents: function(values) {
+		var instance = this,
+			events = [];
+
+		if (isModelList(values)) {
+			events = values.toArray();
+			values.set(SCHEDULER, instance);
+		}
+		else if (isArray(values)) {
+			A.Array.each(values, function(value) {
+				if (isModelList(value)) {
+					events = events.concat(value.toArray());
+					value.set(SCHEDULER, instance);
+				}
+				else {
+					events.push(value);
+				}
+			});
+		}
+		else {
+			events = values;
+		}
+
+		return events;
+	}
+});
+
+A.SchedulerEventSupport = SchedulerEventSupport;
+
+var SchedulerBase = A.Component.create({
+	NAME: SCHEDULER_BASE,
+
+	ATTRS: {
+		activeView: {
+			validator: isSchedulerView
+		},
+
+		date: {
+			value: new Date(),
+			validator: isDate
+		},
+
+		eventRecorder: {
+			setter: '_setEventRecorder'
+		},
+
+		strings: {
+			value: {
+				agenda: 'Agenda',
+				day: 'Day',
+				month: 'Month',
+				today: 'Today',
+				week: 'Week',
+				year: 'Year'
+			}
+		},
+
+		/**
+		 * The function to format the navigation header date.
+		 *
+		 * @attribute navigationDateFormatter
+		 * @default %A - %d %b %Y
+		 * @type Function
+		 */
+		navigationDateFormatter: {
+			value: function(date) {
+				var instance = this;
+
+				return A.DataType.Date.format(
+					date,
+					{
+						format: '%B %d, %Y',
+						locale: instance.get(LOCALE)
+					}
+				);
+			},
+			validator: isFunction
+		},
+
+		views: {
+			setter: '_setViews',
+			value: []
+		},
+
+		viewDate: {
+			getter: '_getViewDate',
+			readOnly: true
+		},
+
+		/**
+		 * First day of the week: Sunday is 0, Monday is 1.
+		 *
+		 * @attribute firstDayOfWeek
+		 * @default 0
+		 * @type Number
+		 */
+		firstDayOfWeek: {
+			value: 0,
+			validator: isNumber
+		},
+
+		/*
+		* HTML_PARSER attributes
+		*/
+		controlsNode: {
+			valueFn: function() {
+				return A.Node.create(TPL_SCHEDULER_CONTROLS);
+			}
+		},
+
+		viewDateNode: {
+			valueFn: function() {
+				return A.Node.create(TPL_SCHEDULER_VIEW_DATE);
+			}
+		},
+
+		headerNode: {
+			valueFn: function() {
+				return A.Node.create(TPL_SCHEDULER_HD);
+			}
+		},
+
+		iconNextNode: {
+			valueFn: function() {
+				return A.Node.create(TPL_SCHEDULER_ICON_NEXT);
+			}
+		},
+
+		iconPrevNode: {
+			valueFn: function() {
+				return A.Node.create(TPL_SCHEDULER_ICON_PREV);
+			}
+		},
+
+		navNode: {
+			valueFn: function() {
+				return A.Node.create(TPL_SCHEDULER_NAV);
+			}
+		},
+
+		todayNode: {
+			valueFn: function() {
+				return A.Node.create(
+					this._processTemplate(TPL_SCHEDULER_TODAY)
+				);
+			}
+		},
+
+		viewsNode: {
+			valueFn: function() {
+				return A.Node.create(TPL_SCHEDULER_VIEWS);
+			}
+		}
+	},
+
+	HTML_PARSER: {
+		controlsNode: _DOT+CSS_SCHEDULER_CONTROLS,
+		viewDateNode: _DOT+CSS_SCHEDULER_VIEW_DATE,
+		headerNode: _DOT+CSS_SCHEDULER_HD,
+		iconNextNode: _DOT+CSS_SCHEDULER_ICON_NEXT,
+		iconPrevNode: _DOT+CSS_SCHEDULER_ICON_PREV,
+		navNode: _DOT+CSS_SCHEDULER_NAV,
+		todayNode: _DOT+CSS_SCHEDULER_TODAY,
+		viewsNode: _DOT+CSS_SCHEDULER_VIEWS
+	},
+
+	UI_ATTRS: [DATE, ACTIVE_VIEW],
+
+	AUGMENTS: [A.SchedulerEventSupport, A.WidgetStdMod],
+
+	prototype: {
+		viewStack: null,
+
+		initializer: function() {
+			var instance = this;
+
+			instance[VIEW_STACK] = {};
+
+			instance[CONTROLS_NODE] = instance.get(CONTROLS_NODE);
+			instance[VIEW_DATE_NODE] = instance.get(VIEW_DATE_NODE);
+			instance[HEADER] = instance.get(HEADER_NODE);
+			instance[ICON_NEXT_NODE] = instance.get(ICON_NEXT_NODE);
+			instance[ICON_PREV_NODE] = instance.get(ICON_PREV_NODE);
+			instance[NAV_NODE] = instance.get(NAV_NODE);
+			instance[TODAY_NODE] = instance.get(TODAY_NODE);
+			instance[VIEWS_NODE] = instance.get(VIEWS_NODE);
+
+			instance.after({
+				activeViewChange: instance._afterActiveViewChange,
+				render: instance._afterRender
+			});
+		},
+
+		bindUI: function() {
+			var instance = this;
+
+			instance._bindDelegate();
+		},
+
+		syncUI: function() {
+			var instance = this;
+
+			instance.syncStdContent();
+		},
+
+		getViewByName: function(name) {
+			var instance = this;
+
+			return instance[VIEW_STACK][name];
+		},
+
+		getStrings: function() {
+			var instance = this;
+
+			return instance.get(STRINGS);
+		},
+
+		getString: function(key) {
+			var instance = this;
+
+			return instance.getStrings()[key];
+		},
+
+		renderView: function(view) {
+			var instance = this;
+
+			if (view) {
+				view.show();
+
+				if (!view.get(RENDERED)) {
+					if (!instance.bodyNode) {
+						instance.setStdModContent(WidgetStdMod.BODY, _EMPTY_STR);
+					}
+
+					view.render(instance.bodyNode);
+				}
+			}
+		},
+
+		plotViewEvents: function(view) {
+			var instance = this;
+
+			view.plotEvents(
+				instance.getEvents()
+			);
+		},
+
+		syncEventsUI: function() {
+			var instance = this,
+				activeView = instance.get(ACTIVE_VIEW);
+
+			if (activeView) {
+				instance.plotViewEvents(activeView);
+			}
+		},
+
+		renderButtonGroup: function() {
+			var instance = this;
+
+			instance.buttonGroup = new A.ButtonGroup({
+				boundingBox: instance[VIEWS_NODE],
+				on: {
+					selectionChange: A.bind(instance._onButtonGroupSelectionChange, instance)
+				}
+			}).render();
+		},
+
+		/**
+		 * Sync SchedulerBase StdContent.
+		 *
+		 * @method syncStdContent
+		 * @protected
+		 */
+		syncStdContent: function() {
+			var instance = this;
+			var views = instance.get(VIEWS);
+
+			instance[NAV_NODE].append(instance[ICON_PREV_NODE]);
+			instance[NAV_NODE].append(instance[ICON_NEXT_NODE]);
+
+			instance[CONTROLS_NODE].append(instance[TODAY_NODE]);
+			instance[CONTROLS_NODE].append(instance[NAV_NODE]);
+			instance[CONTROLS_NODE].append(instance[VIEW_DATE_NODE]);
+
+			A.Array.each(views, function(view) {
+				instance[VIEWS_NODE].append( instance._createViewTriggerNode(view) );
+			});
+
+			instance[HEADER].append(instance[CONTROLS_NODE]);
+			instance[HEADER].append(instance[VIEWS_NODE]);
+
+			instance.setStdModContent(WidgetStdMod.HEADER, instance[HEADER].getDOM());
+		},
+
+
+		_afterActiveViewChange: function(event) {
+			var instance = this;
+
+			if (instance.get(RENDERED)) {
+				var activeView = event.newVal;
+				var lastActiveView = event.prevVal;
+
+				if (lastActiveView) {
+					lastActiveView.hide();
+				}
+
+				instance.renderView(activeView);
+
+				var eventRecorder = instance.get(EVENT_RECORDER);
+
+				if (eventRecorder) {
+					eventRecorder.hideOverlay();
+				}
+
+				instance._uiSetDate(instance.get(DATE));
+			}
+		},
+
+		_afterRender: function(event) {
+			var instance = this,
+				activeView = instance.get(ACTIVE_VIEW);
+
+			instance.renderView(activeView);
+			instance.renderButtonGroup();
+
+			instance._uiSetDate(instance.get(DATE));
+			instance._uiSetActiveView(activeView);
+		},
+
+		_bindDelegate: function() {
+			var instance = this;
+
+			instance[CONTROLS_NODE].delegate('click', instance._onClickPrevIcon, _DOT+CSS_SCHEDULER_ICON_PREV, instance);
+			instance[CONTROLS_NODE].delegate('click', instance._onClickNextIcon, _DOT+CSS_SCHEDULER_ICON_NEXT, instance);
+			instance[CONTROLS_NODE].delegate('click', instance._onClickToday, _DOT+CSS_SCHEDULER_TODAY, instance);
+		},
+
+		_createViewTriggerNode: function(view) {
+			var instance = this;
+
+			if (!view.get(TRIGGER_NODE)) {
+				var name = view.get(NAME);
+
+				view.set(
+					TRIGGER_NODE,
+					A.Node.create(
+						Lang.sub(TPL_SCHEDULER_VIEW, {
+							name: name,
+							label: (instance.getString(name) || name)
+						})
+					)
+				);
+			}
+
+			return view.get(TRIGGER_NODE);
+		},
+
+		_getViewDate: function() {
+			var instance = this,
+				date = instance.get(DATE),
+				activeView = instance.get(ACTIVE_VIEW);
+
+			if (activeView) {
+				date = activeView.getAdjustedViewDate(date);
+			}
+
+			return date;
+		},
+
+		_onClickToday: function(event) {
+			var instance = this,
+				activeView = instance.get(ACTIVE_VIEW);
+
+			if (activeView) {
+				instance.set(DATE, activeView.getToday());
+			}
+
+			event.preventDefault();
+		},
+
+		_onClickNextIcon: function(event) {
+			var instance = this,
+				activeView = instance.get(ACTIVE_VIEW);
+
+			if (activeView) {
+				instance.set(DATE, activeView.get(NEXT_DATE));
+			}
+
+			event.preventDefault();
+		},
+
+		_onClickPrevIcon: function(event) {
+			var instance = this,
+				activeView = instance.get(ACTIVE_VIEW);
+
+			if (activeView) {
+				instance.set(DATE, activeView.get(PREV_DATE));
+			}
+
+			event.preventDefault();
+		},
+
+		_onButtonGroupSelectionChange: function(event) {
+			var instance = this,
+				viewName = event.originEvent.target.attr(DATA_VIEW_NAME);
+
+			instance.set(ACTIVE_VIEW, instance.getViewByName(viewName));
+
+			event.preventDefault();
+		},
+
+		_processTemplate: function(tpl) {
+			var instance = this;
+
+			return Lang.sub(tpl, instance.getStrings());
+		},
+
+		_setEventRecorder: function(val) {
+			var instance = this;
+
+			if (val) {
+				val.setAttrs({
+					scheduler: instance
+				},
+				{ silent: true });
+
+				val.addTarget(instance);
+			}
+		},
+
+		_setViews: function(val) {
+			var instance = this;
+			var views = [];
+
+			A.Array.each(val, function(view) {
+				if (isSchedulerView(view) && !view.get(RENDERED)) {
+					view.setAttrs({
+						scheduler: instance
+					});
+
+					views.push(view);
+
+					instance[VIEW_STACK][view.get(NAME)] = view;
+				}
+			});
+
+			if (!instance.get(ACTIVE_VIEW)) {
+				instance.set(ACTIVE_VIEW, val[0]);
+			}
+
+			return views;
+		},
+
+		_uiSetActiveView: function(val) {
+			var instance = this;
+
+			if (val) {
+				var activeView = val.get(NAME),
+					activeNav = instance[VIEWS_NODE].one(_DOT+CSS_SCHEDULER_VIEW_+activeView);
+
+				if (activeNav) {
+					instance[VIEWS_NODE].all(BUTTON).removeClass(CSS_SCHEDULER_VIEW_SELECTED);
+					activeNav.addClass(CSS_SCHEDULER_VIEW_SELECTED);
+				}
+			}
+		},
+
+		_uiSetDate: function(val) {
+			var instance = this;
+
+			var formatter = instance.get(NAVIGATION_DATE_FORMATTER);
+			var navigationTitle = formatter.call(instance, val);
+
+			if (instance.get(RENDERED)) {
+				var activeView = instance.get(ACTIVE_VIEW);
+
+				if (activeView) {
+					activeView._uiSetDate(val);
+
+					formatter = activeView.get(NAVIGATION_DATE_FORMATTER);
+					navigationTitle = formatter.call(activeView, val);
+				}
+
+				instance[VIEW_DATE_NODE].html(navigationTitle);
+
+				instance.syncEventsUI();
+			}
+		}
+	}
+});
+
+A.Scheduler = SchedulerBase;
---
+++ src/aui-scheduler/js/aui-scheduler-event-recorder.js
@@ -0,0 +1,500 @@
+var Lang = A.Lang,
+	isArray = Lang.isArray,
+	isObject = Lang.isObject,
+	isString = Lang.isString,
+	isUndefined = Lang.isUndefined,
+
+	_serialize = A.IO.prototype._serialize,
+
+	clamp = function(value, min, max) {
+		return Math.min(Math.max(value, min), max);
+	},
+
+	DateMath = A.DataType.DateMath,
+
+	_DASH = '-',
+	_DOT = '.',
+	_EMPTY = '',
+	_SPACE = ' ',
+
+	SCHEDULER_EVENT = 'scheduler-event',
+	SCHEDULER_EVENT_RECORDER = 'scheduler-event-recorder',
+
+	ACTIVE_VIEW = 'activeView',
+	ARROW = 'arrow',
+	BODY = 'body',
+	BODY_CONTENT = 'bodyContent',
+	BOUNDING_BOX = 'boundingBox',
+	CANCEL = 'cancel',
+	CLICK = 'click',
+	CONTENT = 'content',
+	DATE = 'date',
+	DATE_FORMAT = 'dateFormat',
+	DELETE = 'delete',
+	END_DATE = 'endDate',
+	EVENT = 'event',
+	FOOTER_CONTENT = 'footerContent',
+	FORM = 'form',
+	HEADER = 'header',
+	ISO_TIME = 'isoTime',
+	KEY_DOWN = 'keydown',
+	NODE = 'node',
+	OFFSET_HEIGHT = 'offsetHeight',
+	OFFSET_WIDTH = 'offsetWidth',
+	OVERLAY = 'overlay',
+	RECORDER = 'recorder',
+	RENDERED = 'rendered',
+	SAVE = 'save',
+	SCHEDULER = 'scheduler',
+	SCHEDULER_CHANGE = 'schedulerChange',
+	SHADOW = 'shadow',
+	START_DATE = 'startDate',
+	STRINGS = 'strings',
+	SUBMIT = 'submit',
+	TEMPLATE = 'template',
+	TOOLBAR = 'toolbar',
+	TOP = 'top',
+	VISIBLE_CHANGE = 'visibleChange',
+
+	getCN = A.getClassName,
+
+	CSS_SCHEDULER_EVENT = getCN(SCHEDULER_EVENT),
+
+	CSS_SCHEDULER_EVENT_RECORDER = getCN(SCHEDULER_EVENT, RECORDER),
+	CSS_SCHEDULER_EVENT_RECORDER_OVERLAY = getCN(SCHEDULER, EVENT, RECORDER, OVERLAY),
+	CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_ARROW = getCN(SCHEDULER, EVENT, RECORDER, OVERLAY, ARROW),
+	CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_ARROW_TOP = getCN(SCHEDULER, EVENT, RECORDER, OVERLAY, ARROW, TOP),
+	CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_ARROW_SHADOW = getCN(SCHEDULER, EVENT, RECORDER, OVERLAY, ARROW, SHADOW),
+	CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_BODY = getCN(SCHEDULER, EVENT, RECORDER, OVERLAY, BODY),
+	CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_CONTENT = getCN(SCHEDULER, EVENT, RECORDER, OVERLAY, CONTENT),
+	CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_DATE = getCN(SCHEDULER, EVENT, RECORDER, OVERLAY, DATE),
+	CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_FORM = getCN(SCHEDULER, EVENT, RECORDER, OVERLAY, FORM),
+	CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_HEADER = getCN(SCHEDULER, EVENT, RECORDER, OVERLAY, HEADER),
+
+	TPL_OVERLAY_BODY_CONTENT = [
+		'<div class="', CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_ARROW_SHADOW, ' ', CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_ARROW, '"></div>',
+		'<div class="', CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_ARROW, '"></div>',
+		'<input type="hidden" name="startDate" value="{startDate}" />',
+		'<input type="hidden" name="endDate" value="{endDate}" />',
+		'<div class="', CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_HEADER, '">',
+			'<input class="', CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_CONTENT, '" name="content" value="{content}" />',
+		'</div>',
+		'<div class="', CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_BODY, '">',
+			'<label class="', CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_DATE, '">{date}</label>',
+		'</div>'
+	],
+
+	TPL_OVERLAY_FORM = '<form class="' + CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_FORM + '" id="schedulerEventRecorderForm"></form>';
+
+var SchedulerEventRecorder = A.Component.create({
+	NAME: SCHEDULER_EVENT_RECORDER,
+
+	ATTRS: {
+		allDay: {
+			value: false
+		},
+
+		content: {
+		},
+
+		duration: {
+			value: 60
+		},
+
+		dateFormat: {
+			validator: isString,
+			value: '%a, %B %d,'
+		},
+
+		event: {
+		},
+
+		eventClass: {
+			valueFn: function() {
+				return A.SchedulerEvent;
+			}
+		},
+
+		strings: {
+			value: {},
+			setter: function(val) {
+				return A.merge(
+					{
+						'delete': 'Delete',
+						'description-hint': 'e.g., Dinner at Brian\'s',
+						cancel: 'Cancel',
+						description: 'Description',
+						edit: 'Edit',
+						save: 'Save',
+						when: 'When'
+					},
+					val || {}
+				);
+			},
+			validator: isObject
+		},
+
+		overlay: {
+			validator: isObject,
+			value: {
+				constrain: true,
+				visible: false,
+				width: 300,
+				zIndex: 500
+			}
+		},
+
+		// See #2530972
+		overlayOffset: {
+			value: [15, -38],
+			validator: isArray
+		},
+
+		template: {
+			value: TPL_OVERLAY_BODY_CONTENT
+		},
+
+		toolbar: {
+			setter: function(val) {
+				var instance = this;
+				var strings = instance.get(STRINGS);
+
+				return A.merge({
+					children: [
+						[
+							{
+								label: strings[SAVE],
+								on: {
+									click: A.bind(instance._handleSaveEvent, instance)
+								}
+							},
+							{
+								label: strings[CANCEL],
+								on: {
+									click: A.bind(instance._handleCancelEvent, instance)
+								}
+							},
+							{
+								label: strings[DELETE],
+								on: {
+									click: A.bind(instance._handleDeleteEvent, instance)
+								}
+							}
+						]
+					]
+				}, val || {});
+			},
+			validator: isObject,
+			value: {}
+		}
+	},
+
+	EXTENDS: A.SchedulerEvent,
+
+	prototype: {
+
+		initializer: function() {
+			var instance = this;
+
+			instance.get(NODE).addClass(CSS_SCHEDULER_EVENT_RECORDER);
+
+			instance.publish('cancel', {
+				defaultFn: instance._defCancelEventFn
+			});
+
+			instance.publish('delete', {
+				defaultFn: instance._defDeleteEventFn
+			});
+
+			instance.publish('edit', {
+				defaultFn: instance._defEditEventFn
+			});
+
+			instance.publish('save', {
+				defaultFn: instance._defSaveEventFn
+			});
+
+			instance.after(SCHEDULER_CHANGE, instance._afterSchedulerChange);
+
+			instance[OVERLAY] = new A.Overlay(instance.get(OVERLAY));
+			instance[TOOLBAR] = new A.Toolbar(instance.get(TOOLBAR));
+
+			instance[OVERLAY].on(VISIBLE_CHANGE, A.bind(instance._onOverlayVisibleChange, instance));
+		},
+
+		getOverlayContentNode: function() {
+			var instance = this;
+			var overlayBB = instance[OVERLAY].get(BOUNDING_BOX);
+
+			return overlayBB.one(_DOT + CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_CONTENT);
+		},
+
+		getUpdatedSchedulerEvent: function(optAttrMap) {
+			var instance = this,
+				schedulerEvent = instance.get(EVENT),
+				options = {
+					silent: !schedulerEvent
+				},
+				formValues = instance.serializeForm();
+
+			if (!schedulerEvent) {
+				schedulerEvent = instance.clone();
+			}
+
+			schedulerEvent.set(SCHEDULER, instance.get(SCHEDULER), { silent: true });
+			schedulerEvent.setAttrs(A.merge(formValues, optAttrMap), options);
+
+			return schedulerEvent;
+		},
+
+		_afterSchedulerChange: function(event) {
+			var instance = this;
+			var scheduler = event.newVal;
+			var schedulerBB = scheduler.get(BOUNDING_BOX);
+
+			schedulerBB.delegate(CLICK, A.bind(instance._onClickSchedulerEvent, instance), _DOT + CSS_SCHEDULER_EVENT);
+		},
+
+		_defCancelEventFn: function(event) {
+			var instance = this;
+
+			instance.get(NODE).remove();
+
+			instance.hideOverlay();
+		},
+
+		_defDeleteEventFn: function(event) {
+			var instance = this;
+			var scheduler = instance.get(SCHEDULER);
+
+			scheduler.removeEvents(instance.get(EVENT));
+
+			instance.hideOverlay();
+
+			scheduler.syncEventsUI();
+		},
+
+		_defEditEventFn: function(event) {
+			var instance = this;
+			var scheduler = instance.get(SCHEDULER);
+
+			instance.hideOverlay();
+
+			scheduler.syncEventsUI();
+		},
+
+		_defSaveEventFn: function(event) {
+			var instance = this;
+			var scheduler = instance.get(SCHEDULER);
+
+			scheduler.addEvents(event.newSchedulerEvent);
+
+			instance.hideOverlay();
+
+			scheduler.syncEventsUI();
+		},
+
+		_handleCancelEvent: function(event) {
+			var instance = this;
+
+			instance.fire('cancel');
+
+			event.preventDefault();
+		},
+
+		_handleClickOutSide: function(event) {
+			var instance = this;
+
+			instance.fire('cancel');
+		},
+
+		_handleDeleteEvent: function(event) {
+			var instance = this;
+
+			instance.fire('delete', {
+				schedulerEvent: instance.get(EVENT)
+			});
+
+			event.preventDefault();
+		},
+
+		_handleEscapeEvent: function(event) {
+			var instance = this;
+
+			if (instance[OVERLAY].get(RENDERED) && (event.keyCode == A.Event.KeyMap.ESC)) {
+				instance.fire('cancel');
+
+				event.preventDefault();
+			}
+		},
+
+		_handleSaveEvent: function(event) {
+			var instance = this,
+				eventName = instance.get(EVENT) ? 'edit' : 'save';
+
+			instance.fire(eventName, {
+				newSchedulerEvent: instance.getUpdatedSchedulerEvent()
+			});
+
+			event.preventDefault();
+		},
+
+		_onClickSchedulerEvent: function(event) {
+			var instance = this;
+			var evt = event.currentTarget.getData(SCHEDULER_EVENT);
+
+			if (evt) {
+				instance.set(EVENT, evt, { silent: true });
+				instance.showOverlay([event.pageX, event.pageY]);
+
+				instance.get(NODE).remove();
+			}
+		},
+
+		_onOverlayVisibleChange: function(event) {
+			var instance = this;
+
+			if (event.newVal) {
+				instance.populateForm();
+
+				if (!instance.get(EVENT)) {
+					var overlayContentNode = instance.getOverlayContentNode();
+
+					if (overlayContentNode) {
+						setTimeout(function() {
+							overlayContentNode.selectText();
+						}, 0);
+					}
+				}
+			}
+			else {
+				instance.set(EVENT, null, { silent: true });
+
+				instance.get(NODE).remove();
+			}
+		},
+
+		_onSubmitForm: function(event) {
+			var instance = this;
+
+			instance._handleSaveEvent(event);
+		},
+
+		_renderOverlay: function() {
+			var instance = this;
+			var scheduler = instance.get(SCHEDULER);
+			var schedulerBB = scheduler.get(BOUNDING_BOX);
+			var strings = instance.get(STRINGS);
+
+			instance[OVERLAY].render(schedulerBB);
+			instance[TOOLBAR].render(schedulerBB);
+
+			var overlayBB = instance[OVERLAY].get(BOUNDING_BOX);
+			overlayBB.addClass(CSS_SCHEDULER_EVENT_RECORDER_OVERLAY);
+
+			instance[OVERLAY].set(FOOTER_CONTENT, instance[TOOLBAR].get(BOUNDING_BOX));
+
+			instance.formNode = A.Node.create(TPL_OVERLAY_FORM);
+
+			instance[OVERLAY].set(BODY_CONTENT, instance.formNode);
+
+			instance.formNode.on(SUBMIT, A.bind(instance._onSubmitForm, instance));
+
+			scheduler.get(BOUNDING_BOX).on('clickoutside', A.bind(instance._handleClickOutSide, instance));
+		},
+
+		getFormattedDate: function() {
+			var instance = this;
+			var dateFormat = instance.get(DATE_FORMAT);
+			var evt = (instance.get(EVENT) || instance);
+
+			var endDate = evt.get(END_DATE);
+			var scheduler = evt.get(SCHEDULER);
+			var startDate = evt.get(START_DATE);
+			var fmtHourFn = (scheduler.get(ACTIVE_VIEW).get(ISO_TIME) ? DateMath.toIsoTimeString : DateMath.toUsTimeString);
+
+			return [ evt._formatDate(startDate, dateFormat), fmtHourFn(startDate), _DASH, fmtHourFn(endDate) ].join(_SPACE);
+		},
+
+		getTemplateData: function() {
+			var instance = this,
+				strings = instance.get(STRINGS),
+				evt = instance.get(EVENT) || instance,
+				content = evt.get(CONTENT);
+
+			if (isUndefined(content)) {
+				content = strings['description-hint'];
+			}
+
+			return {
+				content: content,
+				date: instance.getFormattedDate(),
+				endDate: evt.get(END_DATE).getTime(),
+				startDate: evt.get(START_DATE).getTime()
+			};
+		},
+
+		hideOverlay: function() {
+			var instance = this;
+
+			instance[OVERLAY].hide();
+		},
+
+		populateForm: function() {
+			var instance = this,
+				tpl = instance.get(TEMPLATE).join(_EMPTY);
+
+			instance.formNode.setContent(
+				A.Lang.sub(tpl, instance.getTemplateData()));
+		},
+
+		serializeForm: function() {
+			var instance = this;
+
+			return A.QueryString.parse(_serialize(instance.formNode.getDOM()));
+		},
+
+		showOverlay: function(xy) {
+			var instance = this,
+				originalXY = xy.concat([]),
+				overlay = instance[OVERLAY],
+				overlayBB = overlay.get(BOUNDING_BOX);
+
+			if (!instance[OVERLAY].get(RENDERED)) {
+				instance._renderOverlay();
+			}
+
+			overlay.show();
+
+			instance.handleEscapeEvent = A.on(KEY_DOWN, A.bind(instance._handleEscapeEvent, instance));
+
+			var arrows = overlayBB.all(_DOT + CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_ARROW),
+				firstArrow = arrows.item(0),
+				firstArrowHeight = firstArrow.get(OFFSET_HEIGHT),
+				firstArrowWidth = firstArrow.get(OFFSET_WIDTH);
+
+			arrows.removeClass(CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_ARROW_TOP).show();
+
+			xy[0] -= overlayBB.get(OFFSET_WIDTH)*0.5;
+			xy[1] -= overlayBB.get(OFFSET_HEIGHT) + firstArrowHeight*0.5;
+
+			var constrainedXY = overlay.getConstrainedXY(xy);
+
+			if (constrainedXY[1] !== xy[1]) {
+				xy[1] = originalXY[1] + firstArrowHeight*0.5;
+
+				arrows.addClass(CSS_SCHEDULER_EVENT_RECORDER_OVERLAY_ARROW_TOP);
+			}
+
+			xy = overlay.getConstrainedXY(xy);
+
+			overlay.set('xy', xy);
+
+			var arrowX = clamp(originalXY[0] - firstArrowWidth*0.5, xy[0], xy[0] + overlayBB.get(OFFSET_WIDTH));
+
+			arrows.setX(arrowX);
+		}
+	}
+});
+
+A.SchedulerEventRecorder = SchedulerEventRecorder;
