54581eaac4572cf1ab26c255a403ec2ed59c8b42
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2013-03-28 08:06:11 -0700

    AUI-865 - Source formatting

--- src/aui-toggler/js/aui-toggler-base.js
+++ src/aui-toggler/js/aui-toggler-base.js
@@ -17,7 +17,6 @@ var Lang = A.Lang,
 	CLICK = 'click',
 	COLLAPSED = 'collapsed',
 	CONTENT = 'content',
-	CUBIC_BEZIER = 'cubic-bezier(0.25, 0.1, 0.25, 1)',
 	DOWN = 'down',
 	ENTER = 'enter',
 	ESC = 'esc',
@@ -105,8 +104,7 @@ var Toggler = A.Component.create({
 		transition: {
 			validator: isObject,
 			value: {
-				duration: 0.4,
-				easing: CUBIC_BEZIER
+				duration: 0.4
 			}
 		}
 
