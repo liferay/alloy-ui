84937ffef2164bce3ed89aaa5e8c58c77b6564e1
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2013-05-02 17:58:52 -0700

    Remove "aui-" css prefixes from src/ (addClass and removeClass pattern)

--- src/aui-color-picker/js/aui-color-picker-base.js
+++ src/aui-color-picker/js/aui-color-picker-base.js
@@ -56,7 +56,7 @@ ColorPickerBase.prototype = {
     TPL_HSV_TRIGGER: '<div class="span6 ' + CSS_HSV_TRIGGER + '">{more}</div>',
 
     TPL_NO_COLOR:   '<div class="span6 ' + CSS_NO_COLOR + '">' +
-                        '<a href class="btn-link"><i class="' + CSS_NO_COLOR_ICON + ' aui-icon-remove-circle"></i>{none}</a>' +
+                        '<a href class="btn-link"><i class="' + CSS_NO_COLOR_ICON + ' icon-remove-circle"></i>{none}</a>' +
                     '</div>',
 
     _currentTrigger: null,
