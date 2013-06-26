da92ef0abcae4853732544b4e5a1e1a3f05a8881
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-06-26 12:35:13 +0200

    AUI-934 - Apply the same pattern to _onAnimatingChange too

--- src/aui-toggler/js/aui-toggler-delegate.js
+++ src/aui-toggler/js/aui-toggler-delegate.js
@@ -209,7 +209,7 @@ var TogglerDelegate = A.Component.create({
             var container = instance.get(CONTAINER);
             var header = instance.get(HEADER);
 
-            instance.on(TOGGLER_ANIMATING_CHANGE, A.bind(instance._onAnimatingChange, instance));
+            instance.on(TOGGLER_ANIMATING_CHANGE, A.bind('_onAnimatingChange', instance));
 
             container.delegate([CLICK, KEYDOWN], A.bind('headerEventHandler', instance), header);
         },
