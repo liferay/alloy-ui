fc273c0cf579be7ad96ad4758220e476065f5e5b
Jonathan Mak <jonathan.mak@liferay.com>
2013-06-28 16:40:17 -0700

    AUI-930 - Source Formatting

--- src/aui-calendar/js/aui-calendar.js
+++ src/aui-calendar/js/aui-calendar.js
@@ -1231,8 +1231,10 @@ var Calendar = A.Component.create(
 
 				var name = instance._getDayNameShort(weekDay);
 
-				if (name.length > 1) {
-					return name.slice(0, name.length - 1);
+				var length = name.length;
+
+				if (length > 1) {
+					return name.slice(0, length - 1);
 				}
 
 				return name;
