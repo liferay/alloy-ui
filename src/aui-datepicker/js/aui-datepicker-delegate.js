1a195f9aece62e6270a76cb6e6ca143319ce4aa8
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2013-08-14 19:10:01 -0300

    Add datepicker module

---
+++ src/aui-datepicker/js/aui-datepicker-delegate.js
@@ -0,0 +1,246 @@
+/**
+ * The DatePickerDelegate Component
+ *
+ * @module aui-datepicker-delegate
+ */
+
+var Lang = A.Lang,
+    isString = Lang.isString,
+
+    _DOCUMENT = A.one(A.config.doc),
+
+    ACTIVE_INPUT = 'activeInput',
+    CLICK = 'click',
+    CONTAINER = 'container',
+    DATE_SEPARATOR = 'dateSeparator',
+    DATEPICKER_SELECTION = 'datepickerSelection',
+    MASK = 'mask',
+    MOUSEDOWN = 'mousedown',
+    SELECTION_CHANGE = 'selectionChange',
+    TRIGGER = 'trigger',
+    FOCUSIN = 'focusin',
+    FOCUSOUT = 'focusout',
+    VALUE_EXTRACTOR = 'valueExtractor',
+    VALUE_FORMATTER = 'valueFormatter';
+
+function DatePickerDelegate() {}
+
+DatePickerDelegate.prototype = {
+    _eventHandles: null,
+
+    _userInteractionInProgress: false,
+
+    initializer: function() {
+        var instance = this;
+
+        instance.bindDelegateUI();
+    },
+
+    destroy: function() {
+        var instance = this;
+
+        instance._eventHandles.detach();
+    },
+
+    /**
+     * Bind the events on the DatePickerDelegate UI. Lifecycle.
+     *
+     * @method bindDelegateUI
+     * @protected
+     */
+    bindDelegateUI: function() {
+        var instance = this,
+            container = instance.get(CONTAINER),
+            trigger = instance.get(TRIGGER);
+
+        instance._eventHandles = [
+            container.delegate(
+                [FOCUSIN, MOUSEDOWN],
+                A.bind('_onceUserInteraction', instance), trigger),
+
+            container.delegate(
+                FOCUSOUT,
+                A.bind('_onUserInteractionRelease', instance), trigger),
+
+            container.delegate(
+                CLICK,
+                A.bind('_onceUserInteractionRelease', instance), trigger)
+        ];
+
+        instance.publish(
+            SELECTION_CHANGE,
+            { defaultFn: instance._defSelectionChangeFn });
+    },
+
+    getSelectedDates: function(node) {
+        var instance = this,
+            activeInput = node || instance.get(ACTIVE_INPUT),
+            selectedDates = activeInput.getData(DATEPICKER_SELECTION);
+
+        if (selectedDates) {
+            return selectedDates;
+        }
+
+        return null;
+    },
+
+    getParsedDatesFromInputValue: function() {
+        var instance = this,
+            valueExtractor = instance.get(VALUE_EXTRACTOR),
+            parsedDates = valueExtractor.call(instance);
+
+        if (parsedDates) {
+            return A.Array.filter(parsedDates, function(parsed) {
+                return parsed !== false;
+            });
+        }
+
+        return null;
+    },
+
+    useInputNode: function() {},
+
+    useInputNodeOnce: function(node) {
+        var instance = this;
+
+        if (!instance._userInteractionInProgress) {
+            instance.useInputNode(node);
+        }
+    },
+
+    _defSelectionChangeFn: function(event) {
+        var instance = this,
+            selection = event.newSelection,
+            activeInput = instance.get(ACTIVE_INPUT),
+            valueFormatter = instance.get(VALUE_FORMATTER);
+
+        valueFormatter.call(instance, selection);
+
+        activeInput.setData(DATEPICKER_SELECTION, selection);
+    },
+
+    _formatDate: function(date) {
+        var instance = this,
+            mask = instance.get(MASK);
+
+        return A.Date.format(date, { format: mask });
+    },
+
+    _onceUserInteraction: function(event) {
+        var instance = this;
+
+        instance.useInputNodeOnce(event.currentTarget);
+
+        instance._userInteractionInProgress = true;
+    },
+
+    _onceUserInteractionRelease: function(event) {
+        var instance = this;
+
+        instance.useInputNodeOnce(event.currentTarget);
+
+        instance._userInteractionInProgress = false;
+    },
+
+    _onUserInteractionRelease: function(event) {
+        var instance = this;
+
+        instance.useInputNode(event.currentTarget);
+
+        instance._userInteractionInProgress = false;
+    },
+
+    _valueExtractorFn: function() {
+        return function() {
+            var instance = this,
+                activeInput = instance.get(ACTIVE_INPUT),
+                activeInputValue = Lang.trim(activeInput.val()),
+                dateSeparator = instance.get(DATE_SEPARATOR),
+                mask = instance.get(MASK),
+                dates;
+
+            if (activeInputValue) {
+                dates = [];
+                A.Array.each(
+                    activeInputValue.split(dateSeparator),
+                    function(text) {
+                        text = Lang.trim(text);
+                        if (text) {
+                            dates.push(A.Date.parse(mask, text));
+                        }
+                    });
+            }
+
+            return dates;
+        };
+    },
+
+    _valueFormatterFn: function() {
+        return function(dates) {
+            var instance = this,
+                activeInput = instance.get(ACTIVE_INPUT),
+                dateSeparator = instance.get(DATE_SEPARATOR),
+                values = [];
+
+            A.Array.each(dates, function(date) {
+                values.push(instance._formatDate(date));
+            });
+
+            activeInput.val( values.join(dateSeparator) );
+        };
+    }
+};
+
+DatePickerDelegate.ATTRS = {
+    /**
+     * The active input element that holds the calendar instance.
+     *
+     * @attribute activeInput
+     */
+    activeInput: {
+    },
+
+    /**
+     * The container of Toggler Delegate instance.
+     *
+     * @attribute container
+     */
+    container: {
+        setter: A.one,
+        value: _DOCUMENT,
+        writeOnce: true
+    },
+
+    dateSeparator: {
+        value: ' \u2014 ',
+        validator: Lang.isString
+    },
+
+    mask: {
+        value: '%m/%d/%Y',
+        validator: Lang.isString
+    },
+
+    /**
+     * The content of a Toogler Delegate instance.
+     *
+     * @attribute content
+     * @type String
+     */
+    trigger: {
+        validator: isString,
+        writeOnce: true
+    },
+
+    valueExtractor: {
+        valueFn: '_valueExtractorFn',
+        validator: Lang.isFunction
+    },
+
+    valueFormatter: {
+        valueFn: '_valueFormatterFn',
+        validator: Lang.isFunction
+    }
+};
+
+A.DatePickerDelegate = DatePickerDelegate;
---
+++ src/aui-datepicker/js/aui-datepicker-native.js
@@ -0,0 +1,173 @@
+var Lang = A.Lang,
+
+    _DASH = '-',
+
+    ACTIVE_INPUT = 'activeInput',
+    CHANGE = 'change',
+    CONTAINER = 'container',
+    DATE = 'date',
+    NATIVE_MASK = 'nativeMask',
+    NATIVE_TYPE = 'nativeType',
+    SELECTION_CHANGE = 'selectionChange',
+    SELECTOR = 'selector',
+    TOUCHSTART = 'touchstart',
+    TIME = 'time',
+    TYPE = 'type';
+
+function DatePickerNativeBase() {}
+
+DatePickerNativeBase.ATTRS = {
+    nativeMask: {
+        validator: Lang.isString,
+        value: '%Y-%m-%d'
+    },
+
+    nativeType: {
+        validator: Lang.isString,
+        value: 'date'
+    }
+};
+
+DatePickerNativeBase.prototype = {
+    initializer: function() {
+        var instance = this;
+
+        instance.bindNativeUI();
+    },
+
+    bindNativeUI: function() {
+        var instance = this,
+            container = instance.get(CONTAINER),
+            selector = instance.get(SELECTOR);
+
+        instance._eventHandles.push(
+            container.delegate(
+                TOUCHSTART,
+                A.bind('_onceUserInteraction', instance), selector),
+
+            container.delegate(
+                CHANGE,
+                A.bind('_afterNativeSelectionChange', instance), selector)
+        );
+    },
+
+    clearSelection: function() {
+        var instance = this,
+            activeInput = instance.get(ACTIVE_INPUT);
+
+        activeInput.val('');
+    },
+
+    deselectDates: function() {
+        var instance = this;
+
+        instance.clearSelection();
+    },
+
+    hide: function() {
+        var instance = this,
+            activeInput = instance.get(ACTIVE_INPUT);
+
+        activeInput.blur();
+    },
+
+    show: function() {
+        var instance = this,
+            activeInput = instance.get(ACTIVE_INPUT);
+
+        activeInput.focus();
+    },
+
+    selectDates: function(dates) {
+        var instance = this,
+            activeInput = instance.get(ACTIVE_INPUT);
+
+        if (Lang.isArray(dates)) {
+            dates = dates[0];
+        }
+
+        if (Lang.isDate(dates)) {
+            activeInput.val(instance._formatDate(dates));
+        }
+    },
+
+    useInputNode: function(node) {
+        var instance = this,
+            nativeType = instance.get(NATIVE_TYPE),
+            type = node.attr(TYPE),
+            parsed;
+
+        instance.set(ACTIVE_INPUT, node);
+
+        if (!instance._isTypeSupported(type)) {
+            parsed = instance.getParsedDatesFromInputValue();
+            if (parsed) {
+                node.val(instance._formatDate(parsed[0]));
+            }
+        }
+
+        node.setAttribute(TYPE, nativeType);
+
+        instance._fireSelectionChange();
+    },
+
+    _addFourDigitsYearPadding: function(text) {
+        return A.Lang.String.repeat('0', 4 - text.indexOf(_DASH)) + text;
+    },
+
+    _afterNativeSelectionChange: function(event) {
+        var instance = this,
+            type = event.currentTarget.attr(TYPE);
+
+        if (instance._isTypeSupported(type)) {
+            instance._fireSelectionChange();
+        }
+    },
+
+    _fireSelectionChange: function() {
+        var instance = this,
+            activeInput = instance.get(ACTIVE_INPUT),
+            parsed = instance._parseDateFromString(activeInput.val());
+
+        instance.fire(
+            SELECTION_CHANGE, { newSelection: parsed ? [parsed] : [] });
+    },
+
+    _formatDate: function(date) {
+        var instance = this,
+            nativeMask = instance.get(NATIVE_MASK),
+            nativeType = instance.get(NATIVE_TYPE),
+            formatted = A.Date.format(date, { format: nativeMask });
+
+        if (nativeType === DATE) {
+            formatted = instance._addFourDigitsYearPadding(formatted);
+        }
+
+        return formatted;
+    },
+
+    _isTypeSupported: function(type) {
+        switch (type.toLowerCase()) {
+            case DATE:
+            case TIME:
+                return true;
+            default:
+                return false;
+        }
+    },
+
+    _parseDateFromString: function(text) {
+        var instance = this,
+            nativeMask = instance.get(NATIVE_MASK);
+
+        if (!text) {
+            return false;
+        }
+
+        return A.Date.parse(nativeMask, text);
+    }
+};
+
+A.DatePickerNativeBase = DatePickerNativeBase;
+
+A.DatePickerNative = A.Base.create('datepicker-native', A.Base, [A.DatePickerDelegate, A.DatePickerNativeBase]);
