bc481307042aaf42069d45660c7bd7fd3f27fc1c
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2013-09-05 15:24:43 -0300

    Use padding since margin is not a clickable area

--- src/aui-button/js/aui-button-search-cancel.js
+++ src/aui-button/js/aui-button-search-cancel.js
@@ -39,7 +39,9 @@ var ButtonSearchCancel = A.Base.create(_NAME, A.Base, [],
          * @default '<i class="btn-search-cancel icon-remove" />'
          * @protected
          */
-        TEMPLATE: '<i class="' + A.getClassName(_NAME) + ' {iconClass}" style="position: absolute; margin: 5px; z-index: {zIndex};"></i>',
+        TEMPLATE: '<div class="' + A.getClassName(_NAME) + '" style="padding: 5px; position: absolute; z-index: {zIndex};">' +
+                    '<i class="{iconClass}"></i>' +
+                  '</div>',
 
         /**
          * Array that holds the created buttons for each element match from
