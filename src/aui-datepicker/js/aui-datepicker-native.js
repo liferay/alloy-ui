e6eb01da0add25d2c2fe18c973c546e30ddf439e
Zeno Rocha <zno.rocha@gmail.com>
2013-09-20 12:23:32 -0300

    Add API Docs placeholder for DatePickerNative

--- src/aui-datepicker/js/aui-datepicker-native.js
+++ src/aui-datepicker/js/aui-datepicker-native.js
@@ -1,3 +1,10 @@
+/**
+ * The DatePicker Component
+ *
+ * @module aui-datepicker
+ * @submodule aui-datepicker-native
+ */
+
 var Lang = A.Lang,
 
     _DASH = '-',
@@ -14,14 +21,43 @@ var Lang = A.Lang,
     TIME = 'time',
     TYPE = 'type';
 
+/**
+ * A base class for DatePickerNativeBase.
+ *
+ * @class A.DatePickerNativeBase
+ * @param config {Object} Object literal specifying widget configuration properties.
+ * @constructor
+ */
 function DatePickerNativeBase() {}
 
+/**
+ * TODO. Wanna help? Please send a Pull Request.
+ *
+ * @property DatePickerNativeBase.ATTRS
+ * @type Object
+ * @static
+ */
 DatePickerNativeBase.ATTRS = {
+
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @attribute nativeMask
+     * @default '%Y-%m-%d'
+     * @type String
+     */
     nativeMask: {
         validator: Lang.isString,
         value: '%Y-%m-%d'
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @attribute nativeType
+     * @default 'date'
+     * @type String
+     */
     nativeType: {
         validator: Lang.isString,
         value: 'date'
@@ -29,12 +65,24 @@ DatePickerNativeBase.ATTRS = {
 };
 
 DatePickerNativeBase.prototype = {
+
+    /**
+     * Construction logic executed during DatePickerNativeBase instantiation. Lifecycle.
+     *
+     * @method initializer
+     * @protected
+     */
     initializer: function() {
         var instance = this;
 
         instance.bindNativeUI();
     },
 
+    /**
+     * Bind the events on the DatePickerNativeBase UI. Lifecycle.
+     *
+     * @method bindNativeUI
+     */
     bindNativeUI: function() {
         var instance = this,
             container = instance.get(CONTAINER),
@@ -51,6 +99,11 @@ DatePickerNativeBase.prototype = {
         );
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method clearSelection
+     */
     clearSelection: function() {
         var instance = this,
             activeInput = instance.get(ACTIVE_INPUT);
@@ -58,12 +111,22 @@ DatePickerNativeBase.prototype = {
         activeInput.val('');
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method deselectDates
+     */
     deselectDates: function() {
         var instance = this;
 
         instance.clearSelection();
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method hide
+     */
     hide: function() {
         var instance = this,
             activeInput = instance.get(ACTIVE_INPUT);
@@ -71,6 +134,11 @@ DatePickerNativeBase.prototype = {
         activeInput.blur();
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method show
+     */
     show: function() {
         var instance = this,
             activeInput = instance.get(ACTIVE_INPUT);
@@ -78,6 +146,12 @@ DatePickerNativeBase.prototype = {
         activeInput.focus();
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method selectDates
+     * @param dates
+     */
     selectDates: function(dates) {
         var instance = this,
             activeInput = instance.get(ACTIVE_INPUT);
@@ -91,6 +165,12 @@ DatePickerNativeBase.prototype = {
         }
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method useInputNode
+     * @param node
+     */
     useInputNode: function(node) {
         var instance = this,
             nativeType = instance.get(NATIVE_TYPE),
@@ -113,10 +193,24 @@ DatePickerNativeBase.prototype = {
         instance._fireSelectionChange();
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method _addFourDigitsYearPadding
+     * @param text
+     * @protected
+     */
     _addFourDigitsYearPadding: function(text) {
         return A.Lang.String.repeat('0', 4 - text.indexOf(_DASH)) + text;
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method _afterNativeSelectionChange
+     * @param event
+     * @protected
+     */
     _afterNativeSelectionChange: function(event) {
         var instance = this,
             type = event.currentTarget.attr(TYPE);
@@ -126,6 +220,12 @@ DatePickerNativeBase.prototype = {
         }
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method _fireSelectionChange
+     * @protected
+     */
     _fireSelectionChange: function() {
         var instance = this,
             activeInput = instance.get(ACTIVE_INPUT),
@@ -135,6 +235,13 @@ DatePickerNativeBase.prototype = {
             SELECTION_CHANGE, { newSelection: parsed ? [parsed] : [] });
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method _formatDate
+     * @param date
+     * @protected
+     */
     _formatDate: function(date) {
         var instance = this,
             nativeMask = instance.get(NATIVE_MASK),
@@ -148,6 +255,13 @@ DatePickerNativeBase.prototype = {
         return formatted;
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method _isTypeSupported
+     * @param type
+     * @protected
+     */
     _isTypeSupported: function(type) {
         switch (type.toLowerCase()) {
             case DATE:
@@ -158,6 +272,13 @@ DatePickerNativeBase.prototype = {
         }
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method _parseDateFromString
+     * @param text
+     * @protected
+     */
     _parseDateFromString: function(text) {
         var instance = this,
             nativeMask = instance.get(NATIVE_MASK);
@@ -172,4 +293,13 @@ DatePickerNativeBase.prototype = {
 
 A.DatePickerNativeBase = DatePickerNativeBase;
 
+/**
+ * A base class for DatePickerNative.
+ *
+ * @class A.DatePickerNative
+ * @extends A.Base
+ * @uses A.DatePickerDelegate, A.DatePickerNativeBase
+ * @param config {Object} Object literal specifying widget configuration properties.
+ * @constructor
+ */
 A.DatePickerNative = A.Base.create('datepicker-native', A.Base, [A.DatePickerDelegate, A.DatePickerNativeBase]);
