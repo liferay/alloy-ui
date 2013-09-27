f1339f090f30bd686226ae52d76bd8d6705ed533
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2013-09-27 09:18:21 -0700

    AUI-999 - Build files

--- build/aui-chart/aui-chart.js
+++ build/aui-chart/aui-chart.js
@@ -1120,4 +1120,4 @@ A.extend(
 
 A.Chart.StackedColumnSeries = StackedColumnSeries;
 
-}, '@VERSION@' ,{requires:['datasource','aui-swf','json'], skinnable:false});
+}, '@VERSION@' ,{skinnable:false, requires:['datasource','aui-swf','json']});
