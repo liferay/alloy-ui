f428605dd2f54b9c18815e96a438b8ac8bf9b798
Byran Zaugg <byran.zaugg@liferay.com>
2013-10-15 09:43:38 -0700

    AUI-989 Add tests for getNodeById() , childrenLength, removeChild(), isRegistered()

--- src/aui-tree/tests/unit/js/tests.js
+++ src/aui-tree/tests/unit/js/tests.js
@@ -2,6 +2,25 @@ YUI.add('module-tests', function(Y) {
 
     var suite = new Y.Test.Suite('aui-tree');
 
+    var createNewTreeView = function() {
+
+        return new Y.TreeView({
+            children: [
+               {
+                    id: 'one',
+                    children: [
+                        {id: 'one-one'},
+                        {id: 'one-two'},
+                        {id: 'one-three'},
+                        {id: 'one-four'}
+                    ]
+                },
+                {id: 'two'},
+                {id: 'three'}
+            ]
+        });
+    };
+
     suite.add(new Y.Test.Case({
         name: 'Tree View',
 
@@ -15,6 +34,82 @@ YUI.add('module-tests', function(Y) {
             var treeNode = new Y.TreeNode();
 
             Y.Assert.isInstanceOf(Y.TreeNode, treeNode, 'treeNode should be an instance of Y.TreeNode.');
+        },
+
+        'getNodeById() should return a valid TreeNode': function() {
+            var tree = createNewTreeView();
+
+            var childNode = tree.getNodeById('one');
+
+            Y.Assert.isInstanceOf(Y.TreeNode, childNode, 'childNode should be an instance of Y.TreeNode.');
+        },
+
+        'getNodeById() should not return a valid TreeNode': function() {
+            var tree = createNewTreeView();
+
+            var childNode = tree.getNodeById('bogey');
+
+            Y.Assert.isUndefined(childNode, 'childNode should be undefined.');
+        },
+
+        'TreeNode should have children': function() {
+            var tree = createNewTreeView();
+
+            var node = tree.getNodeById('one');
+
+            Y.Assert.areSame(4, node.childrenLength, 'node.childrenLength should return 4.');
+        },
+
+        'TreeNode should not have children': function() {
+            var tree = createNewTreeView();
+
+            var node = tree.getNodeById('two');
+
+            Y.Assert.areSame(0, node.childrenLength, 'node.childrenLength should return 0.');
+        },
+
+        'removeChild() should remove child TreeNode': function() {
+            var tree = createNewTreeView();
+
+            var node = tree.getNodeById('two');
+
+            tree.removeChild(node);
+
+            Y.Assert.areSame(2, tree.getChildrenLength(), 'rootNode should have 2 children.');
+
+            node = tree.getNodeById('two');
+
+            Y.Assert.isUndefined(node, 'node should be undefined.');
+
+            Y.Assert.areSame('three', tree.getChildren()[1].get('id'), 'Second node id should be `three`.');
+        },
+
+        'removeChild() should not remove child TreeNode': function() {
+            var tree = createNewTreeView();
+
+            var node = tree.getNodeById('bogey');
+
+            tree.removeChild(node);
+
+            Y.Assert.areSame(3, tree.getChildrenLength(), 'rootNode should have 3 children');
+
+            Y.Assert.areSame('three', tree.getChildren()[2].get('id'), 'second node id should be `three`');
+        },
+
+        'isRegistered() should find TreeNode': function() {
+            var tree = createNewTreeView();
+
+            var node = tree.getNodeById('two');
+
+            Y.Assert.isTrue(tree.isRegistered(node), 'TreeNode should be registered in TreeView');
+        },
+
+        'isRegistered() should not find TreeNode': function() {
+            var tree = createNewTreeView();
+
+            var node = new Y.TreeNode();
+
+            Y.Assert.isFalse(tree.isRegistered(node), 'TreeNode should be registered in TreeView');
         }
     }));
 
