7af54010b3048fc4deaf319924a1634c938f30b6
Byran Zaugg <byran.zaugg@liferay.com>
2013-10-15 09:33:48 -0700

    AUI-989 Add baseline tests.

--- src/aui-tree/tests/unit/js/tests.js
+++ src/aui-tree/tests/unit/js/tests.js
@@ -3,14 +3,23 @@ YUI.add('module-tests', function(Y) {
     var suite = new Y.Test.Suite('aui-tree');
 
     suite.add(new Y.Test.Case({
-        name: 'Automated Tests',
-        'test is empty': function() {
-            Y.Assert.pass('No Tests Provided For This Module');
+        name: 'Tree View',
+
+        'TreeView constructor should work without a config object': function() {
+            var treeView = new Y.TreeView();
+
+            Y.Assert.isInstanceOf(Y.TreeView, treeView, 'treeView should be an instance of Y.TreeView.');
+        },
+
+        'TreeNode constructor should work without a config object': function() {
+            var treeNode = new Y.TreeNode();
+
+            Y.Assert.isInstanceOf(Y.TreeNode, treeNode, 'treeNode should be an instance of Y.TreeNode.');
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['test']
+    requires: ['aui-tree', 'test']
 });
