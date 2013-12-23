3f08624d6ac11aa0db83a09cf3bc77efa95697a3
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2013-12-23 18:33:39 -0300

    AUI-1102 Source formatting

--- src/aui-base/js/aui-loader.js
+++ src/aui-base/js/aui-loader.js
@@ -492,20 +492,20 @@ Y.mix(YUI.Env[Y.version].modules, {
         "condition": {
             "name": "aui-event-input",
             "test": function(A) {
-    var supportsDOMEvent = A.supportsDOMEvent,
-        testFeature = A.Features.test,
-        addFeature = A.Features.add;
+                var supportsDOMEvent = A.supportsDOMEvent,
+                    testFeature = A.Features.test,
+                    addFeature = A.Features.add;
 
-    if (testFeature('event', 'input') === undefined) {
-        addFeature('event', 'input', {
-            test: function() {
-                return supportsDOMEvent(document.createElement('textarea'), 'input');
-            }
-        });
-    }
+                if (testFeature('event', 'input') === undefined) {
+                    addFeature('event', 'input', {
+                        test: function() {
+                            return supportsDOMEvent(document.createElement('textarea'), 'input');
+                        }
+                    });
+                }
 
-    return !testFeature('event', 'input');
-},
+                return !testFeature('event', 'input');
+            },
             "trigger": "aui-event"
         },
         "requires": [
