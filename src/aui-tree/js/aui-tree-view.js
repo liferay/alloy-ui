9fcd39e835a3152123bd0cd511ee4489dfe74c7d
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-01-16 11:24:11 -0300

    AUI-1114 Source formatting

--- src/aui-tree/js/aui-tree-view.js
+++ src/aui-tree/js/aui-tree-view.js
@@ -783,7 +783,7 @@ var TreeViewDD = A.Component.create({
             // cannot drop the dragged element into any of its children
             // nor above an undraggable element
             // using DOM contains method for performance reason
-            if (!!dropTreeNode.get(DRAGGABLE) && !dragNode.contains(dropNode)) {
+            if ( !! dropTreeNode.get(DRAGGABLE) && !dragNode.contains(dropNode)) {
                 // nArea splits the height in 3 areas top/center/bottom these
                 // areas are responsible for defining the state when the mouse
                 // is over any of them
