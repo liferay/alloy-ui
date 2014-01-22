28a5c2a7d0d66af77092fed2fb02e3924384825b
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-01-22 09:43:04 -0300

    AUI-1111 - Source formatting

--- src/aui-diagram-builder/js/aui-diagram-builder-impl.js
+++ src/aui-diagram-builder/js/aui-diagram-builder-impl.js
@@ -985,9 +985,7 @@ var DiagramBuilder = A.Component.create({
         _onDeleteKey: function(event) {
             var instance = this;
 
-            var target = event.target;
-
-            if (isDiagramNode(A.Widget.getByNode(target))) {
+            if (isDiagramNode(A.Widget.getByNode(event.target))) {
                 instance._deleteSelectedNode(event);
             }
         },
