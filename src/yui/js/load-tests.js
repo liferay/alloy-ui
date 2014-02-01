42377c498d661ff74d7c7c94f190bb2c7b153f4c
Jonathan Mak <jonathan.mak@liferay.com>
2014-01-31 18:10:32 -0800

    Revert "AUI-1102 Source formatting"
    This reverts commit 3f08624d6ac11aa0db83a09cf3bc77efa95697a3.

--- src/yui/js/load-tests.js
+++ src/yui/js/load-tests.js
@@ -11,20 +11,20 @@ add('load', '0', {
 add('load', '1', {
     "name": "aui-event-input",
     "test": function(A) {
-        var supportsDOMEvent = A.supportsDOMEvent,
-            testFeature = A.Features.test,
-            addFeature = A.Features.add;
+    var supportsDOMEvent = A.supportsDOMEvent,
+        testFeature = A.Features.test,
+        addFeature = A.Features.add;
 
-        if (testFeature('event', 'input') === undefined) {
-            addFeature('event', 'input', {
-                test: function() {
-                    return supportsDOMEvent(document.createElement('textarea'), 'input');
-                }
-            });
-        }
+    if (testFeature('event', 'input') === undefined) {
+        addFeature('event', 'input', {
+            test: function() {
+                return supportsDOMEvent(document.createElement('textarea'), 'input');
+            }
+        });
+    }
 
-        return !testFeature('event', 'input');
-    },
+    return !testFeature('event', 'input');
+},
     "trigger": "aui-event"
 });
 // aui-node-html5
