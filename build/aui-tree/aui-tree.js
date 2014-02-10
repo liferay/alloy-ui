11425aa4bcf18929ebadaf40a4b1db8b75c2a0f7
Tibor Lipusz <tibor.lipusz@liferay.com>
2014-02-10 16:05:45 +0100

    AUI-905 Build files
    Signed-off-by: Tibor Lipusz <tibor.lipusz@liferay.com>

--- build/aui-tree/aui-tree.js
+++ build/aui-tree/aui-tree.js
@@ -1308,7 +1308,7 @@ var TreeNode = A.Component.create(
 			syncUI: function() {
 				var instance = this;
 
-				instance._syncHitArea( instance.get( CHILDREN ) );
+				instance._syncHitArea();
 			},
 
 			_afterDraggableChange: function(event) {
@@ -1339,7 +1339,7 @@ var TreeNode = A.Component.create(
 			_afterSetChildren: function(event) {
 				var instance = this;
 
-				instance._syncHitArea(event.newVal);
+				instance._syncHitArea();
 			},
 
 			/**
@@ -1429,10 +1429,10 @@ var TreeNode = A.Component.create(
 			 * @param {Array} children
 			 * @protected
 			 */
-			_syncHitArea: function(children) {
+			_syncHitArea: function() {
 				var instance = this;
 
-				if (instance.get(ALWAYS_SHOW_HITAREA) || children.length) {
+				if (instance.get(ALWAYS_SHOW_HITAREA) || instance.getChildrenLength()) {
 					instance.showHitArea();
 				}
 				else {
