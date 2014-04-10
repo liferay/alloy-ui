8e2dda9df5f046398a354ba49777399a3e6f680c
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-04-10 09:57:12 -0700

    AUI-1250 - Source formatting

--- src/aui-rating/tests/unit/js/tests.js
+++ src/aui-rating/tests/unit/js/tests.js
@@ -1,16 +1,45 @@
 YUI.add('module-tests', function(Y) {
 
-    var suite = new Y.Test.Suite('aui-rating');
+    //--------------------------------------------------------------------------
+    // Rating Tests
+    //--------------------------------------------------------------------------
+
+    var suite = new Y.Test.Suite('aui-rating'),
+        rating,
+        selectedValue = 1.9,
+        roundedValue = Math.round(selectedValue);
+
+    rating = new Y.Rating({
+        boundingBox: '#ratinginput'
+    }).render();
+
+    var ratingRounded = new Y.Rating({
+        boundingBox: '#ratinginput2',
+        defaultSelected: selectedValue,
+        disabled: true
+    }).render();
+
+    //--------------------------------------------------------------------------
+    // Test Case for rounding floats
+    //--------------------------------------------------------------------------
 
     suite.add(new Y.Test.Case({
-        name: 'Automated Tests',
-        'test is empty': function() {
-            Y.Assert.pass('No Tests Provided For This Module');
+        name: 'RoundingFloats',
+
+        //----------------------------------------------------------------------
+        // Tests
+        //----------------------------------------------------------------------
+
+        // Tests: AUI-1250
+        'check that a float passed to defaultSelected is rounded': function() {
+            var instance = this;
+
+            Y.Test.Assert.areEqual(roundedValue, ratingRounded.get('elements').filter('.icon-star').size(), 'The number of selected items should be ' + roundedValue);
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['test']
+    requires: ['aui-rating', 'test']
 });
