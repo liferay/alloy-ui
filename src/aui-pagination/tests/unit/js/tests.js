30ec805cb98472ebf23517637ed46996f4fb3949
Patrick Yeo <patrick.yeo@liferay.com>
2014-04-14 12:36:07 -0700

    AUI-1264 - Test skip to first and last page

--- src/aui-pagination/tests/unit/js/tests.js
+++ src/aui-pagination/tests/unit/js/tests.js
@@ -13,6 +13,11 @@ YUI.add('aui-pagination-tests', function(Y) {
         total: 10
     }).render();
 
+    var pagination_first_last = new Y.Pagination({
+        boundingBox: '#pagination_first_last',
+        total: 10
+    }).render();
+
     suite.add(new Y.Test.Case({
 
         name: 'Pagination control rendering tests',
@@ -37,6 +42,56 @@ YUI.add('aui-pagination-tests', function(Y) {
             item.simulate('click');
 
             Y.Test.Assert.isTrue(item.hasClass('active'));
+        }
+    }));
+
+    //--------------------------------------------------------------------------
+    // Test Case for Skip to First and Last Page
+    //--------------------------------------------------------------------------
+
+    suite.add(new Y.Test.Case({
+
+        name: 'Test Pagination Skip to First and Last Page',
+
+        // Tests: AUI-1264
+        '#1 First li should have text First': function() {
+            var listItems = Y.one('#pagination_first_last').all('li');
+
+            Y.Assert.isTrue(
+                listItems.first().get('text') === 'First', 'First li should have text First');
+        },
+
+        '#2 Last li should have text Last': function() {
+            var listItems = Y.one('#pagination_first_last').all('li');
+
+            Y.Assert.isTrue(
+                listItems.last().get('text') === 'Last', 'Last li should have text Last');
+        },
+
+        '#3 Clicking on last button should move to last page and disable last button': function() {
+            var test = this,
+                listItems = Y.one('#pagination_first_last').all('li'),
+                lastButton = listItems.last(),
+                lastPage = listItems.item(listItems.size() - 3);
+
+            lastButton.simulate('click');
+
+            Y.Assert.isTrue(lastPage.hasClass('active'));
+
+            Y.Assert.isTrue(lastButton.hasClass('disabled'));
+        },
+
+        '#4 Clicking on first button should move to first page and disable first button': function() {
+            var test = this,
+                listItems = Y.one('#pagination_first_last').all('li'),
+                firstButton = listItems.item(0),
+                firstPage = listItems.item(2);
+
+            firstButton.simulate('click');
+
+            Y.Assert.isTrue(firstPage.hasClass('active'));
+
+            Y.Assert.isTrue(firstButton.hasClass('disabled'));
         }
     }));
 
