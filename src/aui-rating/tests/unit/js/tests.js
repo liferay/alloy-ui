66f7f200ea423c9c1170f8913b0e7c7558843a6a
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-04-16 10:35:00 -0300

    grunt format

--- src/aui-rating/tests/unit/js/tests.js
+++ src/aui-rating/tests/unit/js/tests.js
@@ -62,7 +62,8 @@ YUI.add('aui-rating-tests', function(Y) {
         'check that a float passed to defaultSelected is rounded': function() {
             var instance = this;
 
-            Y.Test.Assert.areEqual(roundedValue, ratingRounded.get('elements').filter('.icon-star').size(), 'The number of selected items should be ' + roundedValue);
+            Y.Test.Assert.areEqual(roundedValue, ratingRounded.get('elements').filter('.icon-star').size(),
+                'The number of selected items should be ' + roundedValue);
         }
     }));
 
