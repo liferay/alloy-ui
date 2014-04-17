6d9a75225867f4bf8f557c635889764b6a4fb4ab
Maira Bello <maira.araujo@liferay.com>
2014-04-17 18:06:28 -0300

    AUI-1177 Fixing issue when selectDates is called with a single date
    Casting the param to an array before calling 'map' on it.

--- src/aui-datepicker/js/aui-datepicker.js
+++ src/aui-datepicker/js/aui-datepicker.js
@@ -317,6 +317,12 @@ A.mix(DatePickerBase.prototype, {
             return dates;
         }
 
+        // We allow users to send a single date as this param as well,
+        // so converting to array here.
+        if (!Lang.isArray(dates)) {
+            dates = [dates];
+        }
+
         return dates.map(function(date) {
             // In the date picker the time of the selected day is irrelevant.
             // The calendar module (that is used to provide the dates to be
