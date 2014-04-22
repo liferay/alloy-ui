bc51a0bcf2da01b81ea93d73470623da9275039e
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-04-22 14:34:45 -0300

    AUI-843 Source formatting

--- src/aui-io/tests/unit/js/tests.js
+++ src/aui-io/tests/unit/js/tests.js
@@ -11,20 +11,14 @@ YUI.add('aui-io-tests', function(Y) {
         name: 'IO Request Configuration Tests',
 
         // Tests: AUI-843
-        'assert upper and lower case can be used for method attribute': function() {
-            var ioRequest1 = Y.io.request (
-                '',
-                {
-                    method: 'GET'
-                }
-            );
+        'should method attribute be always lower case': function() {
+            var ioRequest1 = Y.io.request('', {
+                method: 'GET'
+            });
 
-            var ioRequest2 = Y.io.request (
-                '',
-                {
-                    method: 'get'
-                }
-            );
+            var ioRequest2 = Y.io.request('', {
+                method: 'get'
+            });
 
             Y.Assert.areSame(ioRequest1.get('method'), ioRequest2.get('method'));
         }
