2356a79e17ee39e788e391d41ff8b124cf139375
Maira Bello <maira.araujo@liferay.com>
2014-04-23 16:09:47 -0300

    AUI-1281 Button search cancel module misspositioning icon
    Adding a new attribute called 'inputWidth' through which the user can specify the icon's width, in case it can't be reliably calculated (due to missing font/image files for example).

--- src/aui-button/js/aui-button-search-cancel.js
+++ src/aui-button/js/aui-button-search-cancel.js
@@ -189,6 +189,8 @@ var ButtonSearchCancel = A.Base.create('btn-search-cancel', A.Base, [], {
             button = instance.getButtonForElement(element),
             gutter,
             buttonRegion,
+            buttonHeight,
+            buttonWidth,
             elementRegion;
 
         if (!element.val()) {
@@ -201,9 +203,12 @@ var ButtonSearchCancel = A.Base.create('btn-search-cancel', A.Base, [], {
         buttonRegion = button.get('region');
         elementRegion = element.get('region');
 
+        buttonHeight = this.get('iconHeight') ? this.get('iconHeight') : buttonRegion.height;
+        buttonWidth = this.get('iconWidth') ? this.get('iconWidth') : buttonRegion.width;
+
         button.setXY([
-                elementRegion.right - buttonRegion.width + gutter[0],
-                elementRegion.top + elementRegion.height / 2 - buttonRegion.height / 2 + gutter[1]]);
+                elementRegion.right - buttonWidth + gutter[0],
+                elementRegion.top + elementRegion.height / 2 - buttonHeight / 2 + gutter[1]]);
     }
 }, {
     /**
@@ -252,6 +257,32 @@ var ButtonSearchCancel = A.Base.create('btn-search-cancel', A.Base, [], {
         iconClass: {
             validator: Lang.isString,
             value: 'icon-remove'
+        },
+
+        /**
+         * Defines the width of the button. Useful when an async request
+         * for resource file (image or font for example) may be necessary
+         * before calculating the button's width.
+         *
+         * @attribute iconWidth
+         * @default 24
+         * @type {Number}
+         */
+        iconWidth: {
+            value: 24
+        },
+
+        /**
+         * Defines the height of the button. Useful when an async request
+         * for resource file (image or font for example) may be necessary
+         * before calculating the button's height.
+         *
+         * @attribute iconHeight
+         * @default 30
+         * @type {Number}
+         */
+        iconHeight: {
+            value: 30
         },
 
         /**
