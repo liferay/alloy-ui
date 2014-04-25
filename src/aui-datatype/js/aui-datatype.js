9303183e3ea9863e435e67e127228e35d44f6ad5
Henrique Vicente <henriquevicente@gmail.com>
2014-04-25 00:27:15 -0300

    AUI-1174 Validate source code with JSHint (remove extra semicolon)
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- src/aui-datatype/js/aui-datatype.js
+++ src/aui-datatype/js/aui-datatype.js
@@ -694,7 +694,7 @@ A.mix(A.DataType.DateMath, {
      */
     isDayOverlap: function(date1, date2) {
         return ((date1.getFullYear() !== date2.getFullYear()) || (date1.getMonth() !== date2.getMonth()) || (date1.getDate() !==
-            date2.getDate()));;
+            date2.getDate()));
     },
 
     /**
