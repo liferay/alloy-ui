aac9146ea98b8b85e665cc1c004476a932315df0
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-04-25 16:29:21 -0300

    AUI-1282 Fix auto source formatting for long lines auto-breaking (grunt format)

--- src/aui-progressbar/tests/unit/js/tests.js
+++ src/aui-progressbar/tests/unit/js/tests.js
@@ -24,11 +24,21 @@ YUI.add('module-tests', function(Y) {
 
             contextBox = this.progressBar.get('contentBox');
 
-            Y.Assert.isTrue(this.progressBar.get('boundingBox').hasClass('progress'), 'Rendered markup should have the \'progress\' CSS class');
+            Y.Assert.isTrue(
+                this.progressBar.get('boundingBox').hasClass('progress'),
+                'Rendered markup should have the \'progress\' CSS class'
+            );
 
-            Y.Assert.isNotNull(contextBox.hasClass('progress-bar'), 'Rendered markup should have a child with the \'progress-bar\' CSS class');
+            Y.Assert.isNotNull(
+                contextBox.hasClass('progress-bar'),
+                'Rendered markup should have a child with the \'progress-bar\' CSS class'
+            );
 
-            Y.Assert.areEqual(labelText, contextBox.get('text'), 'Rendered markup should have the specified label');
+            Y.Assert.areEqual(
+                labelText,
+                contextBox.get('text'),
+                'Rendered markup should have the specified label'
+            );
         },
 
         'should work with pre rendered markup': function() {
@@ -77,11 +87,35 @@ YUI.add('module-tests', function(Y) {
 
             boundingBox = this.progressBar.get('boundingBox');
 
-            Y.Assert.areEqual(label, boundingBox.getAttribute('aria-label'), 'aria-label should be set to specified label');
-            Y.Assert.areEqual(max, boundingBox.getAttribute('aria-valuemax'), 'aria-valuemax should be set to specified max');
-            Y.Assert.areEqual(min, boundingBox.getAttribute('aria-valuemin'), 'aria-valuemin should be set to specified min');
-            Y.Assert.areEqual(orientation, boundingBox.getAttribute('aria-orientation'), 'aria-orientation should be set to specified orientation');
-            Y.Assert.areEqual(value, boundingBox.getAttribute('aria-valuenow'), 'aria-valuenow should be set to specified value');
+            Y.Assert.areEqual(
+                label,
+                boundingBox.getAttribute('aria-label'),
+                'aria-label should be set to specified label'
+            );
+
+            Y.Assert.areEqual(
+                max,
+                boundingBox.getAttribute('aria-valuemax'),
+                'aria-valuemax should be set to specified max'
+            );
+
+            Y.Assert.areEqual(
+                min,
+                boundingBox.getAttribute('aria-valuemin'),
+                'aria-valuemin should be set to specified min'
+            );
+
+            Y.Assert.areEqual(
+                orientation,
+                boundingBox.getAttribute('aria-orientation'),
+                'aria-orientation should be set to specified orientation'
+            );
+
+            Y.Assert.areEqual(
+                value,
+                boundingBox.getAttribute('aria-valuenow'),
+                'aria-valuenow should be set to specified value'
+            );
         },
 
         'should not add ARIA attributes if requested': function() {
@@ -93,10 +127,14 @@ YUI.add('module-tests', function(Y) {
 
             boundingBox = this.progressBar.get('boundingBox');
 
+            Y.Assert.isFalse(
+                boundingBox.hasAttribute('aria-orientation'),
+                'aria-orientation should not be set'
+            );
+
             Y.Assert.isFalse(boundingBox.hasAttribute('aria-label'), 'aria-label should not be set');
             Y.Assert.isFalse(boundingBox.hasAttribute('aria-valuemax'), 'aria-valuemax should not be set');
             Y.Assert.isFalse(boundingBox.hasAttribute('aria-valuemin'), 'aria-valuemin should not be set');
-            Y.Assert.isFalse(boundingBox.hasAttribute('aria-orientation'), 'aria-orientation should not be set');
             Y.Assert.isFalse(boundingBox.hasAttribute('aria-valuenow'), 'aria-valuenow should not be set');
         },
 
@@ -113,7 +151,12 @@ YUI.add('module-tests', function(Y) {
 
             this.progressBar.set('value', 250);
 
-            Y.Assert.areEqual(0.75, this.progressBar.get('ratio'), 'Calculated ratio should be updated to 0.75');
+            Y.Assert.areEqual(
+                0.75,
+                this.progressBar.get('ratio'),
+                'Calculated ratio should be updated to 0.75'
+            );
+
             Y.Assert.areEqual(75, this.progressBar.get('step'), 'Calculated step should be updated to 75');
         },
 
@@ -129,13 +172,31 @@ YUI.add('module-tests', function(Y) {
 
             contentBox = this.progressBar.get('contentBox');
 
-            Y.Assert.areEqual('80px', contentBox.getStyle('width'), 'The width should be set appropriately on horizontal orientation');
-            Y.Assert.areEqual('100%', contentBox.getStyle('height'), 'The height should be 100% on horizontal orientation');
+            Y.Assert.areEqual(
+                '80px',
+                contentBox.getStyle('width'),
+                'The width should be set appropriately on horizontal orientation'
+            );
+
+            Y.Assert.areEqual(
+                '100%',
+                contentBox.getStyle('height'),
+                'The height should be 100% on horizontal orientation'
+            );
 
             this.progressBar.set('orientation', 'vertical');
 
-            Y.Assert.areEqual('100%', contentBox.getStyle('width'), 'The width should be 100% on vertical orientation');
-            Y.Assert.areEqual('80px', contentBox.getStyle('height'), 'The height should be set appropriately on vertical orientation');
+            Y.Assert.areEqual(
+                '100%',
+                contentBox.getStyle('width'),
+                'The width should be 100% on vertical orientation'
+            );
+
+            Y.Assert.areEqual(
+                '80px',
+                contentBox.getStyle('height'),
+                'The height should be set appropriately on vertical orientation'
+            );
         },
 
         'should fire event when the progress bar is complete': function() {
@@ -147,7 +208,11 @@ YUI.add('module-tests', function(Y) {
                 callCount: 1,
                 method: 'onComplete',
                 run: function() {
-                    Y.Assert.areEqual(1, instance.progressBar.get('ratio'), 'This event should only have been called when ratio is 1');
+                    Y.Assert.areEqual(
+                        1,
+                        instance.progressBar.get('ratio'),
+                        'This event should only have been called when ratio is 1'
+                    );
                 }
             });
 
