b09032214919c655169b71c82b1c8a93bac12896
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-04-25 16:23:53 -0300

    AUI-1281 Source formatting

--- src/aui-button/tests/unit/js/tests.js
+++ src/aui-button/tests/unit/js/tests.js
@@ -18,7 +18,9 @@ YUI.add('aui-button-tests', function(Y) {
                 searchButtonCancel.get('gutter.1');
 
         Y.Assert.areSame(
-            Math.round(iconClose.getY()), Math.round(ypos), 'Y-Position of input and search-btn-cancel should be the same.');
+            Math.round(iconClose.getY()), Math.round(ypos),
+            'Y-Position of input and search-btn-cancel should be the same.');
+
         Y.Assert.areSame(
             iconClose.getX(), xpos, 'X-Position of input and search-btn-cancel should be the same.');
     }
