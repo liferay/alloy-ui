63b3d447126369e798f8f6d5877f024d9c0867dc
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-05-26 20:23:34 -0300

    AUI-1315 Modal size is increased automatically when re-size handler is clicked but not dragged

--- src/aui-modal/js/aui-modal.js
+++ src/aui-modal/js/aui-modal.js
@@ -287,10 +287,20 @@ A.Modal = A.Base.create('modal', A.Widget, [
      */
     _syncResizeDimensions: function(event) {
         var instance = this,
+            boundingBox = instance.get('boundingBox'),
             resize = event.info;
 
-        instance.set('width', resize.offsetWidth);
-        instance.set('height', resize.offsetHeight);
+        instance.set(
+            'width',
+            resize.offsetWidth -
+            parseInt(boundingBox.getComputedStyle('borderRightWidth'), 10) -
+            parseInt(boundingBox.getComputedStyle('borderLeftWidth'), 10));
+
+        instance.set(
+            'height',
+            resize.offsetHeight -
+            parseInt(boundingBox.getComputedStyle('borderTopWidth'), 10) -
+            parseInt(boundingBox.getComputedStyle('borderBottomWidth'), 10));
     }
 }, {
 
