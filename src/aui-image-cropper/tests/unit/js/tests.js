abb759eb32f6ec29e3a58400750d18b8ab0581b7
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-06-02 16:13:25 -0700

    AUI-1369 - Adding test

--- src/aui-image-cropper/tests/unit/js/tests.js
+++ src/aui-image-cropper/tests/unit/js/tests.js
@@ -1,16 +1,55 @@
 YUI.add('aui-image-cropper-tests', function(Y) {
 
+    //--------------------------------------------------------------------------
+    // Image Cropper Tests
+    //--------------------------------------------------------------------------
+
+    var IE = Y.UA.ie;
+
     var suite = new Y.Test.Suite('aui-image-cropper');
+
+    var imageCropper = new Y.ImageCropper({
+        srcNode: '#image',
+        x: 100,
+        y: 100
+    }).render();
+
+    //--------------------------------------------------------------------------
+    // Test Case for resizing Image Cropper
+    //--------------------------------------------------------------------------
 
     suite.add(new Y.Test.Case({
         name: 'Automated Tests',
-        'test is empty': function() {
-            Y.Assert.pass('No Tests Provided For This Module');
+
+        // Tests: AUI-1369
+        'assert resize does not trigger image drag': function() {
+            var bottomRightHandle = Y.one('.yui3-resize-handle-br'),
+                simulationEvent = 'mousedown',
+                test = this;
+
+            if (IE) {
+                if (IE == 10) {
+                    simulationEvent = 'MSPointerDown';
+                }
+                else if (IE > 10) {
+                    simulationEvent = 'pointerdown';
+                }
+            }
+
+            bottomRightHandle.simulate(simulationEvent);
+
+            setTimeout(function() {
+                test.resume(function() {
+                    Y.Assert.isFalse(imageCropper._isDragging(), 'Image should not be dragging');
+                });
+            }, 2500);
+
+            test.wait(3500);
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['test']
+    requires: ['test', 'aui-image-cropper', 'node-event-simulate']
 });
