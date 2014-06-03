9092994484ba47da95613b3746ca3d2fb700ec04
Zeno Rocha <zeno.rocha@liferay.com>
2014-06-03 14:21:38 -0300

    AUI-1328 Move "release" tasks from Grunt to Gulp

---
+++ tasks/clean.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+gulp.task('clean-zip', function () {
+    return gulp.src('*.zip', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('clean-build', function () {
+    return gulp.src('build/**', { read: false })
+        .pipe(clean());
+});
