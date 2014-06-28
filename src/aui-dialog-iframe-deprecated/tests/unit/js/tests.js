87ac683560aa1d6a5d7c6fdbacfe5144d445c68a
michael.bradford@liferay.com <michael.bradford@liferay.com>
2014-06-27 17:06:43 -0700

    AUI-1422 - add test

--- src/aui-dialog-iframe-deprecated/tests/unit/js/tests.js
+++ src/aui-dialog-iframe-deprecated/tests/unit/js/tests.js
@@ -1,16 +1,47 @@
-YUI.add('module-tests', function(Y) {
+YUI.add('aui-dialog-iframe-tests', function(Y) {
 
     var suite = new Y.Test.Suite('aui-dialog-iframe-deprecated');
 
+	var modal = new Y.Modal(
+		{
+			headerContent: '<h3>My Dialog</h3>',
+			width: 640,
+			centered: true,
+			height: 400,
+			zIndex: 1,
+			destroyOnHide: true
+		}
+	)
+	.plug(
+		Y.Plugin.DialogIframe,
+		{
+			uri: '../../../../demos/dialog-iframe-deprecated/assets/content.html',
+			iframeCssClass: 'dialog-iframe'
+		}
+	)
+	.render();
+
     suite.add(new Y.Test.Case({
-        name: 'Automated Tests',
-        'test is empty': function() {
-            Y.Assert.pass('No Tests Provided For This Module');
+        name: 'DialogIframe',
+
+        /**
+         * @tests AUI-1422
+         */
+        'test loading mask is destroyed when host is destroyed': function() {
+        	var maskCssClass = modal.bodyNode.loadingmask.overlayMask.get('cssClass'),
+        		maskNode = Y.one('.' + maskCssClass);
+
+        	modal.hide();
+
+        	this.wait(function() {
+        		Y.Assert.isFalse(maskNode.inDoc());
+        	}, 100);
+
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['test']
+    requires: ['test', 'aui-dialog-iframe-deprecated', 'aui-modal', 'node-event-simulate']
 });
