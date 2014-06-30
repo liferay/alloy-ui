3b74f44c2793dd46a68d1f331c6e82aa5e75d32c
Zeno Rocha <zeno.rocha@liferay.com>
2014-06-30 20:14:09 -0300

    AUI-1428 Missing Git commit SHA in the ZIP comment when running gulp release

--- tasks/compress.js
+++ tasks/compress.js
@@ -1,6 +1,7 @@
 var alloy = require('../.alloy');
 var gulp = require('gulp');
 var path = require('path');
+var spawn = require('child_process').spawn;
 var zip = require('gulp-zip');
 
 var ROOT = path.join(__dirname, '..');
@@ -16,14 +17,41 @@ var files = [
     '!tasks/**'
 ];
 
-gulp.task('compress', ['build'], function() {
-    return gulp.src(files, { cwd: ROOT })
-        .pipe(zip('alloy-' + alloy.version + '.zip'))
-        .pipe(gulp.dest('.', { cwd: ROOT }));
+gulp.task('compress', ['build'], function(callback) {
+    getSHA(function(zipComment) {
+        gulp.src(files, { cwd: ROOT })
+            .pipe(zip('alloy-' + alloy.version + '.zip', {
+                comment: zipComment
+            }))
+            .pipe(gulp.dest('.', { cwd: ROOT }))
+            .on('end', callback);
+    });
 });
 
-gulp.task('compress-cdn', ['cdn'], function() {
-    return gulp.src(files, { cwd: ROOT })
-        .pipe(zip('cdn-alloy-' + alloy.version + '.zip'))
-        .pipe(gulp.dest('.', { cwd: ROOT }));
+gulp.task('compress-cdn', ['cdn'], function(callback) {
+    getSHA(function(zipComment) {
+        gulp.src(files, { cwd: ROOT })
+            .pipe(zip('cdn-alloy-' + alloy.version + '.zip', {
+                comment: zipComment
+            }))
+            .pipe(gulp.dest('.', { cwd: ROOT }))
+            .on('end', callback);
+    });
 });
+
+function getSHA(callback) {
+    var stdout = '';
+
+    var child = spawn('git', ['rev-parse', 'HEAD'], {
+        cwd: ROOT
+    });
+
+    child.stdout.on('data', function(data) {
+        stdout += data.toString();
+    });
+
+    child.on('exit', function(data) {
+        var msg = 'SHA: ' + stdout.trim() + ' on ' + new Date();
+        callback(msg);
+    });
+}
