59be98dc625ff361af58b7b2c3e46b4357a89cfc
Henrique Vicente <henriquevicente@gmail.com>
2014-07-03 13:51:21 -0300

    AUI-1332 Fix augmentation param typo and add PropertyBuilderFieldSupport as a dependency for FormBuilderField
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- src/aui-form-builder/js/aui-form-builder-field.js
+++ src/aui-form-builder/js/aui-form-builder-field.js
@@ -450,7 +450,7 @@ var FormBuilderField = A.Component.create({
      * @type Array
      * @static
      */
-    AUGUMENTS: [A.PropertyBuilderFieldSupport],
+    AUGMENTS: [A.PropertyBuilderFieldSupport],
 
     /**
      * Creates the field id.
