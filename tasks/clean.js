83c230029d4f6d9c0f8b7f462caa42f32003a4c6
Zeno Rocha <zeno.rocha@liferay.com>
2014-07-03 15:59:50 -0300

    AUI-1446 gulp-clean was deprecated in favor of gulp-rimraf

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,35 +1,35 @@
-var clean = require('gulp-clean');
 var gulp = require('gulp');
 var path = require('path');
+var rimraf = require('gulp-rimraf');
 
 var ROOT = path.join(__dirname, '..');
 
 gulp.task('clean-aliases', function() {
     return gulp.src(['src/aui-base/js/aui-aliases.js'], { cwd: ROOT, read: false })
-        .pipe(clean());
+        .pipe(rimraf());
 });
 
 gulp.task('clean-api', function() {
     return gulp.src(['api', 'temp'], { cwd: ROOT, read: false })
-        .pipe(clean());
+        .pipe(rimraf());
 });
 
 gulp.task('clean-bower', function() {
     return gulp.src('bower_components', { cwd: ROOT, read: false })
-        .pipe(clean());
+        .pipe(rimraf());
 });
 
 gulp.task('clean-build', function() {
     return gulp.src('build', { cwd: ROOT, read: false })
-        .pipe(clean());
+        .pipe(rimraf());
 });
 
 gulp.task('clean-npm', function() {
     return gulp.src('node_modules', { cwd: ROOT, read: false })
-        .pipe(clean());
+        .pipe(rimraf());
 });
 
 gulp.task('clean-zip', function() {
     return gulp.src('*.zip', { cwd: ROOT, read: false })
-        .pipe(clean());
+        .pipe(rimraf());
 });
