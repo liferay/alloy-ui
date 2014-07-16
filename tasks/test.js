d87c67d886c51efefdd463b5ce43a5be6fc54c7b
Henrique Vicente <henriquevicente@gmail.com>
2014-07-15 21:06:41 -0300

    AUI-1453 Adding the test-coverage-watch task
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- tasks/test.js
+++ tasks/test.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var path = require('path');
+var report = require('./report');
 var spawn = require('spawn-local-bin');
 
 var ROOT = path.join(__dirname, '..');
 var CWD = process.env.INIT_CWD;
+
+function normalizeCwd(cwd) {
+    if (cwd.substr(-11) === '/tests/unit') {
+        return cwd.substr(0, cwd.length - 11);
+    }
+
+    if (cwd.substr(-6) === '/tests') {
+        return cwd.substr(0, cwd.length - 6);
+    }
+
+    return cwd;
+}
 
 gulp.task('test', function(callback) {
     var args = ['test'];
@@ -14,7 +27,7 @@ gulp.task('test', function(callback) {
         .on('exit', callback);
 });
 
-gulp.task('test-coverage', function(callback) {
+function testCoverage(cwd, callback) {
     var args = ['test', '--coverage',
         '--coverdir', path.join(ROOT, 'coverage')];
     var cmd = 'yogi';
@@ -27,8 +40,28 @@ gulp.task('test-coverage', function(callback) {
                     gutil.colors.yellow('gulp report-browser'));
             }
 
-            callback(err);
+            if (callback) {
+                callback(err);
+            }
         });
+}
+
+gulp.task('test-coverage', function(callback) {
+    testCoverage(CWD, callback);
+});
+
+gulp.task('test-coverage-watch', function() {
+    var files = [
+        path.join(ROOT, 'src/**/*.js'),
+        path.join('!', ROOT, 'src/aui-base/js/*.js'),
+        path.join('!', ROOT, 'src/yui/js/*.js')
+    ];
+
+    gulp.watch(files, function(data) {
+        var cwd = normalizeCwd(path.join(path.dirname(data.path), '..'));
+
+        testCoverage(cwd);
+    });
 });
 
 gulp.task('test-browser', function(callback) {
