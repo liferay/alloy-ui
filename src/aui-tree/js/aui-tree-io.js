8c7d3ab45270ae0ece9c862666d585f41c525441
Jonathan Mak <jonathan.mak@liferay.com>
2014-07-21 17:16:58 -0700

    AUI-1450 - Source formatting

--- src/aui-tree/js/aui-tree-io.js
+++ src/aui-tree/js/aui-tree-io.js
@@ -65,17 +65,20 @@ TreeViewIO.prototype = {
 
 		var paginator = instance.get('paginator');
 
-		A.Array.each(A.Array(nodes), function(node) {
-			var childrenLength = instance.getChildrenLength();
+		A.Array.each(
+			A.Array(nodes),
+			function(node) {
+				var childrenLength = instance.getChildrenLength();
 
-			if (paginator && paginator.total > childrenLength) {
-				return;
+				if (paginator && paginator.total <= childrenLength) {
+					return;
+				}
+
+				instance.appendChild(
+					instance.createNode(node)
+				);
 			}
-
-			instance.appendChild(
-				instance.createNode(node);
-			);
-		});
+		);
 
 		instance._syncPaginatorUI(nodes);
 	},
