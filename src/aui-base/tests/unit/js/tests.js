93e9d25002d2eaa12f07c919c2a212ed8a9a305e
michael.bradford@liferay.com <michael.bradford@liferay.com>
2014-07-25 15:40:38 -0700

    AUI-1551 - Create unit test for camelize

--- src/aui-base/tests/unit/js/tests.js
+++ src/aui-base/tests/unit/js/tests.js
@@ -1,7 +1,12 @@
 YUI.add('aui-base-tests', function(Y) {
 
     var escapedEntities = ['&amp;', '&lt;', '&gt;', '&#034;', '&#039;', '&#047;', '&#096;'],
-        symbolEntities = ['&','<','>','"','\'','/','`'];
+        symbolEntities = ['&','<','>','"','\'','/','`'],
+        uncamelizedStrings = ['this-should-be-camelized',
+                              'SoShould-this-ONE',
+                              'Letterpress-Neutra corNhOlE, pug-retro bespoke-kitsch-cliche +1 fanny pack',
+                              'Thundercats-bespoke-cray-locavore-disrupt,-meggings-quinoa-Marfa-meh-lo-fi. Cray-authentic-Helvetica-PBR&B',
+                              'Bacon-ipsum-dolor-sit-amet shoulder pastrami-pork loin, fatback-boudin-Beef pancettA-andouille beef riBs-meatball'];
 
     var Assert = Y.Assert,
         suite = new Y.Test.Suite('aui-base');
@@ -29,6 +34,42 @@ YUI.add('aui-base-tests', function(Y) {
 
             for (var j = 0; j < symbolEntities.length; j++) {
                 Assert.areEqual(symbols[i], symbolEntities[i]);
+            }
+        },
+
+        'should camelize strings correctly': function() {
+            for (var i = 0; i < uncamelizedStrings.length; i++) {
+                var toBeCamelized = uncamelizedStrings[i],
+                    camelized = Y.Lang.String.camelize(toBeCamelized),
+                    capitalIndices = [],
+                    character = null,
+                    dashCount = 0;
+
+                //find the dash and capitalized indicies
+                for(var j = 0; j < toBeCamelized.length; j++) {
+                    character = toBeCamelized[j];
+
+                    if (character === '-') {
+                        capitalIndices.push(j - dashCount);
+
+                        dashCount++;
+                    }
+                    else if (character.toUpperCase() === character) {
+                        capitalIndices.push(j - dashCount);
+                    }
+                }
+
+                //ensure the result is camelized
+                for (var k = 0; k < camelized.length; k++) {
+                    character = camelized[k];
+
+                    if (capitalIndices.indexOf(k) === -1) {
+                        Assert.areSame(character.toLowerCase(), character);
+                    }
+                    else {
+                        Assert.areSame(character.toUpperCase(), character);
+                    }
+                }
             }
         }
     }));
