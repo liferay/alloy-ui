685b545c42b5c485d76c1d8646b2e82dee951871
Maira Bello <maira.araujo@liferay.com>
2014-08-15 12:34:39 -0300

    AUI-1612 Adding wrapper for textfield and textarea
    When working on this bug I noticed that https://github.com/liferay/alloy-ui/commit/841a65ce71cf048140cc2696a7570626495ddd73 has broken the width feature for textfield and textarea. The wrapper was actually still necessary for that, bootstrap requires it. So I changed aui-form-builder-field to find the actual input inside a templateNode, and make changes on that instead. It now works well for both inputs with wrappers and without them.

--- src/aui-form-builder/js/aui-form-builder-field-text.js
+++ src/aui-form-builder/js/aui-form-builder-field-text.js
@@ -17,9 +17,9 @@ var L = A.Lang,
     CSS_FORM_BUILDER_FIELD = getCN('form-builder-field'),
     CSS_FORM_BUILDER_FIELD_NODE = getCN('form-builder-field', 'node'),
 
-    TPL_INPUT = '<input id="{id}" class="' + [CSS_FORM_BUILDER_FIELD_NODE,
+    TPL_INPUT = '<div class="form-builder-field-wrapper"><input id="{id}" class="' + [CSS_FORM_BUILDER_FIELD_NODE,
         CSS_FIELD_INPUT, CSS_FIELD_INPUT_TEXT,
-        CSS_FORM_CONTROL].join(' ') + '" name="{name}" type="text" value="{value}" />',
+        CSS_FORM_CONTROL].join(' ') + '" name="{name}" type="text" value="{value}" /></div>',
 
     WIDTH_VALUES_MAP = {
         small: 'col-xs-4',
--- src/aui-form-builder/js/aui-form-builder-field-textarea.js
+++ src/aui-form-builder/js/aui-form-builder-field-textarea.js
@@ -16,9 +16,9 @@ var AArray = A.Array,
     CSS_FORM_BUILDER_FIELD = getCN('form-builder-field'),
     CSS_FORM_BUILDER_FIELD_NODE = getCN('form-builder-field', 'node'),
 
-    TPL_TEXTAREA = '<textarea id="{id}" class="' + [
+    TPL_TEXTAREA = '<div class="form-builder-field-wrapper"><textarea id="{id}" class="' + [
         CSS_FORM_BUILDER_FIELD_NODE, CSS_FIELD, CSS_FIELD_TEXT,
-        CSS_FIELD_TEXTAREA, CSS_FORM_CONTROL].join(' ') + '" name="{name}">{value}</textarea>';
+        CSS_FIELD_TEXTAREA, CSS_FORM_CONTROL].join(' ') + '" name="{name}">{value}</textarea></div>';
 
 /**
  * A base class for `A.FormBuilderTextAreaField`.
