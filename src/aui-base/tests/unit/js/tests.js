7f2dcc44e30d8e0f27ed8daa8933290bbfea95cc
Jonathan Mak <jonathan.mak@liferay.com>
2014-08-22 19:57:20 -0700

    AUI-1559 - Source formatting

--- src/aui-base/tests/unit/js/tests.js
+++ src/aui-base/tests/unit/js/tests.js
@@ -26,7 +26,19 @@ YUI.add('aui-base-tests', function(Y) {
             'Lorem ipsum<br />\r lorem-ipsum-dolor-sit-amet \r dolor.'
         ],
         numbersToPad = [1, 10, 2.5, 6.789, 123.4, 3000.3102, .5, .10001, 500000.0],
+        pluralizedStrings = [
+            'apples',
+            'fish',
+            'mailmen',
+            'octopi'
+        ],
         regExCharacters = ['(', ')', '^', '$', '.', '*', '?', '/', '+', '|', '[', ']', '\\'],
+        singularStrings = [
+            'apple',
+            'fish',
+            'mailman',
+            'octopus'
+        ],
         symbolEntities = ['&','<','>','"','\'','/','`'],
         uncamelizedStrings = [
             'lorem-ipsum-dolor-sit-amet',
@@ -34,18 +46,6 @@ YUI.add('aui-base-tests', function(Y) {
             'Lorem-Ipsum-doLOR. sit-amet +1',
             'lorem-ipsum-dolor-sit-amet, LOREM-ipsum-D&OLOR',
             'Lorem-ipsum-dolor-sit-amet. lorem-ipsum-dolor-sit-amet, lorem-Ipsum-Dolor-Sit-Amet'
-        ],
-        singularStrings = [
-            'octopus',
-            'fish',
-            'apple',
-            'mailman'
-        ],
-        pluralizedStrings = [
-            'octopi',
-            'fish',
-            'apples',
-            'mailmen'
         ];
 
     var Assert = Y.Assert,
@@ -166,11 +166,11 @@ YUI.add('aui-base-tests', function(Y) {
 
         'should pluralize known words correctly': function() {
             for (var i = 0; i < singularStrings.length; i++) {
-                Assert.areEqual(Y.Lang.String.pluralize( 0, singularStrings[i], pluralizedStrings[i]), '0 '.concat(pluralizedStrings[i]));
-                Assert.areEqual(Y.Lang.String.pluralize( 1, singularStrings[i], pluralizedStrings[i]), '1 '.concat(singularStrings[i]));
-                Assert.areEqual(Y.Lang.String.pluralize( 2, singularStrings[i], pluralizedStrings[i]), '2 '.concat(pluralizedStrings[i]));
-                Assert.areEqual(Y.Lang.String.pluralize( 3, singularStrings[i], pluralizedStrings[i]), '3 '.concat(pluralizedStrings[i]));
-                Assert.areEqual(Y.Lang.String.pluralize( 4, singularStrings[i], pluralizedStrings[i]), '4 '.concat(pluralizedStrings[i]));
+                Assert.areEqual(Y.Lang.String.pluralize(0, singularStrings[i], pluralizedStrings[i]), '0 '.concat(pluralizedStrings[i]));
+                Assert.areEqual(Y.Lang.String.pluralize(1, singularStrings[i], pluralizedStrings[i]), '1 '.concat(singularStrings[i]));
+                Assert.areEqual(Y.Lang.String.pluralize(2, singularStrings[i], pluralizedStrings[i]), '2 '.concat(pluralizedStrings[i]));
+                Assert.areEqual(Y.Lang.String.pluralize(3, singularStrings[i], pluralizedStrings[i]), '3 '.concat(pluralizedStrings[i]));
+                Assert.areEqual(Y.Lang.String.pluralize(4, singularStrings[i], pluralizedStrings[i]), '4 '.concat(pluralizedStrings[i]));
             }
 
         }
