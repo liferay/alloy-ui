132e2b366d4b3e46a6b68b9c9542972d5cf2dbfd
Maira Bello <maira.araujo@liferay.com>
2014-10-01 17:30:42 -0300

    AUI-1686 Create an editor for text data

---
+++ src/aui-data-editor/js/aui-text-data-editor.js
@@ -0,0 +1,107 @@
+/**
+ * The Text Data Editor Component
+ *
+ * @module aui-text-data-editor
+ */
+
+var CSS_TEXT_DATA_EDITOR = A.getClassName('text', 'data', 'editor');
+
+/**
+ * A base class for Text Data Editor.
+ *
+ * @class A.TextDataEditor
+ * @extends A.DataEditor
+ * @param {Object} config Object literal specifying widget configuration
+ *     properties.
+ * @constructor
+ */
+A.TextDataEditor = A.Base.create('text-data-editor', A.DataEditor, [], {
+    TPL_EDITOR: '<div class="' + CSS_TEXT_DATA_EDITOR + '">' +
+        '<div class="form-group"><label></label>' +
+        '<input type="text" class="form-control"></input></div></div>',
+
+    /**
+     * Constructor for the `A.TextDataEditor`. Lifecycle.
+     *
+     * @method initializer
+     * @protected
+     */
+    initializer: function() {
+        this.after('labelChange', this._afterLabelChange);
+
+        this._uiSetLabel(this.get('label'));
+    },
+
+    /**
+     * Gets the edited value of the data from the editor.
+     *
+     * @method getEditedValue
+     */
+    getEditedValue: function() {
+        return this.get('node').one('.form-control').get('value');
+    },
+
+    /**
+     * Fired after the `label` attribute is set.
+     *
+     * @method _afterLabelChange
+     * @protected
+     */
+    _afterLabelChange: function() {
+        this._uiSetLabel(this.get('label'));
+    },
+
+    /**
+     * Updates the ui according to the value of the `originalValue` attribute.
+     *
+     * @method _uiSetOriginalValue
+     * @param originalValue
+     * @protected
+     */
+    _uiSetOriginalValue: function(originalValue) {
+        this.get('node').one('.form-control').set('value', originalValue);
+    },
+
+    /**
+     * Updates the ui according to the value of the `label` attribute.
+     *
+     * @method _uiSetLabel
+     * @param {String} label
+     * @protected
+     */
+    _uiSetLabel: function(label) {
+        return this.get('node').one('label').set('text', label);
+    }
+}, {
+    /**
+     * Static property used to define the default attribute configuration
+     * for the `A.TextDataEditor`.
+     *
+     * @property ATTRS
+     * @type Object
+     * @static
+     */
+    ATTRS: {
+        /**
+         * The label to be used by this text editor.
+         *
+         * @attribute label
+         * @default ''
+         * @type String
+         */
+        label: {
+            value: ''
+        },
+
+        /**
+         * The value to be edited.
+         *
+         * @attribute originalValue
+         * @default ''
+         * @type String
+         */
+        originalValue: {
+            value: ''
+        }
+    }
+});
---
+++ src/aui-data-editor/tests/unit/js/aui-text-data-editor-tests.js
@@ -0,0 +1,41 @@
+YUI.add('aui-text-data-editor-tests', function(Y) {
+
+    var suite = new Y.Test.Suite('aui-text-data-editor');
+
+    suite.add(new Y.Test.Case({
+        name: 'AUI Text Data Editor Unit Tests',
+
+        'should set original value on the ui': function() {
+            var editor = new Y.TextDataEditor({
+                originalValue: 'original'
+            });
+
+            Y.Assert.areEqual('original', editor.get('node').one('input').get('value'));
+            Y.Assert.areEqual('original', editor.getEditedValue());
+        },
+
+        'should get edited value from the ui': function() {
+            var editor = new Y.TextDataEditor({
+                originalValue: 'original'
+            });
+
+            editor.get('node').one('input').set('value', 'new');
+            Y.Assert.areEqual('new', editor.getEditedValue());
+        },
+
+        'should set label': function() {
+            var editor = new Y.TextDataEditor({
+                label: 'My Label'
+            });
+
+            Y.Assert.areEqual('My Label', editor.get('node').one('label').get('text'));
+
+            editor.set('label', 'New Label');
+            Y.Assert.areEqual('New Label', editor.get('node').one('label').get('text'));
+        }
+    }));
+
+    Y.Test.Runner.add(suite);
+
+
+},'', { requires: [ 'aui-text-data-editor' ] });
