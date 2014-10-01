11d8879489f8643dfd8e82a2519521ac0e96c203
Maira Bello <maira.araujo@liferay.com>
2014-10-01 18:01:43 -0300

    AUI-1686 Create an editor for boolean data

---
+++ src/aui-data-editor/js/aui-boolean-data-editor.js
@@ -0,0 +1,107 @@
+/**
+ * The Boolean Data Editor Component
+ *
+ * @module aui-boolean-data-editor
+ */
+
+var CSS_BOOLEAN_DATA_EDITOR = A.getClassName('boolean', 'data', 'editor');
+
+/**
+ * A base class for Boolean Data Editor.
+ *
+ * @class A.BooleanDataEditor
+ * @extends A.DataEditor
+ * @param {Object} config Object literal specifying widget configuration
+ *     properties.
+ * @constructor
+ */
+A.BooleanDataEditor = A.Base.create('boolean-data-editor', A.DataEditor, [], {
+    TPL_EDITOR: '<div class="' + CSS_BOOLEAN_DATA_EDITOR + '">' +
+        '<div class="form-group"><label></label>' +
+        '<input type="checkbox"></input></div></div>',
+
+    /**
+     * Constructor for the `A.BooleanDataEditor`. Lifecycle.
+     *
+     * @method initializer
+     * @protected
+     */
+    initializer: function() {
+        this.after('labelChange', this._afterLabelChange);
+
+        this._uiSetLabel(this.get('label'));
+    },
+
+    /**
+     * Gets the edited value of the data from the editor.
+     *
+     * @method getEditedValue
+     */
+    getEditedValue: function() {
+        return this.get('node').one('input').get('checked');
+    },
+
+    /**
+     * Fired after the `label` attribute is set.
+     *
+     * @method _afterLabelChange
+     * @protected
+     */
+    _afterLabelChange: function() {
+        this._uiSetLabel(this.get('label'));
+    },
+
+    /**
+     * Updates the ui according to the value of the `originalValue` attribute.
+     *
+     * @method _uiSetOriginalValue
+     * @param originalValue
+     * @protected
+     */
+    _uiSetOriginalValue: function(originalValue) {
+        this.get('node').one('input').set('checked', originalValue);
+    },
+
+    /**
+     * Updates the ui according to the value of the `label` attribute.
+     *
+     * @method _uiSetLabel
+     * @param {String} label
+     * @protected
+     */
+    _uiSetLabel: function(label) {
+        return this.get('node').one('label').set('text', label);
+    }
+}, {
+    /**
+     * Static property used to define the default attribute configuration
+     * for the `A.BooleanDataEditor`.
+     *
+     * @property ATTRS
+     * @type Object
+     * @static
+     */
+    ATTRS: {
+        /**
+         * The label to be used by this boolean editor.
+         *
+         * @attribute label
+         * @default ''
+         * @type String
+         */
+        label: {
+            value: ''
+        },
+
+        /**
+         * The value to be edited.
+         *
+         * @attribute originalValue
+         * @default false
+         * @type Boolean
+         */
+        originalValue: {
+            value: false
+        }
+    }
+});
---
+++ src/aui-data-editor/tests/unit/js/aui-boolean-data-editor-tests.js
@@ -0,0 +1,47 @@
+YUI.add('aui-boolean-data-editor-tests', function(Y) {
+
+    var suite = new Y.Test.Suite('aui-boolean-data-editor');
+
+    suite.add(new Y.Test.Case({
+        name: 'AUI Boolean Data Editor Unit Tests',
+
+        'should set original value on the ui': function() {
+            var editor = new Y.BooleanDataEditor({
+                originalValue: true
+            });
+
+            Y.Assert.isTrue(editor.get('node').one('input').get('checked'));
+            Y.Assert.isTrue(editor.getEditedValue());
+
+            editor.set('originalValue', false);
+            Y.Assert.isFalse(editor.get('node').one('input').get('checked'));
+            Y.Assert.isFalse(editor.getEditedValue());
+        },
+
+        'should get edited value from the ui': function() {
+            var editor = new Y.BooleanDataEditor({
+                originalValue: 'original'
+            });
+
+            editor.get('node').one('input').set('checked', true);
+            Y.Assert.isTrue(editor.getEditedValue());
+
+            editor.get('node').one('input').set('checked', false);
+            Y.Assert.isFalse(editor.getEditedValue());
+        },
+
+        'should set label': function() {
+            var editor = new Y.BooleanDataEditor({
+                label: 'My Label'
+            });
+
+            Y.Assert.areEqual('My Label', editor.get('node').one('label').get('text'));
+
+            editor.set('label', 'New Label');
+            Y.Assert.areEqual('New Label', editor.get('node').one('label').get('text'));
+        }
+    }));
+
+    Y.Test.Runner.add(suite);
+
+},'', { requires: [ 'aui-boolean-data-editor' ] });
