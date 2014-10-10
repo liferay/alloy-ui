9246a3822d84797fe380ed0e7946351533ac7070
Maira Bello <maira.araujo@liferay.com>
2014-10-10 15:57:34 -0300

    AUI-1624 Source formatting

--- src/aui-toggler/js/aui-toggler-base.js
+++ src/aui-toggler/js/aui-toggler-base.js
@@ -380,8 +380,7 @@ var Toggler = A.Component.create({
                     return expand;
                 }
 
-            instance._animation(expand, payload);
-                
+                instance._animation(expand, payload);
             }
             else {
                 instance.set(EXPANDED, expand, payload);
@@ -394,7 +393,9 @@ var Toggler = A.Component.create({
          * Apply animation on `toggle`.
          *
          * @method _animation
-         * @param expand
+         * @param {Boolean} expand
+         * @param {Object} payload
+         * @protected
          */
         _animation: function(expand, payload) {
             var instance = this;
