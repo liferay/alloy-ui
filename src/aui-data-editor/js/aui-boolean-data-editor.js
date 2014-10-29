9540985d21c5cd6e8644303ada29d219b97c8ade
Thiago Rocha <thiago.rocha@liferay.com>
2014-10-29 18:14:31 -0300

    AUI-1691 Add checked and unchecked content to Boolean Date Editor

--- src/aui-data-editor/js/aui-boolean-data-editor.js
+++ src/aui-data-editor/js/aui-boolean-data-editor.js
@@ -17,17 +17,80 @@ var CSS_BOOLEAN_DATA_EDITOR = A.getClassName('boolean', 'data', 'editor');
  */
 A.BooleanDataEditor = A.Base.create('boolean-data-editor', A.DataEditor, [], {
     TPL_EDITOR_CONTENT: '<div class="' + CSS_BOOLEAN_DATA_EDITOR + '">' +
-        '<div class="form-group">' +
+        '<div class="form-group"><div class="content"></div>' +
         '<input type="checkbox"></input></div></div>',
 
     /**
-     * Gets the edited value of the data from the editor.
+     * Constructor for the `A.BooleanDataEditor`. Lifecycle.
      *
-     * @method _getEditedValue
+     * @method initializer
      * @protected
      */
-    _getEditedValue: function() {
-        return this.get('node').one('input').get('checked');
+    initializer: function() {
+        this._setUncheckedContent();
+
+        this.get('node').one('input').after('change', A.bind(this._afterClickCheckbox, this));
+        this.after('editedValueChange', this._afterEditedValueChange);
+    },
+
+    /**
+     * Fired after the checkbox is clicked.
+     *
+     * @method _afterClickCheckbox
+     * @param {CustomEvent} event The fired event
+     * @protected
+     */
+    _afterClickCheckbox: function(event) {
+        this.set('editedValue', event.currentTarget.get('checked'));
+    },
+
+    /**
+     * Fired after the `editedValue` attribute is set.
+     *
+     * @method _afterEditedValueChange
+     * @param {CustomEvent} event The fired event
+     * @protected
+     */
+    _afterEditedValueChange: function(event) {
+        if (event.newVal) {
+            this._setCheckedContent();
+        }
+        else {
+            this._setUncheckedContent();
+        }
+    },
+
+    /**
+     * Updates the ui according to the value of the `checkedContent` attribute.
+     *
+     * @method _setCheckedContent
+     * @protected
+     */
+    _setCheckedContent: function() {
+        this.get('node').one('.content').setHTML(this.get('checkedContent'));
+    },
+
+    /**
+     * Updates the ui according to the value of the `uncheckedContent` attribute.
+     *
+     * @method _setUncheckedContent
+     * @protected
+     */
+    _setUncheckedContent: function() {
+        this.get('node').one('.content').setHTML(this.get('uncheckedContent'));
+    },
+
+    /**
+     * Updates the ui according to the value of the `label` attribute.
+     *
+     * @method _uiSetLabel
+     * @param {String} label
+     * @protected
+     */
+    _uiSetLabel: function(label) {
+        if (!this.get('checkedContent')) {
+            A.BooleanDataEditor.superclass._uiSetLabel.call(this, label);
+        }
     },
 
     /**
@@ -39,6 +102,24 @@ A.BooleanDataEditor = A.Base.create('boolean-data-editor', A.DataEditor, [], {
      */
     _uiSetOriginalValue: function(originalValue) {
         this.get('node').one('input').set('checked', originalValue);
+        this.set('editedValue', originalValue);
+    },
+
+    /**
+     * Updates the ui according to the value of the `visible` attribute.
+     *
+     * @method _uiSetVisible
+     * @param visible
+     * @protected
+     */
+    _setVisible: function(visible) {
+        if (visible) {
+            this.get('node').show();
+        }
+        else {
+            this.get('node').hide();
+        }
+        return visible;
     }
 }, {
     /**
@@ -53,12 +134,33 @@ A.BooleanDataEditor = A.Base.create('boolean-data-editor', A.DataEditor, [], {
         /**
          * The value after edition.
          *
+         * @attribute checkedContent
+         * @default null
+         * @type Node
+         */
+        checkedContent: {
+            value: null
+        },
+
+        /**
+         * The value after edition.
+         *
+         * @attribute uncheckedContent
+         * @default null
+         * @type Node
+         */
+        uncheckedContent: {
+            value: null
+        },
+
+        /**
+         * The value after edition.
+         *
          * @attribute editedValue
          * @default false
          * @type Boolean
          */
         editedValue: {
-            getter: '_getEditedValue',
             value: false
         },
 
@@ -71,6 +173,19 @@ A.BooleanDataEditor = A.Base.create('boolean-data-editor', A.DataEditor, [], {
          */
         originalValue: {
             value: false
+        },
+
+        /**
+         * 
+         *
+         * @attribute visible
+         * @default false
+         * @type Boolean
+         */
+        visible: {
+            lazyAdd: false,
+            setter: '_setVisible',
+            value: true
         }
     }
 });
