2be661e40251afcc83c182aa5dcec78af2a83390
Maira Bello <maira.araujo@liferay.com>
2014-11-06 11:41:27 -0300

    AUI-1772 Improve performance of SchedulerTableView plotEvents method
    This is changing SchedulerViewTable to look for intersections in a single loop and caching them before it starts rendering the view. When the rendering is done, the cached values will be used and a lot of duplicate calculations will be avoided.

--- src/aui-scheduler/js/aui-scheduler-base.js
+++ src/aui-scheduler/js/aui-scheduler-base.js
@@ -207,16 +207,19 @@ A.mix(SchedulerEventSupport.prototype, {
      * @method getEvents
      * @param {Function} filterFn (optional) Filters `events` and returns a list
      *     of events.
+     * @param {Boolean} skipSort
      * @return {Array}
      */
-    getEvents: function(filterFn) {
+    getEvents: function(filterFn, skipSort) {
         var instance = this,
             events = instance._events;
 
         // TODO: Check why the items are not being sorted on add
-        events.sort({
-            silent: true
-        });
+        if (!skipSort) {
+            events.sort({
+                silent: true
+            });
+        }
 
         if (filterFn) {
             events = events.filter(filterFn);
