0d696baf95f0e67e5e55a84ae7e08ade34be04ed
drewbrokke <drew.brokke@liferay.com>
2014-12-05 14:03:05 -0800

    AUI-1783 Add test

--- src/aui-datepicker/tests/unit/js/tests.js
+++ src/aui-datepicker/tests/unit/js/tests.js
@@ -135,6 +135,38 @@ YUI.add('aui-datepicker-tests', function(Y) {
             this.selectDate(1, { ctrlKey: true });
 
             Y.Assert.areEqual(5, selectionChangeCount);
+        },
+
+        'enterKey event should fire when enter key is pressed in the active input': function() {
+            var enterKeydownCount = 0,
+                triggers = [
+                    '#inputTriggerA',
+                    '#inputTriggerB',
+                    '#inputTriggerC',
+                    '#inputTriggerB',
+                    '#inputTriggerA'
+                ];
+
+            this.datePicker = new Y.DatePicker({
+                on: {
+                    enterKey: function() {
+                        enterKeydownCount++;
+                    }
+                },
+                trigger: triggers.join()
+            });
+
+            triggers.forEach(function(trigger) {
+                trigger = Y.one(trigger);
+
+                trigger.focus();
+
+                trigger.simulate('keydown', {
+                    keyCode: 13
+                });
+            });
+
+            Y.Assert.areEqual(5, enterKeydownCount);
         }
     }));
 
