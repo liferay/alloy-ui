9f5027232ebe0b0243e547f5bae37246e8956596
drewbrokke <drew.brokke@liferay.com>
2014-12-05 14:03:05 -0800

    AUI-1783 Add test

--- src/aui-datepicker/tests/unit/js/tests.js
+++ src/aui-datepicker/tests/unit/js/tests.js
@@ -3,14 +3,43 @@ YUI.add('module-tests', function(Y) {
     var suite = new Y.Test.Suite('aui-datepicker');
 
     suite.add(new Y.Test.Case({
-        name: 'Automated Tests',
-        'test is empty': function() {
-            Y.Assert.pass('No Tests Provided For This Module');
+        name: 'Datepicker Tests',
+
+        'enterKey event should fire when enter key is pressed in the active input': function() {
+            var enterKeydownCount = 0,
+                triggers = [
+                    '#inputTriggerA',
+                    '#inputTriggerB',
+                    '#inputTriggerC',
+                    '#inputTriggerB',
+                    '#inputTriggerA'
+                ];
+
+            this.datePicker = new Y.DatePicker({
+                on: {
+                    enterKey: function() {
+                        enterKeydownCount++;
+                    }
+                },
+                trigger: triggers.join()
+            });
+
+            triggers.forEach(function(trigger) {
+                trigger = Y.one(trigger);
+
+                trigger.focus();
+
+                trigger.simulate('keydown', {
+                    keyCode: 13
+                });
+            });
+
+            Y.Assert.areEqual(5, enterKeydownCount);
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['test']
+    requires: ['test', 'aui-datepicker', 'node-event-simulate']
 });
