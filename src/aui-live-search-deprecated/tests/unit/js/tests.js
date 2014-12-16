7ac5d146fc7bbda6f0b0296104874d08b76c01c9
Byran Zaugg <byran.zaugg@liferay.com>
2014-12-16 14:27:50 -0800

    AUI-1817 Add tests.

--- src/aui-live-search-deprecated/tests/unit/js/tests.js
+++ src/aui-live-search-deprecated/tests/unit/js/tests.js
@@ -1,16 +1,28 @@
-YUI.add('module-tests', function(Y) {
+YUI.add('aui-live-search-tests', function(Y) {
 
     var suite = new Y.Test.Suite('aui-live-search-deprecated');
 
     suite.add(new Y.Test.Case({
-        name: 'Automated Tests',
-        'test is empty': function() {
-            Y.Assert.pass('No Tests Provided For This Module');
+        name: 'Live Search',
+
+        'search() should update UI results': function() {
+            var liveSearch = new Y.LiveSearch(
+                {
+                    input: '#myInput',
+                    nodes: '#myList > li'
+                }
+            );
+
+            liveSearch.search('bar');
+
+            var visibleItems = Y.all('#myList > li:not([hidden])');
+
+            Y.Assert.areSame(1, visibleItems.size(), 'size() should return 1.');
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['test']
+    requires: ['aui-live-search-deprecated', 'test']
 });
