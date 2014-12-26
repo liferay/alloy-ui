4fccc2aa406e6fa642d09445732bf16612df1673
Thiago Rocha <thiago.rocha@liferay.com>
2014-12-26 15:40:09 -0300

    AUI-1802 Typo

--- src/aui-form-field/js/aui-form-field-scale.js
+++ src/aui-form-field/js/aui-form-field-scale.js
@@ -70,7 +70,7 @@ A.FormFieldScale = A.Base.create('form-field-scale', A.FormField, [], {
      * Updates the ui according to the value of the `range` attribute.
      *
      * @method _uiSetRange
-     * @param {String} range
+     * @param {Array} range
      * @protected
      */
     _uiSetRange: function(range) {
