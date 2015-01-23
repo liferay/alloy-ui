6c492ff47cdce12d0f59175542b786d6ac711772
weston_hancock <weston.hancock@liferay.com>
2015-01-23 09:47:42 -0800

    AUI-1836 Add unit test for aui-editable-deprecated

--- src/aui-editable-deprecated/HISTORY.md
+++ src/aui-editable-deprecated/HISTORY.md
@@ -1,2 +1,7 @@
-aui-editable-deprecated
-========
+# AUI Editable Deprecated
+
+> Documentation and test modifications are not included in this changelog. For more details, see [full commit history](https://github.com/liferay/alloy-ui/commits/master-deprecated/src/aui-editable-deprecated).
+
+## @VERSION@
+
+* [AUI-1836](https://issues.liferay.com/browse/AUI-1836) Hide editable on window resize
--- src/aui-editable-deprecated/tests/unit/js/tests.js
+++ src/aui-editable-deprecated/tests/unit/js/tests.js
@@ -1,16 +1,37 @@
-YUI.add('module-tests', function(Y) {
+YUI.add('aui-editable-deprecated-tests', function(Y) {
 
     var suite = new Y.Test.Suite('aui-editable-deprecated');
 
     suite.add(new Y.Test.Case({
-        name: 'Automated Tests',
-        'test is empty': function() {
-            Y.Assert.pass('No Tests Provided For This Module');
+        name: 'Editable Tests',
+
+        setUp: function() {
+            Y.one('#logger').append('<div id="editable">Testing</div>');
+            this.editable = Y.one('#editable');
+            this.editableInstance = new Y.Editable({node: this.editable});
+        },
+
+        tearDown: function() {
+            this.editableInstance.destroy();
+            this.editable.remove();
+        },
+
+        'editable should open on click': function() {
+            this.editable.simulate('click');
+            this.editableOpen = Y.one('.editable');
+            Y.Assert.isObject(this.editableOpen);
+        },
+
+        'editable should be hidden on window resize': function() {
+            this.editable.simulate('click');
+            Y.getWin().simulate('resize');
+            this.editableHidden = Y.one('.editable-hidden');
+            Y.Assert.isObject(this.editableHidden);
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['test']
+    requires: ['aui-editable-deprecated', 'node-base', 'node-event-simulate', 'test']
 });
