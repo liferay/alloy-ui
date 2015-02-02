0fe576b39e78ff690db721dae37801d3124e54af
Thiago Rocha <thiago.rocha@liferay.com>
2015-02-02 17:34:46 -0300

    AUI-1787 Implement new scale field's behavior

--- src/aui-form-field/js/aui-form-field-scale.js
+++ src/aui-form-field/js/aui-form-field-scale.js
@@ -5,12 +5,16 @@
  */
 
 var CSS_FIELD_SCALE = A.getClassName('form', 'field', 'scale'),
-    CSS_FIELD_SCALE_INPUT = A.getClassName('form', 'field', 'scale', 'input'),
     CSS_FIELD_SCALE_INPUT_HIGHER_VALUE =
         A.getClassName('form', 'field', 'scale', 'input', 'higher', 'value'),
     CSS_FIELD_SCALE_INPUT_LOWER_VALUE =
         A.getClassName('form', 'field', 'scale', 'input', 'lower', 'value'),
-    CSS_FIELD_SCALE_RANGE = A.getClassName('form', 'field', 'scale', 'range');
+    CSS_FIELD_SCALE_RAIL = A.getClassName('form', 'field', 'scale', 'rail'),
+    CSS_FIELD_SCALE_RANGE = A.getClassName('form', 'field', 'scale', 'range'),
+    CSS_FIELD_SCALE_SLIDER = A.getClassName('form', 'field', 'scale', 'slider'),
+    CSS_FIELD_SCALE_THUMB = A.getClassName('form', 'field', 'scale', 'thumb'),
+    CSS_FIELD_SCALE_THUMB_TOOLTIP = A.getClassName('form', 'field', 'scale', 'thumb', 'tooltip'),
+    CSS_FIELD_SCALE_THUMB_TOOLTIP_INNER = A.getClassName('form', 'field', 'scale', 'thumb', 'tooltip', 'inner');
 
 /**
  * A base class for `A.FormFieldScale`.
@@ -24,8 +28,18 @@ var CSS_FIELD_SCALE = A.getClassName('form', 'field', 'scale'),
 A.FormFieldScale = A.Base.create('form-field-scale', A.FormField, [A.FormFieldRequired], {
     TPL_FIELD_CONTENT: '<div class="' + CSS_FIELD_SCALE_RANGE + ' clearfix">' +
         '<div class="' + CSS_FIELD_SCALE_INPUT_LOWER_VALUE + '"><label></label></div>' +
-        '<div class="' + CSS_FIELD_SCALE_INPUT + '"></div>' +
         '<div class="' + CSS_FIELD_SCALE_INPUT_HIGHER_VALUE + '"><label></label></div>' +
+        '<div class="' + CSS_FIELD_SCALE_SLIDER + '">' +
+        '<div class="' + CSS_FIELD_SCALE_RAIL + '">' +
+        '</div>' +
+        '<div class="' + CSS_FIELD_SCALE_THUMB + '">' +
+        '<div class="' + CSS_FIELD_SCALE_THUMB_TOOLTIP + ' tooltip top" role="tooltip">' +
+        '<div class="tooltip-arrow"></div>' +
+        '<div class="' + CSS_FIELD_SCALE_THUMB_TOOLTIP_INNER + ' tooltip-inner"></div>' +
+        '</div>' +
+        '<p></p>' +
+        '</div>' +
+        '</div>' +
         '</div>',
 
     /**
@@ -36,7 +50,8 @@ A.FormFieldScale = A.Base.create('form-field-scale', A.FormField, [A.FormFieldRe
      */
     initializer: function() {
         this.after({
-            rangeChange: this._afterRangeChange
+            rangeChange: this._afterRangeChange,
+            valueChange: this._afterValueChange
         });
     },
 
@@ -54,6 +69,32 @@ A.FormFieldScale = A.Base.create('form-field-scale', A.FormField, [A.FormFieldRe
         content.addClass(CSS_FIELD_SCALE);
 
         this._uiSetRange(this.get('range'));
+        this._uiSetValue(this.get('value'));
+
+        this._renderDrag();
+    },
+
+    /**
+     * Fired when the `drag:start` event is triggered.
+     *
+     * @method _onDragStart
+     * @protected
+     */
+    _onDragStart: function(event) {
+        event.target.get('dragNode').hide();
+        this.get('content').all('.' + CSS_FIELD_SCALE_THUMB).one('p').hide();
+        this.get('content').all('.' + CSS_FIELD_SCALE_THUMB_TOOLTIP).setStyle('opacity', 1);
+    },
+
+    /**
+     * Fired when the `drag:end` event is triggered.
+     *
+     * @method _onDragEnd
+     * @protected
+     */
+    _onDragEnd: function() {
+        this.get('content').all('.' + CSS_FIELD_SCALE_THUMB).one('p').show();
+        this.get('content').all('.' + CSS_FIELD_SCALE_THUMB_TOOLTIP).setStyle('opacity', 0);
     },
 
     /**
@@ -64,6 +105,70 @@ A.FormFieldScale = A.Base.create('form-field-scale', A.FormField, [A.FormFieldRe
      */
     _afterRangeChange: function() {
         this._uiSetRange(this.get('range'));
+    },
+
+    /**
+     * Fired after the `value` attribute is set.
+     *
+     * @method _afterValueChange
+     * @protected
+     */
+    _afterValueChange: function() {
+        this._uiSetValue(this.get('value'));
+    },
+
+    /**
+     * Sync the thumb position based on `value` attribute.
+     *
+     * @method _onDrag
+     * @param {EventFacade} event
+     * @protected
+     */
+    _onDrag: function(event) {
+        var content = this.get('content'),
+            rail = content.one('.' + CSS_FIELD_SCALE_RAIL),
+            changeScale = (this.get('range')[1] - this.get('range')[0]) / rail.get('offsetWidth'),
+            lowerValue = this.get('range')[0],
+            thumbX = (event.pageX - rail.getX());
+
+        this.set('value', Math.round(thumbX * changeScale) + lowerValue);
+    },
+
+    /**
+     * Plug Drag into Field Scale.
+     *
+     * @method _renderDrag
+     * @protected
+     */
+    _renderDrag: function() {
+        var content = this.get('content'),
+            drag;
+
+        drag = new A.DD.Drag({
+            node: content.one('.' + CSS_FIELD_SCALE_THUMB),
+            dragMode: 'strict'
+        });
+
+        drag.plug(
+            A.Plugin.DDConstrained, {
+                constrain: content.one('.' + CSS_FIELD_SCALE_RAIL),
+                tickY: 10,
+                stickX: true
+            }
+        );
+
+        drag.plug(
+            A.Plugin.DDProxy, {
+                cloneNode: true,
+                moveOnEnd: false
+            }
+        );
+
+        drag.on('drag:start', this._onDragStart, this);
+        drag.on('drag:end', this._onDragEnd, this);
+        drag.on('drag:drag', this._onDrag, this);
+
+        this._drag = drag;
     },
 
     /**
@@ -80,6 +185,26 @@ A.FormFieldScale = A.Base.create('form-field-scale', A.FormField, [A.FormFieldRe
 
         lowerValue.set('text', range[0]);
         higherValue.set('text', range[1]);
+
+        this._uiSetValue(this.get('value'));
+    },
+
+    /**
+     * Updates the ui according to the value of the `value` attribute.
+     *
+     * @method _uiSetValue
+     * @param {Number} value
+     * @protected
+     */
+    _uiSetValue: function(value) {
+        var thumb = this.get('content').one('.' + CSS_FIELD_SCALE_THUMB),
+            range = this.get('range')[1] - this.get('range')[0],
+            lowerValue = this.get('range')[0];
+
+        thumb.setStyle('left', (((value - lowerValue) * 100) / range) + '%');
+        thumb.one('p').setHTML(value);
+
+        this.get('content').one('.' + CSS_FIELD_SCALE_THUMB_TOOLTIP_INNER).setHTML(value);
     }
 }, {
     /**
@@ -107,12 +232,24 @@ A.FormFieldScale = A.Base.create('form-field-scale', A.FormField, [A.FormFieldRe
          * Array with the lower and the higher value to the scale.
          *
          * @attribute range
-         * @default []
+         * @default [1, 10]
          * @type {Array}
          */
         range: {
             validator: A.Lang.isArray,
             value: [1, 10]
+        },
+
+        /**
+         * It's chosen value of field scale.
+         *
+         * @attribute value
+         * @default 0
+         * @type {Number}
+         */
+        value: {
+            validator: A.Lang.isNumber,
+            value: 0
         }
     }
 });
--- src/aui-form-field/tests/unit/js/aui-form-field-scale-tests.js
+++ src/aui-form-field/tests/unit/js/aui-form-field-scale-tests.js
@@ -5,24 +5,104 @@ YUI.add('aui-form-field-scale-tests', function(Y) {
     suite.add(new Y.Test.Case({
         name: 'Form Field Scale Tests',
 
+        /**
+         * Simulates dragging the given dragHandle.
+         *
+         * @param {Y.Test.Case} test
+         * @param {Node} dragHandle
+         * @param {Array} position
+         * @param {Function} callback The function to be called when the simulation is
+         *   done.
+         * @protected
+         */
+        _simulateDrag: function(test, dragHandle, position, callback) {
+            dragHandle.simulate('mousedown');
+            test.wait(function() {
+                Y.one(Y.one(Y.config.doc)).simulate('mousemove', {
+                    clientX: position[0],
+                    clientY: position[1]
+                });
+
+                dragHandle.simulate('mouseup');
+
+                callback();
+            }, Y.DD.DDM.get('clickTimeThresh') + 100);
+        },
+
         tearDown: function() {
             if (this._field) {
                 this._field.destroy();
             }
         },
 
-        'should render input according to range attribute': function() {
+        'should render input according to `range` attribute': function() {
             var fieldNode;
 
-            this._field = new Y.FormFieldScale();
-            this._field.set('range', [2, 11]);
+            this._field = new Y.FormFieldScale({
+                range: [2, 11]
+            });
 
             fieldNode = this._field.get('content');
 
             Y.Assert.areEqual(fieldNode.one('.form-field-scale-input-lower-value').get('text'), 2);
             Y.Assert.areEqual(fieldNode.one('.form-field-scale-input-higher-value').get('text'), 11);
+
+            this._field.set('range', [0, 5]);
+
+            Y.Assert.areEqual(fieldNode.one('.form-field-scale-input-lower-value').get('text'), 0);
+            Y.Assert.areEqual(fieldNode.one('.form-field-scale-input-higher-value').get('text'), 5);
+        },
+
+        'should update thumb position based on `value` attribute change': function() {
+            var thumbNode;
+
+            this._field = new Y.FormFieldScale({
+                range: [1, 3],
+                value: 2
+            });
+
+            thumbNode = this._field.get('content').one('.form-field-scale-thumb');
+
+            Y.Assert.areEqual(thumbNode.getStyle('left'), '50%');
+
+            this._field.set('value', 3);
+            Y.Assert.areEqual(thumbNode.getStyle('left'), '100%');
+        },
+
+        'should update `value` attribute on drag thumb': function() {
+            var instance = this,
+                thumbNode;
+
+            this._field = new Y.FormFieldScale({
+                range: [1, 3],
+                value: 1
+            });
+            Y.one('#container').append(this._field.get('content'));
+            
+            thumbNode = this._field.get('content').one('.form-field-scale-thumb');
+
+            this._simulateDrag(this, thumbNode, [80000, 0], function () {
+                Y.Assert.areEqual(instance._field.get('value'), 3);
+            });
+            
+        },
+
+        'should update thumb position on `range` attribute change': function() {
+            var thumbNode;
+
+            this._field = new Y.FormFieldScale({
+                range: [0, 2],
+                value: 1
+            });
+
+            thumbNode = this._field.get('content').one('.form-field-scale-thumb');
+
+            Y.Assert.areEqual(thumbNode.getStyle('left'), '50%');
+
+            this._field.set('range', [0, 4]);
+            Y.Assert.areEqual(thumbNode.getStyle('left'), '25%');
         }
     }));
 
     Y.Test.Runner.add(suite);
-}, '', {requires: ['aui-form-field-scale', 'test']});
+}, '', {requires: ['aui-form-field-scale', 'node-event-simulate', 'test']});
