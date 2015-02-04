c9db7b452d38cb0aa994a1ba39f30540d20af1c7
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-02-04 12:34:23 -0800

    AUI-1837 - add unit tests

--- src/aui-image-cropper/tests/unit/js/tests.js
+++ src/aui-image-cropper/tests/unit/js/tests.js
@@ -8,11 +8,8 @@ YUI.add('aui-image-cropper-tests', function(Y) {
 
     var suite = new Y.Test.Suite('aui-image-cropper');
 
-    var imageCropper = new Y.ImageCropper({
-        srcNode: '#image',
-        x: 100,
-        y: 100
-    }).render();
+    var cropHeight = 50;
+    var cropWidth = 200;
 
     //--------------------------------------------------------------------------
     // Test Case for resizing Image Cropper
@@ -21,8 +18,28 @@ YUI.add('aui-image-cropper-tests', function(Y) {
     suite.add(new Y.Test.Case({
         name: 'Automated Tests',
 
+        setUp: function() {
+            var imageNode = '<img id="image" src="../../../../demos/image-cropper/assets/lfr-soccer-1.jpg" />';
+
+            Y.one('#imageWrapper').append(imageNode);
+
+            this.imageCropper = new Y.ImageCropper({
+                srcNode: '#image',
+                x: 100,
+                y: 100,
+                cropHeight: cropHeight,
+                cropWidth: cropWidth
+            });
+        },
+
+        tearDown: function() {
+            this.imageCropper.destroy();
+        },
+
         // Tests: AUI-1369
         'assert resize does not trigger image drag': function() {
+            this.imageCropper.render();
+
             var bottomRightHandle = Y.one('.yui3-resize-handle-br'),
                 simulationEvent = 'mousedown',
                 test = this;
@@ -40,11 +57,40 @@ YUI.add('aui-image-cropper-tests', function(Y) {
 
             setTimeout(function() {
                 test.resume(function() {
-                    Y.Assert.isFalse(imageCropper._isDragging(), 'Image should not be dragging');
+                    Y.Assert.isFalse(this.imageCropper._isDragging(), 'Image should not be dragging');
                 });
             }, 2500);
 
             test.wait(3500);
+        },
+
+        // Tests: AUI-1837
+        'assert image cropper works correctly when image is centered': function() {
+            this.imageCropper.render();
+
+            var imageNode = this.imageCropper.get('srcNode'),
+                cropNode = this.imageCropper.cropNode,
+                inRegion = imageNode.intersect(cropNode).inRegion;
+
+            Y.Assert.isTrue(inRegion);
+        },
+
+        'assert cropNode dimensions are correct when image is hidden when rendered': function() {
+            var imageWrapper = Y.one('#imageWrapper');
+
+            imageWrapper.addClass('hide');
+
+            this.imageCropper.render();
+
+            imageWrapper.removeClass('hide');
+
+            var cropNode = this.imageCropper.cropNode;
+
+            var renderedCropHeight = cropNode.outerHeight();
+            var renderedCropWidth = cropNode.outerWidth();
+
+            Y.Assert.areSame(renderedCropHeight, cropHeight);
+            Y.Assert.areSame(renderedCropWidth, cropWidth);
         }
     }));
 
