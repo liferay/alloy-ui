liferay/alloy-ui

commit 23255bcfd14e17053f352b2d7ee49ff8dcaf866f

AUI-1115 Unable to catch the expand event on TreeView

src/aui-tree/js/aui-tree-data.js
@@ -100,6 +100,8 @@ A.mix(TreeData.prototype, {
     initializer: function() {
         var instance = this;
 
+        A.Array.invoke(this.get('children'), 'addTarget', this);
+
         // binding on initializer, needed before .render() phase
         instance.publish('move');
         instance.publish('append', {
@@ -108,6 +110,8 @@ A.mix(TreeData.prototype, {
         instance.publish('remove', {
             defaultFn: instance._removeChild
         });
+
+        instance.after('childrenChange', A.bind(instance._afterChildrenChange, instance));
     },
 
     /**
@@ -456,6 +460,18 @@ A.mix(TreeData.prototype, {
         var output = instance.getEventOutputMap(node);
 
         instance.bubbleEvent('append', output, cancelBubbling);
+    },
+ 
+    /**
+     * Fires after children changes.
+     *
+     * @method _afterChildrenChange
+     * @param {EventFacade} event
+     * @protected
+     */
+    _afterChildrenChange: function(event) {
+        A.Array.invoke(event.prevVal, 'removeTarget', this);
+        A.Array.invoke(event.newVal, 'addTarget', this);
     },
 
     /**

src/aui-tree/tests/unit/js/tests.js
@@ -764,6 +764,42 @@ YUI.add('aui-tree-tests', function(Y) {
                     Y.Assert.isTrue(content.hasClass('tree-collapsed'));
                 }
             });
+        },
+
+        'should fire expandedChange event on click': function() {
+            var treeView,
+            expanded = false;
+
+            treeView = new Y.TreeView({
+                children: [
+                    {
+                        children: [
+                            {
+                                id: 'one-one'
+                            },
+                            {
+                                id: 'one-two'
+                            },
+                            {
+                                id: 'one-three'
+                            },
+                            {
+                                id: 'one-four'
+                            }
+                        ],
+                        id: 'one'
+                    }
+                ]
+            }).render();
+
+            treeView.after('tree-node:expandedChange', function () {
+                expanded = true;
+            });
+
+            setTimeout(function () {
+                Y.one('.tree-hitarea').simulate('click');
+                Y.Assert.isTrue(expanded);
+            }, 100); 
         }
     }));
 

commit eaf679e145d841b391b089a0ab0f6b463207db46

AUI-1115 Reverting unnecessary changes and fixing test to listen to nested events
The changes done in https://github.com/liferay/alloy-ui/commit/23255bcfd14e17053f352b2d7ee49ff8dcaf866f caused regressions in some cases, and in the end we noticed that they're not necessary for allowing users to listen to expand events on tree nodes. So, I'm removing the code changes.

src/aui-tree/js/aui-tree-data.js
@@ -100,8 +100,6 @@ A.mix(TreeData.prototype, {
     initializer: function() {
         var instance = this;
 
-        A.Array.invoke(this.get('children'), 'addTarget', this);
-
         // binding on initializer, needed before .render() phase
         instance.publish('move');
         instance.publish('append', {
@@ -110,8 +108,6 @@ A.mix(TreeData.prototype, {
         instance.publish('remove', {
             defaultFn: instance._removeChild
         });
-
-        instance.after('childrenChange', A.bind(instance._afterChildrenChange, instance));
     },
 
     /**
@@ -460,18 +456,6 @@ A.mix(TreeData.prototype, {
         var output = instance.getEventOutputMap(node);
 
         instance.bubbleEvent('append', output, cancelBubbling);
-    },
- 
-    /**
-     * Fires after children changes.
-     *
-     * @method _afterChildrenChange
-     * @param {EventFacade} event
-     * @protected
-     */
-    _afterChildrenChange: function(event) {
-        A.Array.invoke(event.prevVal, 'removeTarget', this);
-        A.Array.invoke(event.newVal, 'addTarget', this);
     },
 
     /**