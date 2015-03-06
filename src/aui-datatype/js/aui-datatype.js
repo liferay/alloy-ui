37e2b3a6db644a84f1cafe15a494e385b7afba98
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2015-03-06 19:47:03 -0300

    AUI-1864 Method for counting number of days between dates

--- src/aui-datatype/js/aui-datatype.js
+++ src/aui-datatype/js/aui-datatype.js
@@ -366,7 +366,35 @@ A.mix(A.DataType.DateMath, {
     },
 
     /**
-     * Subtract the specified amount of time from the this instance.
+     * Counts the number of days between two dates excluding the last one. The
+     * order of the dates is not important.
+     *
+     * For example, if the first one is March 8, 2015 and the second one is
+     * March 14, 2015, then the returned value should be 6. If the first one is
+     * March 29, 2015 and the second one is April 4, 2015, then the returned
+     * value should be 6.
+     *
+     * This method iterates over all days between the dates so it can be slow
+     * for dates that are too much far from one another.
+     *
+     * @method countDays
+     * @param d1 One of the days
+     * @param d2 The other day
+     * @return the number of days between the two dates.
+     */
+    countDays: function(d1, d2) {
+        var count = 0;
+        var step = this.before(d1, d2) ? 1 : -1;
+
+        for (var d = d1; this.isDayOverlap(d, d2); d = this.add(d, this.DAY, step)) {
+            count++;
+        }
+
+        return count;
+    },
+
+    /**
+     * Subtracts the specified amount of time from the this instance.
      *
      * @method subtract
      * @param {Date} date	The JavaScript Date object to perform subtraction on
