a1ac401247513d542447e8aa2966edb5e2011d56
Maira Bello <maira.araujo@liferay.com>
2015-03-09 16:55:33 -0300

    AUI-1864 Source formatting

--- src/aui-datatype/tests/unit/js/tests.js
+++ src/aui-datatype/tests/unit/js/tests.js
@@ -6,24 +6,24 @@ YUI.add('module-tests', function(Y) {
         name: 'Automated Tests',
 
         'should count the number of days between two dates': function() {
-            var d1 = new Date();
-            var d2 = Y.DataType.DateMath.add(d1, Y.DataType.DateMath.DAY, 5);
+            var d1 = new Date(),
+                d2 = Y.DataType.DateMath.add(d1, Y.DataType.DateMath.DAY, 5);
 
             Y.Assert.areEqual(
                 5,
                 Y.DataType.DateMath.countDays(d1, d2),
-                'There should count five days between dates'
+                'It should count five days between dates'
             );
         },
 
         'should count if latter date is given first': function() {
-            var d1 = new Date();
-            var d2 = Y.DataType.DateMath.add(d1, Y.DataType.DateMath.DAY, 5);
+            var d1 = new Date(),
+                d2 = Y.DataType.DateMath.add(d1, Y.DataType.DateMath.DAY, 5);
 
             Y.Assert.areEqual(
                 5,
                 Y.DataType.DateMath.countDays(d2, d1),
-                'There should count five days between dates'
+                'It should count five days between dates'
             );
         }
 
