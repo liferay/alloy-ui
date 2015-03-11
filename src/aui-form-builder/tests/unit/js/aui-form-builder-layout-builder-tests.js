3de376cc8f4f0495042ee3a47c187bc55f4dafab
Maira Bello <maira.araujo@liferay.com>
2015-03-11 13:34:11 -0300

    AUI-1859 Source formatting

--- src/aui-form-builder/tests/unit/js/aui-form-builder-layout-builder-tests.js
+++ src/aui-form-builder/tests/unit/js/aui-form-builder-layout-builder-tests.js
@@ -466,21 +466,18 @@ YUI.add('aui-form-builder-layout-builder-tests', function(Y) {
                 rowNode,
                 visibleTargets;
 
-            this._createFormBuilder({
-                mode: Y.FormBuilder.MODES.REGULAR
-            });
+            this._createFormBuilder();
 
             this._formBuilder.get('layout').get('rows')[1].get('cols')[2].set('movableContent', false);
 
-            this._toolbar = this._formBuilder._fieldToolbar;
             this._openToolbar();
 
-            moveItem = this._toolbar._toolbar.one('.glyphicon-move').ancestor();
+            moveItem = Y.one('.glyphicon-move').ancestor();
+            moveItem.simulate('click');
 
             row = this._formBuilder.get('layout').get('rows')[1];
             rowNode = row.get('node');
 
-            moveItem.simulate('click');
             visibleTargets = rowNode.all('.form-builder-field-move-target').filter(function(node) {
                 return node.getStyle('visibility') !== 'hidden' && node.getStyle('display') !== 'none';
             });
