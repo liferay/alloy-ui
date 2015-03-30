585f6ec6a267550b514302a1c983978b9b5d93c5
Thiago Rocha <thiago.rocha@liferay.com>
2015-03-30 14:42:09 -0300

    AUI-1853 Use default checkbox/radio/dropdown for FormBuilderChoiceField

--- src/aui-form-field/js/aui-form-field-choice.js
+++ src/aui-form-field/js/aui-form-field-choice.js
@@ -5,25 +5,34 @@
  */
 
 var CSS_FIELD_CHOICE = A.getClassName('form', 'field', 'choice'),
+    CSS_FIELD_CHOICE_CONTENT = A.getClassName('form', 'field', 'choice', 'content'),
     CSS_FIELD_CHOICE_CHECKBOX = A.getClassName('form', 'field', 'choice', 'checkbox'),
-    CSS_FIELD_CHOICE_DROPDOWN = A.getClassName('form', 'field', 'choice', 'dropdown'),
-    CSS_FIELD_CHOICE_DROPDOWN_CONTENT = A.getClassName('form', 'field', 'choice', 'dropdown', 'content'),
-    CSS_FIELD_CHOICE_DROPDOWN_PLACEHOLDER = A.getClassName('form', 'field', 'choice', 'dropdown', 'placeholder'),
-    CSS_FIELD_CHOICE_DROPDOWN_TOGGLE = A.getClassName('form', 'field', 'choice', 'dropdown', 'toggle'),
-    CSS_FIELD_CHOICE_OPTION = A.getClassName('form', 'field', 'choice', 'option'),
-    CSS_FIELD_CHOICE_OPTION_BUTTON = A.getClassName('form', 'field', 'choice', 'option', 'button'),
-    CSS_FIELD_CHOICE_OPTION_OTHER = A.getClassName('form', 'field', 'choice', 'option', 'other'),
-    CSS_FIELD_CHOICE_OPTIONS = A.getClassName('form', 'field', 'choice', 'options'),
+    CSS_FIELD_CHOICE_LIST = A.getClassName('form', 'field', 'choice', 'list'),
+    CSS_FIELD_CHOICE_FORM = A.getClassName('form', 'field', 'choice', 'form'),
     CSS_FIELD_CHOICE_RADIO = A.getClassName('form', 'field', 'choice', 'radio'),
+    CSS_FIELD_CHOICE_OPTION = A.getClassName('form', 'field', 'choice', 'option'),
+    CSS_FIELD_CHOICE_OPTIONS_CONTAINER =
+        A.getClassName('form', 'field', 'choice', 'options', 'container'),
+    CSS_FIELD_CHOICE_OPTION_OTHER =
+        A.getClassName('form', 'field', 'choice', 'option', 'other'),
 
-    TPL_FIELD_CHOICE_OPTION = '<div class="' + CSS_FIELD_CHOICE_OPTION + ' cleafix">' +
-        '<div class="' + CSS_FIELD_CHOICE_OPTION_BUTTON + '"></div>' +
-        '<label>{label}</label></div>',
+    TPL_FIELD_CHOICE_FORM =
+        '<form class="' + CSS_FIELD_CHOICE_FORM + ' ' +
+        CSS_FIELD_CHOICE_OPTIONS_CONTAINER + '"></form>',
+    TPL_FIELD_CHOICE_FORM_OPTION =
+        '<div class="' + CSS_FIELD_CHOICE_OPTION + '">' +
+        '<input type="{type}" name="option"></input><label>{label}</label></div>',
+    TPL_FIELD_CHOICE_LIST =
+        '<select class="' + CSS_FIELD_CHOICE_LIST + ' ' +
+        CSS_FIELD_CHOICE_OPTIONS_CONTAINER + '"></select>',
+    TPL_FIELD_CHOICE_LIST_OPTION =
+        '<option action="" class="' + CSS_FIELD_CHOICE_OPTION +
+        '"><label>{label}</label></option>',
 
     TYPES = {
-        RADIO: 'radio',
         CHECKBOX: 'checkbox',
-        DROPDOWN: 'dropdown'
+        LIST: 'list',
+        RADIO: 'radio'
     };
 
 /**
@@ -36,11 +45,7 @@ var CSS_FIELD_CHOICE = A.getClassName('form', 'field', 'choice'),
  * @constructor
  */
 A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormFieldRequired], {
-    TPL_FIELD_CONTENT: '<div class="' + CSS_FIELD_CHOICE_OPTIONS + ' form-group"></div>' +
-        '<div class="' + CSS_FIELD_CHOICE_DROPDOWN_CONTENT + '">' +
-        '<div class="' + CSS_FIELD_CHOICE_DROPDOWN_TOGGLE + '">' +
-        '<span class="glyphicon glyphicon-chevron-down"></span></div>' +
-        '<div class="' + CSS_FIELD_CHOICE_DROPDOWN_PLACEHOLDER + '">Select an option</div></div>',
+    TPL_FIELD_CONTENT: '<div class ="' + CSS_FIELD_CHOICE_CONTENT + '"></div>',
 
     /**
      * Constructor for the `A.FormFieldChoice`. Lifecycle.
@@ -49,6 +54,8 @@ A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormField
      * @protected
      */
     initializer: function() {
+        this._oldTypeValue = this.get('type');
+
         this.after({
             optionsChange: this._afterOptionsChange,
             otherOptionChange: this._afterOtherOptionChange,
@@ -69,9 +76,8 @@ A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormField
 
         content.addClass(CSS_FIELD_CHOICE);
 
+        this._updateOptionsContainer();
         this._uiSetOptions(this.get('options'));
-        this._uiSetOtherOption(this.get('otherOption'));
-        this._uiSetType(this.get('type'));
     },
 
     /**
@@ -98,31 +104,79 @@ A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormField
      * Fired after the `type` attribute is set.
      *
      * @method _afterTypeChange
-     * @param {EventFacade} event
      * @protected
      */
-    _afterTypeChange: function(event) {
-        this.get('content').removeClass(this._getClassForType(event.prevVal));
+    _afterTypeChange: function() {
         this._uiSetType(this.get('type'));
     },
 
     /**
-     * Gets the appropriate css class for the given type.
+     * Remove all option element from ui.
      *
-     * @method _getClassForType
-     * @param {String} type
-     * @return {String}
+     * @method _cleanOptionsContainer
      * @protected
      */
-    _getClassForType: function(type) {
+    _cleanOptionsContainer: function() {
+        this.get('content').one('.' + CSS_FIELD_CHOICE_OPTIONS_CONTAINER).empty();
+    },
+
+    /**
+     * Create a option node.
+     *
+     * @method _createOptionNode
+     * @param {String} optionLabel
+     * @return {Node} A new Option Node
+     * 
+     * @protected
+     */
+    _createOptionNode: function(optionLabel) {
+        var currentType = this.get('type');
+
+        if (currentType === TYPES.LIST) {
+            return A.Node.create(A.Lang.sub(TPL_FIELD_CHOICE_LIST_OPTION, {
+                label: optionLabel
+            }));
+        }
+        else {
+            return A.Node.create(A.Lang.sub(TPL_FIELD_CHOICE_FORM_OPTION, {
+                label: optionLabel,
+                type: currentType
+            }));
+        }
+    },
+    /**
+     * Gets the appropriate css class to form for the given type.
+     *
+     * @method _getFormClass
+     * @param {String} type
+     * @return {String}
+     */
+    _getFormClass: function(type) {
         switch (type) {
             case TYPES.CHECKBOX:
                 return CSS_FIELD_CHOICE_CHECKBOX;
             case TYPES.RADIO:
                 return CSS_FIELD_CHOICE_RADIO;
-            case TYPES.DROPDOWN:
-                return CSS_FIELD_CHOICE_DROPDOWN;
-        }
+    }
+},
+    /**
+     * Switch option type between radio and checkbox.
+     *
+     * @method _setFormType
+     * @param {String} type
+     * @protected
+     */
+    _setFormType: function(type) {
+        var content = this.get('content'),
+            optionsContainer = content.one('.' +CSS_FIELD_CHOICE_OPTIONS_CONTAINER),
+            elements = content.all('.' + CSS_FIELD_CHOICE_OPTION);
+
+        optionsContainer.replaceClass(this._getFormClass(this._oldTypeValue),
+            this._getFormClass(type));
+
+        elements.each(function(element) {
+            element.set('type', type);
+        });
     },
 
     /**
@@ -133,19 +187,19 @@ A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormField
      * @protected
      */
     _uiSetOptions: function(options) {
-        var optionsContainer = this.get('content').one('.' + CSS_FIELD_CHOICE_OPTIONS),
-            otherOptionNode = this.get('content').one('.' + CSS_FIELD_CHOICE_OPTION_OTHER),
-            optionNode;
+        var instance = this,
+            optionsContainer,
+            optionTemplate;
 
-        optionsContainer.empty();
+        this._cleanOptionsContainer();
+        optionsContainer = this.get('content').one('.' + CSS_FIELD_CHOICE_OPTIONS_CONTAINER);
+
         A.Array.each(options, function(option) {
-            optionNode = A.Node.create(A.Lang.sub(TPL_FIELD_CHOICE_OPTION, {
-                label: option
-            }));
-            optionsContainer.append(optionNode);
+            optionTemplate = instance._createOptionNode(option);
+            optionsContainer.append(optionTemplate);
         });
 
-        optionsContainer.append(otherOptionNode);
+        this._uiSetOtherOption(this.get('otherOption'));
     },
 
     /**
@@ -156,21 +210,18 @@ A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormField
      * @protected
      */
     _uiSetOtherOption: function(otherOption) {
-        var optionsContainer = this.get('content').all('.' + CSS_FIELD_CHOICE_OPTIONS),
-            optionNode;
+        var optionsContainer = this.get('content').one('.' + CSS_FIELD_CHOICE_OPTIONS_CONTAINER),
+            otherOptionNode = this.get('content').one('.' + CSS_FIELD_CHOICE_OPTION_OTHER),
+            optionTemplate;
+
+        if (otherOptionNode) {
+            otherOptionNode.remove();
+        }
 
         if (otherOption) {
-            optionNode = A.Node.create(A.Lang.sub(TPL_FIELD_CHOICE_OPTION, {
-                label: 'Other'
-            }));
-            optionNode.addClass(CSS_FIELD_CHOICE_OPTION_OTHER);
-            optionsContainer.append(optionNode);
-        }
-        else {
-            optionNode = this.get('content').one('.' + CSS_FIELD_CHOICE_OPTION_OTHER);
-            if (optionNode) {
-                optionNode.remove(true);
-            }
+            optionTemplate = this._createOptionNode('Other');
+            optionTemplate.addClass(CSS_FIELD_CHOICE_OPTION_OTHER);
+            optionsContainer.append(optionTemplate);
         }
     },
 
@@ -182,7 +233,39 @@ A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormField
      * @protected
      */
     _uiSetType: function(type) {
-        this.get('content').addClass(this._getClassForType(type));
+        if (type === TYPES.LIST || this._oldTypeValue === TYPES.LIST) {
+            this._updateOptionsContainer();
+            this._uiSetOptions(this.get('options'));
+            this._uiSetOtherOption(this.get('otherOption'));
+        }
+        else {
+            this._setFormType(this.get('type'));
+        }
+
+        this._oldTypeValue = type;
+    },
+
+    /**
+     * Set a new options container based on `type` attribute.
+     *
+     * @method _updateOptionsContainer
+     * @protected
+     */
+    _updateOptionsContainer: function() {
+        var content = this.get('content').one('.' + CSS_FIELD_CHOICE_CONTENT),
+            optionsContainer;
+
+        content.empty();
+
+        if (this.get('type') === TYPES.LIST) {
+            optionsContainer = A.Node.create(TPL_FIELD_CHOICE_LIST);
+        }
+        else {
+            optionsContainer = A.Node.create(TPL_FIELD_CHOICE_FORM);
+            optionsContainer.addClass(this._getFormClass(this.get('type')));
+        }
+
+        content.append(optionsContainer);
     }
 }, {
     /**
@@ -194,18 +277,6 @@ A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormField
      * @static
      */
     ATTRS: {
-        /**
-         * If the user can choose multiple options, or just one.
-         *
-         * @attribute multiple
-         * @default false
-         * @type Boolean
-         */
-        multiple: {
-            validator: A.Lang.isBoolean,
-            value: false
-        },
-
         /**
          * Id to reference form data after a form is submitted.
          *
@@ -244,7 +315,8 @@ A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormField
 
         /**
          * The type of the choice field. Can be any of the values listed in
-         * the `A.FormFieldChoice.TYPES` constant (radio, checkbox or dropdown);
+         * the `A.FormFieldChoice.TYPES` constant (radio, checkbox or list).
+         * @default radio
          * @type {Object}
          */
         type: {
