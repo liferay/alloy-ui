005ca93a142513e865ad0282b7c81c199de2f037
Byran Zaugg <byran.zaugg@liferay.com>
2015-08-27 15:52:43 -0700

    AUI-1958 Revert AUI-533. Custom rules should not be required.

--- src/aui-form-validator/js/aui-form-validator.js
+++ src/aui-form-validator/js/aui-form-validator.js
@@ -897,7 +897,7 @@ var FormValidator = A.Component.create({
                 var required = instance.normalizeRuleValue(fieldRules.required);
 
                 validatable = (required || (!required && defaults.RULES.required.apply(instance, [field.val(),
-                    field])) || fieldRules.custom);
+                    field])));
             }
 
             return !!validatable;
