c1c3052c4a1a20ae4d97a914f25c1be0da28fe39
Maira Bello <maira.araujo@liferay.com>
2015-09-25 11:24:42 -0300

    AUI-1996 Fixes lint warning

--- src/aui-scheduler/tests/unit/js/tests.js
+++ src/aui-scheduler/tests/unit/js/tests.js
@@ -674,7 +674,7 @@ YUI.add('aui-scheduler-tests', function(Y) {
             );
         },
 
-        "an absent skipSyncUI event property shouldn't prevent syncEventsUI() being called": function() {
+        'an absent skipSyncUI event property shouldn\'t prevent syncEventsUI() being called': function() {
             var displayDate = new Date(2015, 9, 24);
             var eventDate = new Date(2015, 9, 24);
 
