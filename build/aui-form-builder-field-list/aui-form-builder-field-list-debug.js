729e17c91c04f62bd011cd240c9ef616d5527c9f
Jonathan Mak <jonathan.mak@liferay.com>
2015-12-18 12:34:02 -0800

    AUI-1891 - Build

---
+++ build/aui-form-builder-field-list/aui-form-builder-field-list-debug.js
@@ -0,0 +1,308 @@
+YUI.add('aui-form-builder-field-list', function (A, NAME) {
+
+/**
+ * The Form Builder Field List Component
+ *
+ * @module aui-form-builder-field-list
+ */
+
+var CSS_FIELD_LIST = A.getClassName('form', 'builder', 'field', 'list'),
+    CSS_FIELD_LIST_ADD_BUTTON =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button'),
+        CSS_FIELD_LIST_ADD_BUTTON_ICON =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'icon'),
+    CSS_FIELD_LIST_ADD_BUTTON_LABEL =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'label'),
+        CSS_FIELD_LIST_ADD_BUTTON_PLUS_ICON =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'plus', 'icon'),
+        CSS_FIELD_LIST_ADD_BUTTON_PLUS_LABEL =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'plus', 'label'),
+    CSS_FIELD_LIST_ADD_BUTTON_VISIBLE =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'visible'),
+    CSS_FIELD_LIST_ADD_CONTAINER =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'container'),
+    CSS_FIELD_LIST_CONTAINER =
+        A.getClassName('form', 'builder', 'field', 'list', 'container'),
+    CSS_FIELD_LIST_EMPTY = A.getClassName('form', 'builder', 'field', 'list', 'empty'),
+    CSS_FIELD_MOVE_TARGET =
+        A.getClassName('form', 'builder', 'field', 'move', 'target'),
+    CSS_LIST_MOVE_TARGET =
+        A.getClassName('form', 'builder', 'list', 'move', 'target');
+
+/**
+ * A base class for `A.FormBuilderFieldList`.
+ *
+ * @class A.FormBuilderFieldList
+ * @extends A.Base
+ * @param {Object} config Object literal specifying widget configuration
+ *     properties.
+ * @constructor
+ */
+A.FormBuilderFieldList  = A.Base.create('form-builder-field-list', A.Base, [], {
+    TPL_ADD_FIELD: '<div class="' + CSS_FIELD_LIST_ADD_CONTAINER + '">' +
+        '<div class="' + CSS_FIELD_LIST_ADD_BUTTON + ' ' +
+        CSS_FIELD_LIST_ADD_BUTTON_VISIBLE + '" tabindex="9">' +
+        '<div class="' + CSS_FIELD_LIST_ADD_BUTTON_ICON + '">' +
+        '<div class="' + CSS_FIELD_LIST_ADD_BUTTON_PLUS_ICON + '">+</div>' +
+        '</div>' +
+        '<label class="' + CSS_FIELD_LIST_ADD_BUTTON_LABEL + '">' +
+        '<label class="' + CSS_FIELD_LIST_ADD_BUTTON_PLUS_LABEL + '">+</label> ADD FIELD' +
+        '</label>' +
+        '</div></div>',
+    TPL_FIELD_LIST: '<div class="' + CSS_FIELD_LIST + '">' +
+        '<div class="' + CSS_FIELD_LIST_CONTAINER + '"></div>' +
+        '</div>',
+    TPL_FIELD_MOVE_TARGET: '<button type="button" class="' + CSS_FIELD_MOVE_TARGET + ' ' + CSS_LIST_MOVE_TARGET +
+        ' layout-builder-move-target layout-builder-move-col-target btn btn-default">' +
+        '{pasteHere}</button>',
+
+    /**
+     * Construction logic executed during the `A.FormBuilderFieldList`
+     * instantiation. Lifecycle.
+     *
+     * @method initializer
+     * @protected
+     */
+    initializer: function() {
+        var content = this.get('content');
+
+        this._uiSetFields(this.get('fields'));
+
+        content.delegate('click', this._onClickAddField, '.' + CSS_FIELD_LIST_ADD_BUTTON, this);
+        content.delegate('mouseenter', this._onMouseEnterAddButton, '.' + CSS_FIELD_LIST_ADD_CONTAINER, this);
+        content.delegate('mouseleave', this._onMouseLeaveAddButton, '.' + CSS_FIELD_LIST_ADD_CONTAINER, this);
+
+        this.after('fieldsChange', A.bind(this._afterFieldsChange, this));
+    },
+
+    /**
+     * Adds a new field to the `A.FormBuilderFieldList`.
+     *
+     * @method addField
+     * @param {A.FormBuilderFieldBase} field
+     * @param {Number} index
+     */
+    addField: function(field, index) {
+        var fields = this.get('fields');
+
+        if (A.Lang.isNumber(index)) {
+            fields.splice(index, 0, field);
+        }
+        else {
+            fields.splice(this._newFieldIndex, 0, field);
+        }
+
+        this.set('fields', fields);
+    },
+
+    /**
+     * Removes the given field from the `A.FormBuilderFieldList`.
+     *
+     * @method removeField
+     * @param {A.FormBuilderFieldBase} field
+     */
+    removeField: function(field) {
+        var fields = this.get('fields'),
+            indexOf = fields.indexOf(field);
+
+        fields.splice(indexOf, 1);
+        this.set('fields', fields);
+    },
+
+    /**
+     * Fired after the `fields` attribute is set.
+     *
+     * @method _afterFieldsChange
+     * @protected
+     */
+    _afterFieldsChange: function() {
+        var fields = this.get('fields');
+
+        this._uiSetFields(fields);
+        this._updateRemovableLayoutColProperty();
+    },
+
+    /**
+     * Appends add field on the given container.
+     *
+     * @method _appendAddFieldNode
+     * @param {Node} container
+     * @param {Number} index
+     * @param {Boolean} visible
+     * @protected
+     */
+    _appendAddFieldNode: function(container, index, visible) {
+        var addFieldNode,
+            moveTargetNode;
+
+        addFieldNode = A.Node.create(this.TPL_ADD_FIELD);
+        moveTargetNode = A.Node.create(A.Lang.sub(this.TPL_FIELD_MOVE_TARGET, {
+            pasteHere: this.get('strings').pasteHere
+        }));
+
+        if (!visible) {
+            addFieldNode.removeClass(CSS_FIELD_LIST_ADD_BUTTON_VISIBLE);
+        }
+
+        moveTargetNode.setData('field-list-index', index);
+
+        container.append(addFieldNode);
+        container.append(moveTargetNode);
+    },
+
+    /**
+     * Fired when the button for adding a new field is clicked.
+     *
+     * @method _onClickAddField
+     * @param {EventFacade} event
+     * @protected
+     */
+    _onClickAddField: function(event) {
+        var emptyFieldList = this.get('content').all('.' + CSS_FIELD_LIST_ADD_BUTTON);
+
+        this._newFieldIndex = emptyFieldList.indexOf(event.currentTarget);
+    },
+
+    /**
+     * Fired when mouse enters the button for adding a new field.
+     *
+     * @method _onMouseEnterAddButton
+     * @param {EventFacade} event
+     * @protected
+     */
+    _onMouseEnterAddButton: function(event) {
+        var addButtonNode = event.currentTarget;
+
+        if (this.get('fields').length > 0) {
+            addButtonNode.addClass(CSS_FIELD_LIST_ADD_BUTTON_VISIBLE);
+        }
+    },
+
+    /**
+     * Fired when mouse leaves the button for adding a new field.
+     *
+     * @method _onMouseLeaveAddButton
+     * @param {EventFacade} event
+     * @protected
+     */
+    _onMouseLeaveAddButton: function(event) {
+        var addButtonNode = event.currentTarget;
+
+        if (this.get('fields').length > 0) {
+            addButtonNode.removeClass(CSS_FIELD_LIST_ADD_BUTTON_VISIBLE);
+        }
+    },
+
+    /**
+     * Updates the ui according to the value of the `fields` attribute.
+     *
+     * @method _uiSetFields
+     * @param {Array} fields
+     * @protected
+     */
+    _uiSetFields: function(fields) {
+        var content = this.get('content'),
+            container = content.one('.' + CSS_FIELD_LIST_CONTAINER),
+            index;
+
+        container.empty();
+
+        for (index = 0; index < fields.length; index++) {
+            this._appendAddFieldNode(container, index, false);
+            container.append(fields[index].get('content'));
+        }
+
+        this._appendAddFieldNode(container, index, true);
+
+        content.toggleClass(CSS_FIELD_LIST_EMPTY, !fields.length);
+
+        if (fields.length === 0) {
+            content.one('.' + CSS_FIELD_LIST_ADD_CONTAINER).addClass(CSS_FIELD_LIST_ADD_BUTTON_VISIBLE);
+            content.all('.' + CSS_FIELD_LIST_ADD_BUTTON_ICON).removeClass('hidden');
+            content.all('.' + CSS_FIELD_LIST_ADD_BUTTON_PLUS_LABEL).addClass('hidden');
+        }
+        else {
+            content.all('.' + CSS_FIELD_LIST_ADD_BUTTON_ICON).addClass('hidden');
+            content.all('.' + CSS_FIELD_LIST_ADD_BUTTON_PLUS_LABEL).removeClass('hidden');
+        }
+    },
+
+    /**
+     * Update removable property for layout cols.
+     *
+     * @method _updateRemovableLayoutColProperty
+     * @protected
+     */
+    _updateRemovableLayoutColProperty: function() {
+        var fields = this.get('fields'),
+            content = this.get('content'),
+            col = content.ancestor('.col'),
+            layoutCol;
+
+        if (col) {
+            layoutCol = col.getData('layout-col');
+            layoutCol.set('removable', fields.length === 0);
+        }
+    }
+}, {
+
+    /**
+     * Static property used to define the default attribute
+     * configuration for the `A.FormBuilderFieldList`.
+     *
+     * @property ATTRS
+     * @type Object
+     * @static
+     */
+    ATTRS: {
+
+        /**
+         * Node containing the contents of this field list.
+         *
+         * @attribute content
+         * @type Node
+         */
+        content: {
+            validator: function(val) {
+                return A.instanceOf(val, A.Node);
+            },
+            valueFn: function() {
+                return A.Node.create(this.TPL_FIELD_LIST);
+            },
+            writeOnce: 'initOnly'
+        },
+
+        /**
+         * List of field.
+         *
+         * @attribute fields
+         * @type {Array}
+         */
+        fields: {
+            value: []
+        },
+
+        /**
+         * Collection of strings used to label elements of the UI.
+         *
+         * @attribute strings
+         * @type {Object}
+         */
+        strings: {
+            value: {
+                pasteHere: 'Paste here'
+            },
+            writeOnce: true
+        }
+    }
+});
+
+
+}, '3.0.3-deprecated.4', {
+    "requires": [
+        "aui-form-builder-field-type",
+        "aui-form-builder-field-types",
+        "aui-form-builder-layout-builder"
+    ],
+    "skinnable": true
+});
---
+++ build/aui-form-field-choice/aui-form-field-choice-debug.js
@@ -0,0 +1,341 @@
+YUI.add('aui-form-field-choice', function (A, NAME) {
+
+/**
+ * The Form Field Choice Component
+ *
+ * @module aui-form-field-choice
+ */
+
+var CSS_FIELD_CHOICE = A.getClassName('form', 'field', 'choice'),
+    CSS_FIELD_CHOICE_CONTENT = A.getClassName('form', 'field', 'choice', 'content'),
+    CSS_FIELD_CHOICE_CHECKBOX = A.getClassName('form', 'field', 'choice', 'checkbox'),
+    CSS_FIELD_CHOICE_FORM = A.getClassName('form', 'field', 'choice', 'form'),
+    CSS_FIELD_CHOICE_LIST = A.getClassName('form', 'field', 'choice', 'list'),
+    CSS_FIELD_CHOICE_OPTION = A.getClassName('form', 'field', 'choice', 'option'),
+    CSS_FIELD_CHOICE_OPTIONS_CONTAINER =
+        A.getClassName('form', 'field', 'choice', 'options', 'container'),
+    CSS_FIELD_CHOICE_OPTION_OTHER =
+        A.getClassName('form', 'field', 'choice', 'option', 'other'),
+    CSS_FIELD_CHOICE_RADIO = A.getClassName('form', 'field', 'choice', 'radio'),
+
+    TPL_FIELD_CHOICE_FORM =
+        '<form class="' + CSS_FIELD_CHOICE_FORM + ' ' +
+        CSS_FIELD_CHOICE_OPTIONS_CONTAINER + '"></form>',
+    TPL_FIELD_CHOICE_FORM_OPTION =
+        '<div class="' + CSS_FIELD_CHOICE_OPTION + '">' +
+        '<input type="{type}" name="option"></input><label>{label}</label></div>',
+    TPL_FIELD_CHOICE_LIST =
+        '<select class="' + CSS_FIELD_CHOICE_LIST + ' ' +
+        CSS_FIELD_CHOICE_OPTIONS_CONTAINER + '"></select>',
+    TPL_FIELD_CHOICE_LIST_OPTION =
+        '<option action="" class="' + CSS_FIELD_CHOICE_OPTION +
+        '"><label>{label}</label></option>',
+
+    TYPES = {
+        CHECKBOX: 'checkbox',
+        LIST: 'list',
+        RADIO: 'radio'
+    };
+
+/**
+ * A base class for Form Field Choice.
+ *
+ * @class A.FormFieldChoice
+ * @extends A.FormField
+ * @param {Object} config Object literal specifying widget configuration
+ *     properties.
+ * @constructor
+ */
+A.FormFieldChoice = A.Base.create('form-field-choice', A.FormField, [A.FormFieldRequired], {
+    TPL_FIELD_CONTENT: '<div class ="' + CSS_FIELD_CHOICE_CONTENT + '"></div>',
+
+    /**
+     * Constructor for the `A.FormFieldChoice`. Lifecycle.
+     *
+     * @method initializer
+     * @protected
+     */
+    initializer: function() {
+        this._oldTypeValue = this.get('type');
+
+        this.after({
+            optionsChange: this._afterOptionsChange,
+            otherOptionChange: this._afterOtherOptionChange,
+            typeChange: this._afterTypeChange
+        });
+    },
+
+    /**
+     * Create the DOM structure for the `A.FormFieldChoice`. Lifecycle.
+     *
+     * @method renderUI
+     * @protected
+     */
+    renderUI: function() {
+        var content = this.get('content');
+
+        A.FormFieldChoice.superclass.renderUI.call(this);
+
+        content.addClass(CSS_FIELD_CHOICE);
+
+        this._updateOptionsContainer();
+        this._uiSetOptions(this.get('options'));
+    },
+
+    /**
+     * Fired after the `options` attribute is set.
+     *
+     * @method _afterOptionsChange
+     * @protected
+     */
+    _afterOptionsChange: function() {
+        this._uiSetOptions(this.get('options'));
+    },
+
+    /**
+     * Fired after the `otherOption` attribute is set.
+     *
+     * @method _afterOtherOptionChange
+     * @protected
+     */
+    _afterOtherOptionChange: function() {
+        this._uiSetOtherOption(this.get('otherOption'));
+    },
+
+    /**
+     * Fired after the `type` attribute is set.
+     *
+     * @method _afterTypeChange
+     * @protected
+     */
+    _afterTypeChange: function() {
+        this._uiSetType(this.get('type'));
+    },
+
+    /**
+     * Remove all option element from ui.
+     *
+     * @method _cleanOptionsContainer
+     * @protected
+     */
+    _cleanOptionsContainer: function() {
+        this.get('content').one('.' + CSS_FIELD_CHOICE_OPTIONS_CONTAINER).empty();
+    },
+
+    /**
+     * Create a option node.
+     *
+     * @method _createOptionNode
+     * @param {String} optionLabel
+     * @return {Node} A new Option Node
+     * @protected
+     */
+    _createOptionNode: function(optionLabel) {
+        var currentType = this.get('type');
+
+        if (currentType === TYPES.LIST) {
+            return A.Node.create(A.Lang.sub(TPL_FIELD_CHOICE_LIST_OPTION, {
+                label: optionLabel
+            }));
+        }
+        else {
+            return A.Node.create(A.Lang.sub(TPL_FIELD_CHOICE_FORM_OPTION, {
+                label: optionLabel,
+                type: currentType
+            }));
+        }
+    },
+    /**
+     * Gets the appropriate css class to form for the given type.
+     *
+     * @method _getFormClass
+     * @param {String} type
+     * @return {String}
+     */
+    _getFormClass: function(type) {
+        switch (type) {
+            case TYPES.CHECKBOX:
+                return CSS_FIELD_CHOICE_CHECKBOX;
+            case TYPES.RADIO:
+                return CSS_FIELD_CHOICE_RADIO;
+        }
+    },
+
+    /**
+     * Switch option type between radio and checkbox.
+     *
+     * @method _setFormType
+     * @param {String} type
+     * @protected
+     */
+    _setFormType: function(type) {
+        var content = this.get('content'),
+            optionsContainer = content.one('.' +CSS_FIELD_CHOICE_OPTIONS_CONTAINER),
+            elements = content.all('.' + CSS_FIELD_CHOICE_OPTION);
+
+        optionsContainer.replaceClass(this._getFormClass(this._oldTypeValue),
+            this._getFormClass(type));
+
+        elements.each(function(element) {
+            element.set('type', type);
+        });
+    },
+
+    /**
+     * Updates the ui according to the value of the `options` attribute.
+     *
+     * @method _uiSetOptions
+     * @param {Array} options
+     * @protected
+     */
+    _uiSetOptions: function(options) {
+        var instance = this,
+            optionsContainer,
+            optionTemplate;
+
+        this._cleanOptionsContainer();
+        optionsContainer = this.get('content').one('.' + CSS_FIELD_CHOICE_OPTIONS_CONTAINER);
+
+        A.Array.each(options, function(option) {
+            optionTemplate = instance._createOptionNode(option);
+            optionsContainer.append(optionTemplate);
+        });
+
+        this._uiSetOtherOption(this.get('otherOption'));
+    },
+
+    /**
+     * Updates the ui according to the value of the `otherOption` attribute.
+     *
+     * @method _uiSetOtherOption
+     * @param {Boolean} otherOption
+     * @protected
+     */
+    _uiSetOtherOption: function(otherOption) {
+        var optionsContainer = this.get('content').one('.' + CSS_FIELD_CHOICE_OPTIONS_CONTAINER),
+            otherOptionNode = this.get('content').one('.' + CSS_FIELD_CHOICE_OPTION_OTHER),
+            optionTemplate;
+
+        if (otherOptionNode) {
+            otherOptionNode.remove();
+        }
+
+        if (otherOption) {
+            optionTemplate = this._createOptionNode('Other');
+            optionTemplate.addClass(CSS_FIELD_CHOICE_OPTION_OTHER);
+            optionsContainer.append(optionTemplate);
+        }
+    },
+
+    /**
+     * Updates the ui according to the value of the `type` attribute.
+     *
+     * @method _uiSetType
+     * @param {String} type
+     * @protected
+     */
+    _uiSetType: function(type) {
+        if (type === TYPES.LIST || this._oldTypeValue === TYPES.LIST) {
+            this._updateOptionsContainer();
+            this._uiSetOptions(this.get('options'));
+            this._uiSetOtherOption(this.get('otherOption'));
+        }
+        else {
+            this._setFormType(this.get('type'));
+        }
+
+        this._oldTypeValue = type;
+    },
+
+    /**
+     * Set a new options container based on `type` attribute.
+     *
+     * @method _updateOptionsContainer
+     * @protected
+     */
+    _updateOptionsContainer: function() {
+        var content = this.get('content').one('.' + CSS_FIELD_CHOICE_CONTENT),
+            optionsContainer;
+
+        content.empty();
+
+        if (this.get('type') === TYPES.LIST) {
+            optionsContainer = A.Node.create(TPL_FIELD_CHOICE_LIST);
+        }
+        else {
+            optionsContainer = A.Node.create(TPL_FIELD_CHOICE_FORM);
+            optionsContainer.addClass(this._getFormClass(this.get('type')));
+        }
+
+        content.append(optionsContainer);
+    }
+}, {
+    /**
+     * Static property used to define the default attribute configuration
+     * for the `A.FormFieldChoice`.
+     *
+     * @property ATTRS
+     * @type Object
+     * @static
+     */
+    ATTRS: {
+        /**
+         * Id to reference form data after a form is submitted.
+         *
+         * @attribute name
+         * @default ''
+         * @type String
+         */
+        name: {
+            validator: A.Lang.isString,
+            value: ''
+        },
+
+        /**
+         * The options that can be chosen.
+         *
+         * @attribute options
+         * @default []
+         * @type String
+         */
+        options: {
+            validator: A.Lang.isArray,
+            value: []
+        },
+
+        /**
+         * If there should be a special "Other" option.
+         *
+         * @attribute otherOption
+         * @default false
+         * @type Boolean
+         */
+        otherOption: {
+            validator: A.Lang.isBoolean,
+            value: false
+        },
+
+        /**
+         * The type of the choice field. Can be any of the values listed in
+         * the `A.FormFieldChoice.TYPES` constant (radio, checkbox or list).
+         * @default radio
+         * @type {Object}
+         */
+        type: {
+            validator: A.Lang.isString,
+            value: TYPES.RADIO
+        }
+    },
+
+    /**
+     * Static property which contains all the valid `A.FormFieldChoice` types.
+     *
+     * @property TYPES
+     * @type Object
+     * @static
+     */
+    TYPES: TYPES
+});
+
+
+}, '3.0.3-deprecated.4', {"requires": ["aui-form-field-required"], "skinnable": true});
