71571bbe920e18e1df19fdb856d44e079352b1d4
Byran Zaugg <byran.zaugg@liferay.com>
2016-01-19 14:32:36 -0800

    AUI-2037 Extent clearFieldError & resetField to allow passing fieldName. Refactor calls to allow passing fieldName even when fieldNode is not in DOM.
    Lang.isNode is not available in 2.0.x.

--- src/aui-form-validator/HISTORY.md
+++ src/aui-form-validator/HISTORY.md
@@ -3,7 +3,7 @@ AUI Form Validator
 
 @VERSION@
 ------
-
+	* [AUI-2037](https://issues.liferay.com/browse/AUI-2037) validateField() does not reset error states for fields no longer in the DOM, preventing form submission
 	* #AUI-2043 Make required rule the default error message when showAllMessages is false.
 	* #AUI-2027 Allow requiring custom validation without field values
 	* #AUI-2040 Change validation for file input on change event
--- src/aui-form-validator/js/aui-form-validator.js
+++ src/aui-form-validator/js/aui-form-validator.js
@@ -16,6 +16,10 @@ var Lang = A.Lang,
     isString = Lang.isString,
     trim = Lang.trim,
 
+    isNode = function(v) {
+        return (v instanceof A.Node);
+    },
+
     defaults = A.namespace('config.FormValidator'),
 
     getRegExp = A.DOM._getRegExp,
@@ -591,15 +595,19 @@ var FormValidator = A.Component.create({
         },
 
         /**
-         * TODO. Wanna help? Please send a Pull Request.
+         * Deletes the field from the errors property object.
          *
          * @method clearFieldError
-         * @param field
+         * @param {Node|String} field
          */
         clearFieldError: function(field) {
             var instance = this;
 
-            delete instance.errors[field.get(NAME)];
+            var fieldName = isNode(field) ? field.get('name') : field;
+
+            if (isString(fieldName)) {
+                delete instance.errors[fieldName];
+            }
         },
 
         /**
@@ -858,26 +866,31 @@ var FormValidator = A.Component.create({
 
             instance.eachRule(
                 function(rule, fieldName) {
-                    var field = instance.getField(fieldName);
-
-                    instance.resetField(field);
+                    instance.resetField(fieldName);
                 }
             );
         },
 
         /**
-         * TODO. Wanna help? Please send a Pull Request.
+         * Resets the CSS class and error status of a field.
          *
          * @method resetField
-         * @param field
+         * @param {Node|String} field
          */
         resetField: function(field) {
-            var instance = this,
-                stackContainer = instance.getFieldStackErrorContainer(field);
+            var instance = this;
 
-            stackContainer.remove();
-            instance.resetFieldCss(field);
             instance.clearFieldError(field);
+
+            var fieldNode = isString(field) ? instance.getField(field) : field;
+
+            if (isNode(fieldNode)) {
+                var stackContainer = instance.getFieldStackErrorContainer(fieldNode);
+
+                stackContainer.remove();
+
+                instance.resetFieldCss(fieldNode);
+            }
         },
 
         /**
@@ -948,13 +961,14 @@ var FormValidator = A.Component.create({
          * @param field
          */
         validateField: function(field) {
-            var instance = this,
-                fieldNode = instance.getField(field);
+            var instance = this;
 
-            if (fieldNode) {
+            instance.resetField(field);
+
+            var fieldNode = isString(field) ? instance.getField(field) : field;
+
+            if (isNode(fieldNode)) {
                 var validatable = instance.validatable(fieldNode);
-
-                instance.resetField(fieldNode);
 
                 if (validatable) {
                     instance.fire(EV_VALIDATE_FIELD, {
