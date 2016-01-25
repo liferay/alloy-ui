19a2f175267493a1c5e2e5b50d0532b96899fbf8
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-01-25 04:55:15 -0800

    AUI-2061 Avoid brittle time arithmetics.

--- src/aui-scheduler/js/aui-scheduler-view-table.js
+++ src/aui-scheduler/js/aui-scheduler-view-table.js
@@ -767,11 +767,11 @@ var SchedulerTableView = A.Component.create({
          */
         loopDates: function(startDate, endDate, fn, incrementBy, factor) {
             var instance = this;
+            var countDays = DateMath.countDays(startDate, endDate) + 1;
             var curDate = DateMath.clone(startDate);
-            var endDateMs = endDate.getTime();
             var index;
 
-            for (index = 0; curDate.getTime() <= endDateMs; index++) {
+            for (index = 0; index < countDays; index++) {
                 fn.apply(instance, [curDate, index]);
 
                 curDate = DateMath.add(curDate, (incrementBy || DateMath.DAY), (factor || 1));
