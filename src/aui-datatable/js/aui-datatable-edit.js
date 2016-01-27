2accdf37be13703cb5e674f39b527df3e40e35d7
Byran Zaugg <byran.zaugg@liferay.com>
2016-01-26 17:06:39 -0800

    AUI-2038 Add Custom Validator rules for options.

--- src/aui-datatable/js/aui-datatable-edit.js
+++ src/aui-datatable/js/aui-datatable-edit.js
@@ -1221,6 +1221,77 @@ var BaseOptionsCellEditor = A.Component.create({
         },
 
         /**
+         * Defines the custom rules used to validate options.
+         *
+         * @attribute optionsValidatorCustomRules
+         * @type Object
+         */
+        optionsValidatorCustomRules: {
+            validator: isObject,
+            valueFn: function() {
+                var instance = this;
+
+                return {
+                    'uniqueValue': {
+                        condition: function(fieldValue, fieldNode) {
+                            var instance = this;
+
+                            var editContainerNode = fieldNode.ancestor('.' + CSS_CELLEDITOR_EDIT);
+
+                            var inputValueNodelist = editContainerNode.all('.' + CSS_CELLEDITOR_EDIT_INPUT_VALUE);
+
+                            var validate = function (validateNode, nodelist, recurse) {
+                                var duplicates = false;
+
+                                nodelist.each(
+                                    function(node){
+                                        if (validateNode !== node) {
+                                            if (validateNode.val() == node.val()) {
+                                                instance.highlight(validateNode);
+                                                instance.addFieldError(validateNode, 'uniqueValue');
+
+                                                duplicates = true;
+                                            }
+
+                                            if (recurse) {
+                                                validate(node, inputValueNodelist, false);
+                                            }
+                                        }
+                                    }
+                                );
+
+                                if (!duplicates) {
+                                    instance.resetField(validateNode);
+                                    instance.highlight(validateNode, true);
+                                }
+
+                                return !duplicates;
+                            };
+
+                            return validate(fieldNode, inputValueNodelist, true);
+                        },
+                        errorMessage: instance.getStrings().valueNotUnique
+                    }
+                };
+            }
+        },
+
+        /**
+         * Defines the initial rules used to validate options.
+         *
+         * @attribute optionsValidatorInputRules
+         * @default {custom: true, uniqueValue: true}
+         * @type Object
+         */
+        optionsValidatorInputRules: {
+            validator: isObject,
+            value: {
+                custom: true,
+                uniqueValue: true
+            }
+        },
+
+        /**
          * TODO. Wanna help? Please send a Pull Request.
          *
          * @attribute outputFormatter
@@ -1261,7 +1332,8 @@ var BaseOptionsCellEditor = A.Component.create({
                 remove: 'Remove',
                 save: 'Save',
                 stopEditing: 'Stop editing',
-                value: 'Value'
+                value: 'Value',
+                valueNotUnique: 'Value not unique.'
             }
         }
     },
@@ -1325,6 +1397,8 @@ var BaseOptionsCellEditor = A.Component.create({
             instance.on(EDIT, instance._onEditEvent);
             instance.on(SAVE, instance._onSave);
             instance.after(INIT_TOOLBAR, instance._afterInitToolbar);
+
+            A.FormValidator.addCustomRules(instance.get('optionsValidatorCustomRules'));
         },
 
         /**
@@ -1490,14 +1564,16 @@ var BaseOptionsCellEditor = A.Component.create({
         _createEditOption: function(name, value) {
             var instance = this;
 
-            var optionValueName = A.guid() + '_value';
+            var fieldName = A.guid() + '_value';
             var strings = instance.getStrings();
+
+            instance.validator.get('rules')[fieldName] = instance.get('optionsValidatorInputRules');
 
             return Lang.sub(
                 instance.EDIT_OPTION_ROW_TEMPLATE, {
                     labelOptionName: AEscape.html(strings[OPTION_NAME]),
                     labelOptionValue: AEscape.html(strings[OPTION_VALUE]),
-                    optionValueName: AEscape.html(optionValueName),
+                    optionValueName: AEscape.html(fieldName),
                     remove: strings[REMOVE],
                     titleName: AEscape.html(strings[NAME]),
                     titleValue: AEscape.html(strings[VALUE]),
