9df01902d86f9b926704dc52879a07859be202de
Maira Bello <maira.araujo@liferay.com>
2016-02-17 15:18:08 -0300

    AUI-2083 Create a task to publish Alloy webjars to maven repository

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
+var liferayGulpTasks = require('liferay-gulp-tasks');
 var requireDir = require('require-dir');
 var tasksDir = requireDir('./tasks');
+
+liferayGulpTasks.registerTasks();
 
 // Add the node_modules/.bin directory to the PATH
 require('spawn-local-bin').path(__dirname);
