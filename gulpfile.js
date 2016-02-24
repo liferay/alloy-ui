019687f27bfa84161e2db556154ee1a2b46ee282
Chema Balsas <jose.balsas@liferay.com>
2016-02-24 13:13:08 +0100

    Do not include bower_components folder in the artifact

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var liferayGulpTasks = require('liferay-gulp-tasks');
 var requireDir = require('require-dir');
 var tasksDir = requireDir('./tasks');
 
-liferayGulpTasks.registerTasks();
+liferayGulpTasks.registerTasks({
+	artifactSrc: ['**/*', '!node_modules/', '!node_modules/**', '!bower_components/', '!bower_components/**']
+});
 
 // Add the node_modules/.bin directory to the PATH
 require('spawn-local-bin').path(__dirname);
