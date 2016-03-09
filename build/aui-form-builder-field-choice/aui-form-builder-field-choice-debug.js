b64ca5bce688c3ce77b1e6d22e63b47ee7af0e18
Jonathan Mak <jonathan.mak@liferay.com>
2016-03-09 15:38:18 -0800

    AUI-2089; AUI-2090; AUI-2091 - Build

---
+++ build/aui-form-builder-field-choice/aui-form-builder-field-choice-debug.js
@@ -0,0 +1,100 @@
+YUI.add('aui-form-builder-field-choice', function (A, NAME) {
+
+/**
+ * The Form Builder Field Choice Component
+ *
+ * @module aui-form-builder
+ * @submodule aui-form-builder-field-choice
+ */
+
+/**
+ * A base class for Form Builder Field Choice.
+ *
+ * @class A.FormBuilderFieldChoice
+ * @extends A.FormFieldChoice
+ * @uses A.FormBuilderFieldBase
+ * @param {Object} config Object literal specifying widget configuration
+ *     properties.
+ * @constructor
+ */
+A.FormBuilderFieldChoice = A.Base.create('form-builder-field-choice', A.FormFieldChoice, [A.FormBuilderFieldBase], {
+    /**
+     * Fills the settings array with the information for this field.
+     *
+     * @method _fillSettings
+     * @override
+     * @protected
+     */
+    _fillSettings: function() {
+
+        this._settings.push(
+            {
+                attrName: 'required',
+                editor: new A.BooleanDataEditor({
+                    label: 'Required'
+                })
+            },
+            {
+                attrName: 'type',
+                editor: new A.TabsDataEditor({
+                    tabs: [
+                        {
+                            label: 'Radio button',
+                            value: A.FormFieldChoice.TYPES.RADIO
+                        },
+                        {
+                            label: 'Checkbox',
+                            value: A.FormFieldChoice.TYPES.CHECKBOX
+                        },
+                        {
+                            label: 'List Select',
+                            value: A.FormFieldChoice.TYPES.LIST
+                        }
+                    ]
+                })
+            },
+            {
+                attrName: 'options',
+                editor: new A.OptionsDataEditor({
+                    required: true
+                })
+            },
+            {
+                attrName: 'otherOption',
+                editor: new A.BooleanDataEditor({
+                    label: 'Add an option called "Other"'
+                })
+            }
+        );
+    },
+
+    /**
+     * Fills the advanced settings array with the information for this field.
+     *
+     * @method _fillAdvancedSettings
+     * @override
+     * @protected
+     */
+    _fillAdvancedSettings: function() {
+        this._advancedSettings = [
+            {
+                attrName: 'name',
+                footerLabel: 'Name',
+                editor: new A.TextDataEditor({
+                    label: 'Name'
+                })
+            }
+        ];
+    }
+});
+
+
+}, '3.0.3-deprecated.21', {
+    "requires": [
+        "aui-boolean-data-editor",
+        "aui-options-data-editor",
+        "aui-tabs-data-editor",
+        "aui-form-builder-field-base",
+        "aui-form-field-choice"
+    ]
+});
