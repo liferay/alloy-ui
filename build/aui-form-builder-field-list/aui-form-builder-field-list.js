fb14320f3420f1b71a6e7bdb6cf51e171de75903
Jonathan Mak <jonathan.mak@liferay.com>
2016-05-20 09:14:12 -0700

    AUI-2118 - Build

---
+++ build/aui-form-builder-field-list/aui-form-builder-field-list.js
@@ -0,0 +1,380 @@
+YUI.add('aui-form-builder-field-list', function (A, NAME) {
+
+/**
+ * The Form Builder Field List Component
+ *
+ * @module aui-form-builder-field-list
+ */
+
+var CSS_FIELD_LIST = A.getClassName('form', 'builder', 'field', 'list'),
+    CSS_FIELD_LIST_ADD_BUTTON =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button'),
+    CSS_FIELD_LIST_ADD_BUTTON_ICON =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'icon'),
+    CSS_FIELD_LIST_ADD_BUTTON_LABEL =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'label'),
+    CSS_FIELD_LIST_ADD_BUTTON_LARGE =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'large'),
+    CSS_FIELD_LIST_ADD_BUTTON_PLUS_ICON =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'plus', 'icon'),
+    CSS_FIELD_LIST_ADD_BUTTON_VISIBLE =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'button', 'visible'),
+    CSS_FIELD_LIST_ADD_CONTAINER =
+        A.getClassName('form', 'builder', 'field', 'list', 'add', 'container'),
+    CSS_FIELD_LIST_CONTAINER =
+        A.getClassName('form', 'builder', 'field', 'list', 'container'),
+    CSS_FIELD_LIST_EMPTY = A.getClassName('form', 'builder', 'field', 'list', 'empty'),
+    CSS_FIELD_MOVE_TARGET =
+        A.getClassName('form', 'builder', 'field', 'move', 'target'),
+    CSS_FIELD_MOVE_TARGET_LABEL =
+        A.getClassName('form', 'builder', 'field', 'move', 'target', 'label'),
+    CSS_LIST_MOVE_TARGET =
+        A.getClassName('form', 'builder', 'list', 'move', 'target');
+
+/**
+ * A base class for `A.FormBuilderFieldList`.
+ *
+ * @class A.FormBuilderFieldList
+ * @extends A.Base
+ * @param {Object} config Object literal specifying widget configuration
+ *     properties.
+ * @constructor
+ */
+A.FormBuilderFieldList  = A.Base.create('form-builder-field-list', A.Base, [], {
+    TPL_ADD_FIELD: '<div class="' + CSS_FIELD_LIST_ADD_CONTAINER + '">' +
+        '<a class="btn ' + CSS_FIELD_LIST_ADD_BUTTON + '" href="javascript:;">' +
+        '<span class="' + CSS_FIELD_LIST_ADD_BUTTON_ICON + ' ' + CSS_FIELD_LIST_ADD_BUTTON_PLUS_ICON + '">+</span>' +
+        '<label class="' + CSS_FIELD_LIST_ADD_BUTTON_LABEL + '">' +
+        '{addField}' +
+        '</label>' +
+        '</a></div>',
+    TPL_FIELD_LIST: '<div class="' + CSS_FIELD_LIST + '">' +
+        '<div class="' + CSS_FIELD_LIST_CONTAINER + '"></div>' +
+        '</div>',
+    TPL_FIELD_MOVE_TARGET: '<button type="button" class="' + CSS_FIELD_MOVE_TARGET + ' ' + CSS_LIST_MOVE_TARGET +
+        ' layout-builder-move-target layout-builder-move-col-target">' +
+        '<label class="' + CSS_FIELD_MOVE_TARGET_LABEL + '">{pasteHere}</label></button>',
+
+    /**
+     * Construction logic executed during the `A.FormBuilderFieldList`
+     * instantiation. Lifecycle.
+     *
+     * @method initializer
+     * @protected
+     */
+    initializer: function() {
+        var content = this.get('content');
+
+        this._uiSetFields(this.get('fields'));
+
+        content.delegate('click', this._onClickAddField, '.' + CSS_FIELD_LIST_ADD_BUTTON, this);
+        content.delegate('mouseenter', this._onMouseEnterAddButton, '.' + CSS_FIELD_LIST_ADD_CONTAINER, this);
+        content.delegate('mouseleave', this._onMouseLeaveAddButton, '.' + CSS_FIELD_LIST_ADD_CONTAINER, this);
+
+        this.after('fieldsChange', A.bind(this._afterFieldsChange, this));
+        this.after('enableAddFieldsChange', A.bind(this._afterEnableAddFieldsChange, this));
+    },
+
+    /**
+     * Adds a new field to the `A.FormBuilderFieldList`.
+     *
+     * @method addField
+     * @param {A.FormBuilderFieldBase} field
+     * @param {Number} index
+     */
+    addField: function(field, index) {
+        var fields = this.get('fields');
+
+        if (!this.get('enableAddFields')) {
+            return;
+        }
+
+        if (A.Lang.isNumber(index)) {
+            fields.splice(index, 0, field);
+        }
+        else {
+            fields.splice(this._newFieldIndex, 0, field);
+        }
+
+        this.set('fields', fields);
+    },
+
+    /**
+     * Removes the given field from the `A.FormBuilderFieldList`.
+     *
+     * @method removeField
+     * @param {A.FormBuilderFieldBase} field
+     */
+    removeField: function(field) {
+        var fields = this.get('fields'),
+            indexOf = fields.indexOf(field);
+
+        fields.splice(indexOf, 1);
+        this.set('fields', fields);
+    },
+
+    /**
+     * Executed to the last empty column.
+     *
+     * @method _addEmptyColumnFieldClasses
+     * @param {Node} content
+     * @protected
+     */
+    _addEmptyColumnFieldClasses: function(content) {
+        content.one('.' + CSS_FIELD_LIST_ADD_CONTAINER).addClass(CSS_FIELD_LIST_ADD_BUTTON_VISIBLE);
+        content.one('.' + CSS_FIELD_LIST_ADD_BUTTON).addClass(CSS_FIELD_LIST_ADD_BUTTON_LARGE);
+    },
+
+    /**
+     * Fired after the `enableAddFields` attribute change.
+     *
+     * @method _afterEnableAddFieldsChange
+     * @protected
+     */
+    _afterEnableAddFieldsChange: function() {
+        this._uiToggleDisableAddField();
+    },
+
+    /**
+     * Fired after the `fields` attribute is set.
+     *
+     * @method _afterFieldsChange
+     * @protected
+     */
+    _afterFieldsChange: function() {
+        var fields = this.get('fields');
+
+        this._uiSetFields(fields);
+        this._updateRemovableLayoutColProperty();
+    },
+
+    /**
+     * Appends add field on the given container.
+     *
+     * @method _appendAddFieldNode
+     * @param {Node} container
+     * @protected
+     */
+    _appendAddFieldNode: function(container) {
+        var addFieldNode;
+
+        addFieldNode = A.Node.create(A.Lang.sub(this.TPL_ADD_FIELD, {
+            addField: this.get('strings').addField
+        }));
+
+        container.append(addFieldNode);
+
+        return addFieldNode;
+    },
+
+    /**
+     * Appends move target button on the given container.
+     *
+     * @method _appendAddMoveTargetNode
+     * @param {Node} container
+     * @param {Number} index
+     * @protected
+     */
+    _appendAddMoveTargetNode: function(container, index) {
+        var moveTargetNode;
+
+        moveTargetNode = A.Node.create(A.Lang.sub(this.TPL_FIELD_MOVE_TARGET, {
+            pasteHere: this.get('strings').pasteHere
+        }));
+
+        moveTargetNode.setData('field-list-index', index);
+
+        container.append(moveTargetNode);
+
+        return moveTargetNode;
+    },
+
+    /**
+     * Fired when the button for adding a new field is clicked.
+     *
+     * @method _onClickAddField
+     * @param {EventFacade} event
+     * @protected
+     */
+    _onClickAddField: function(event) {
+        var emptyFieldList = this.get('content').all('.' + CSS_FIELD_LIST_ADD_BUTTON);
+
+        this._newFieldIndex = emptyFieldList.indexOf(event.currentTarget);
+    },
+
+    /**
+     * Fired when mouse enters the button for adding a new field.
+     *
+     * @method _onMouseEnterAddButton
+     * @param {EventFacade} event
+     * @protected
+     */
+    _onMouseEnterAddButton: function(event) {
+        var addButtonNode = event.currentTarget;
+
+        if (this.get('fields').length > 0) {
+            addButtonNode.addClass(CSS_FIELD_LIST_ADD_BUTTON_VISIBLE);
+        }
+    },
+
+    /**
+     * Fired when mouse leaves the button for adding a new field.
+     *
+     * @method _onMouseLeaveAddButton
+     * @param {EventFacade} event
+     * @protected
+     */
+    _onMouseLeaveAddButton: function(event) {
+        var addButtonNode = event.currentTarget;
+
+        if (this.get('fields').length > 0) {
+            addButtonNode.removeClass(CSS_FIELD_LIST_ADD_BUTTON_VISIBLE);
+        }
+    },
+
+    /**
+     * Append a field and its necessaries elements into a given container.
+     *
+     * @method _uiSetField
+     * @param {Node} container
+     * @param {Node} field
+     * @param {Number} index
+     * @protected
+     */
+    _uiSetField: function(container, field, index) {
+        this._appendAddFieldNode(container);
+        this._appendAddMoveTargetNode(container, index);
+
+        container.append(field.get('content'));
+    },
+
+    /**
+     * Updates the ui according to the value of the `fields` attribute.
+     *
+     * @method _uiSetFields
+     * @param {Array} fields
+     * @protected
+     */
+    _uiSetFields: function(fields) {
+        var content = this.get('content'),
+            container = content.one('.' + CSS_FIELD_LIST_CONTAINER),
+            index;
+
+        container.empty();
+
+        for (index = 0; index < fields.length; index++) {
+            this._uiSetField(container, fields[index], index);
+        }
+
+        this._appendAddFieldNode(container);
+        this._appendAddMoveTargetNode(container, index);
+
+        content.toggleClass(CSS_FIELD_LIST_EMPTY, !fields.length);
+
+        if (fields.length === 0) {
+            this._addEmptyColumnFieldClasses(content);
+        }
+    },
+
+    /**
+     * Toggle disabled class to the add field button.
+     *
+     * @method _uiToggleDisableAddField
+     * @protected
+     */
+    _uiToggleDisableAddField: function() {
+        this.get('content').one('.' + CSS_FIELD_LIST_ADD_BUTTON).toggleClass('disabled', !this.get('enableAddFields'));
+    },
+
+    /**
+     * Update removable property for layout cols.
+     *
+     * @method _updateRemovableLayoutColProperty
+     * @protected
+     */
+    _updateRemovableLayoutColProperty: function() {
+        var fields = this.get('fields'),
+            content = this.get('content'),
+            col = content.ancestor('.col'),
+            layoutCol;
+
+        if (col) {
+            layoutCol = col.getData('layout-col');
+            layoutCol.set('removable', fields.length === 0);
+        }
+    }
+}, {
+
+    /**
+     * Static property used to define the default attribute
+     * configuration for the `A.FormBuilderFieldList`.
+     *
+     * @property ATTRS
+     * @type Object
+     * @static
+     */
+    ATTRS: {
+
+        /**
+         * Node containing the contents of this field list.
+         *
+         * @attribute content
+         * @type Node
+         */
+        content: {
+            validator: function(val) {
+                return A.instanceOf(val, A.Node);
+            },
+            valueFn: function() {
+                return A.Node.create(this.TPL_FIELD_LIST);
+            },
+            writeOnce: 'initOnly'
+        },
+
+        /**
+         * Attribute responsible for enabling or disabling the feature of adding fields.
+         *
+         * @attribute enableAddFields
+         * @type {Boolean}
+         */
+        enableAddFields: {
+            validator: A.Lang.isBoolean,
+            value: true
+        },
+
+        /**
+         * List of field.
+         *
+         * @attribute fields
+         * @type {Array}
+         */
+        fields: {
+            validator: A.Lang.isArray,
+            value: []
+        },
+
+        /**
+         * Collection of strings used to label elements of the UI.
+         *
+         * @attribute strings
+         * @type {Object}
+         */
+        strings: {
+            value: {
+                addField: 'Add Field',
+                pasteHere: 'Paste here'
+            },
+            writeOnce: true
+        }
+    }
+});
+
+
+}, '3.0.3-deprecated.33', {
+    "requires": [
+        "aui-form-builder-field-type",
+        "aui-form-builder-field-types",
+        "aui-form-builder-layout-builder"
+    ],
+    "skinnable": true
+});
---
+++ build/aui-progressbar/aui-progressbar-debug.js
@@ -0,0 +1,410 @@
+YUI.add('aui-progressbar', function (A, NAME) {
+
+/**
+ * The ProgressBar Utility provides a visual means of showing progress of an
+ * ongoing operation. The ProgressBar can be enhanced via CSS styles to provide
+ * different colors, shapes and textures. The bar can move horizontally or
+ * vertically. The movement can be enhanced by using the Animation utility.
+ *
+ * @module aui-progressbar
+ */
+
+var toNumber = function(v) {
+        return parseFloat(v) || 0;
+    },
+
+    CSS_BAR = A.getClassName('progress', 'bar'),
+    CSS_HORIZONTAL = A.getClassName('horizontal'),
+    CSS_VERTICAL = A.getClassName('vertical'),
+
+    TPL_TEXT = '<p></p>';
+
+/**
+ * A base class for Progressbar, providing:
+ *
+ * - Widget Lifecycle (initializer, renderUI, bindUI, syncUI, destructor)
+ * - A visual means of showing progress of an ongoing operation
+ * - Can be enhanced via CSS styles to provide different colors, shapes and
+ *   textures
+ * - The bar can move horizontally or vertically
+ * - The movement can be enhanced by using the Animation utility
+ *
+ * Check the [live demo](http://alloyui.com/examples/progress-bar/).
+ *
+ * @class A.ProgressBar
+ * @extends A.Component
+ * @param {Object} config Object literal specifying widget configuration
+ *     properties.
+ * @constructor
+ * @include http://alloyui.com/examples/progress-bar/basic-markup.html
+ * @include http://alloyui.com/examples/progress-bar/basic.js
+ */
+var ProgressBar = A.Component.create({
+    /**
+     * Static property provides a string to identify the class.
+     *
+     * @property NAME
+     * @type String
+     * @static
+     */
+    NAME: 'progress',
+
+    /**
+     * Static property used to define the default attribute
+     * configuration for the ProgressBar.
+     *
+     * @property ATTRS
+     * @type Object
+     * @static
+     */
+    ATTRS: {
+
+        /**
+         * Display height of the progressbar.
+         *
+         * @attribute height
+         * @default 25
+         * @type int | String
+         */
+        height: {
+            valueFn: function() {
+                return this.get('boundingBox').get('offsetHeight') || 25;
+            }
+        },
+
+        /**
+         * Display label of the progressbar. If not specified try to query
+         * using HTML_PARSER an element inside boundingBox which matches
+         * `aui-progressbar-text` and get its innerHTML to be
+         * used as label.
+         *
+         * @attribute label
+         * @default undefined
+         * @type String
+         */
+        label: {},
+
+        /**
+         * Represents the top value for the bar. The bar will be fully
+         * extended when reaching this value. Values higher than this will
+         * be ignored.
+         *
+         * @attribute max
+         * @default 100
+         * @type Number
+         */
+        max: {
+            validator: A.Lang.isNumber,
+            value: 100
+        },
+
+        /**
+         * Represents the lowest value for the bar. The bar will be
+         * totally collapsed when reaching this value. Values lower than
+         * this will be ignored.
+         *
+         * @attribute min
+         * @default 0
+         * @type Number
+         */
+        min: {
+            validator: A.Lang.isNumber,
+            value: 0
+        },
+
+        /**
+         * Display orientation of the progressbar (i.e. vertical or
+         * horizontal).
+         *
+         * @attribute orientation
+         * @default HORIZONTAL
+         * @type String
+         */
+        orientation: {
+            value: 'horizontal',
+            validator: function(val) {
+                return A.Lang.isString(val) && (val === 'horizontal' || val === 'vertical');
+            }
+        },
+
+        /**
+         * Calculate the ratio based on `max` and `min` values.
+         *
+         * @attribute ratio
+         * @readOnly
+         * @type number
+         */
+        ratio: {
+            getter: '_getRatio',
+            readOnly: true
+        },
+
+        /**
+         * Calculate the progressbar step based on `ratio` value.
+         *
+         * @attribute step
+         * @readOnly
+         * @type number
+         */
+        step: {
+            getter: '_getStep',
+            readOnly: true
+        },
+
+        /**
+         * Specify the tab order of elements.
+         *
+         * @attribute tabIndex
+         * @default 1
+         * @type Number
+         */
+        tabIndex: {
+            value: 1
+        },
+
+        /**
+         * DOM Node to display the text of the progressbar. If not
+         * specified try to query using HTML_PARSER an element inside
+         * contentBox which matches `aui-progressbar-text`.
+         *
+         * @attribute textNode
+         * @default Generated div element.
+         * @type String
+         */
+        textNode: {
+            valueFn: function() {
+                return A.Node.create(TPL_TEXT);
+            }
+        },
+
+        /**
+         * Boolean indicating if use of the WAI-ARIA Roles and States
+         * should be enabled.
+         *
+         * @attribute useARIA
+         * @default true
+         * @type Boolean
+         */
+        useARIA: {
+            value: true
+        },
+
+        /**
+         * The value for the bar. Valid values are in between the minValue
+         * and maxValue attributes.
+         *
+         * @attribute value
+         * @default 0
+         * @type Number | String
+         */
+        value: {
+            setter: toNumber,
+            validator: function(val) {
+                return A.Lang.isNumber(toNumber(val)) && ((val >= this.get('min')) && (val <= this.get('max')));
+            },
+            value: 0
+        }
+    },
+
+    /**
+     * Object hash, defining how attribute values are to be parsed from
+     * markup contained in the widget's bounding box.
+     *
+     * @property HTML_PARSER
+     * @type Object
+     * @static
+     */
+    HTML_PARSER: {
+        label: function(boundingBox) {
+            var textNode = boundingBox.one('p');
+
+            if (textNode) {
+                return textNode.html();
+            }
+        },
+
+        textNode: 'p'
+    },
+
+    /**
+     * Static property used to define the UI attributes.
+     *
+     * @property UI_ATTRS
+     * @type Array
+     * @static
+     */
+    UI_ATTRS: ['label', 'orientation', 'value'],
+
+    prototype: {
+
+        /**
+         * Create the DOM structure for the ProgressBar. Lifecycle.
+         *
+         * @method renderUI
+         * @protected
+         */
+        renderUI: function() {
+            this.get('contentBox').addClass(CSS_BAR);
+
+            this._renderTextNodeIfLabelSet();
+        },
+
+        /**
+         * Sync the ProgressBar UI. Lifecycle.
+         *
+         * @method syncUI
+         * @protected
+         */
+        syncUI: function() {
+            if (this.get('useARIA')) {
+                this.plug(A.Plugin.Aria, {
+                    attributes: {
+                        value: 'valuenow',
+                        max: 'valuemax',
+                        min: 'valuemin',
+                        orientation: 'orientation'
+                    },
+                    roleName: 'progressbar'
+                });
+            }
+        },
+
+        /**
+         * Calculate the boundingBox size based on the
+         * `orientation` of the progressbar. If the orientation
+         * is HORIZONTAL get the width, if the orientation is VERTICAL get
+         * the height.
+         *
+         * @method _getBoundingBoxSize
+         * @protected
+         * @return {Number}
+         */
+        _getBoundingBoxSize: function() {
+            return toNumber(
+                this.get('boundingBox').getStyle(
+                    this.get('orientation') === 'horizontal' ? 'width' : 'height'
+                )
+            );
+        },
+
+        /**
+         * Calculate the ratio based on `max` and `min` values.
+         *
+         * @method _getRatio
+         * @protected
+         * @return {Number}
+         */
+        _getRatio: function() {
+            var min = this.get('min'),
+                ratio = (this.get('value') - min) / (this.get('max') - min);
+
+            return Math.max(ratio, 0);
+        },
+
+        /**
+         * Calculate the progressbar step based on `ratio` value.
+         *
+         * @method _getStep
+         * @protected
+         * @return {Number}
+         */
+        _getStep: function() {
+            return this.get('ratio') * 100;
+        },
+
+        /**
+         * Render the `textNode` of the progressbar if label is set.
+         *
+         * @method _renderTextNodeIfLabelSet
+         * @protected
+         */
+        _renderTextNodeIfLabelSet: function() {
+            if (!A.Lang.isUndefined(this.get('label'))) {
+                this.get('contentBox').append(this.get('textNode'));
+            }
+        },
+
+        /**
+         * Invoked automatically by the UI_ATTRS Widget API when bind or sync
+         * the `label` attribute.
+         *
+         * @method _uiSetLabel
+         * @param {String} val Display label
+         * @protected
+         */
+        _uiSetLabel: function(val) {
+            var textNode = this.get('textNode');
+
+            if (!textNode.inDoc()) {
+                this._renderTextNodeIfLabelSet();
+            }
+
+            textNode.html(val);
+        },
+
+        /**
+         * Invoked automatically by the UI_ATTRS Widget API when bind or sync
+         * the `orientation` attribute.
+         *
+         * @method _uiSetOrientation
+         * @param {String} val Orientation
+         * @protected
+         */
+        _uiSetOrientation: function(val) {
+            var boundingBox = this.get('boundingBox'),
+                horizontal = (val === 'horizontal');
+
+            boundingBox.toggleClass(CSS_HORIZONTAL, horizontal);
+            boundingBox.toggleClass(CSS_VERTICAL, !horizontal);
+
+            this._uiSetValue(this.get('value'));
+            this._uiSizeTextNode();
+        },
+
+        /**
+         * Invoked automatically by the UI_ATTRS Widget API when bind or sync
+         * the `value` attribute.
+         *
+         * @method _uiSetValue
+         * @protected
+         */
+        _uiSetValue: function() {
+            var percentStep = this._getStep(),
+                styles;
+
+            if (this.get('orientation') === 'horizontal') {
+                styles = {
+                    height: '100%',
+                    width: percentStep + '%'
+                };
+            }
+            else {
+                styles = {
+                    height: percentStep + '%',
+                    width: '100%'
+                };
+            }
+
+            if (this.get('step') >= 100) {
+                this.fire('complete');
+            }
+
+            this.get('contentBox').setStyles(styles);
+        },
+
+        /**
+         * Sync the size of the ProgressBar when some UI attribute change.
+         *
+         * @method _uiSizeTextNode
+         * @protected
+         */
+        _uiSizeTextNode: function() {
+            this.get('textNode').setStyle('lineHeight', this.get('boundingBox').getStyle('height'));
+        }
+    }
+});
+
+A.ProgressBar = ProgressBar;
+
+
+}, '3.0.3-deprecated.33', {"requires": ["aui-node", "aui-component", "aui-aria"], "skinnable": true});
