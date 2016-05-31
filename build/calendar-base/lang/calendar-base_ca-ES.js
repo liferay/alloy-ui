dcf028bc36d5ccdbce3ee2b80d603d9fb67e98a9
Jonathan Mak <jonathan.mak@liferay.com>
2016-05-31 16:49:37 -0700

    AUI-2116; AUI-2281 - Build

---
+++ build/calendar-base/lang/calendar-base_ca-ES.js
@@ -0,0 +1,1 @@
+YUI.add("lang/calendar-base_ca-ES",function(e){e.Intl.add("calendar-base","ca-ES",{very_short_weekdays:["Dg","Dl","Dt","Dc","Dj","Dv","Ds"],first_weekday:1,weekends:[0,6]})},"patched-v3.18.1");
