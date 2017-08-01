620d839096af2e6214e57be47b40732e639300ee
Jonathan Mak <jonathan.mak@liferay.com>
2017-08-01 12:58:28 -0700

    AUI-1205 - add unit tests

--- src/aui-datepicker/tests/unit/js/tests.js
+++ src/aui-datepicker/tests/unit/js/tests.js
@@ -5,6 +5,131 @@ YUI.add('module-tests', function(Y) {
     suite.add(new Y.Test.Case({
         name: 'Datepicker Tests',
 
+        tearDown: function() {
+            this.datePicker.destroy();
+        },
+
+        selectDate: function(dayIndex, options) {
+            var popover = this.datePicker.getPopover(),
+                dayCells = popover.bodyNode.all('.yui3-calendar-day'),
+                toClick = dayCells.item(dayIndex);
+
+            toClick.simulate('click', options || {});
+        },
+
+        'selectionChange event should only fire when selection changes': function() {
+            var selectionChangeCount = 0,
+                trigger = Y.one('#trigger');
+
+            this.datePicker = new Y.DatePicker({
+                on: {
+                    selectionChange: function(event) {
+                        selectionChangeCount++;
+                    }
+                },
+                popover: {
+                    zIndex: 1
+                },
+                panes: 1,
+                trigger: '#trigger'
+            });
+
+            Y.Assert.areEqual(0, selectionChangeCount);
+
+            trigger.simulate('click');
+
+            Y.Assert.areEqual(0, selectionChangeCount);
+
+            this.selectDate(0);
+
+            Y.Assert.areEqual(1, selectionChangeCount);
+
+            trigger.simulate('click');
+
+            this.selectDate(1);
+
+            Y.Assert.areEqual(2, selectionChangeCount);
+        },
+
+        'selectionChange event should not fire when switching between datepickers': function() {
+            var selectionChangeCount = 0,
+                triggerA = Y.one('#triggerA'),
+                triggerB = Y.one('#triggerB');
+
+            this.datePicker = new Y.DatePicker({
+                on: {
+                    selectionChange: function(event) {
+                        selectionChangeCount++;
+                    }
+                },
+                popover: {
+                    zIndex: 1
+                },
+                panes: 1,
+                trigger: '#triggerA, #triggerB'
+            });
+
+            triggerA.simulate('click');
+
+            this.selectDate(0);
+
+            triggerB.simulate('click');
+
+            this.selectDate(1);
+
+            Y.Assert.areEqual(2, selectionChangeCount);
+
+            triggerA.simulate('click');
+
+            Y.Assert.areEqual(2, selectionChangeCount);
+
+            triggerB.simulate('click');
+
+            Y.Assert.areEqual(2, selectionChangeCount);
+        },
+
+        'selectionChange event should fire when removing date from selection': function() {
+            var selectionChangeCount = 0,
+                trigger = Y.one('#inputTrigger');
+
+            this.datePicker = new Y.DatePicker({
+                calendar: {
+                    selectionMode: 'multiple'
+                },
+                mask: '%a, %b %d, %Y',
+                on: {
+                    selectionChange: function(event) {
+                        selectionChangeCount++;
+                    }
+                },
+                popover: {
+                    zIndex: 1
+                },
+                panes: 1,
+                trigger: '#inputTrigger'
+            });
+
+            trigger.focus();
+
+            trigger.simulate('click');
+
+            this.selectDate(0);
+
+            trigger.simulate('click');
+
+            this.selectDate(1, { ctrlKey: true });
+
+            trigger.simulate('click');
+
+            this.selectDate(2, { ctrlKey: true });
+
+            trigger.simulate('click');
+
+            this.selectDate(1, { ctrlKey: true });
+
+            Y.Assert.areEqual(5, selectionChangeCount);
+        },
+
         'enterKey event should fire when enter key is pressed in the active input': function() {
             var test = this,
                 enterKeydownCount = 0,
