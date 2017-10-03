a9b763765dfefc43636a1fbfdc81ea881d7c0a3d
Dustin Ryerson <dustin.ryerson@liferay.com>
2017-10-03 08:55:37 -0700

    AUI-3127 SF existing code

--- src/aui-dialog-iframe-deprecated/tests/unit/js/tests.js
+++ src/aui-dialog-iframe-deprecated/tests/unit/js/tests.js
@@ -2,24 +2,24 @@ YUI.add('aui-dialog-iframe-tests', function(Y) {
 
     var suite = new Y.Test.Suite('aui-dialog-iframe-deprecated');
 
-	var modal = new Y.Modal(
-		{
-			headerContent: '<h3>My Dialog</h3>',
-			width: 640,
-			centered: true,
-			height: 400,
-			zIndex: 1,
-			destroyOnHide: true
-		}
-	)
-	.plug(
-		Y.Plugin.DialogIframe,
-		{
-			uri: '../../../../demos/dialog-iframe-deprecated/assets/content.html',
-			iframeCssClass: 'dialog-iframe'
-		}
-	)
-	.render();
+    var modal = new Y.Modal(
+        {
+            headerContent: '<h3>My Dialog</h3>',
+            width: 640,
+            centered: true,
+            height: 400,
+            zIndex: 1,
+            destroyOnHide: true
+        }
+    )
+    .plug(
+        Y.Plugin.DialogIframe,
+        {
+            uri: '../../../../demos/dialog-iframe-deprecated/assets/content.html',
+            iframeCssClass: 'dialog-iframe'
+        }
+    )
+    .render();
 
     suite.add(new Y.Test.Case({
         name: 'DialogIframe',
@@ -28,15 +28,14 @@ YUI.add('aui-dialog-iframe-tests', function(Y) {
          * @tests AUI-1422
          */
         'test loading mask is destroyed when host is destroyed': function() {
-        	var maskCssClass = modal.bodyNode.loadingmask.overlayMask.get('cssClass'),
-        		maskNode = Y.one('.' + maskCssClass);
+            var maskCssClass = modal.bodyNode.loadingmask.overlayMask.get('cssClass'),
+                maskNode = Y.one('.' + maskCssClass);
 
-        	modal.hide();
+            modal.hide();
 
-        	this.wait(function() {
-        		Y.Assert.isFalse(maskNode.inDoc());
-        	}, 100);
-
+            this.wait(function() {
+                Y.Assert.isFalse(maskNode.inDoc());
+            }, 100);
         }
     }));
 
