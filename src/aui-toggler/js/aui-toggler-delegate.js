1567019e219bf07cde40891df09a9de7bc69eb57
Jonathan Mak <jonathan.mak@liferay.com>
2017-10-31 13:53:19 -0700

    Revert "AUI-1196 Start all toggles on TogglerDelegate when `expanded` attribute is true"
    This reverts commit 8ad4802f43ef4e816e3503c0a804e4a7e605c75d.

--- src/aui-toggler/js/aui-toggler-delegate.js
+++ src/aui-toggler/js/aui-toggler-delegate.js
@@ -190,7 +190,7 @@ var TogglerDelegate = A.Component.create({
         renderUI: function() {
             var instance = this;
 
-            if (instance.get('closeAllOnExpand') || instance.get('expanded')) {
+            if (instance.get('closeAllOnExpand')) {
                 instance.createAll();
             }
         },
