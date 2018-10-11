f1bcacd3a39ebf503a613a695ab00ef197def748
Jonathan Mak <jonathan.mak@liferay.com>
2018-10-11 11:37:37 -0700

    AUI-3153; AUI-3156 - Build

---
+++ build/aui-form-builder-settings-modal/aui-form-builder-settings-modal-debug.js
@@ -0,0 +1,189 @@
+YUI.add('aui-form-builder-settings-modal', function (A, NAME) {
+
+/**
+ * The Form Builder Settings Modal Component
+ *
+ * @module aui-form-builder
+ * @submodule aui-form-builder-settings-modal
+ */
+
+var CSS_BTN_PRIMARY = A.getClassName('btn', 'primary'),
+    CSS_FIELD_SETTINGS = A.getClassName('form', 'builder', 'field', 'settings'),
+    CSS_FIELD_SETTINGS_CANCEL =
+        A.getClassName('form', 'builder', 'field', 'settings', 'cancel'),
+    CSS_FIELD_SETTINGS_SAVE =
+        A.getClassName('form', 'builder', 'field', 'settings', 'save'),
+    CSS_MODAL_TITLE = A.getClassName('modal', 'title');
+
+/**
+ * Fired when the modal is hidden.
+ *
+ * @event hide
+ */
+
+/**
+ * Fired when the modal is saved.
+ *
+ * @event save
+ */
+
+/**
+ * A base class for `A.FormBuilderSettingsModal`. It's responsible for the modal that
+ * is used by the form builder for editing a field's settings.
+ *
+ * @class A.FormBuilderSettingsModal
+ * @extends A.Base
+ * @param {Object} config Object literal specifying widget configuration
+ *     properties.
+ * @constructor
+ */
+A.FormBuilderSettingsModal = A.Base.create('form-builder-settings-modal', A.Base, [], {
+    TPL_FIELD_SETTINGS_HEAD_CONTENT: '<h4 class="' + CSS_MODAL_TITLE + '"></h4>',
+
+    /**
+     * Destructor lifecycle implementation for the `A.FormBuilderSettingsModal` class.
+     * Lifecycle.
+     *
+     * @method destructor
+     * @protected
+     */
+    destructor: function() {
+        if (this._modal) {
+            this._modal.destroy();
+        }
+    },
+
+    /**
+     * Hides the settings modal for the given field.
+     *
+     * @method hide
+     */
+    hide: function() {
+        if (this._modal) {
+            this._modal.hide();
+        }
+
+        if (this._eventToggleAdvancedContent) {
+            this._eventToggleAdvancedContent.detach();
+        }
+    },
+
+    /**
+     * Shows the settings panel for the given field.
+     *
+     * @method show
+     * @param {A.FormField} field
+     * @param {String} typeName The name of the field type.
+     */
+    show: function(field, typeName) {
+        var bodyNode,
+            firstInput;
+
+        if (!this._modal) {
+            this._create();
+        }
+
+        bodyNode = this._modal.getStdModNode(A.WidgetStdMod.BODY);
+        bodyNode.empty();
+        field.renderSettingsPanel(bodyNode);
+
+        this._modal.get('boundingBox').one('.' + CSS_MODAL_TITLE).set('text', typeName);
+
+        this._modal.show();
+        this._modal.align();
+
+        firstInput = bodyNode.one('input[type="text"]');
+        if (firstInput) {
+            firstInput.focus();
+        }
+
+        this._eventToggleAdvancedContent = field.on('contentToggle', A.bind(this._updateModalOnContentToggle, this));
+
+        this._fieldBeingEdited = field;
+    },
+
+    /**
+     * Fired after the `visible` attribute from the modal instance changes.
+     *
+     * @method _afterModalVisibleChange
+     * @param {EventFacade} event
+     * @protected
+     */
+    _afterModalVisibleChange: function(event) {
+        if (!event.newVal) {
+            this.fire('hide');
+        }
+    },
+
+    /**
+     * Creates and renders the field settings modal for the first time.
+     *
+     * @method _create
+     * @protected
+     */
+    _create: function() {
+        this._modal = new A.Modal({
+            centered: true,
+            cssClass: CSS_FIELD_SETTINGS,
+            draggable: false,
+            modal: true,
+            headerContent: this.TPL_FIELD_SETTINGS_HEAD_CONTENT,
+            resizable: false,
+            zIndex: 4
+        }).render();
+
+        this._modal.addToolbar(
+            [
+                {
+                    cssClass: [CSS_BTN_PRIMARY, CSS_FIELD_SETTINGS_SAVE].join(' '),
+                    label: 'Save',
+                    on: {
+                        click: A.bind(this._save, this)
+                    },
+                    render: true
+                },
+                {
+                    cssClass: CSS_FIELD_SETTINGS_CANCEL,
+                    label: 'Cancel',
+                    on: {
+                        click: A.bind(this.hide, this)
+                    },
+                    render: true
+                }
+            ],
+            A.WidgetStdMod.FOOTER
+        );
+
+        this._modal.after('visibleChange', A.bind(this._afterModalVisibleChange, this));
+    },
+
+    /**
+     * Saves the settings for the field currently being edited.
+     *
+     * @method _save
+     * @protected
+     */
+    _save: function() {
+        if (this._fieldBeingEdited.validateSettings()) {
+            this._fieldBeingEdited.saveSettings();
+
+            this.fire('save', {
+                field: this._fieldBeingEdited
+            });
+            this.hide();
+        }
+    },
+
+    /**
+     * Toggle back and close button on small screen and update Modal height.
+     *
+     * @method _updateModalOnContentToggle
+     * @protected
+     */
+    _updateModalOnContentToggle: function() {
+        this._modal.align();
+    }
+});
+
+
+}, '3.1.0-deprecated.50', {"requires": ["aui-classnamemanager", "aui-modal", "base", "node-base"], "skinnable": true});
---
+++ build/aui-layout-builder/aui-layout-builder.js
@@ -0,0 +1,231 @@
+YUI.add('aui-layout-builder', function (A, NAME) {
+
+/**
+ * The Layout Builder Component
+ *
+ * @module aui-layout-builder
+ */
+
+var CSS_LAYOUT_BUILDER_LAYOUT_CONTAINER = A.getClassName('layout', 'builder', 'layout', 'container'),
+
+    TPL_LAYOUT_CONTAINER = '<div class="' + CSS_LAYOUT_BUILDER_LAYOUT_CONTAINER + '"></div>';
+
+/**
+ * A base class for Layout Builder.
+ *
+ * Check the [live demo](http://alloyui.com/examples/layout-builder/).
+ *
+ * @class A.LayoutBuilder
+ * @extends Base
+ * @uses A.LayoutBuilderAddCol, A.LayoutBuilderRemoveCol
+ * @param {Object} config Object literal specifying layout builder configuration
+ *     properties.
+ * @constructor
+ */
+A.LayoutBuilder = A.Base.create('layout-builder', A.Base, [
+    A.LayoutBuilderAddCol,
+    A.LayoutBuilderMove,
+    A.LayoutBuilderRemoveRow,
+    A.LayoutBuilderResizeCol,
+    // It's necessary that A.LayoutBuilderAddRow is the last to be loaded.
+    A.LayoutBuilderAddRow
+], {
+
+    /**
+     * The node where the layout will be rendered.
+     *
+     * @property _layoutContainer
+     * @type {Node}
+     * @protected
+     */
+    _layoutContainer: null,
+
+    /**
+     * Construction logic executed during LayoutBuilder instantiation. Lifecycle.
+     *
+     * @method initializer
+     * @protected
+     */
+    initializer: function() {
+        var container = this.get('container'),
+            layout = this.get('layout');
+
+        this._createLayoutContainer(container);
+
+        layout.addTarget(this);
+
+        this._eventHandles = [
+            this.after('layoutChange', A.bind(this._afterLayoutChange, this)),
+            this.on('moveStart', A.bind(this._afterLayoutMoveStart, this)),
+            this.on('moveEnd', A.bind(this._afterLayoutMoveEnd, this))
+        ];
+
+        layout.draw(this._layoutContainer);
+
+        this._layoutContainer.unselectable();
+    },
+
+    /**
+     * Destructor implementation for the `A.LayoutBuilder` class. Lifecycle.
+     *
+     * @method destructor
+     * @protected
+     */
+    destructor: function() {
+        (new A.EventHandle(this._eventHandles)).detach();
+
+        this.get('container').empty();
+    },
+
+    /**
+     * Fires after layout:moveEnd event. 
+     *
+     * @method _afterLayoutMoveEnd
+     * @protected
+     */
+    _afterLayoutMoveEnd: function() {
+        if (this.isResizeColsEnabled) {
+            this.set('enableResizeCols', true);
+        }
+
+        if (this.isRemoveRowsEnabled) {
+            this.set('enableRemoveRows', true);
+        }
+
+        if (this.isAddRowsEnabled) {
+            this.set('enableAddRows', true);
+        }
+    },
+
+    /**
+     * Fires after layout:moveStart event. 
+     *
+     * @method _afterLayoutMoveStart
+     * @protected
+     */
+    _afterLayoutMoveStart: function() {
+        this.isResizeColsEnabled = this.get('enableResizeCols');
+        this.isRemoveRowsEnabled = this.get('enableRemoveRows');
+        this.isAddRowsEnabled = this.get('enableAddRows');
+
+        if (this.isResizeColsEnabled) {
+            this.set('enableResizeCols', false);
+        }
+
+        if (this.isRemoveRowsEnabled) {
+            this.set('enableRemoveRows', false);
+        }
+
+        if (this.isAddRowsEnabled) {
+            this.set('enableAddRows', false);
+        }
+    },
+
+    /**
+     * Fires after layout changes.
+     *
+     * @method _afterLayoutChange
+     * @param {EventFacade} event
+     * @protected
+     */
+    _afterLayoutChange: function(event) {
+        var newLayout = event.newVal,
+            prevLayout = event.prevVal;
+
+        this._layoutContainer.empty();
+
+        prevLayout.removeTarget(this);
+
+        newLayout.addTarget(this);
+        newLayout.draw(this._layoutContainer);
+    },
+
+    /**
+     * Create layout container node.
+     *
+     * @method _createLayoutContainer
+     * @param {Node} container Node that will append the _layoutContainer node.
+     * @protected
+     */
+    _createLayoutContainer: function(container) {
+        this._layoutContainer = container.one('.' + CSS_LAYOUT_BUILDER_LAYOUT_CONTAINER);
+
+        if (!this._layoutContainer) {
+            this._layoutContainer = A.Node.create(TPL_LAYOUT_CONTAINER);
+            container.prepend(this._layoutContainer);
+        }
+    }
+}, {
+    /**
+     * Static property used to define the default attribute
+     * configuration for LayoutBuilder.
+     *
+     * @property ATTRS
+     * @type Object
+     * @static
+     */
+    ATTRS: {
+        /**
+         * Node that that will be inserted the layout.
+         *
+         * @attribute container
+         * @type {String | Node}
+         * @initOnly
+         */
+        container: {
+            setter: A.one,
+            validator: function(val) {
+                return A.Lang.isString(val) || A.instanceOf(val, A.Node);
+            },
+            writeOnce: 'initOnly'
+        },
+
+        /**
+         * Object with layout configuration.
+         *
+         * @attribute layout
+         * @type {A.Layout}
+         */
+        layout: {
+            validator: function(val) {
+                return A.instanceOf(val, A.Layout);
+            },
+            valueFn: function() {
+                return new A.Layout();
+            }
+        },
+
+        /**
+         * Collection of strings used to label elements of the UI.
+         *
+         * @attribute strings
+         * @type {Object}
+         */
+        strings: {
+            value: {
+                addColumn: 'Add Column',
+                addRow: 'Add Row',
+                pasteHere: 'Paste Here'
+            },
+            writeOnce: true
+        }
+    }
+});
+
+
+}, '3.1.0-deprecated.50', {
+    "requires": [
+        "aui-classnamemanager",
+        "aui-layout",
+        "aui-layout-builder-add-col",
+        "aui-layout-builder-add-row",
+        "aui-layout-builder-move",
+        "aui-layout-builder-remove-row",
+        "aui-layout-builder-resize-col",
+        "aui-node-base",
+        "base-build",
+        "node-event-delegate",
+        "node-screen",
+        "node-style"
+    ]
+});
