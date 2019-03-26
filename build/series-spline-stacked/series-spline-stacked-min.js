fd75b4761f564f27982367c83179c7430be0f930
Jonathan Mak <jonathan.mak@liferay.com>
2019-03-26 11:35:08 -0700

    AUI-3165 - Build

---
+++ build/series-spline-stacked/series-spline-stacked-min.js
@@ -0,0 +1,1 @@
+YUI.add("series-spline-stacked",function(e,s){e.StackedSplineSeries=e.Base.create("stackedSplineSeries",e.SplineSeries,[e.StackingUtil],{setAreaData:function(){e.StackedSplineSeries.superclass.setAreaData.apply(this),this._stackCoordinates.apply(this)}},{ATTRS:{type:{value:"stackedSpline"}}})},"patched-v3.18.1",{requires:["series-stacked","series-spline"]});
