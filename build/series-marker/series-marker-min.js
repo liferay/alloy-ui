1190184a6e0a09b0f7cd08c047f555f68ba009c9
Jonathan Mak <jonathan.mak@liferay.com>
2019-04-10 17:02:12 -0700

    AUI-3160 - Build

---
+++ build/series-marker/series-marker-min.js
@@ -0,0 +1,1 @@
+YUI.add("series-marker",function(e,r){e.MarkerSeries=e.Base.create("markerSeries",e.CartesianSeries,[e.Plots],{_setStyles:function(r){return r.marker||(r={marker:r}),r=this._parseMarkerStyles(r),e.MarkerSeries.superclass._mergeStyles.apply(this,[r,this._getDefaultStyles()])}},{ATTRS:{type:{value:"marker"}}})},"patched-v3.18.1",{requires:["series-cartesian","series-plot-util"]});
