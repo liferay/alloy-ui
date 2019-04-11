b628e88b7bc82cba716c650d0de7d8a82c33035a
Jonathan Mak <jonathan.mak@liferay.com>
2019-04-11 10:07:42 -0700

    AUI-3167 - Build

---
+++ build/aui-classnamemanager/aui-classnamemanager-min.js
@@ -0,0 +1,1 @@
+YUI.add("aui-classnamemanager",function(e,t){var n=e.ClassNameManager,r=n.getClassName;e.getClassName=e.cached(function(){var t=e.Array(arguments,0,!0);return t[t.length]=!0,r.apply(n,t)})},"3.1.0-deprecated.60",{requires:["classnamemanager"]});
