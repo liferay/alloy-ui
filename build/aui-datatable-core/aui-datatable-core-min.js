ce65a9547183f979a6a0dee1d1a770e4f2a8dde9
Jonathan Mak <jonathan.mak@liferay.com>
2019-06-13 09:33:26 -0700

    AUI-3170 - Build

---
+++ build/aui-datatable-core/aui-datatable-core-min.js
@@ -0,0 +1,1 @@
+YUI.add("aui-datatable-core",function(e,t){e.DataTable.NAME="datatable",e.DataTable.CSS_PREFIX="table"},"3.0.3-deprecated.92",{requires:["aui-datatable-body","datatable-base","event-key","aui-event-base"],skinnable:!0});
