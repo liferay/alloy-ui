54cc08424afb05905f5557e5d9163c51e94e101b
Jonathan Mak <jonathan.mak@liferay.com>
2019-09-24 11:47:16 -0700

    AUI-3173 - Build

---
+++ build/aui-form-builder-field-sentence/aui-form-builder-field-sentence-min.js
@@ -0,0 +1,1 @@
+YUI.add("aui-form-builder-field-sentence",function(e,t){e.FormBuilderFieldSentence=e.Base.create("form-builder-field-sentence",e.FormField,[e.FormBuilderFieldBase])},"3.0.3-deprecated.93",{requires:["aui-form-builder-field-base","aui-form-field"]});
