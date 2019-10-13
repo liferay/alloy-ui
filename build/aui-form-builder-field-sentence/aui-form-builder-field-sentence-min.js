3330601a95de7e2a5db135b5f8c1c30fc5a7484e
Jonathan Mak <jonathan.mak@liferay.com>
2019-10-13 10:19:08 -0700

    AUI-3182 - Build

---
+++ build/aui-form-builder-field-sentence/aui-form-builder-field-sentence-min.js
@@ -0,0 +1,1 @@
+YUI.add("aui-form-builder-field-sentence",function(e,t){e.FormBuilderFieldSentence=e.Base.create("form-builder-field-sentence",e.FormField,[e.FormBuilderFieldBase])},"3.1.0-deprecated.69",{requires:["aui-form-builder-field-base","aui-form-field"]});
---
+++ build/dd-drop-plugin/dd-drop-plugin-min.js
@@ -0,0 +1,1 @@
+YUI.add("dd-drop-plugin",function(d,n){var o=function(d){d.node=d.host,o.superclass.constructor.apply(this,arguments)};o.NAME="dd-drop-plugin",o.NS="drop",d.extend(o,d.DD.Drop),d.namespace("Plugin"),d.Plugin.Drop=o},"patched-v3.18.1",{requires:["dd-drop"]});
