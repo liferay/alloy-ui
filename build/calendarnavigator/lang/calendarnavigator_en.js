d36f566119e8842d0b6bee236be2efc506509b9d
Jonathan Mak <jonathan.mak@liferay.com>
2020-02-13 09:29:48 -0800

    AUI-3187 - Build

---
+++ build/calendarnavigator/lang/calendarnavigator_en.js
@@ -0,0 +1,1 @@
+YUI.add("lang/calendarnavigator_en",function(o){o.Intl.add("calendarnavigator","en",{go_to_previous_month:"Go to previous month",go_to_next_month:"Go to next month"})},"patched-v3.18.1");
