abeca21075579a875205126cdc467a9ac7557843
Jonathan Mak <jonathan.mak@liferay.com>
2020-04-03 23:47:42 -0700

    AUI-3191 - Build

---
+++ build/aui-form-builder-field-types-modal/aui-form-builder-field-types-modal.js
@@ -0,0 +1,223 @@
+YUI.add('aui-form-builder-field-types-modal', function (A, NAME) {
+
+/**
+ * The Form Builder Field Types Modal Component
+ *
+ * @module aui-form-builder
+ * @submodule aui-form-builder-field-types-modal
+ */
+
+var CSS_FIELD_TYPE = A.getClassName('field', 'type'),
+    CSS_FIELD_TYPES_LIST = A.getClassName('form', 'builder', 'field', 'types', 'list'),
+    CSS_FIELD_TYPES_MODAL_TITLE = A.getClassName('modal', 'title');
+
+/**
+ * `A.FormBuilder` extension, which is responsible for all the logic related
+ * to displaying the field types for the user.
+ *
+ * @class A.FormBuilderFieldTypesModal
+ * @param {Object} config Object literal specifying layout builder configuration
+ *     properties.
+ * @constructor
+ */
+
+A.FormBuilderFieldTypesModal = A.Base.create('form-builder-field-types-modal', A.Modal, [], {
+    TPL_HEADER_LABEL: '<h4 class="' + CSS_FIELD_TYPES_MODAL_TITLE + '">{addField}</h4>',
+    TPL_TYPES_LIST: '<div class="clearfix ' + CSS_FIELD_TYPES_LIST + '" role="main"></div>',
+
+    /**
+     * Construction logic executed during the `A.FormBuilderFieldTypesModal`
+     * instantiation. Lifecycle.
+     *
+     * @method initializer
+     * @protected
+     */
+    initializer: function() {
+        this.after('fieldTypesChange', this._afterFieldTypesChange);
+    },
+
+    /**
+     * Bind the events on the FormBuilderFieldTypesModal UI. Lifecycle.
+     *
+     * @method bindUI
+     * @protected
+     */
+    bindUI: function() {
+        var bodyNode = this.getStdModNode('body');
+
+        A.FormBuilderFieldTypesModal.superclass.bindUI.apply(this, arguments);
+
+        this._eventHandles.push(
+            bodyNode.delegate('click', this._onClickFieldType, '.' + CSS_FIELD_TYPE, this),
+            bodyNode.delegate('key', A.bind(this._onKeyPressFieldType, this), 'enter', '.' + CSS_FIELD_TYPE)
+        );
+    },
+
+    /**
+     * Renders the FormBuilderFieldTypesModal component instance. Lifecycle.
+     *
+     * @method renderUI
+     * @protected
+     */
+    renderUI: function() {
+        A.FormBuilderFieldTypesModal.superclass.renderUI.apply(this, arguments);
+
+        this._uiSetFieldTypes(this.get('fieldTypes'));
+    },
+
+    /**
+     * Fires after `fieldTypes` attribute changes.
+     *
+     * @method _afterFieldTypesChange
+     * @protected
+     */
+    _afterFieldTypesChange: function() {
+        this._uiSetFieldTypes(this.get('fieldTypes'));
+    },
+
+    /**
+     * Fired when a field type is clicked.
+     *
+     * @method _onClickFieldType
+     * @param {EventFacade} event
+     * @protected
+     */
+    _onClickFieldType: function(event) {
+        var fieldType = event.currentTarget.getData('fieldType');
+
+        this.hide();
+
+        this.fire('selectFieldType', {
+            fieldType: fieldType
+        });
+    },
+
+    /**
+     * Fired when the close button for the field types modal is clicked.
+     *
+     * @method _onFieldTypesModalCloseClick
+     * @protected
+     */
+    _onFieldTypesModalCloseClick: function() {
+        this.hide();
+    },
+
+    /**
+     * Fired when a field type is keypressed.
+     *
+     * @method _onKeyPressFieldType
+     * @param {EventFacade} event
+     * @protected
+     */
+    _onKeyPressFieldType: function(event) {
+        this._onClickFieldType(event);
+    },
+
+    /**
+     * Updates the ui according to the value of the `fieldTypes` attribute.
+     *
+     * @method _uiSetFieldTypes
+     * @param {Array} fieldTypes
+     * @protected
+     */
+    _uiSetFieldTypes: function(fieldTypes) {
+        var fieldTypesListNode = A.Node.create(this.TPL_TYPES_LIST);
+
+        fieldTypesListNode.get('children').remove();
+
+        A.Array.each(fieldTypes, function(type) {
+            fieldTypesListNode.append(type.get('node'));
+        });
+
+        this.set('bodyContent', fieldTypesListNode);
+    },
+
+    /**
+     * Returns the template of the modal header.
+     *
+     * @method _valueHeaderContent
+     * @return {Object}
+     * @protected
+     */
+    _valueHeaderContent: function() {
+        return A.Lang.sub(this.TPL_HEADER_LABEL, {
+            addField: this.get('strings').addField
+        });
+    },
+
+    /**
+     * Returns the configuration object for the field types modal
+     * toolbar.
+     *
+     * @method _valueToolbars
+     * @return {Object}
+     * @protected
+     */
+    _valueToolbars: function() {
+        return {
+            header: [
+                {
+                    cssClass: 'close',
+                    discardDefaultButtonCssClasses: true,
+                    labelHTML: '<span> \u00D7 </span>',
+                    on: {
+                        click: A.bind(this._onFieldTypesModalCloseClick, this)
+                    }
+                }
+            ]
+        };
+    }
+}, {
+    ATTRS: {
+        /**
+         * The collection of field types that can be selected as fields for
+         * this form builder.
+         *
+         * @attribute fieldTypes
+         * @default []
+         * @type Array
+         */
+        fieldTypes: {
+            validator: A.Lang.isArray,
+            value: []
+        },
+
+        /**
+         * The content displayed in the modal header.
+         *
+         * @attribute headerContent
+         * @type String
+         */
+        headerContent: {
+            valueFn: '_valueHeaderContent'
+        },
+
+        /**
+         * The text strings displayed by this component.
+         *
+         * @attribute strings
+         * @type Object
+         */
+        strings: {
+            value: {
+                addField: 'Add Field'
+            }
+        },
+
+        /**
+         * The toolbar configuration object.
+         *
+         * @attribute toolbar
+         * @type Object
+         */
+        toolbars: {
+            validator: A.Lang.isObject,
+            valueFn: '_valueToolbars'
+        }
+    },
+
+    CSS_PREFIX: 'modal-dialog'
+});
+
+
+}, '3.1.0-deprecated.76', {"requires": ["aui-modal"], "skinnable": true});
---
+++ build/aui-form-builder-field-types/aui-form-builder-field-types.js
@@ -0,0 +1,397 @@
+YUI.add('aui-form-builder-field-types', function (A, NAME) {
+
+/**
+ * The Form Builder Field Types Component
+ *
+ * @module aui-form-builder
+ * @submodule aui-form-builder-field-types
+ */
+
+/**
+ * `A.FormBuilder` extension, which is responsible for all the logic related
+ * to field types.
+ *
+ * @class A.FormBuilderFieldTypes
+ * @param {Object} config Object literal specifying layout builder configuration
+ *     properties.
+ * @constructor
+ */
+A.FormBuilderFieldTypes = function() {};
+
+A.FormBuilderFieldTypes.prototype = {
+    /**
+     * Construction logic executed during the `A.FormBuilderFieldTypes`
+     * instantiation. Lifecycle.
+     *
+     * @method initializer
+     * @protected
+     */
+    initializer: function() {
+        this.after('fieldTypesChange', this._afterFieldTypesChange);
+        this.after('form-builder-field-types-modal:selectFieldType', this._afterSelectFieldType);
+    },
+
+    /**
+     * Destructor lifecycle implementation for the `A.FormBuilderFieldTypes` class.
+     * Lifecycle.
+     *
+     * @method destructor
+     * @protected
+     */
+    destructor: function() {
+        A.Array.each(this.get('fieldTypes'), function(field) {
+            field.destroy();
+        });
+
+        this.get('fieldTypesModal').destroy();
+    },
+
+    /**
+     * Disables unique fields for the field class that the given field is an
+     * instance of.
+     *
+     * @method disableUniqueFieldType
+     * @param {A.FormField} field
+     */
+    disableUniqueFieldType: function(field) {
+        var fieldType = this.findTypeOfField(field);
+
+        if (fieldType.get('unique')) {
+            fieldType.set('disabled', true);
+        }
+    },
+
+    /**
+     * Finds the type of the given field.
+     *
+     * @method findTypeOfField
+     * @param {A.FormBuilderFieldBase} field
+     */
+    findTypeOfField: function(field) {
+        var fieldTypes = this.get('fieldTypes'),
+            i;
+
+        for (i = 0; i < fieldTypes.length; i++) {
+            if (field.constructor === fieldTypes[i].get('fieldClass')) {
+                return fieldTypes[i];
+            }
+        }
+    },
+
+    /**
+     * Hides the fields modal.
+     *
+     * @method hideFieldsPanel
+     */
+    hideFieldsPanel: function() {
+        var fieldTypesModal = this.get('fieldTypesModal');
+
+        fieldTypesModal.hide();
+    },
+
+    /**
+     * Adds a the given field types to this form builder.
+     *
+     * @method registerFieldTypes
+     * @param {Array | Object | A.FormBuilderFieldType} typesToAdd This can be
+     *   either an array of items or a single item. Each item should be either
+     *   an instance of `A.FormBuilderFieldType`, or the configuration object
+     *   to be used when instantiating one.
+     */
+    registerFieldTypes: function(typesToAdd) {
+        var fieldTypes = this.get('fieldTypes');
+
+        typesToAdd = A.Lang.isArray(typesToAdd) ? typesToAdd : [typesToAdd];
+
+        A.Array.each(typesToAdd, function(type) {
+            fieldTypes.push(type);
+        });
+
+        this.set('fieldTypes', fieldTypes);
+    },
+
+    /**
+     * Shows the fields modal.
+     *
+     * @method showFieldsPanel
+     */
+    showFieldsPanel: function() {
+        var fieldTypesModal = this.get('fieldTypesModal');
+
+        if (!fieldTypesModal.get('rendered')) {
+            fieldTypesModal.render();
+        }
+
+        fieldTypesModal.show();
+    },
+
+    /**
+     * Removes the given field types from this form builder.
+     *
+     * @method unregisterFieldTypes
+     * @param {Array | String | A.FormBuilderFieldType} typesToRemove This can be
+     *   either an array of items, or a single one. For each item, if it's a
+     *   string, the form builder will remove all registered field types with
+     *   a field class that matches it. For items that are instances of
+     *   `A.FormBuilderFieldType`, only the same instances will be removed.
+     */
+    unregisterFieldTypes: function(typesToRemove) {
+        var instance = this;
+
+        typesToRemove = A.Lang.isArray(typesToRemove) ? typesToRemove : [typesToRemove];
+
+        A.Array.each(typesToRemove, function(type) {
+            instance._unregisterFieldType(type);
+        });
+
+        this.set('fieldTypes', this.get('fieldTypes'));
+    },
+
+    /**
+     * Fired after the `fieldTypes` attribute is set.
+     *
+     * @method _afterFieldTypesChange
+     * @param {EventFacade} event
+     * @protected
+     */
+    _afterFieldTypesChange: function(event) {
+        this.get('fieldTypesModal').set('fieldTypes', event.newVal);
+    },
+
+    /**
+     * Fired after a field type is selected by the user.
+     *
+     * @method _afterSelectFieldType
+     * @param {EventFacade} event
+     * @protected
+     */
+    _afterSelectFieldType: function(event) {
+        var field,
+            fieldType = event.fieldType;
+
+        if (!fieldType.get('disabled')) {
+            field = new(fieldType.get('fieldClass'))(fieldType.get('defaultConfig'));
+            this.showFieldSettingsPanel(field, fieldType.get('label'));
+        }
+    },
+
+    /**
+     * Check on all created fields if there is one of the same type
+     * of the parameter.
+     *
+     * @method _checkActiveLayoutHasFieldType
+     * @param {Object} fieldType
+     * @return {Boolean}
+     * @protected
+     */
+    _checkActiveLayoutHasFieldType: function(fieldType) {
+        var col,
+            cols,
+            fieldList,
+            row,
+            rows = this.getActiveLayout().get('rows');
+
+        for (row = 0; row < rows.length; row++) {
+            cols = rows[row].get('cols');
+            for (col = 0; col < cols.length; col++) {
+                fieldList = cols[col].get('value');
+                if (fieldList && this._checkListHasFieldType(fieldList, fieldType)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    },
+
+    /**
+     * Checks on all fields of a field list if there is one of the
+     * same type of the parameter.
+     *
+     * @method _checkListHasFieldType
+     * @param {A.FormBuilderFIeldList} fieldList
+     * @param {Object} fieldType
+     * @return {Boolean}
+     * @protected
+     */
+    _checkListHasFieldType: function(fieldList, fieldType) {
+        var fields = fieldList.get('fields'),
+            i;
+
+        for (i = 0; i < fields.length; i++) {
+            if (this._hasFieldType(fieldType, fields[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    },
+
+    /**
+     * Checks if the given field is of the given field type.
+     *
+     * @method _hasFieldType
+     * @param  {A.FormBuilderFieldType} fieldType
+     * @param  {A.FormField} field
+     * @return {Boolean}
+     * @protected
+     */
+    _hasFieldType: function(fieldType, field) {
+        var i,
+            nestedFields = field.get('nestedFields');
+
+        if (field.constructor === fieldType.get('fieldClass')) {
+            return true;
+        }
+
+        for (i = 0; i < nestedFields.length; i++) {
+            if (this._hasFieldType(fieldType, nestedFields[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    },
+
+    /**
+     * Sets the `fieldTypes` attribute.
+     *
+     * @method _setFieldTypes
+     * @param {Object | A.FormBuilderFieldType} val
+     * @return {A.FormBuilderFieldType}
+     * @protected
+     */
+    _setFieldTypes: function(val) {
+        for (var i = 0; i < val.length; i++) {
+            if (!A.instanceOf(val[i], A.FormBuilderFieldType)) {
+                val[i] = new A.FormBuilderFieldType(val[i]);
+            }
+        }
+
+        return val;
+    },
+
+    /**
+     * Removes a single given field type from this form builder.
+     *
+     * @method _unregisterFieldType
+     * @param {String | A.FormBuilderFieldType} fieldType
+     * @protected
+     */
+    _unregisterFieldType: function(fieldType) {
+        var fieldTypes = this.get('fieldTypes'),
+            i;
+
+        if (A.Lang.isFunction(fieldType)) {
+            for (i = fieldTypes.length - 1; i >= 0; i--) {
+                if (fieldTypes[i].get('fieldClass') === fieldType) {
+                    this._unregisterFieldTypeByIndex(i);
+                }
+            }
+        }
+        else {
+            this._unregisterFieldTypeByIndex(fieldTypes.indexOf(fieldType));
+        }
+    },
+
+    /**
+     * Unregisters the field type at the given index.
+     *
+     * @method _unregisterFieldTypeByIndex
+     * @param {Number} index
+     * @protected
+     */
+    _unregisterFieldTypeByIndex: function(index) {
+        var fieldTypes = this.get('fieldTypes');
+
+        if (index !== -1) {
+            fieldTypes[index].destroy();
+            fieldTypes.splice(index, 1);
+        }
+    },
+
+    /**
+     * Enable or disable unique FieldTypes based on created Fields.
+     *
+     * @method _updateUniqueFieldType
+     * @protected
+     */
+    _updateUniqueFieldType: function() {
+        var instance = this;
+
+        A.Array.each(instance.get('fieldTypes'), function(fieldType) {
+            if (fieldType.get('unique')) {
+                fieldType.set('disabled', instance._checkActiveLayoutHasFieldType(fieldType));
+            }
+        });
+    },
+
+    /**
+     * Default value for the modal displayed to select a field.
+     *
+     * @method _valueFieldTypesModal
+     * @return {A.FormBuilderFieldTypesModal}
+     * @protected
+     */
+    _valueFieldTypesModal: function() {
+        var fieldTypesModal = new A.FormBuilderFieldTypesModal({
+            centered: true,
+            cssClass: 'form-builder-modal',
+            draggable: false,
+            fieldTypes: this.get('fieldTypes'),
+            modal: true,
+            resizable: false,
+            visible: false,
+            zIndex: 4
+        });
+
+        fieldTypesModal.addTarget(this);
+
+        return fieldTypesModal;
+    }
+};
+
+/**
+ * Static property used to define the default attribute
+ * configuration for the `A.FormBuilderFieldTypes`.
+ *
+ * @property ATTRS
+ * @type Object
+ * @static
+ */
+A.FormBuilderFieldTypes.ATTRS = {
+    /**
+     * The collection of field types that can be selected as fields for
+     * this form builder.
+     *
+     * @attribute fieldTypes
+     * @default []
+     * @type Array
+     */
+    fieldTypes: {
+        setter: '_setFieldTypes',
+        validator: A.Lang.isArray,
+        value: []
+    },
+
+    /**
+     * The modal that will be used to select a field type.
+     *
+     * @attribute fieldTypesModal
+     * @type `A.FormBuilderFieldTypesModal`
+     */
+    fieldTypesModal: {
+        valueFn: '_valueFieldTypesModal'
+    }
+};
+
+
+}, '3.1.0-deprecated.76', {
+    "requires": [
+        "aui-classnamemanager",
+        "aui-form-builder-field-types-modal",
+        "base",
+        "node-base"
+    ],
+    "skinnable": true
+});
