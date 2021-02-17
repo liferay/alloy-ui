b376e14707f21c12795e652b255de2bacdee1ee6
Jonathan Mak <jonathan.mak@liferay.com>
2021-02-17 11:47:29 -0800

    AUI-3196 - Build

---
+++ build/calendar-base/lang/calendar-base_eu-ES.js
@@ -0,0 +1,1 @@
+YUI.add("lang/calendar-base_eu-ES",function(e){e.Intl.add("calendar-base","eu-ES",{very_short_weekdays:["Ig","Al","Ar","Az","Og","Ol","Lr"],first_weekday:1,weekends:[0,6]})},"patched-v3.18.1");
